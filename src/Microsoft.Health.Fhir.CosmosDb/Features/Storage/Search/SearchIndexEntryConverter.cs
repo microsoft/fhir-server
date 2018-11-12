microsoft/fhir-server

commit 465f08b6316ae1ada13a5d1296b43159a12b44e6

Allow multiple search index entries to be generated by one ISearchValue (#173)
- Defer creation of the cartesian product for composite search values.
- Updated the code to allow multiple entries to be generated by SearchIndexEntryJObjectGenerator.

src/Microsoft.Health.Fhir.CosmosDb/Features/Storage/Search/SearchIndexEntryConverter.cs
@@ -5,8 +5,8 @@
 
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using Microsoft.Health.Fhir.Core.Features.Search;
-using Microsoft.Health.Fhir.CosmosDb.Features.Search;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -37,25 +37,21 @@ namespace Microsoft.Health.Fhir.CosmosDb.Features.Storage.Search
                 generator = new SearchIndexEntryJObjectGenerator();
             }
 
-            JObject generatedObj;
+            IReadOnlyList<JObject> generatedObjects;
 
             try
             {
-                searchIndexEntry.Value.AcceptVisitor(generator);
-
-                generatedObj = generator.Output;
+                generatedObjects = generator.Generate(searchIndexEntry);
             }
             finally
             {
-                generator.Reset();
-
                 CachedGenerators.Enqueue(generator);
             }
 
-            generatedObj.AddFirst(
-                new JProperty(SearchValueConstants.ParamName, searchIndexEntry.ParamName));
-
-            generatedObj.WriteTo(writer);
+            foreach (JObject generatedObj in generatedObjects)
+            {
+                generatedObj.WriteTo(writer);
+            }
         }
     }
 }

src/Microsoft.Health.Fhir.Core.UnitTests/Features/Search/SearchValues/CompositeSearchValueTests.cs
@@ -4,6 +4,7 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
 using Xunit;
 
@@ -22,21 +23,7 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Search.SearchValues
         [Fact]
         public void GivenAnEmptyComponents_WhenInitializing_ThenExceptionShouldBeThrown()
         {
-            Assert.Throws<ArgumentException>(ParamNameComponents, () => new CompositeSearchValue(new StringSearchValue[0]));
-        }
-
-        [Fact]
-        public void GivenComponents_WhenInitialized_ThenCorrectComponentsShouldBeAssigned()
-        {
-            var components = new ISearchValue[]
-            {
-                new StringSearchValue("abc"),
-                new NumberSearchValue(123),
-            };
-
-            var value = new CompositeSearchValue(components);
-
-            Assert.Same(components, value.Components);
+            Assert.Throws<ArgumentException>(ParamNameComponents, () => new CompositeSearchValue(new IReadOnlyList<ISearchValue>[] { }));
         }
 
         [Fact]
@@ -47,7 +34,7 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Search.SearchValues
                 new StringSearchValue("abc"),
             };
 
-            var value = new CompositeSearchValue(components);
+            var value = new CompositeSearchValue(new[] { components });
 
             Assert.False(value.IsValidAsCompositeComponent);
         }
@@ -55,15 +42,23 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Search.SearchValues
         [Fact]
         public void GivenASearchValue_WhenToStringIsCalled_ThenCorrectStringShouldBeReturned()
         {
-            var components = new ISearchValue[]
+            var components = new ISearchValue[][]
             {
-                new TokenSearchValue("system", "code", "text"),
-                new NumberSearchValue(123),
+                new ISearchValue[]
+                {
+                    new TokenSearchValue("system1", "code1", "text1"),
+                    new TokenSearchValue("system2", "code2", "text2"),
+                },
+                new ISearchValue[]
+                {
+                    new NumberSearchValue(123),
+                    new NumberSearchValue(789),
+                },
             };
 
             var value = new CompositeSearchValue(components);
 
-            Assert.Equal("system|code$123", value.ToString());
+            Assert.Equal("(system1|code1), (system2|code2) $ (123), (789)", value.ToString());
         }
     }
 }

src/Microsoft.Health.Fhir.Core/Features/Search/SearchIndexer.cs
@@ -12,7 +12,6 @@ using EnsureThat;
 using Hl7.Fhir.FhirPath;
 using Hl7.Fhir.Model;
 using Microsoft.Extensions.Logging;
-using Microsoft.Health.Fhir.Core.Extensions;
 using Microsoft.Health.Fhir.Core.Features.Definition;
 using Microsoft.Health.Fhir.Core.Features.Search.Converters;
 using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
@@ -132,13 +131,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Search
                     continue;
                 }
 
-                // Create the list of searchable values.
-                foreach (IEnumerable<ISearchValue> compositeSearchValues in componentValues.CartesianProduct())
-                {
-                    yield return new SearchIndexEntry(
-                        searchParameter.Name,
-                        new CompositeSearchValue(compositeSearchValues.ToArray()));
-                }
+                yield return new SearchIndexEntry(searchParameter.Name, new CompositeSearchValue(componentValues));
             }
         }
 

src/Microsoft.Health.Fhir.Core/Features/Search/SearchValues/CompositeSearchValue.cs
@@ -4,6 +4,7 @@
 // -------------------------------------------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Linq;
 using EnsureThat;
 
 namespace Microsoft.Health.Fhir.Core.Features.Search.SearchValues
@@ -17,7 +18,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Search.SearchValues
         /// Initializes a new instance of the <see cref="CompositeSearchValue"/> class.
         /// </summary>
         /// <param name="components">The composite component values.</param>
-        public CompositeSearchValue(IReadOnlyList<ISearchValue> components)
+        public CompositeSearchValue(IReadOnlyList<IReadOnlyList<ISearchValue>> components)
         {
             EnsureArg.IsNotNull(components, nameof(components));
             EnsureArg.HasItems(components, nameof(components));
@@ -28,7 +29,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Search.SearchValues
         /// <summary>
         /// Gets the composite component values.
         /// </summary>
-        public IReadOnlyList<ISearchValue> Components { get; }
+        public IReadOnlyList<IReadOnlyList<ISearchValue>> Components { get; }
 
         /// <inheritdoc />
         public bool IsValidAsCompositeComponent => false;
@@ -44,7 +45,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Search.SearchValues
         /// <inheritdoc />
         public override string ToString()
         {
-            return string.Join("$", Components);
+            return string.Join(" $ ", Components.Select(component => string.Join(", ", component.Select(v => $"({v})"))));
         }
     }
 }

src/Microsoft.Health.Fhir.CosmosDb.UnitTests/Features/Storage/Search/SearchIndexEntryJObjectGeneratorTests.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Hl7.Fhir.Model;
+using Microsoft.Health.Fhir.Core.Features.Search;
 using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
 using Microsoft.Health.Fhir.Core.Models;
 using Microsoft.Health.Fhir.CosmosDb.Features.Storage.Search;
@@ -38,22 +39,108 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
             const string system3 = "s3";
             const string code3 = "c3";
 
-            var value = new CompositeSearchValue(new ISearchValue[]
-            {
-                new TokenSearchValue(system1, code1, text1),
-                new TokenSearchValue(system2, code2, text2),
-                new QuantitySearchValue(system3, code3, quantity),
-            });
+            var value = new CompositeSearchValue(
+                new[]
+                {
+                    new ISearchValue[] { new TokenSearchValue(system1, code1, text1) },
+                    new ISearchValue[] { new TokenSearchValue(system2, code2, text2) },
+                    new ISearchValue[] { new QuantitySearchValue(system3, code3, quantity) },
+                });
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple("s_0", system1),
                 CreateTuple("c_0", code1),
                 CreateTuple("s_1", system2),
                 CreateTuple("c_1", code2),
                 CreateTuple("s_2", system3),
                 CreateTuple("c_2", code3),
-                CreateTuple("q_2", quantity));
+                CreateTuple("q_2", quantity),
+            };
+
+            TestAndValidateOutput(
+                "composite",
+                value,
+                expectedValues);
+        }
+
+        [Fact]
+        public void GivenACompositeSearchValueWithMultipleValues_WhenGenerated_ThenCorrectJObjectShouldBeCreated()
+        {
+            const string system1 = "s1";
+            const string code1 = "s2";
+            const string text1 = "t1";
+            const string system2 = "s2";
+            const string code2 = "c2";
+            const string text2 = "T2";
+            const decimal quantity = 123.5m;
+            const string system3 = "s3";
+            const string code3 = "c3";
+            const string s1 = "test1";
+            const string s2 = "test2";
+            const string normalizedS1 = "TEST1";
+            const string normalizedS2 = "TEST2";
+
+            var value = new CompositeSearchValue(
+                new[]
+                {
+                    new ISearchValue[]
+                    {
+                        new TokenSearchValue(system1, code1, text1),
+                        new TokenSearchValue(system2, code2, text2),
+                    },
+                    new ISearchValue[] { new QuantitySearchValue(system3, code3, quantity) },
+                    new ISearchValue[]
+                    {
+                        new StringSearchValue(s1),
+                        new StringSearchValue(s2),
+                    },
+                });
+
+            var expectedValues = new[]
+            {
+                new[]
+                {
+                    CreateTuple("s_0", system1),
+                    CreateTuple("c_0", code1),
+                    CreateTuple("s_1", system3),
+                    CreateTuple("c_1", code3),
+                    CreateTuple("q_1", quantity),
+                    CreateTuple("n_s_2", normalizedS1),
+                },
+                new[]
+                {
+                    CreateTuple("s_0", system1),
+                    CreateTuple("c_0", code1),
+                    CreateTuple("s_1", system3),
+                    CreateTuple("c_1", code3),
+                    CreateTuple("q_1", quantity),
+                    CreateTuple("n_s_2", normalizedS2),
+                },
+                new[]
+                {
+                    CreateTuple("s_0", system2),
+                    CreateTuple("c_0", code2),
+                    CreateTuple("s_1", system3),
+                    CreateTuple("c_1", code3),
+                    CreateTuple("q_1", quantity),
+                    CreateTuple("n_s_2", normalizedS1),
+                },
+                new[]
+                {
+                    CreateTuple("s_0", system2),
+                    CreateTuple("c_0", code2),
+                    CreateTuple("s_1", system3),
+                    CreateTuple("c_1", code3),
+                    CreateTuple("q_1", quantity),
+                    CreateTuple("n_s_2", normalizedS2),
+                },
+            };
+
+            TestAndValidateOutput(
+                "composite",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -63,10 +150,16 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
                 new PartialDateTime(2000),
                 new PartialDateTime(2001));
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple("st", "2000-01-01T00:00:00.0000000+00:00"),
-                CreateTuple("et", "2001-12-31T23:59:59.9999999+00:00"));
+                CreateTuple("et", "2001-12-31T23:59:59.9999999+00:00"),
+            };
+
+            TestAndValidateOutput(
+                "date",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -77,8 +170,9 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
             var value = new NumberSearchValue(number);
 
             TestAndValidateOutput(
+                "number",
                 value,
-                CreateTuple("n", number));
+                new[] { CreateTuple("n", number) });
         }
 
         [Fact]
@@ -93,11 +187,17 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
                 code,
                 quantity);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple(SystemName, system),
                 CreateTuple(CodeName, code),
-                CreateTuple("q", quantity));
+                CreateTuple("q", quantity),
+            };
+
+            TestAndValidateOutput(
+                "quantity",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -107,10 +207,16 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
 
             var value = new ReferenceSearchValue(ReferenceKind.InternalOrExternal, null, ResourceType.Immunization, resourceId);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple(ReferenceResourceTypeName, "Immunization"),
-                CreateTuple(ReferenceResourceIdName, resourceId));
+                CreateTuple(ReferenceResourceIdName, resourceId),
+            };
+
+            TestAndValidateOutput(
+                "reference",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -121,11 +227,17 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
 
             var value = new ReferenceSearchValue(ReferenceKind.Internal, baseUri, ResourceType.Account, resourceId);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple("rb", baseUri.ToString()),
                 CreateTuple(ReferenceResourceTypeName, "Account"),
-                CreateTuple(ReferenceResourceIdName, resourceId));
+                CreateTuple(ReferenceResourceIdName, resourceId),
+            };
+
+            TestAndValidateOutput(
+                "reference",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -135,10 +247,16 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
 
             StringSearchValue value = new StringSearchValue(s);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple("s", s),
-                CreateTuple("n_s", s.ToUpperInvariant()));
+                CreateTuple("n_s", s.ToUpperInvariant()),
+            };
+
+            TestAndValidateOutput(
+                "string",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -149,10 +267,16 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
 
             var value = new TokenSearchValue(null, code, text);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple(CodeName, code),
-                CreateTuple(TextName, text));
+                CreateTuple(TextName, text),
+            };
+
+            TestAndValidateOutput(
+                "token",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -163,10 +287,16 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
 
             var value = new TokenSearchValue(system, null, text);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple(SystemName, system),
-                CreateTuple(TextName, text));
+                CreateTuple(TextName, text),
+            };
+
+            TestAndValidateOutput(
+                "token",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -177,10 +307,16 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
 
             var value = new TokenSearchValue(system, code, null);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple(SystemName, system),
-                CreateTuple(CodeName, code));
+                CreateTuple(CodeName, code),
+            };
+
+            TestAndValidateOutput(
+                "token",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -192,11 +328,17 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
 
             var value = new TokenSearchValue(system, code, text);
 
-            TestAndValidateOutput(
-                value,
+            var expectedValues = new[]
+            {
                 CreateTuple(SystemName, system),
                 CreateTuple(CodeName, code),
-                CreateTuple(TextName, text.ToUpperInvariant()));
+                CreateTuple(TextName, text.ToUpperInvariant()),
+            };
+
+            TestAndValidateOutput(
+                "token",
+                value,
+                expectedValues);
         }
 
         [Fact]
@@ -207,25 +349,37 @@ namespace Microsoft.Health.Fhir.CosmosDb.UnitTests.Features.Storage.Search
             var value = new UriSearchValue(uri);
 
             TestAndValidateOutput(
+                "uri",
                 value,
-                CreateTuple("u", uri));
+                new[] { CreateTuple("u", uri) });
         }
 
-        private static Tuple<string, object> CreateTuple(string key, object value)
+        private static (string Name, object Value) CreateTuple(string key, object value)
         {
-            return new Tuple<string, object>(key, value);
+            return (key, value);
         }
 
-        private void TestAndValidateOutput(ISearchValue value, params Tuple<string, object>[] expectedValues)
+        private void TestAndValidateOutput(string parameterName, ISearchValue value, params (string Name, object Value)[][] expectedValues)
         {
-            value.AcceptVisitor(_generator);
+            SearchIndexEntry entry = new SearchIndexEntry(parameterName, value);
 
-            JObject result = _generator.Output;
+            IReadOnlyList<JObject> generatedObjects = _generator.Generate(entry);
 
-            Assert.NotNull(result);
+            Assert.NotNull(generatedObjects);
             Assert.Collection(
-                result,
-                expectedValues.Select(v => (Action<KeyValuePair<string, JToken>>)(p => ValidateProperty(v.Item1, v.Item2, p))).ToArray());
+                generatedObjects,
+                expectedValues.Select(v => (Action<JObject>)(p => ValidateObject(parameterName, v, p))).ToArray());
+        }
+
+        private void ValidateObject(string expectedParameterName, IEnumerable<(string Name, object Value)> expectedValues, JObject obj)
+        {
+            // Add the parameter name validation.
+            expectedValues = expectedValues.Prepend(("p", expectedParameterName));
+
+            Assert.NotNull(obj);
+            Assert.Collection(
+                obj,
+                expectedValues.Select(v => (Action<KeyValuePair<string, JToken>>)(p => ValidateProperty(v.Name, v.Value, p))).ToArray());
         }
 
         private void ValidateProperty(string expectedName, object expectedValue, KeyValuePair<string, JToken> property)

src/Microsoft.Health.Fhir.CosmosDb/Features/Storage/Search/SearchIndexEntryJObjectGenerator.cs
@@ -3,7 +3,11 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Globalization;
+using EnsureThat;
+using Microsoft.Health.Fhir.Core.Extensions;
+using Microsoft.Health.Fhir.Core.Features.Search;
 using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
 using Microsoft.Health.Fhir.CosmosDb.Features.Search;
 using Newtonsoft.Json.Linq;
@@ -12,42 +16,59 @@ namespace Microsoft.Health.Fhir.CosmosDb.Features.Storage.Search
 {
     internal class SearchIndexEntryJObjectGenerator : ISearchValueVisitor
     {
+        private List<JObject> _generatedObjects = new List<JObject>();
+
         public SearchIndexEntryJObjectGenerator()
         {
-            Reset();
         }
+
+        private SearchIndexEntry Entry { get; set; }
 
         private int? Index { get; set; }
 
-        private bool ExcludeTokenText { get; set; }
+        private bool IsCompositeComponent => Index != null;
 
-        public JObject Output
+        private JObject CurrentEntry { get; set; }
+
+        public IReadOnlyList<JObject> Generate(SearchIndexEntry entry)
         {
-            get;
-            private set;
+            EnsureArg.IsNotNull(entry, nameof(entry));
+
+            Entry = entry;
+            CurrentEntry = null;
+            _generatedObjects.Clear();
+
+            entry.Value.AcceptVisitor(this);
+
+            return _generatedObjects;
         }
 
-        public void Visit(CompositeSearchValue composite)
+        void ISearchValueVisitor.Visit(CompositeSearchValue composite)
         {
-            try
+            foreach (IEnumerable<ISearchValue> componentValues in composite.Components.CartesianProduct())
             {
-                // Set the component index and process individual component of the composite value.
-                for (int i = 0; i < composite.Components.Count; i++)
-                {
-                    Index = i;
-                    ExcludeTokenText = true;
+                int index = 0;
 
-                    composite.Components[i].AcceptVisitor(this);
+                CreateEntry();
+
+                try
+                {
+                    foreach (ISearchValue componentValue in componentValues)
+                    {
+                        // Set the component index and process individual component of the composite value.
+                        Index = index++;
+
+                        componentValue.AcceptVisitor(this);
+                    }
+                }
+                finally
+                {
+                    Index = null;
                 }
             }
-            finally
-            {
-                Index = null;
-                ExcludeTokenText = false;
-            }
         }
 
-        public void Visit(DateTimeSearchValue dateTime)
+        void ISearchValueVisitor.Visit(DateTimeSearchValue dateTime)
         {
             // By default, Json.NET will serialize date time object using format
             // "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFFK". The 'F' format specifier
@@ -60,51 +81,60 @@ namespace Microsoft.Health.Fhir.CosmosDb.Features.Storage.Search
             AddProperty(SearchValueConstants.DateTimeEndName, dateTime.End.ToString("o", CultureInfo.InvariantCulture));
         }
 
-        public void Visit(NumberSearchValue number)
+        void ISearchValueVisitor.Visit(NumberSearchValue number)
         {
             AddProperty(SearchValueConstants.NumberName, number.Number);
         }
 
-        public void Visit(QuantitySearchValue quantity)
+        void ISearchValueVisitor.Visit(QuantitySearchValue quantity)
         {
-            AddProperty(SearchValueConstants.SystemName, quantity.System);
-            AddProperty(SearchValueConstants.CodeName, quantity.Code);
+            AddPropertyIfNotNull(SearchValueConstants.SystemName, quantity.System);
+            AddPropertyIfNotNull(SearchValueConstants.CodeName, quantity.Code);
             AddProperty(SearchValueConstants.QuantityName, quantity.Quantity);
         }
 
-        public void Visit(ReferenceSearchValue reference)
+        void ISearchValueVisitor.Visit(ReferenceSearchValue reference)
         {
             AddPropertyIfNotNull(SearchValueConstants.ReferenceBaseUriName, reference.BaseUri?.ToString());
             AddPropertyIfNotNull(SearchValueConstants.ReferenceResourceTypeName, reference.ResourceType?.ToString());
             AddProperty(SearchValueConstants.ReferenceResourceIdName, reference.ResourceId);
         }
 
-        public void Visit(StringSearchValue s)
+        void ISearchValueVisitor.Visit(StringSearchValue s)
         {
-            AddProperty(SearchValueConstants.StringName, s.String);
+            if (!IsCompositeComponent)
+            {
+                AddProperty(SearchValueConstants.StringName, s.String);
+            }
+
             AddProperty(SearchValueConstants.NormalizedStringName, s.String.ToUpperInvariant());
         }
 
-        public void Visit(TokenSearchValue token)
+        void ISearchValueVisitor.Visit(TokenSearchValue token)
         {
             AddPropertyIfNotNull(SearchValueConstants.SystemName, token.System);
             AddPropertyIfNotNull(SearchValueConstants.CodeName, token.Code);
 
-            if (!ExcludeTokenText)
+            if (!IsCompositeComponent)
             {
                 // Since text is case-insensitive search, it will always be normalized.
                 AddPropertyIfNotNull(SearchValueConstants.NormalizedTextName, token.Text?.ToUpperInvariant());
             }
         }
 
-        public void Visit(UriSearchValue uri)
+        void ISearchValueVisitor.Visit(UriSearchValue uri)
         {
             AddProperty(SearchValueConstants.UriName, uri.Uri);
         }
 
-        public void Reset()
+        private void CreateEntry()
         {
-            Output = new JObject();
+            CurrentEntry = new JObject
+            {
+                new JProperty(SearchValueConstants.ParamName, Entry.ParamName),
+            };
+
+            _generatedObjects.Add(CurrentEntry);
         }
 
         private void AddProperty(string name, object value)
@@ -114,7 +144,12 @@ namespace Microsoft.Health.Fhir.CosmosDb.Features.Storage.Search
                 name = $"{name}_{Index}";
             }
 
-            Output.Add(new JProperty(name, value));
+            if (CurrentEntry == null)
+            {
+                CreateEntry();
+            }
+
+            CurrentEntry.Add(new JProperty(name, value));
         }
 
         private void AddPropertyIfNotNull(string name, string value)

commit 88fbf57989058cb9d242e82e75b233c9ea4c30fc

Fixed the concurrency issue. (#193)
This is a regression from #173. When multiple requests are processed simultaneously, the collection could be updated while the enumeration is happening, which results in error.

Addresses #192

src/Microsoft.Health.Fhir.CosmosDb/Features/Storage/Search/SearchIndexEntryConverter.cs
@@ -42,15 +42,15 @@ namespace Microsoft.Health.Fhir.CosmosDb.Features.Storage.Search
             try
             {
                 generatedObjects = generator.Generate(searchIndexEntry);
+
+                foreach (JObject generatedObj in generatedObjects)
+                {
+                    generatedObj.WriteTo(writer);
+                }
             }
             finally
             {
                 CachedGenerators.Enqueue(generator);
-            }
-
-            foreach (JObject generatedObj in generatedObjects)
-            {
-                generatedObj.WriteTo(writer);
             }
         }
     }