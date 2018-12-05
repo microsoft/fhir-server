d9354c0f42a4c80180238180c98a0d71dad7bfc8
Michael Hansen <mihansen@microsoft.com>
2018-12-05 14:55:45 -0800

    Added SmartLauncher and sample-app

---
+++ samples/apps/SmartLauncher/Models/ConfigurationModel.cs
@@ -0,0 +1,12 @@
+
+using System.Collections.Generic;
+
+namespace SmartLauncher
+{
+    public class SmartLauncherConfig
+    {
+        public string FhirServerUrl { get; set; }
+        public string SmartAppUrl { get; set; }
+        public string ClientId { get; set; }
+    }
+}
---
+++ samples/apps/SmartLauncher/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace SmartLauncher
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Map("/config", a =>
+            {
+                a.Run(async (context) => 
+                {
+                    SmartLauncherConfig config = new SmartLauncherConfig();
+                    Configuration.Bind(config);
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(config));
+                });
+            });
+        }
+    }
+}
