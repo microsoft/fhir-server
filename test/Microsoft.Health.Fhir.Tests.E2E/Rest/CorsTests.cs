e3d9e3ab19f03af19604af21310baf1164d3203f
Brandon Pollett <brandon.pollett@gmail.com>
2019-02-14 21:20:18 -0800

    Simplify test.

--- test/Microsoft.Health.Fhir.Tests.E2E/Rest/CorsTests.cs
+++ test/Microsoft.Health.Fhir.Tests.E2E/Rest/CorsTests.cs
@@ -37,7 +37,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest
 
             message.Headers.Add(HeaderNames.Origin, "http://local");
             message.Headers.Add(HeaderNames.AccessControlRequestMethod, "PUT");
-            message.Headers.Add(HeaderNames.AccessControlRequestMethod, "POST");
             message.Headers.Add(HeaderNames.AccessControlRequestHeaders, "authorization");
             message.Headers.Add(HeaderNames.AccessControlRequestHeaders, "content-type");
             message.RequestUri = new Uri(_client.HttpClient.BaseAddress, "/patient");
@@ -47,7 +46,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest
             Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
             Assert.Contains("*", response.Headers.GetValues(HeaderNames.AccessControlAllowOrigin));
 
-            Assert.Contains("\"PUT,POST\"", response.Headers.GetValues(HeaderNames.AccessControlAllowMethods));
+            Assert.Contains("PUT", response.Headers.GetValues(HeaderNames.AccessControlAllowMethods));
 
             Assert.Contains("authorization,content-type", response.Headers.GetValues(HeaderNames.AccessControlAllowHeaders));
 
