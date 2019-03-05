34399ad1972f34646c88bf710f386963a5779ae4
Joyce Cunningham <joycech@microsoft.com>
2019-03-04 16:16:28 -0800

    Update FHIR context after authentication so claims are logged in audits

---
+++ src/Microsoft.Health.Fhir.Api/Features/Context/FhirRequestContextAfterAuthenticationMiddleware.cs
@@ -0,0 +1,36 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Health.Fhir.Core.Features.Context;
+
+namespace Microsoft.Health.Fhir.Api.Features.Context
+{
+    public class FhirRequestContextAfterAuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FhirRequestContextAfterAuthenticationMiddleware(RequestDelegate next)
+        {
+            EnsureArg.IsNotNull(next, nameof(next));
+
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context, IFhirRequestContextAccessor fhirRequestContextAccessor)
+        {
+            // Set the user again to pick up anything that was set during authentication, e.g. claims.
+            if (context.User != null)
+            {
+                fhirRequestContextAccessor.FhirRequestContext.Principal = context.User;
+            }
+
+            // Call the next delegate/middleware in the pipeline
+            return _next(context);
+        }
+    }
+}
