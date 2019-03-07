b55f7fac047ed5f47088756dc1fc9db8816d6a39
Narasimhan Madabusi <namadabu@microsoft.com>
2019-03-06 16:11:44 -0800

    Added a filter attribute to validate the headers in export operations.

---
+++ src/Microsoft.Health.Fhir.Api/Features/Filters/ValidateOperationHeadersFilterAttribute.cs
@@ -0,0 +1,47 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using EnsureThat;
+using FluentValidation.Results;
+using Hl7.Fhir.Model;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Health.Fhir.Core.Features.Validation;
+using Microsoft.Net.Http.Headers;
+
+namespace Microsoft.Health.Fhir.Api.Features.Filters
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    internal class ValidateOperationHeadersFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            EnsureArg.IsNotNull(context, nameof(context));
+
+            if (context.HttpContext.Request.Headers.TryGetValue(HeaderNames.Accept, out var acceptHeaderValue))
+            {
+                if (acceptHeaderValue.Count != 1 || !string.Equals(acceptHeaderValue[0], "application/fhir+json", StringComparison.Ordinal))
+                {
+                    throw new ResourceNotValidException(new List<ValidationFailure>
+                    {
+                        new ValidationFailure(nameof(Base.TypeName), Resources.UnsupportedAcceptHeader),
+                    });
+                }
+            }
+
+            if (context.HttpContext.Request.Headers.TryGetValue("Prefer", out var preferHeaderValue))
+            {
+                if (preferHeaderValue.Count != 1 || !string.Equals(preferHeaderValue[0], "respond-async", StringComparison.Ordinal))
+                {
+                    throw new ResourceNotValidException(new List<ValidationFailure>
+                    {
+                        new ValidationFailure(nameof(Base.TypeName), Resources.UnsupportedPreferHeader),
+                    });
+                }
+            }
+        }
+    }
+}
