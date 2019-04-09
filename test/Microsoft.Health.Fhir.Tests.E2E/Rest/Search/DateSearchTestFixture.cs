db3f3491af124ec0c341b8598a11ed4f3e01553d
Brendan Kowitz <brendan.kowitz@gmail.com>
2019-04-09 09:46:12 -0700

    Updates Hl7.Fhir.Stu3 to version 1.2 (#405)
    * Updates Hl7.Fhir.Stu3 to version 1.2
    * Updates date search tests

--- test/Microsoft.Health.Fhir.Tests.E2E/Rest/Search/DateSearchTestFixture.cs
+++ test/Microsoft.Health.Fhir.Tests.E2E/Rest/Search/DateSearchTestFixture.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using Hl7.Fhir.Model;
 using Microsoft.Health.Fhir.Tests.E2E.Common;
@@ -15,19 +16,38 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
         public DateSearchTestFixture()
             : base()
         {
-            FhirClient.DeleteAllResources(ResourceType.Patient).Wait();
+            // Creates a unique code for searches
+            Coding = new Coding
+            {
+                Code = Guid.NewGuid().ToString(),
+                System = "http://fhir-server-test/guid",
+            };
 
-            Patients = FhirClient.CreateResourcesAsync<Patient>(
-                p => p.BirthDate = "1979-12-31",                // 1979-12-31T00:00:00.0000000 <-> 1979-12-31T23:59:59.9999999
-                p => p.BirthDate = "1980",                      // 1980-01-01T00:00:00.0000000 <-> 1980-12-31T23:59:59.9999999
-                p => p.BirthDate = "1980-05",                   // 1980-05-01T00:00:00.0000000 <-> 1980-05-31T23:59:59.9999999
-                p => p.BirthDate = "1980-05-11",                // 1980-05-11T00:00:00.0000000 <-> 1980-05-11T23:59:59.9999999
-                p => p.BirthDate = "1980-05-11T16:32",          // 1980-05-11T16:32:00.0000000 <-> 1980-05-11T16:32:59.9999999
-                p => p.BirthDate = "1980-05-11T16:32:15",       // 1980-05-11T16:32:15.0000000 <-> 1980-05-11T16:32:15.9999999
-                p => p.BirthDate = "1980-05-11T16:32:15.500",   // 1980-05-11T16:32:15.5000000 <-> 1980-05-11T16:32:15.5000000
-                p => p.BirthDate = "1981-01-01").Result;        // 1981-01-01T00:00:00.0000000 <-> 1981-12-31T23:59:59.9999999
+            Observations = FhirClient.CreateResourcesAsync<Observation>(
+                p => SetObservation(p, "1979-12-31"),                // 1979-12-31T00:00:00.0000000 <-> 1979-12-31T23:59:59.9999999
+                p => SetObservation(p, "1980"),                      // 1980-01-01T00:00:00.0000000 <-> 1980-12-31T23:59:59.9999999
+                p => SetObservation(p, "1980-05"),                   // 1980-05-01T00:00:00.0000000 <-> 1980-05-31T23:59:59.9999999
+                p => SetObservation(p, "1980-05-11"),                // 1980-05-11T00:00:00.0000000 <-> 1980-05-11T23:59:59.9999999
+                p => SetObservation(p, "1980-05-11T16:32:15"),       // 1980-05-11T16:32:15.0000000 <-> 1980-05-11T16:32:15.9999999
+                p => SetObservation(p, "1980-05-11T16:32:15.500"),   // 1980-05-11T16:32:15.5000000 <-> 1980-05-11T16:32:15.5000000
+                p => SetObservation(p, "1981-01-01")).Result;        // 1981-01-01T00:00:00.0000000 <-> 1981-12-31T23:59:59.9999999
+
+            void SetObservation(Observation observation, string date)
+            {
+                observation.Status = ObservationStatus.Final;
+                observation.Code = new CodeableConcept
+                {
+                    Coding = new List<Coding>
+                    {
+                        Coding,
+                    },
+                };
+                observation.Effective = new FhirDateTime(date);
+            }
         }
 
-        public IReadOnlyList<Patient> Patients { get; }
+        public Coding Coding { get; }
+
+        public IReadOnlyList<Observation> Observations { get; }
     }
 }
