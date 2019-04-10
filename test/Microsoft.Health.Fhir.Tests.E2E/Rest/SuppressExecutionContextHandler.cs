cbcd2f2d9b8a8add5a6c7fd669992bc864b027a9
Brandon Pollett <brandon.pollett@gmail.com>
2019-04-10 10:48:41 -0700

    Add test fixture workaround, update dotnet

---
+++ test/Microsoft.Health.Fhir.Tests.E2E/Rest/SuppressExecutionContextHandler.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.Fhir.Tests.E2E.Rest
+{
+    internal class SuppressExecutionContextHandler : DelegatingHandler
+    {
+        public SuppressExecutionContextHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // NOTE: We DO NOT want to 'await' the task inside this using. We're just suppressing execution context flow
+            // while the task itself is created (which is what would capture the context). After that we just return the
+            // (now detached task) to the caller.
+            Task<HttpResponseMessage> t;
+            using (ExecutionContext.SuppressFlow())
+            {
+                t = Task.Run(() => base.SendAsync(request, cancellationToken));
+            }
+
+            return t;
+        }
+    }
+}
