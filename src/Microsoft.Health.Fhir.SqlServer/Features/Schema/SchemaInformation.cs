767d394f314fb77adee080135ef01c5df471dec2
Brandon Pollett <brandon.pollett@gmail.com>
2019-04-23 13:50:48 -0700

    Add schema initializer.

---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/SchemaInformation.cs
@@ -0,0 +1,22 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Fhir.SqlServer.Features.Schema
+{
+    public class SchemaInformation
+    {
+        public SchemaInformation()
+        {
+            Min = SchemaVersion.V1;
+            Max = SchemaVersion.V1;
+        }
+
+        public SchemaVersion Min { get; }
+
+        public SchemaVersion Max { get; }
+
+        public SchemaVersion? Current { get; set; }
+    }
+}
