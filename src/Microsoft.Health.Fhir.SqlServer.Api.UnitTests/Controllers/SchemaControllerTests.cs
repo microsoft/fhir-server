2aaef8e4f16b7c9f66544b6077922748896353be
brandonpollett <brpoll@microsoft.com>
2019-05-01 10:55:43 -0700

    Add available schema API (#435)

--- src/Microsoft.Health.Fhir.SqlServer.Api.UnitTests/Controllers/SchemaControllerTests.cs
+++ src/Microsoft.Health.Fhir.SqlServer.Api.UnitTests/Controllers/SchemaControllerTests.cs
@@ -4,8 +4,15 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Health.Fhir.Core.Features.Routing;
 using Microsoft.Health.Fhir.SqlServer.Api.Controllers;
+using Microsoft.Health.Fhir.SqlServer.Api.Features.Routing;
+using Microsoft.Health.Fhir.SqlServer.Features.Schema;
+using Newtonsoft.Json.Linq;
+using NSubstitute;
 using Xunit;
 
 namespace Microsoft.Health.Fhir.SqlServer.Api.UnitTests.Controllers
@@ -16,7 +23,10 @@ namespace Microsoft.Health.Fhir.SqlServer.Api.UnitTests.Controllers
 
         public SchemaControllerTests()
         {
-            _schemaController = new SchemaController(NullLogger<SchemaController>.Instance);
+            var schemaInformation = new SchemaInformation();
+            var urlResolver = Substitute.For<IUrlResolver>();
+            urlResolver.ResolveRouteNameUrl(RouteNames.Script, Arg.Any<IDictionary<string, object>>()).Returns(new Uri("https://localhost/script"));
+            _schemaController = new SchemaController(schemaInformation, urlResolver, NullLogger<SchemaController>.Instance);
         }
 
         [Fact]
@@ -26,9 +36,19 @@ namespace Microsoft.Health.Fhir.SqlServer.Api.UnitTests.Controllers
         }
 
         [Fact]
-        public void GivenAnAvailableVersionsRequest_WhenNotImplemented_ThenNotImplementedShouldBeThrown()
+        public void GivenAnAvailableVersionsRequest_WhenCurrentVersionIsNull_ThenAllVersionsReturned()
         {
-            Assert.Throws<NotImplementedException>(() => _schemaController.AvailableVersions());
+            ActionResult result = _schemaController.AvailableVersions();
+
+            var jsonResult = result as JsonResult;
+            Assert.NotNull(jsonResult);
+
+            var jArrayResult = JArray.FromObject(jsonResult.Value);
+            Assert.Equal(Enum.GetNames(typeof(SchemaVersion)).Length, jArrayResult.Count);
+
+            JToken firstResult = jArrayResult.First;
+            Assert.Equal(1, firstResult["id"]);
+            Assert.Equal("https://localhost/script", firstResult["script"]);
         }
 
         [Fact]
