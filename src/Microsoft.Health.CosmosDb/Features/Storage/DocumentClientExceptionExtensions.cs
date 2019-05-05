5abe8209db467b411e7c9c04e4dae071edcdf3d1
John Stairs <jostairs@microsoft.com>
2019-05-05 11:25:28 -0700

    Update comment

--- src/Microsoft.Health.CosmosDb/Features/Storage/DocumentClientExceptionExtensions.cs
+++ src/Microsoft.Health.CosmosDb/Features/Storage/DocumentClientExceptionExtensions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Health.CosmosDb.Features.Storage
     public static class DocumentClientExceptionExtensions
     {
         /// <summary>
-        /// When a javascript procedure throws an exception in code, such as
+        /// When a javascript stored procedure throws an exception in code, such as
         /// throw new Error(404, "Document not found");
         /// the status code of the exception will be 400 bad request, but there will be a substatus header
         /// with the error code specified in the Error object (404 in this example).
