4b801200bbd69adac1f63bc787ff8d67e67528aa
John Stairs <jostairs@microsoft.com>
2019-05-15 00:51:38 -0700

    A couple of tests

---
+++ src/Microsoft.Health.Fhir.SqlServer.UnitTests/SqlMetadataUtilitiesTests.cs
@@ -0,0 +1,31 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Data;
+using Microsoft.Health.Fhir.SqlServer.Features.Schema.Model;
+using Microsoft.SqlServer.Server;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.SqlServer.UnitTests
+{
+    public class SqlMetadataUtilitiesTests
+    {
+        [Fact]
+        public void GivenASqlMetadataInstanceWithDefaultScaleAndPrecision_WhenGettingMinAndMaxValues_ReturnsCorrectValues()
+        {
+            var sqlMetaData = new SqlMetaData("foo", SqlDbType.Decimal);
+            Assert.Equal(-999999999999999999M, SqlMetadataUtilities.GetMinValueForDecimalColumn(sqlMetaData));
+            Assert.Equal(999999999999999999M, SqlMetadataUtilities.GetMaxValueForDecimalColumn(sqlMetaData));
+        }
+
+        [Fact]
+        public void GivenASqlMetadataInstanceWithSpecifiedScaleAndPrecision_WhenGettingMinAndMaxValues_ReturnsCorrectValues()
+        {
+            var sqlMetaData = new SqlMetaData("foo", SqlDbType.Decimal, precision: 10, scale: 3);
+            Assert.Equal(-9999999.999M, SqlMetadataUtilities.GetMinValueForDecimalColumn(sqlMetaData));
+            Assert.Equal(9999999.999M, SqlMetadataUtilities.GetMaxValueForDecimalColumn(sqlMetaData));
+        }
+    }
+}
--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/ResourceMetadata.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/ResourceMetadata.cs
@@ -11,6 +11,9 @@ using Microsoft.Health.Fhir.Core.Features.Search;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 {
+    /// <summary>
+    /// Contains grouped search indices, compartments and write claims of a resource
+    /// </summary>
     internal class ResourceMetadata
     {
         private readonly ILookup<Type, SearchIndexEntry> _groupedSearchIndices;
@@ -26,6 +29,14 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 
         public IReadOnlyCollection<KeyValuePair<string, string>> WriteClaims { get; }
 
+        /// <summary>
+        /// Gets the search index entries by their type key. The type should be one returned by
+        /// <see cref="SearchParameterToSearchValueTypeMap.GetSearchValueType(Hl7.Fhir.Model.SearchParameter)"/>:
+        /// either implementing ISearchValue or for composites a Tuple with the component types as type arguments,
+        /// for example: <see cref="Tuple{UriSearchValue}"/>
+        /// </summary>
+        /// <param name="type">A type representing the search parameter type</param>
+        /// <returns>The parameters corresponding to the given type.</returns>
         public IEnumerable<SearchIndexEntry> GetSearchIndexEntriesByType(Type type)
         {
             // TODO: ensure all types are consumed before disposal
