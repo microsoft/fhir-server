de24db3e7c876563e877b3c6ac33a2eb2e5dee66
Brandon Pollett <brandon.pollett@gmail.com>
2019-05-31 23:12:42 -0700

    Add string search tests.

---
+++ test/Microsoft.Health.Fhir.Tests.E2E/Rest/Search/StringSearchTestFixture.cs
@@ -0,0 +1,39 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.Health.Fhir.Core.Models;
+using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
+using Microsoft.Health.Fhir.Tests.E2E.Common;
+
+namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
+{
+    public class StringSearchTestFixture : HttpIntegrationTestFixture
+    {
+        public StringSearchTestFixture(DataStore dataStore, Format format, FhirVersion fhirVersion)
+            : base(dataStore, format, fhirVersion)
+        {
+            // Prepare the resources used for string search tests.
+            FhirClient.DeleteAllResources("Patient").Wait();
+
+            Patients = FhirClient.CreateResourcesAsync(
+                    FhirClient.GetDefaultPatient,
+                    p => SetPatientInfo(p, "Seattle", "Smith"),
+                    p => SetPatientInfo(p, "Portland", "Williams"),
+                    p => SetPatientInfo(p, "Vancouver", "Anderson"))
+                .Result;
+
+            ResourceElement SetPatientInfo(ResourceElement patient, string city, string family)
+            {
+                patient = FhirClient.UpdatePatientAddressCity(patient, city);
+                patient = FhirClient.UpdatePatientFamilyName(patient, family);
+
+                return patient;
+            }
+        }
+
+        public IReadOnlyList<ResourceElement> Patients { get; }
+    }
+}
---
+++ test/Microsoft.Health.Fhir.Tests.E2E/Rest/Search/StringSearchTests.cs
@@ -0,0 +1,71 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Hl7.Fhir.ElementModel;
+using Microsoft.Health.Fhir.Core.Models;
+using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
+{
+    [HttpIntegrationFixtureArgumentSets(DataStore.CosmosDb, Format.Json, FhirVersion.All)]
+    public class StringSearchTests : SearchTestsBase<StringSearchTestFixture>
+    {
+        public StringSearchTests(StringSearchTestFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        [Theory]
+        [Trait(Traits.Priority, Priority.One)]
+        [InlineData("", "seattle", true)]
+        [InlineData("", "SEATTLE", true)]
+        [InlineData("", "Seattle", true)]
+        [InlineData("", "Sea", true)]
+        [InlineData("", "sea", true)]
+        [InlineData("", "123", false)]
+        [InlineData(":exact", "Seattle", true)]
+        [InlineData(":exact", "seattle", false)]
+        [InlineData(":exact", "SEATTLE", false)]
+        [InlineData(":exact", "Sea", false)]
+        [InlineData(":contains", "att", true)]
+        [InlineData(":contains", "EAT", true)]
+        [InlineData(":contains", "123", false)]
+        public async Task GivenAStringSearchParam_WhenSearched_ThenCorrectBundleShouldBeReturned(string modifier, string valueToSearch, bool shouldMatch)
+        {
+            string query = $"address-city{modifier}={valueToSearch}";
+
+            ResourceElement bundle = await Client.SearchAsync("Patient", query);
+
+            Assert.NotNull(bundle);
+
+            ResourceElement expectedPatient = Fixture.Patients[0];
+
+            if (shouldMatch)
+            {
+                IEnumerable<ITypedElement> bundleEntries = bundle.Select("Resource.entry.resource");
+                Assert.NotEmpty(bundleEntries);
+                Assert.Collection(
+                    bundleEntries,
+                    e => Assert.True(Client.Compare(expectedPatient, e)));
+            }
+            else
+            {
+                Assert.Empty(bundle.Select("Resource.entry.resource"));
+            }
+        }
+
+        [Fact]
+        public async Task GivenAStringSearchParamWithMultipleValues_WhenSearched_ThenCorrectBundleShouldBeReturned()
+        {
+            ResourceElement bundle = await Client.SearchAsync("Patient", "family=Smith,Ander");
+
+            ValidateBundle(bundle, Fixture.Patients[0], Fixture.Patients[2]);
+        }
+    }
+}
