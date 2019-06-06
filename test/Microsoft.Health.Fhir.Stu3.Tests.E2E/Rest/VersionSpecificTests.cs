975ba47e3a330c8c693c325e24cd9c364f8812c2
Jack Liu <jackliu@microsoft.com>
2019-06-06 13:55:45 -0700

    Added R4 specific tests.

---
+++ test/Microsoft.Health.Fhir.Stu3.Tests.E2E/Rest/VersionSpecificTests.cs
@@ -0,0 +1,23 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Fhir.Web;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.Health.Fhir.Tests.E2E.Rest
+{
+    /// <summary>
+    /// Provides R4 specific tests.
+    /// </summary>
+    public partial class VersionSpecificTests : IClassFixture<HttpIntegrationTestFixture<Startup>>
+    {
+        [Fact]
+        public async Task GivenStu3Server_WhenCapabilityStatementIsRetrieved_ThenCorrectVersionShouldBeReturned()
+        {
+            await TestCapabilityStatementFhirVersion("3.0.1");
+        }
+    }
+}
