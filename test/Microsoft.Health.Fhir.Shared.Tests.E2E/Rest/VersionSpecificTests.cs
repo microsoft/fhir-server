231de7c33f7db99195201a6d9a99a9301409be77
Jack <jack.liu@microsoft.com>
2019-06-14 16:52:21 -0700

    Added R4 specific tests (#542)
    Added R4 specific tests.

---
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/VersionSpecificTests.cs
@@ -0,0 +1,36 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Hl7.Fhir.Model;
+using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
+using Microsoft.Health.Fhir.Web;
+using Xunit;
+using FhirClient = Microsoft.Health.Fhir.Tests.E2E.Common.FhirClient;
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.Health.Fhir.Tests.E2E.Rest
+{
+    /// <summary>
+    /// Provides version specific tests.
+    /// </summary>
+    [HttpIntegrationFixtureArgumentSets(DataStore.CosmosDb, Format.Json)]
+    public partial class VersionSpecificTests : IClassFixture<HttpIntegrationTestFixture<Startup>>
+    {
+        private readonly FhirClient _client;
+
+        public VersionSpecificTests(HttpIntegrationTestFixture<Startup> fixture)
+        {
+            _client = fixture.FhirClient;
+        }
+
+        private async Task TestCapabilityStatementFhirVersion(string expectedVersion)
+        {
+            CapabilityStatement capabilityStatement = await _client.ReadAsync<CapabilityStatement>("metadata");
+
+            Assert.NotNull(capabilityStatement.FhirVersionElement);
+            Assert.Equal(expectedVersion, capabilityStatement.FhirVersionElement.ObjectValue);
+        }
+    }
+}
