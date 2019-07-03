49c6355dfa6f92277c896e6a25573ab2c74de301
Jared Erwin <jared.erwin@gmail.com>
2019-07-03 13:44:40 -0700

    Move crucible tests to their own project, and split for Stu3 and R4 (#557)
    This updates our E2E test projects to run the Crucible tests for both Stu3 and R4.  Initial goal was to separate out the Crucible tests to their own test project to be able to more easily run them independently.  However, the test runs were not successfull.  They are still combined and the split may be done later.
    
    * Move crucible tests to their own project, and split for Stu3 and R4
    
    * Separate Known Crucible tests by Stu3 and R4.
    
    * Clean up extra comments and move string to KnownCrucible file.
    
    * Update environment variables using environment constant.
    
    * Undo change to split Crucible tests to their own project.
    
    * Add test resourcetest_immunizationevaluation/X010_ImmunizationEvaluation with POCO literal issue
    
    * Add one more crucible test failure.
    
    * Another crucible failure.

--- test/Microsoft.Health.Fhir.R4.Tests.E2E/KnownCrucibleTests.cs
+++ test/Microsoft.Health.Fhir.R4.Tests.E2E/KnownCrucibleTests.cs
@@ -3,18 +3,11 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Health.Fhir.Tests.Common;
-using Xunit;
-using Xunit.Abstractions;
-
 namespace Microsoft.Health.Fhir.Tests.E2E.Crucible
 {
-    public class CrucibleTestFixture : IClassFixture<CrucibleDataSource>
+    public class KnownCrucibleTests
     {
-        private static readonly string[] KnownFailures =
+        public static readonly string[] KnownFailures =
         {
             "argonautproviderconnectathontest/APCT03",
             "argonautproviderconnectathontest/APCT04",
@@ -176,18 +169,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Crucible
             "connectathonpatchtracktest/C12PATCH_2_(XML)",
             "connectathonpatchtracktest/C12PATCH_3_(JSON)",
             "connectathonpatchtracktest/C12PATCH_3_(XML)",
-            "history001/HI01",
-            "history001/HI02",
-            "history001/HI06",
-            "history001/HI08",
             "history001/HI10",
-            "history001/HI11",
-            "resourcetest_adverseevent/X010_AdverseEvent",
-            "resourcetest_adverseevent/X020_AdverseEvent",
-            "resourcetest_claim/X010_Claim",
-            "resourcetest_claim/X020_Claim",
-            "resourcetest_claimresponse/X010_ClaimResponse",
-            "resourcetest_claimresponse/X020_ClaimResponse",
             "resourcetest_imagingmanifest/X010_ImagingManifest",
             "resourcetest_imagingmanifest/X020_ImagingManifest",
             "search001/SE03G",
@@ -214,7 +196,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Crucible
             "searchtest_account/SE01P_Account",
             "searchtest_activitydefinition/SE01G_ActivityDefinition",
             "searchtest_activitydefinition/SE01P_ActivityDefinition",
-            "searchtest_adverseevent/S000_AdverseEvent",
             "searchtest_adverseevent/SE01G_AdverseEvent",
             "searchtest_adverseevent/SE01P_AdverseEvent",
             "searchtest_allergyintolerance/SE01G_AllergyIntolerance",
@@ -454,7 +435,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Crucible
             "transactionandbatchtest/XFER12",
         };
 
-        private static readonly string[] KnownBroken =
+        public static readonly string[] KnownBroken =
         {
             "argonautproviderconnectathontest/APCT02",
             "connectathonschedulingtracktest/CST01",
@@ -474,90 +455,22 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Crucible
             "connectathon-15-patient-fhirserver-99-all-server-id-xml/02-UpdatePatient",
             "connectathon-15-patient-fhirserver-99-all-server-id-json/07-PatientDelete",
             "connectathon-15-patient-fhirserver-99-all-server-id-xml/07-PatientDelete",
-            "history001/HI09",  // Related to Cosmos DB failure, issue: https://github.com/microsoft/fhir-server/issues/475
+            "history001/HI09",  // Related to Cosmos DB failure, issue: https://github.com/microsoft/fhir-server/issues/475"
+            "resourcetest_questionnaire/X030_Questionnaire", // POCO RequestGroup is not a valid enumeration value for  CarePlanActivityKind
+            "resourcetest_clinicalimpression/X010_ClinicalImpression",
+            "resourcetest_clinicalimpression/X020_ClinicalImpression",
+            "resourcetest_clinicalimpression/X030_ClinicalImpression",
+            "resourcetest_careplan/X010_CarePlan",
+            "resourcetest_immunizationevaluation/X030_ImmunizationEvaluation",
+            "resourcetest_immunizationevaluation/X020_ImmunizationEvaluation",
+            "resourcetest_immunizationevaluation/X010_ImmunizationEvaluation",
+            "resourcetest_location/X010_Location",
+            "resourcetest_location/X020_Location",
+            "format001/FT07",
+            "format001/FT08",
+            "resourcetest_careplan/X020_CarePlan",
         };
 
-        private readonly CrucibleDataSource _dataSource;
-        private readonly ITestOutputHelper _output;
-
-        public CrucibleTestFixture(CrucibleDataSource dataSource, ITestOutputHelper output)
-        {
-            _dataSource = dataSource;
-            _output = output;
-        }
-
-        [Theory]
-        [MemberData(nameof(GetTests))]
-        [Trait(Traits.Category, Categories.Crucible)]
-        public void Run(string id)
-        {
-            if (string.IsNullOrEmpty(id))
-            {
-                return;
-            }
-
-            var findTest = _dataSource.TestRun.Value.TestRun.TestResults.FirstOrDefault(x => x.TestId == id);
-
-            if (findTest != null)
-            {
-                var failures = findTest.Result
-                    .Where(x =>
-                    {
-                        var testName = $"{x.TestId ?? findTest.TestId}/{x.Id}";
-                        return x.Status == "fail" && !KnownFailures.Contains(testName) && !KnownBroken.Contains(testName);
-                    })
-                    .ToArray();
-
-                if (failures.Any())
-                {
-                    var messages = failures
-                        .Select(x =>
-                            $"Failure in \"{x.TestId ?? findTest.TestId}/{x.Id}\", reason: \"{x.Message}\", description: \"{x.Description}\", see: {_dataSource.TestRun.Value.GetPermalink(x, findTest.TestId)}");
-
-                    Assert.True(false, string.Join(Environment.NewLine, messages));
-                }
-                else
-                {
-                    var passing = findTest.Result.Where(x => x.Status == "pass").Select(x => $"{x.TestId ?? findTest.TestId}/{x.Id}").ToList();
-                    if (passing.Count > 0)
-                    {
-                        _output.WriteLine($"Passing tests: {Environment.NewLine}{string.Join(Environment.NewLine, passing)}");
-                    }
-
-                    var failing = findTest.Result.Where(x => x.Status == "fail").Select(x => $"{x.TestId ?? findTest.TestId}/{x.Id}").ToList();
-                    if (failing.Count > 0)
-                    {
-                        _output.WriteLine($"Excluded tests: {Environment.NewLine}{string.Join(Environment.NewLine, failing)}");
-                    }
-                }
-
-                var shouldBeFailing = findTest.Result
-                    .Where(x => x.Status == "pass" && KnownFailures.Contains($"{x.TestId ?? findTest.TestId}/{x.Id}"))
-                    .ToArray();
-
-                if (shouldBeFailing.Any())
-                {
-                    var messages = shouldBeFailing
-                        .Select(x =>
-                            $"Previously failing test \"{x.TestId ?? findTest.TestId}/{x.Id}\" is now passing, this should be removed from known failures.");
-
-                    Assert.True(false, string.Join(Environment.NewLine, messages));
-                }
-            }
-        }
-
-        public static IEnumerable<object[]> GetTests()
-        {
-            var client = CrucibleDataSource.CreateClient();
-
-            if (client == null)
-            {
-                return Enumerable.Repeat(new object[] { null }, 1);
-            }
-
-            var ids = CrucibleDataSource.GetSupportedIdsAsync(client).GetAwaiter().GetResult();
-
-            return ids.Select(x => new[] { x }).ToArray();
-        }
+        public static string BundleCountFilter => "Search bundle returned does not report a total entry count";
     }
 }
