03077c71b5d98936b732994894ba7bd38519e249
Yazan Al-Alul <yazanal@microsoft.com>
2019-07-12 14:15:55 -0700

    Metrics progress

---
+++ src/Microsoft.Health.Fhir.Core/Features/Metrics/NullMetricLogger.cs
@@ -0,0 +1,17 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Fhir.Core.Features.Metrics
+{
+    /// <summary>
+    /// An implementation of IMetricLogger that does nothing.
+    /// </summary>
+    public class NullMetricLogger : IMetricLogger
+    {
+        public void LogMetric(long inputValue, params string[] dimensions)
+        {
+        }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.Core/Features/Metrics/NullMetricLoggerFactory.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Fhir.Core.Features.Metrics
+{
+    /// <summary>
+    /// An implementation of IMetricLoggerFactory that returns NullMetricLogger instances.
+    /// </summary>
+    public class NullMetricLoggerFactory : IMetricLoggerFactory
+    {
+        public IMetricLogger CreateMetricLogger(string metricName, params string[] dimensions)
+        {
+            return new NullMetricLogger();
+        }
+    }
+}
