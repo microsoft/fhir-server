462d960d3e83021a057a5117829b47636d353212
Robin Todd <rotodd@microsoft.com>
2019-08-19 14:50:31 -0700

    Enabled pretty formatting for xml-formatted responses.

--- src/Microsoft.Health.Fhir.Shared.Api/Features/Formatters/HttpExtensions.cs
+++ src/Microsoft.Health.Fhir.Shared.Api/Features/Formatters/HttpExtensions.cs
@@ -8,10 +8,12 @@ using System.Linq;
 using Hl7.Fhir.Rest;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Health.Fhir.Core.Features;
 
 namespace Microsoft.Health.Fhir.Api.Features.Formatters
 {
-    public static class SummaryTypeExtensions
+    // TODO: Rename to HttpExtensions
+    public static class HttpExtensions
     {
         public static SummaryType GetSummaryType(this HttpContext context, ILogger logger)
         {
@@ -36,6 +38,34 @@ namespace Microsoft.Health.Fhir.Api.Features.Formatters
             }
 
             return SummaryType.False;
+        }
+
+        public static bool GetIsPretty(this HttpContext context, ILogger logger)
+        {
+            var query = context.Request.Query[KnownQueryParameterNames.Pretty].FirstOrDefault();
+
+            if (!string.IsNullOrWhiteSpace(query))
+            {
+                try
+                {
+                    if (!bool.TryParse(query, out bool isPretty))
+                    {
+                        // Assume no pretty formatting if parameter can't be parsed.
+                        isPretty = default;
+                    }
+
+                    logger.LogDebug("Changing response pretty formatting to '{0}'", isPretty);
+
+                    return isPretty;
+                }
+                catch (Exception ex)
+                {
+                    logger.LogWarning(ex, ex.Message);
+                    throw;
+                }
+            }
+
+            return false;
         }
     }
 }
