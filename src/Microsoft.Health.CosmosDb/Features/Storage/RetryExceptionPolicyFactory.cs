47fdb3a8f46081b29e43807efc0d66e0a4bd95b7
Yazan Al-Alul <yazanal@microsoft.com>
2019-08-30 10:34:24 -0700

    Revert RetryExceptionPolicyFactory changes

--- src/Microsoft.Health.CosmosDb/Features/Storage/RetryExceptionPolicyFactory.cs
+++ src/Microsoft.Health.CosmosDb/Features/Storage/RetryExceptionPolicyFactory.cs
@@ -15,7 +15,6 @@ namespace Microsoft.Health.CosmosDb.Features.Storage
     public class RetryExceptionPolicyFactory
     {
         private readonly int _maxNumberOfRetries;
-        private const string RequestChargeKey = "RequestCharge";
 
         public RetryExceptionPolicyFactory(CosmosDataStoreConfiguration configuration)
         {
@@ -24,44 +23,16 @@ namespace Microsoft.Health.CosmosDb.Features.Storage
             _maxNumberOfRetries = configuration.RetryOptions.MaxNumberOfRetries;
         }
 
-        public AsyncRetryPolicy CreateRetryPolicy()
-        {
-            return CreateTrackedRetryPolicy(null);
-        }
-
         public AsyncRetryPolicy CreateTrackedRetryPolicy(Context context)
         {
-            if (context != null)
-            {
-                context.Add(RequestChargeKey, 0D);
-            }
-
             var policy = Policy
                 .Handle<DocumentClientException>(RetryExceptionPolicy.IsTransient)
                 .WaitAndRetryAsync(
                     _maxNumberOfRetries,
-                    (retryAttempt, ctx) =>
-                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
-                    (ex, ts, ctx) =>
-                    {
-                        if (ctx == null)
-                        {
-                            return;
-                        }
-
-                        double existing = (double)ctx[RequestChargeKey];
-                        ctx[RequestChargeKey] = existing + ((DocumentClientException)ex).RequestCharge;
-                    });
+                    retryAttempt =>
+                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 
             return policy;
-        }
-
-        public static double? GetRequestChargeForTrackedRetryPolicy(Context context)
-        {
-            EnsureArg.IsNotNull(context, nameof(context));
-
-            object output;
-            return context.TryGetValue(RequestChargeKey, out output) ? (double)output : (double?)null;
         }
     }
 }
