1c0aa726f668e987dbf532d825fac7840905132c
Robin Todd <rotodd@microsoft.com>
2019-09-09 09:48:00 -0700

    Separates out E2E test for ETag header to account for differing behaviour in R4 and STU3.

---
+++ test/Microsoft.Health.Fhir.Stu3.Tests.E2E/Rest/UpdateTests.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using Hl7.Fhir.Model;
+using Microsoft.Health.Fhir.Core.Extensions;
+using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Fhir.Tests.E2E.Common;
+using Xunit;
+using Task = System.Threading.Tasks.Task;
+
+namespace Microsoft.Health.Fhir.Tests.E2E.Rest
+{
+    public partial class UpdateTests : IClassFixture<HttpIntegrationTestFixture>
+    {
+        [Fact]
+        [Trait(Traits.Priority, Priority.One)]
+        public async Task WhenUpdatingAResourceWithIncorrectETagHeader_GivenStu3Server_TheServerShouldReturnAConflictResponse()
+        {
+            Observation createdResource = await Client.CreateAsync(Samples.GetDefaultObservation().ToPoco<Observation>());
+
+            FhirException ex = await Assert.ThrowsAsync<FhirException>(() => Client.UpdateAsync(createdResource, Guid.NewGuid().ToString()));
+
+            Assert.Equal(System.Net.HttpStatusCode.Conflict, ex.StatusCode);
+        }
+    }
+}
