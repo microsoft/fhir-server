d2d7f352ab86203a0db403cce7c3bc0cc0325b9f
Robin Todd <rotodd@microsoft.com>
2019-09-11 13:51:23 -0700

    Adds version specific E2E tests for if-match header update requests.

--- test/Microsoft.Health.Fhir.R4.Tests.E2E/Rest/UpdateTests.cs
+++ test/Microsoft.Health.Fhir.R4.Tests.E2E/Rest/UpdateTests.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
 using Hl7.Fhir.Model;
 using Microsoft.Health.Fhir.Core.Extensions;
 using Microsoft.Health.Fhir.Tests.Common;
@@ -15,13 +14,29 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest
 {
     public partial class UpdateTests : IClassFixture<HttpIntegrationTestFixture>
     {
+        [Theory]
+        [InlineData("invalidVersion")]
+        [InlineData("-1")]
+        [InlineData("0")]
+        [Trait(Traits.Priority, Priority.One)]
+        public async Task WhenUpdatingAResourceWithInvalidETagHeader_GivenR4Server_TheServerShouldReturnAPreconditionFailedResponse(string versionId)
+        {
+            Observation createdResource = await Client.CreateAsync(Samples.GetDefaultObservation().ToPoco<Observation>());
+
+            FhirException ex = await Assert.ThrowsAsync<FhirException>(() => Client.UpdateAsync(createdResource, versionId));
+
+            Assert.Equal(System.Net.HttpStatusCode.PreconditionFailed, ex.StatusCode);
+        }
+
         [Fact]
         [Trait(Traits.Priority, Priority.One)]
         public async Task WhenUpdatingAResourceWithIncorrectETagHeader_GivenR4Server_TheServerShouldReturnAPreconditionFailedResponse()
         {
             Observation createdResource = await Client.CreateAsync(Samples.GetDefaultObservation().ToPoco<Observation>());
 
-            FhirException ex = await Assert.ThrowsAsync<FhirException>(() => Client.UpdateAsync(createdResource, Guid.NewGuid().ToString()));
+            // Specify a version that is one off from the version of the existing resource
+            var incorrectVersionId = int.Parse(createdResource.Meta.VersionId) + 1;
+            FhirException ex = await Assert.ThrowsAsync<FhirException>(() => Client.UpdateAsync(createdResource, incorrectVersionId.ToString()));
 
             Assert.Equal(System.Net.HttpStatusCode.PreconditionFailed, ex.StatusCode);
         }
