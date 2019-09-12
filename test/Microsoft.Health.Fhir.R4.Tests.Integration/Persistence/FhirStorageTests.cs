75a0f7404b4e2086445249f90288d36f5fda47da
Robin Todd <rotodd@microsoft.com>
2019-09-12 09:34:51 -0700

    Adds tests for updating nonexistent resource with invalid if-match header.

--- test/Microsoft.Health.Fhir.R4.Tests.Integration/Persistence/FhirStorageTests.cs
+++ test/Microsoft.Health.Fhir.R4.Tests.Integration/Persistence/FhirStorageTests.cs
@@ -3,10 +3,13 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
+using Hl7.Fhir.Model;
 using Microsoft.Health.Fhir.Core.Exceptions;
 using Microsoft.Health.Fhir.Core.Extensions;
 using Microsoft.Health.Fhir.Core.Features.Persistence;
 using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
 using Xunit;
 using Task = System.Threading.Tasks.Task;
 
@@ -15,12 +18,34 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
     public partial class FhirStorageTests : IClassFixture<FhirStorageTestsFixture>
     {
         [Fact]
-        public async Task WhenUpsertingASavedResourceWithIncorrectVersionId_GivenR4Server_ThenPreconditionFailedIsThrown()
+        public async Task WhenUpsertingASavedResourceWithInvalidVersionId_GivenR4Server_ThenPreconditionFailedIsThrown()
         {
             var saveResult = await Mediator.UpsertResourceAsync(Samples.GetJsonSample("Weight"));
 
             var newResourceValues = Samples.GetJsonSample("WeightInGrams").ToPoco();
             newResourceValues.Id = saveResult.Resource.Id;
+
+            await Assert.ThrowsAsync<PreconditionFailedException>(async () =>
+                await Mediator.UpsertResourceAsync(newResourceValues.ToResourceElement(), WeakETag.FromVersionId("incorrectVersion")));
+        }
+
+        [Fact]
+        [FhirStorageTestsFixtureArgumentSets(DataStore.CosmosDb)]
+        public async Task WhenUpsertingANonexistentResourceWithInvalidVersionId_GivenR4ServerAndCosmosDb_ThenResourceNotFoundIsThrown()
+        {
+            Resource newResourceValues = Samples.GetJsonSample("WeightInGrams").ToPoco();
+            newResourceValues.Id = Guid.NewGuid().ToString();
+
+            await Assert.ThrowsAsync<ResourceNotFoundException>(async () =>
+                await Mediator.UpsertResourceAsync(newResourceValues.ToResourceElement(), WeakETag.FromVersionId("incorrectVersion")));
+        }
+
+        [Fact]
+        [FhirStorageTestsFixtureArgumentSets(DataStore.SqlServer)]
+        public async Task WhenUpsertingANonexistentResourceWithInvalidVersionId_GivenR4ServerAndSqlServer_ThenPreconditionFailedIsThrown()
+        {
+            Resource newResourceValues = Samples.GetJsonSample("WeightInGrams").ToPoco();
+            newResourceValues.Id = Guid.NewGuid().ToString();
 
             await Assert.ThrowsAsync<PreconditionFailedException>(async () =>
                 await Mediator.UpsertResourceAsync(newResourceValues.ToResourceElement(), WeakETag.FromVersionId("incorrectVersion")));
--- test/Microsoft.Health.Fhir.Stu3.Tests.Integration/Persistence/FhirStorageTests.cs
+++ test/Microsoft.Health.Fhir.Stu3.Tests.Integration/Persistence/FhirStorageTests.cs
@@ -3,11 +3,13 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using Hl7.Fhir.ElementModel;
-using MediatR;
+using System;
+using Hl7.Fhir.Model;
+using Microsoft.Health.Fhir.Core.Exceptions;
 using Microsoft.Health.Fhir.Core.Extensions;
 using Microsoft.Health.Fhir.Core.Features.Persistence;
 using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
 using Xunit;
 using Task = System.Threading.Tasks.Task;
 
@@ -16,12 +18,34 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
     public partial class FhirStorageTests : IClassFixture<FhirStorageTestsFixture>
     {
         [Fact]
-        public async Task WhenUpsertingASavedResourceWithIncorrectVersionId_GivenStu3Server_ThenAResourceConflictIsThrown()
+        public async Task WhenUpsertingASavedResourceWithInvalidVersionId_GivenStu3Server_ThenAResourceConflictIsThrown()
         {
             var saveResult = await Mediator.UpsertResourceAsync(Samples.GetJsonSample("Weight"));
 
             var newResourceValues = Samples.GetJsonSample("WeightInGrams").ToPoco();
             newResourceValues.Id = saveResult.Resource.Id;
+
+            await Assert.ThrowsAsync<ResourceConflictException>(async () =>
+                await Mediator.UpsertResourceAsync(newResourceValues.ToResourceElement(), WeakETag.FromVersionId("incorrectVersion")));
+        }
+
+        [Fact]
+        [FhirStorageTestsFixtureArgumentSets(DataStore.CosmosDb)]
+        public async Task WhenUpsertingANonexistentResourceWithInvalidVersionId_GivenStu3ServerAndCosmosDb_ThenResourceNotFoundIsThrown()
+        {
+            Resource newResourceValues = Samples.GetJsonSample("WeightInGrams").ToPoco();
+            newResourceValues.Id = Guid.NewGuid().ToString();
+
+            await Assert.ThrowsAsync<ResourceNotFoundException>(async () =>
+                await Mediator.UpsertResourceAsync(newResourceValues.ToResourceElement(), WeakETag.FromVersionId("incorrectVersion")));
+        }
+
+        [Fact]
+        [FhirStorageTestsFixtureArgumentSets(DataStore.SqlServer)]
+        public async Task WhenUpsertingANonexistentResourceWithInvalidVersionId_GivenStu3ServerAndSqlServer_ThenAResourceConflictIsThrown()
+        {
+            Resource newResourceValues = Samples.GetJsonSample("WeightInGrams").ToPoco();
+            newResourceValues.Id = Guid.NewGuid().ToString();
 
             await Assert.ThrowsAsync<ResourceConflictException>(async () =>
                 await Mediator.UpsertResourceAsync(newResourceValues.ToResourceElement(), WeakETag.FromVersionId("incorrectVersion")));
