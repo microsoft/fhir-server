7e022a0d3c9c1c44de3b22361e1e6ba794a00bcb
brandonpollett <brpoll@microsoft.com>
2019-10-18 10:46:02 -0700

    Add PATCH route and E2E test (#690)

--- src/Microsoft.Health.Fhir.Api/Resources.Designer.cs
+++ src/Microsoft.Health.Fhir.Api/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Health.Fhir.Api {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -192,6 +192,15 @@ namespace Microsoft.Health.Fhir.Api {
         public static string PageTitle {
             get {
                 return ResourceManager.GetString("PageTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to PATCH is not currently supported..
+        /// </summary>
+        public static string PatchNotSupported {
+            get {
+                return ResourceManager.GetString("PatchNotSupported", resourceCulture);
             }
         }
         
