c1b052f84455deba6d90c4e85d4bc85a927d6de8
Brandon Pollett <brandon.pollett@gmail.com>
2019-11-25 13:17:21 -0800

    Simplify extension, add unit tests.

--- src/Microsoft.Health.Fhir.Shared.Api/Features/Resources/ResourceExtensions.cs
+++ src/Microsoft.Health.Fhir.Shared.Api/Features/Resources/ResourceExtensions.cs
@@ -18,34 +18,21 @@ namespace Microsoft.Health.Fhir.Api.Features.Resources
         public static IEnumerable<T> GetAllChildren<T>(
             this Resource resource)
         {
-            var stack = new Stack<(PropertyInfo propertyInfo, object baseObject)>(resource.GetType().GetProperties().Select(x => (x, resource as object)));
+            var stack = new Stack<(PropertyInfo propertyInfo, object baseObject)>(resource.GetType().GetProperties().Where(x => x.Name == ChildrenPropertyName).Select(x => (x, resource as object)));
             while (stack.Any())
             {
                 var next = stack.Pop();
-
-                if (next.propertyInfo.PropertyType == typeof(T))
-                {
-                    if (next.propertyInfo.GetValue(next.baseObject) is T resourceReference)
-                    {
-                        yield return resourceReference;
-                    }
-                }
-
-                if (next.propertyInfo.PropertyType == typeof(List<T>))
-                {
-                    foreach (object resourceReference in (IEnumerable)next.propertyInfo.GetValue(next.baseObject, null))
-                    {
-                        if (resourceReference is T castResourceReference)
-                        {
-                            yield return castResourceReference;
-                        }
-                    }
-                }
 
                 if (next.propertyInfo.PropertyType == typeof(IEnumerable<Base>) && next.propertyInfo.Name == ChildrenPropertyName)
                 {
                     foreach (object child in (IEnumerable)next.propertyInfo.GetValue(next.baseObject, null))
                     {
+                        if (child is T castResourceReference)
+                        {
+                            yield return castResourceReference;
+                            continue;
+                        }
+
                         foreach (PropertyInfo propertyInfo in child.GetType().GetProperties())
                         {
                             stack.Push((propertyInfo, child));
