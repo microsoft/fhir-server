c059a0ae7f264b8c6166dde6ca2e23d03377fc01
Richa Bansal <ribans@microsoft.com>
2019-12-02 10:10:36 -0800

    Batch support enabled

--- test/Microsoft.Health.Fhir.R4.Tests.E2E/KnownCrucibleTests.cs
+++ test/Microsoft.Health.Fhir.R4.Tests.E2E/KnownCrucibleTests.cs
@@ -434,8 +434,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Crucible
             "testscript-example-update/SETUP",
             "transactionandbatchtest/XFER0",
             "transactionandbatchtest/XFER10",
-            "transactionandbatchtest/XFER11",
-            "transactionandbatchtest/XFER12",
         };
 
         public static readonly string[] KnownBroken =
