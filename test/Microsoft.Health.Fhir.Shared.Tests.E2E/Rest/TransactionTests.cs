cd618eec0ef87c31cfe82263ed061fdf642a21bd
Pooja Adhikari <poadhika@microsoft.com>
2020-01-02 15:16:21 -0800

    Resolved Test failures

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/TransactionTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/TransactionTests.cs
@@ -4,7 +4,6 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using Hl7.Fhir.Model;
