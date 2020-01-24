37046efdbf6fd097ddcc45dff2e3981ad08f8356
Richa Bansal <ribans@microsoft.com>
2020-01-23 16:00:45 -0800

    upgraded to latest fhir version

--- src/Microsoft.Health.Fhir.R4.Core.UnitTests/Operations/Versions/VersionSpecificTests.cs
+++ src/Microsoft.Health.Fhir.R4.Core.UnitTests/Operations/Versions/VersionSpecificTests.cs
@@ -26,7 +26,7 @@ namespace Microsoft.Health.Fhir.R4.Core.UnitTests.Operations.Versions
         {
             var version = _provider.SupportedVersion.ToString();
 
-            Assert.Equal("4.0.0", version);
+            Assert.Equal("4.0.1", version);
         }
     }
 }
