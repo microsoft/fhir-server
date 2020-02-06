d68e469d4d7234c94560ffcde19b706a947c160e
Brendan Kowitz <bkowitz@microsoft.com>
2020-02-06 09:35:27 -0800

    Adding R5 projects

--- src/Microsoft.Health.Fhir.R5.Core/AssemblyInfo.cs
+++ src/Microsoft.Health.Fhir.R5.Core/AssemblyInfo.cs
@@ -3,11 +3,14 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: NeutralResourcesLanguage("en-us")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.CosmosDb")]
+[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.CosmosDb.UnitTests")]
+[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.Core.UnitTests")]
 [assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R4.Api.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.Stu3.Api.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.Stu3.Tests.Integration")]
+[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R4.Core.UnitTests")]
+[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.Tests.Common")]
+[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R4.Tests.E2E")]
 [assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R4.Tests.Integration")]
