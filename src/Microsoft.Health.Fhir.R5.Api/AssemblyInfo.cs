3d95d801b6c1c79542832d52239696cd7d771e99
Yazan Al-Alul <yazanal@microsoft.com>
2020-02-21 13:20:53 -0800

    Add R5.Api and API.UnitTests

--- src/Microsoft.Health.Fhir.R5.Api/AssemblyInfo.cs
+++ src/Microsoft.Health.Fhir.R5.Api/AssemblyInfo.cs
@@ -6,10 +6,5 @@
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: NeutralResourcesLanguage("en-us")]
-[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R5.Api")]
 [assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R5.Api.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R4.Api")]
-[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.R4.Api.UnitTests")]
-[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.Stu3.Api")]
-[assembly: InternalsVisibleTo("Microsoft.Health.Fhir.Stu3.Api.UnitTests")]
+[assembly: NeutralResourcesLanguage("en-us")]
