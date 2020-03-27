752e826096050c54bfbe7f979ec6710b143b0ee7
namadabu <41395116+namadabu@users.noreply.github.com>
2020-03-26 20:33:37 -0700

    Support _since paramter for $export requests (#947)

--- src/Microsoft.Health.Fhir.Core/Messages/Export/CreateExportRequest.cs
+++ src/Microsoft.Health.Fhir.Core/Messages/Export/CreateExportRequest.cs
@@ -6,21 +6,25 @@
 using System;
 using EnsureThat;
 using MediatR;
+using Microsoft.Health.Fhir.Core.Models;
 
 namespace Microsoft.Health.Fhir.Core.Messages.Export
 {
     public class CreateExportRequest : IRequest<CreateExportResponse>
     {
-        public CreateExportRequest(Uri requestUri, string resourceType = null)
+        public CreateExportRequest(Uri requestUri, string resourceType = null, PartialDateTime since = null)
         {
             EnsureArg.IsNotNull(requestUri, nameof(requestUri));
 
             RequestUri = requestUri;
             ResourceType = resourceType;
+            Since = since;
         }
 
         public Uri RequestUri { get; }
 
         public string ResourceType { get; }
+
+        public PartialDateTime Since { get; }
     }
 }
