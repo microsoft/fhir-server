318cb5d11f9a2ed664dd43affc77090f6e809391
Richa Bansal <ribans@microsoft.com>
2020-03-26 21:14:07 -0700

    Revised initial commit

---
+++ tools/FhirSchemaManager/Model/CurrentVersion.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using EnsureThat;
+
+namespace FhirSchemaManager.Model
+{
+    public class CurrentVersion
+    {
+        public CurrentVersion(int id, string status, List<string> servers)
+        {
+            EnsureArg.IsNotNull(status, nameof(status));
+
+            Id = id;
+            Status = status;
+            Servers = servers;
+        }
+
+        public int Id { get; }
+
+        public string Status { get; }
+
+        public List<string> Servers { get; }
+    }
+}
