2d0af203246b88275640a663de6f1930599ed6b9
LTA-Thinking <rbrucej@gmail.com>
2020-03-31 15:12:06 -0700

    Add frontend header

---
+++ src/Microsoft.Health.Fhir.Api/Features/Context/FrontendHeadersMiddleware.cs
@@ -0,0 +1,32 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Health.Fhir.Api.Features.Context
+{
+    public class FrontendHeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FrontendHeadersMiddleware(RequestDelegate next)
+        {
+            EnsureArg.IsNotNull(next, nameof(next));
+
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            EnsureArg.IsNotNull(context, nameof(context));
+
+            context.Request.Scheme = "https";
+
+            await _next(context);
+        }
+    }
+}
