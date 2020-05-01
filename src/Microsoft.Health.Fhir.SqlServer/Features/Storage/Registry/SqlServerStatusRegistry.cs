7aefba4a43ee64869c7c90848956975e347c4766
Robin Todd <rotodd@microsoft.com>
2020-05-01 14:51:07 -0700

    Adds sql status registry skeleton.

---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/Registry/SqlServerStatusRegistry.cs
@@ -0,0 +1,36 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.Health.Fhir.Core.Features.Search.Registry;
+using Microsoft.Health.SqlServer.Features.Client;
+
+namespace Microsoft.Health.Fhir.SqlServer.Features.Storage.Registry
+{
+    public class SqlServerStatusRegistry : ISearchParameterRegistry
+    {
+        private readonly SqlConnectionWrapperFactory _sqlConnectionWrapperFactory;
+
+        public SqlServerStatusRegistry(SqlConnectionWrapperFactory sqlConnectionWrapperFactory)
+        {
+            EnsureArg.IsNotNull(sqlConnectionWrapperFactory, nameof(sqlConnectionWrapperFactory));
+
+            _sqlConnectionWrapperFactory = sqlConnectionWrapperFactory;
+        }
+
+        public Task<IReadOnlyCollection<ResourceSearchParameterStatus>> GetSearchParameterStatuses()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateStatuses(IEnumerable<ResourceSearchParameterStatus> statuses)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
