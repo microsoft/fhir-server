4a6d3f4161cad723f720c62e92a9f74c64032598
Yazan Al-Alul <yazanal@microsoft.com>
2020-06-07 14:39:55 -0700

    Add tests for excluded endpoints.

--- src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Features/Throttling/ThrottlingMiddlewareTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Features/Throttling/ThrottlingMiddlewareTests.cs
@@ -4,8 +4,10 @@
 // -------------------------------------------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AngleSharp.Io;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
@@ -35,7 +37,13 @@ namespace Microsoft.Health.Fhir.Shared.Api.UnitTests.Features.Throttling
                 async x =>
                 {
                     x.Response.StatusCode = 200;
-                    await Task.Delay(5000, _cts.Token);
+                    try
+                    {
+                        await Task.Delay(5000, _cts.Token);
+                    }
+                    catch (TaskCanceledException) when (_cts.Token.IsCancellationRequested)
+                    {
+                    }
                 },
                 Options.Create(throttlingConfiguration),
                 NullLogger<ThrottlingMiddleware>.Instance);
@@ -49,7 +57,7 @@ namespace Microsoft.Health.Fhir.Shared.Api.UnitTests.Features.Throttling
         {
             var tasks = SetupPreexistingRequests(numberOfConcurrentRequests);
 
-            tasks.Add(_middleware.Invoke(_httpContext));
+            tasks.Add((_middleware.Invoke(_httpContext), _httpContext));
 
             Assert.Equal(200, _httpContext.Response.StatusCode);
             _cts.Cancel();
@@ -61,7 +69,7 @@ namespace Microsoft.Health.Fhir.Shared.Api.UnitTests.Features.Throttling
         [InlineData(10)]
         public async Task GivenRequestsAtOrAboveThreshold_WhenInvoked_ReturnsTooManyRequests(int numberOfConcurrentRequests)
         {
-            var tasks = SetupPreexistingRequests(numberOfConcurrentRequests);
+            _ = SetupPreexistingRequests(numberOfConcurrentRequests);
 
             await _middleware.Invoke(_httpContext);
 
@@ -70,17 +78,48 @@ namespace Microsoft.Health.Fhir.Shared.Api.UnitTests.Features.Throttling
             _cts.Dispose();
         }
 
-        private List<Task> SetupPreexistingRequests(int numberOfConcurrentRequests)
+        [Fact]
+        public async Task GivenRequestsToExcludedEndpoint_WhenInvoked_Executes()
         {
-            List<Task> tasks = new List<Task>();
+            var mapping = SetupPreexistingRequests(10, "health/check");
+            _cts.Cancel();
+            await Task.WhenAll(mapping.Select(x => x.task));
+
+            foreach (var context in mapping.Select(x => x.httpContext))
+            {
+                Assert.Equal(200, context.Response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task GivenRequestToExcludedEndpoint_WhenAlreadyThrottled_Succeeds()
+        {
+            var mapping = SetupPreexistingRequests(6);
+
+            _httpContext.Request.Path = "/health/check";
+            _httpContext.Request.Method = HttpMethod.Get.ToString();
+            var task = _middleware.Invoke(_httpContext);
+            _cts.Cancel();
+            Assert.Equal(200, _httpContext.Response.StatusCode);
+
+            await Task.WhenAll(mapping.Select(x => x.task));
+
+            Assert.Contains(mapping, x => x.httpContext.Response.StatusCode == 429);
+        }
+
+        private List<(Task task, HttpContext httpContext)> SetupPreexistingRequests(int numberOfConcurrentRequests, string path = "")
+        {
+            List<(Task, HttpContext)> output = new List<(Task, HttpContext)>();
 
             for (int count = 0; count < numberOfConcurrentRequests; count++)
             {
                 var context = new DefaultHttpContext();
-                tasks.Add(_middleware.Invoke(context));
+                context.Request.Path = $"/{path}";
+                context.Request.Method = HttpMethod.Get.ToString();
+                output.Add((_middleware.Invoke(context), context));
             }
 
-            return tasks;
+            return output;
         }
     }
 }
