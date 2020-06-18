debb7a779b1ebfceba8efab5ada4ceee7ae985bc
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-06-18 16:29:58 +0300

    finalyzing _list functionality

--- src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Search/Preprocessors/ListSearchPreProcessorTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Search/Preprocessors/ListSearchPreProcessorTests.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading;
 using Hl7.Fhir.Serialization;
 using Microsoft.Health.Extensions.DependencyInjection;
@@ -15,6 +16,7 @@ using Microsoft.Health.Fhir.Core.Features.Search.PreProcessors;
 using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
 using Microsoft.Health.Fhir.Core.Messages.Search;
 using Microsoft.Health.Fhir.Core.Models;
+using Microsoft.Health.Fhir.Tests.Common;
 using NSubstitute;
 using Xunit;
 using Task = System.Threading.Tasks.Task;
@@ -25,23 +27,48 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Validation
     {
         private readonly ResourceDeserializer _resourceDeserializer;
 
-        // private readonly FhirJsonSerializer _fhirJsonSerializer = new FhirJsonSerializer();
         private readonly FhirJsonParser _fhirJsonParser = new FhirJsonParser();
 
         private readonly IFhirDataStore _fhirDataStore;
 
-        // private readonly ISearchService _searchService;
         private IScoped<IFhirDataStore> scope;
+
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private readonly CancellationToken _cancellationToken;
 
         public ListSearchPreProcessorTests()
         {
+            _cancellationToken = _cancellationTokenSource.Token;
+
             _resourceDeserializer = new ResourceDeserializer(
                (FhirResourceFormat.Json, new Func<string, string, DateTimeOffset, ResourceElement>((str, version, lastUpdated) => _fhirJsonParser.Parse(str).ToResourceElement())));
 
             _fhirDataStore = Substitute.For<IFhirDataStore>();
 
+            // for an 'existing list' return a list with Patients
+            _fhirDataStore.GetAsync(Arg.Is<ResourceKey>(x => x.Id == "existing-list"), Arg.Any<CancellationToken>()).Returns(
+                x =>
+                {
+                    var longList = Samples.GetDefaultList();
+                    var rawResourceFactory = new RawResourceFactory(new FhirJsonSerializer());
+                    return new ResourceWrapper(
+                        longList,
+                        rawResourceFactory.Create(longList),
+                        new ResourceRequest(HttpMethod.Post, "http://fhir"),
+                        false,
+                        null,
+                        null,
+                        null);
+                        });
+
             scope = Substitute.For<IScoped<IFhirDataStore>>();
             scope.Value.Returns(_fhirDataStore);
+        }
+
+        private ResourceKey<Hl7.Fhir.Model.List> CreateArg(string lstName)
+        {
+            return new ResourceKey<Hl7.Fhir.Model.List>(lstName);
         }
 
         [Fact]
@@ -55,7 +82,7 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Validation
                 Tuple.Create("firstItem", Guid.NewGuid().ToString()),
                 Tuple.Create("secondItem", Guid.NewGuid().ToString()),
             };
-            var getResourceRequest = new SearchResourceRequest("Observation", list);
+            var getResourceRequest = new SearchResourceRequest("Patient", list);
 
             await preProcessor.Process(getResourceRequest, CancellationToken.None);
             Assert.True(getResourceRequest.Queries.Count == 2);
@@ -74,7 +101,7 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Validation
                 Tuple.Create("_id", Guid.NewGuid().ToString()),
             };
 
-            var getResourceRequest = new SearchResourceRequest("Observation", list);
+            var getResourceRequest = new SearchResourceRequest("Patient", list);
 
             Assert.True(getResourceRequest.Queries.Count == 3);
             await preProcessor.Process(getResourceRequest, CancellationToken.None);
@@ -84,7 +111,8 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Validation
         [Fact]
         public async Task GivenARequest_WhenListValueExistsButValueNotFound_ListQueryRemovedAndFalseQueryAdded()
         {
-            var preProcessor = new ListSearchPreProcessor(scope, _resourceDeserializer, new ReferenceSearchValueParser(new FhirRequestContextAccessor()));
+            var preProcessor =
+            new ListSearchPreProcessor(scope, _resourceDeserializer, new ReferenceSearchValueParser(new FhirRequestContextAccessor()));
 
             IReadOnlyList<Tuple<string, string>> list =
             new[]
@@ -94,11 +122,16 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Validation
                 Tuple.Create("_id", Guid.NewGuid().ToString()),
             };
 
-            var getResourceRequest = new SearchResourceRequest("Observation", list);
+            var getResourceRequest = new SearchResourceRequest("Patient", list);
 
             Assert.True(getResourceRequest.Queries.Count == 3);
+
             await preProcessor.Process(getResourceRequest, CancellationToken.None);
+
             Assert.True(getResourceRequest.Queries.Count == 4);
+            Assert.Equal("_id", getResourceRequest.Queries[2].Item1);
+            Assert.Equal("_id", getResourceRequest.Queries[3].Item1);
+            Assert.NotEqual(getResourceRequest.Queries[2].Item2, getResourceRequest.Queries[3].Item2);
         }
 
         [Fact]
@@ -106,10 +139,19 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Validation
         {
             var preProcessor = new ListSearchPreProcessor(scope, _resourceDeserializer, new ReferenceSearchValueParser(new FhirRequestContextAccessor()));
 
-            IReadOnlyList<Tuple<string, string>> list = new[] { Tuple.Create("_list", Guid.NewGuid().ToString()) };
-            var getResourceRequest = new SearchResourceRequest("Observation", list);
+            IReadOnlyList<Tuple<string, string>> list = new[] { Tuple.Create("_list", "existing-list") };
+            var getResourceRequest = new SearchResourceRequest("Patient", list);
 
-            await preProcessor.Process(getResourceRequest, CancellationToken.None);
+            Assert.True(getResourceRequest.Queries.Count == 1);
+            Assert.Equal("_list", getResourceRequest.Queries[0].Item1);
+            Assert.Equal("existing-list", getResourceRequest.Queries[0].Item2);
+
+            await preProcessor.Process(getResourceRequest, _cancellationToken);
+
+            Assert.Equal("_id", getResourceRequest.Queries[0].Item1);
+            Assert.Contains("pat1", getResourceRequest.Queries[0].Item2);
+            Assert.Contains("pat2", getResourceRequest.Queries[0].Item2);
+            Assert.Contains("pat3", getResourceRequest.Queries[0].Item2);
         }
     }
 }
