b32ae0f3127591007b301fc679a179bb18daf721
John Stairs <jostairs@microsoft.com>
2020-06-30 14:30:27 -0400

    Added some comments

--- src/Microsoft.Health.Fhir.SqlServer/Features/Search/Expressions/TableExpression.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Search/Expressions/TableExpression.cs
@@ -15,6 +15,16 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search.Expressions
     /// </summary>
     internal class TableExpression : Expression
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="TableExpression"/> class.
+        /// </summary>
+        /// <param name="searchParameterQueryGenerator">The search parameter query generator</param>
+        /// <param name="normalizedPredicate">The search expression over a columns belonging exclusively to a search parameter table.
+        /// Applies to the chain target if a chained expression.</param>
+        /// <param name="denormalizedPredicate">The search expression over columns that exist the Resource table. Applies to the chain target if a chained expression.</param>
+        /// <param name="kind">The table expression kind.</param>
+        /// <param name="chainLevel">The nesting chain nesting level of the current expression. 0 if not a chain expression.</param>
+        /// <param name="denormalizedPredicateOnChainRoot">The search expression over columns that exist the Resource table. Applies to the chain root if a chained expression.</param>
         public TableExpression(
             NormalizedSearchParameterQueryGenerator searchParameterQueryGenerator,
             Expression normalizedPredicate,
@@ -45,15 +55,28 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search.Expressions
 
         public TableExpressionKind Kind { get; }
 
+        /// <summary>
+        /// The nesting chain nesting level of the current expression. 0 if not a chain expression.
+        /// </summary>
         public int ChainLevel { get; }
-
-        public Expression DenormalizedPredicateOnChainRoot { get; }
 
         public NormalizedSearchParameterQueryGenerator SearchParameterQueryGenerator { get; }
 
+        /// <summary>
+        /// The search expression over a columns belonging exclusively to a search parameter table.
+        /// Applies to the chain target if a chained expression.
+        /// </summary>
         public Expression NormalizedPredicate { get; }
 
+        /// <summary>
+        /// The search expression over columns that exist the Resource table. Applies to the chain target if a chained expression.
+        /// </summary>
         public Expression DenormalizedPredicate { get; }
+
+        /// <summary>
+        /// The search expression over columns that exist the Resource table. Applies to the chain root if a chained expression.
+        /// </summary>
+        public Expression DenormalizedPredicateOnChainRoot { get; }
 
         public override TOutput AcceptVisitor<TContext, TOutput>(IExpressionVisitor<TContext, TOutput> visitor, TContext context)
         {
