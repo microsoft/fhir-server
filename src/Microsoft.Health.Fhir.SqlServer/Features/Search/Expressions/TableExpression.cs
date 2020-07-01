60f8975a6a39e1376d37227a2eafce211ee0f9bd
John Stairs <jostairs@microsoft.com>
2020-07-01 17:14:42 -0400

    Fix continuation token issues with chained searches (#1106)
    * ExportDataValidationTests fail if there are duplicate IDs
    
    * Promote denormalized predicates to chained table expressions
    
    * Added some comments

--- src/Microsoft.Health.Fhir.SqlServer/Features/Search/Expressions/TableExpression.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Search/Expressions/TableExpression.cs
@@ -15,12 +15,23 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search.Expressions
     /// </summary>
     internal class TableExpression : Expression
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="TableExpression"/> class.
+        /// </summary>
+        /// <param name="searchParameterQueryGenerator">The search parameter query generator</param>
+        /// <param name="normalizedPredicate">The search expression over a columns belonging exclusively to a search parameter table.
+        /// Applies to the chain target if a chained expression.</param>
+        /// <param name="denormalizedPredicate">The search expression over columns that exist the Resource table. Applies to the chain target if a chained expression.</param>
+        /// <param name="kind">The table expression kind.</param>
+        /// <param name="chainLevel">The nesting chain nesting level of the current expression. 0 if not a chain expression.</param>
+        /// <param name="denormalizedPredicateOnChainRoot">The search expression over columns that exist the Resource table. Applies to the chain root if a chained expression.</param>
         public TableExpression(
             NormalizedSearchParameterQueryGenerator searchParameterQueryGenerator,
             Expression normalizedPredicate,
             Expression denormalizedPredicate,
             TableExpressionKind kind,
-            int chainLevel = 0)
+            int chainLevel = 0,
+            Expression denormalizedPredicateOnChainRoot = null)
         {
             switch (normalizedPredicate)
             {
@@ -39,17 +50,33 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search.Expressions
             DenormalizedPredicate = denormalizedPredicate;
             Kind = kind;
             ChainLevel = chainLevel;
+            DenormalizedPredicateOnChainRoot = denormalizedPredicateOnChainRoot;
         }
 
         public TableExpressionKind Kind { get; }
 
+        /// <summary>
+        /// The nesting chain nesting level of the current expression. 0 if not a chain expression.
+        /// </summary>
         public int ChainLevel { get; }
 
         public NormalizedSearchParameterQueryGenerator SearchParameterQueryGenerator { get; }
 
+        /// <summary>
+        /// The search expression over a columns belonging exclusively to a search parameter table.
+        /// Applies to the chain target if a chained expression.
+        /// </summary>
         public Expression NormalizedPredicate { get; }
 
+        /// <summary>
+        /// The search expression over columns that exist the Resource table. Applies to the chain target if a chained expression.
+        /// </summary>
         public Expression DenormalizedPredicate { get; }
+
+        /// <summary>
+        /// The search expression over columns that exist the Resource table. Applies to the chain root if a chained expression.
+        /// </summary>
+        public Expression DenormalizedPredicateOnChainRoot { get; }
 
         public override TOutput AcceptVisitor<TContext, TOutput>(IExpressionVisitor<TContext, TOutput> visitor, TContext context)
         {
@@ -63,7 +90,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search.Expressions
 
         public override string ToString()
         {
-            return $"(Table {Kind} {(ChainLevel == 0 ? null : $"ChainLevel:{ChainLevel} ")}{SearchParameterQueryGenerator?.Table} Normalized:{NormalizedPredicate} Denormalized:{DenormalizedPredicate})";
+            return $"(Table {Kind} {(ChainLevel == 0 ? null : $"ChainLevel:{ChainLevel} ")}{SearchParameterQueryGenerator?.Table} Normalized:{NormalizedPredicate} Denormalized:{DenormalizedPredicate} DenormalizedOnChainRoot:{DenormalizedPredicateOnChainRoot})";
         }
     }
 }
