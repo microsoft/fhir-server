f9be724cebc6e13f2941da7c6db924afb3be6650
Narasimhan Madabusi <namadabu@microsoft.com>
2020-07-14 15:34:10 -0700

    Initial commit

---
+++ src/Microsoft.Health.Fhir.Core.UnitTests/Features/Search/SearchHelperUtilitiesTests.cs
@@ -0,0 +1,75 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Health.Fhir.Core.Features.Search;
+using Microsoft.Health.Fhir.Core.Models;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Search
+{
+    public class SearchHelperUtilitiesTests
+    {
+        [Fact]
+        public void GivenNullSearchParamInfo_WhenCalculateSearchParamHash_ThenThrowsException()
+        {
+            Assert.ThrowsAny<Exception>(() => SearchHelperUtilities.CalculateSearchParameterNameHash(null));
+        }
+
+        [Fact]
+        public void GivenEmptySearchParamInfo_WhenCalculateSearchParamHash_ThenThrowsException()
+        {
+            Assert.ThrowsAny<Exception>(() => SearchHelperUtilities.CalculateSearchParameterNameHash(new List<SearchParameterInfo>()));
+        }
+
+        [Fact]
+        public void GivenTwoSameListsOfSearchParamInfo_WhenCalculateSearchParamHash_ThenHashIsSame()
+        {
+            IEnumerable<SearchParameterInfo> paramInfo1 = GenerateSearchParamInfo("_type", "supplement", "study");
+            IEnumerable<SearchParameterInfo> paramInfo2 = GenerateSearchParamInfo("_type", "supplement", "study");
+
+            string hash1 = SearchHelperUtilities.CalculateSearchParameterNameHash(paramInfo1);
+            string hash2 = SearchHelperUtilities.CalculateSearchParameterNameHash(paramInfo2);
+
+            Assert.Equal(hash1, hash2);
+        }
+
+        [Fact]
+        public void GivenTwoDifferentListsOfSearchParamInfo_WhenCalculateSearchParamHash_ThenHashIsNotSame()
+        {
+            IEnumerable<SearchParameterInfo> paramInfo1 = GenerateSearchParamInfo("_type", "supplement", "study");
+            IEnumerable<SearchParameterInfo> paramInfo2 = GenerateSearchParamInfo("_type", "study");
+
+            string hash1 = SearchHelperUtilities.CalculateSearchParameterNameHash(paramInfo1);
+            string hash2 = SearchHelperUtilities.CalculateSearchParameterNameHash(paramInfo2);
+
+            Assert.NotEqual(hash1, hash2);
+        }
+
+        [Fact]
+        public void GivenTwoSameListsDifferentOrderOfSearchParamInfo_WhenCalculateSearchParamHash_ThenHashIsNotSame()
+        {
+            IEnumerable<SearchParameterInfo> paramInfo1 = GenerateSearchParamInfo("_type", "supplement", "study");
+            IEnumerable<SearchParameterInfo> paramInfo2 = GenerateSearchParamInfo("_type", "study", "supplement");
+
+            string hash1 = SearchHelperUtilities.CalculateSearchParameterNameHash(paramInfo1);
+            string hash2 = SearchHelperUtilities.CalculateSearchParameterNameHash(paramInfo2);
+
+            Assert.Equal(hash1, hash2);
+        }
+
+        private List<SearchParameterInfo> GenerateSearchParamInfo(params string[] searchParamNames)
+        {
+            List<SearchParameterInfo> paramInfo = new List<SearchParameterInfo>();
+            foreach (string paramName in searchParamNames)
+            {
+                paramInfo.Add(new SearchParameterInfo(paramName));
+            }
+
+            return paramInfo;
+        }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.Core/Features/Search/SearchHelperUtilities.cs
@@ -0,0 +1,40 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnsureThat;
+using Microsoft.Health.Core.Extensions;
+using Microsoft.Health.Fhir.Core.Models;
+
+namespace Microsoft.Health.Fhir.Core.Features.Search
+{
+    public static class SearchHelperUtilities
+    {
+        /// <summary>
+        /// Given a list of <see cref="SearchParameterInfo"/> calculates a hash using the
+        /// <see cref="SearchParameterInfo.Name"/> values of each component. The collection of
+        /// search parameters (irrespective of the order they are present) will return the
+        /// same hash.
+        /// </summary>
+        /// <param name="searchParamInfo">A list of <see cref="SearchSearchParameterInfo" /></param>
+        /// <returns>A hash based on the search parameter names present in the input.</returns>
+        public static string CalculateSearchParameterNameHash(IEnumerable<SearchParameterInfo> searchParamInfo)
+        {
+            EnsureArg.IsNotNull(searchParamInfo, nameof(searchParamInfo));
+            EnsureArg.IsGt(searchParamInfo.Count(), 0, nameof(searchParamInfo));
+
+            StringBuilder sb = new StringBuilder();
+            foreach (string paramName in searchParamInfo.Select(x => x.Name).OrderBy(x => x))
+            {
+                sb.Append(paramName);
+            }
+
+            string hash = sb.ToString().ComputeHash();
+            return hash;
+        }
+    }
+}
