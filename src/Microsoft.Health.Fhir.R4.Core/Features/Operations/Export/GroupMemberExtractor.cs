26130b2d90e590a652b6b32f2b5ab087945913b0
LTA-Thinking <rbrucej@gmail.com>
2020-07-17 16:20:12 -0700

    Move group member extractor to version projects

---
+++ src/Microsoft.Health.Fhir.R4.Core/Features/Operations/Export/GroupMemberExtractor.cs
@@ -0,0 +1,44 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using Hl7.Fhir.Model;
+using Microsoft.Health.Fhir.Core.Extensions;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Core.Models;
+
+namespace Microsoft.Health.Fhir.Core.Features.Operations.Export
+{
+    public class GroupMemberExtractor : IGroupMemberExtractor
+    {
+        private readonly IFhirDataStore _fhirDataStore;
+        private readonly ResourceDeserializer _resourceDeserializer;
+
+        public GroupMemberExtractor(
+            IFhirDataStore fhirDataStore,
+            ResourceDeserializer resourceDeserializer)
+        {
+            EnsureArg.IsNotNull(fhirDataStore, nameof(fhirDataStore));
+            EnsureArg.IsNotNull(resourceDeserializer, nameof(resourceDeserializer));
+
+            _fhirDataStore = fhirDataStore;
+            _resourceDeserializer = resourceDeserializer;
+        }
+
+        public async Task<List<Tuple<string, string>>> GetGroupMembers(string groupId, CancellationToken cancellationToken)
+        {
+            var groupResource = await _fhirDataStore.GetAsync(new ResourceKey(KnownResourceTypes.Group, groupId), cancellationToken);
+
+            var group = _resourceDeserializer.Deserialize(groupResource);
+            var groupContents = group.ToPoco<Group>().Member;
+
+            return groupContents.ConvertAll(member => new Tuple<string, string>(member.Entity.Reference, member.Entity.Type));
+        }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.Stu3.Core/Features/Operations/Export/GroupMemberExtractor.cs
@@ -0,0 +1,80 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using Hl7.Fhir.Model;
+using Microsoft.Health.Extensions.DependencyInjection;
+using Microsoft.Health.Fhir.Core.Extensions;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Core.Features.Search;
+using Microsoft.Health.Fhir.Core.Models;
+
+namespace Microsoft.Health.Fhir.Core.Features.Operations.Export
+{
+    public class GroupMemberExtractor : IGroupMemberExtractor
+    {
+        private readonly IFhirDataStore _fhirDataStore;
+        private readonly ResourceDeserializer _resourceDeserializer;
+        private readonly Func<IScoped<ISearchService>> _searchServiceFactory;
+
+        public GroupMemberExtractor(
+            IFhirDataStore fhirDataStore,
+            ResourceDeserializer resourceDeserializer,
+            Func<IScoped<ISearchService>> searchServiceFactory)
+        {
+            EnsureArg.IsNotNull(fhirDataStore, nameof(fhirDataStore));
+            EnsureArg.IsNotNull(resourceDeserializer, nameof(resourceDeserializer));
+            EnsureArg.IsNotNull(searchServiceFactory, nameof(searchServiceFactory));
+
+            _fhirDataStore = fhirDataStore;
+            _resourceDeserializer = resourceDeserializer;
+            _searchServiceFactory = searchServiceFactory;
+        }
+
+        public async Task<List<Tuple<string, string>>> GetGroupMembers(string groupId, CancellationToken cancellationToken)
+        {
+            var groupResource = await _fhirDataStore.GetAsync(new ResourceKey(KnownResourceTypes.Group, groupId), cancellationToken);
+
+            var group = _resourceDeserializer.Deserialize(groupResource);
+            var groupContents = group.ToPoco<Group>().Member;
+
+            var members = new List<Tuple<string, string>>();
+
+            using (IScoped<ISearchService> searchService = _searchServiceFactory())
+            {
+                foreach (Group.MemberComponent member in groupContents)
+                {
+                    var queryParametersList = new List<Tuple<string, string>>()
+                    {
+                        Tuple.Create(KnownQueryParameterNames.Id, member.Entity.Reference),
+                    };
+
+                    var searchResult = await searchService.Value.SearchAsync(
+                        resourceType: null,
+                        queryParametersList,
+                        cancellationToken);
+
+                    if (searchResult.Results.Count() > 1)
+                    {
+                        // throw exception...
+                    }
+                    else if (searchResult.Results.Count() == 0)
+                    {
+                        // throw exception...
+                    }
+
+                    members.Add(new Tuple<string, string>(member.Entity.Reference, searchResult.Results.First().Resource.ResourceTypeName));
+                }
+            }
+
+            return members;
+        }
+    }
+}
