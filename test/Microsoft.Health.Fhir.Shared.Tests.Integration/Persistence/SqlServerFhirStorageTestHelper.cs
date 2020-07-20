4c45f55d6263ba1ce136a10e240a326fa0cab254
Robin Todd <rotodd@microsoft.com>
2020-07-20 16:28:36 -0700

    Fixes reference to registry.

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerFhirStorageTestHelper.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerFhirStorageTestHelper.cs
@@ -121,7 +121,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
         {
             using (var connection = new SqlConnection(_connectionString))
             {
-                var command = new SqlCommand("DELETE FROM dbo.SearchParamStatusRegistry WHERE Uri = @uri", connection);
+                var command = new SqlCommand("DELETE FROM dbo.SearchParam WHERE Uri = @uri", connection);
                 command.Parameters.AddWithValue("@uri", uri);
 
                 await command.Connection.OpenAsync(cancellationToken);
