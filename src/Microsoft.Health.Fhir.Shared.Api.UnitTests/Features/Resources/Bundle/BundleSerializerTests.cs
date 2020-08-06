20274f7d7ac3125acf09aea552e8ebb82cedd920
Yazan Al-Alul <yazanal@microsoft.com>
2020-08-06 15:59:22 -0700

    More BundleSerializer tests

--- src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Features/Resources/Bundle/BundleSerializerTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Features/Resources/Bundle/BundleSerializerTests.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Hl7.Fhir.ElementModel;
-using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Health.Core;
@@ -19,6 +18,7 @@ using Microsoft.Health.Fhir.Core.Features.Context;
 using Microsoft.Health.Fhir.Core.Features.Persistence;
 using Microsoft.Health.Fhir.Core.Features.Search;
 using Microsoft.Health.Fhir.Core.Features.Security;
+using Microsoft.Health.Fhir.Core.Models;
 using Microsoft.Health.Fhir.Shared.Core.Features.Search;
 using Microsoft.Health.Fhir.Tests.Common;
 using NSubstitute;
@@ -46,44 +46,46 @@ namespace Microsoft.Health.Fhir.Shared.Api.UnitTests.Features.Resources.Bundle
         }
 
         [Fact]
-        public async Task GivenBundle_WhenSerialized_MatchesSerializationByBuiltInSerializer()
+        public async Task GivenBundleWithNoEntry_WhenSerialized_ShouldMatchSerializationByBuiltInSerializer()
         {
-            var patientResource = Samples.GetDefaultPatient().ToPoco<Patient>();
-            patientResource.VersionId = "1";
-            patientResource.Meta.LastUpdated = Clock.UtcNow;
+            var (rawBundle, bundle) = CreateBundle();
 
-            var patientWrapper = _wrapperFactory.Create(patientResource.ToResourceElement(), deleted: false, keepMeta: true);
-            patientWrapper.Version = "1";
+            await Validate(rawBundle, bundle);
+        }
 
-            var bundleWithRawEntry = new Hl7.Fhir.Model.Bundle
-            {
-                Id = Guid.NewGuid().ToString(),
-                Type = BundleType.Searchset,
-                Entry = new List<EntryComponent>
-                {
-                    new RawBundleEntryComponent(patientWrapper),
-                },
-            };
+        [Fact]
+        public async Task GivenBundleWithOneEntry_WhenSerialized_MatchesSerializationByBuiltInSerializer()
+        {
+            var patientResource = Samples.GetDefaultPatient();
 
-            var bundle = new Hl7.Fhir.Model.Bundle
-            {
-                Id = bundleWithRawEntry.Id,
-                Type = BundleType.Searchset,
-                Entry = new List<EntryComponent>
-                {
-                    new EntryComponent
-                    {
-                        Resource = patientResource,
-                    },
-                },
-            };
+            var (rawBundle, bundle) = CreateBundle(patientResource);
 
+            await Validate(rawBundle, bundle);
+        }
+
+        [Fact]
+        public async Task GivenBundleWithMultipleEntries_WhenSerialized_MatchesSerializationByBuiltInSerializer()
+        {
+            var patientResource = Samples.GetDefaultPatient();
+            var observationResource = Samples.GetDefaultObservation().ToPoco();
+            var organizationResource = Samples.GetDefaultOrganization().ToPoco();
+
+            observationResource.Id = Guid.NewGuid().ToString();
+            organizationResource.Id = Guid.NewGuid().ToString();
+
+            var (rawBundle, bundle) = CreateBundle(patientResource, observationResource.ToResourceElement(), organizationResource.ToResourceElement());
+
+            await Validate(rawBundle, bundle);
+        }
+
+        private async Task Validate(Hl7.Fhir.Model.Bundle rawBundle, Hl7.Fhir.Model.Bundle bundle)
+        {
             string serialized;
 
             using (var ms = new MemoryStream())
             using (var sr = new StreamReader(ms))
             {
-                await BundleSerializer.Serialize(bundleWithRawEntry, ms);
+                await BundleSerializer.Serialize(rawBundle, ms);
 
                 ms.Seek(0, SeekOrigin.Begin);
                 serialized = await sr.ReadToEndAsync();
@@ -91,11 +93,35 @@ namespace Microsoft.Health.Fhir.Shared.Api.UnitTests.Features.Resources.Bundle
 
             string originalSerializer = bundle.ToJson();
             Assert.Equal(originalSerializer, serialized);
-            patientWrapper.RawResource.Data = serialized;
 
             var deserializedBundle = new FhirJsonParser(DefaultParserSettings.Settings).Parse(serialized) as Hl7.Fhir.Model.Bundle;
 
             Assert.True(deserializedBundle.IsExactly(bundle));
+        }
+
+        private (Hl7.Fhir.Model.Bundle rawBundle, Hl7.Fhir.Model.Bundle bundle) CreateBundle(params ResourceElement[] resources)
+        {
+            string id = Guid.NewGuid().ToString();
+            var rawBundle = new Hl7.Fhir.Model.Bundle();
+            var bundle = new Hl7.Fhir.Model.Bundle();
+
+            rawBundle.Id = bundle.Id = id;
+            rawBundle.Type = bundle.Type = BundleType.Searchset;
+            rawBundle.Entry = new List<EntryComponent>();
+            bundle.Entry = new List<EntryComponent>();
+
+            foreach (var resource in resources)
+            {
+                var poco = resource.ToPoco();
+                poco.VersionId = "1";
+                poco.Meta.LastUpdated = Clock.UtcNow;
+                var wrapper = _wrapperFactory.Create(poco.ToResourceElement(), deleted: false, keepMeta: true);
+                wrapper.Version = "1";
+                rawBundle.Entry.Add(new RawBundleEntryComponent(wrapper));
+                bundle.Entry.Add(new EntryComponent { Resource = poco });
+            }
+
+            return (rawBundle, bundle);
         }
     }
 }
