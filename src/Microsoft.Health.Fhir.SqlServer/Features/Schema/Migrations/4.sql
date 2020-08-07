f6ca115e32c5eabb6452c7761d4285913906a814
Robin Todd <54082711+rotodd@users.noreply.github.com>
2020-08-07 13:39:01 -0700

    Combines search parameter SQL tables. (#1169)
    Moves SQL search parameter status information to existing table.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/4.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/4.sql
@@ -126,7 +126,10 @@ GO
 CREATE TABLE dbo.SearchParam
 (
     SearchParamId smallint IDENTITY(1,1) NOT NULL,
-    Uri varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL
+    Uri varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
+    Status varchar(10) NOT NULL,
+    LastUpdated datetimeoffset(7) NOT NULL,
+    IsPartiallySupported bit NOT NULL
 )
 
 CREATE UNIQUE CLUSTERED INDEX IXC_SearchParam ON dbo.SearchParam
@@ -2154,41 +2157,27 @@ END
 GO
 
 /*************************************************************
-    Search Parameter Status Registry
+    Search Parameter Status Information
 **************************************************************/
 
-CREATE TYPE dbo.SearchParamStatusRegistryTableType_1 AS TABLE
+CREATE TYPE dbo.SearchParamTableType_1 AS TABLE
 (
     Uri varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
     Status varchar(10) NOT NULL,
     IsPartiallySupported bit NOT NULL
-)
-
-CREATE TABLE dbo.SearchParamStatusRegistry
-(
-    Uri varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    Status varchar(10) NOT NULL,
-    LastUpdated datetimeoffset(7) NOT NULL,
-    IsPartiallySupported bit NOT NULL
-)
-
-CREATE UNIQUE CLUSTERED INDEX IXC_SearchParamStatusRegistry
-ON dbo.SearchParamStatusRegistry
-(
-    Uri
 )
 
 GO
 
 /*************************************************************
-    Stored procedures for the search parameter status registry
+    Stored procedures for search parameter information
 **************************************************************/
 --
 -- STORED PROCEDURE
 --     Gets all the search parameters and their statuses.
 --
 -- DESCRIPTION
---     Retrieves and returns the contents of the search parameter registry.
+--     Retrieves and returns select contents from the search parameter table.
 --
 -- RETURN VALUE
 --     The search parameters and their statuses.
@@ -2197,23 +2186,23 @@ CREATE PROCEDURE dbo.GetSearchParamStatuses
 AS
     SET NOCOUNT ON
 
-    SELECT * FROM dbo.SearchParamStatusRegistry
+    SELECT Uri, Status, LastUpdated, IsPartiallySupported FROM dbo.SearchParam
 GO
 
 --
 -- STORED PROCEDURE
---     Given a table of search parameters, upserts the registry.
+--     Given a set of search parameters, creates or updates the parameters.
 --
 -- DESCRIPTION
---     If a parameter with a matching URI already exists in the registry, it is updated.
+--     If a parameter with a matching URI already exists in the table, it is updated.
 --     If not, a new entry is created.
 --
 -- PARAMETERS
---     @searchParamStatuses
---         * The updated or new search parameter statuses
+--     @searchParams
+--         * The updated or new search parameters
 --
-CREATE PROCEDURE dbo.UpsertSearchParamStatuses
-    @searchParamStatuses dbo.SearchParamStatusRegistryTableType_1 READONLY
+CREATE PROCEDURE dbo.UpsertSearchParams
+    @searchParams dbo.SearchParamTableType_1 READONLY
 AS
     SET NOCOUNT ON
     SET XACT_ABORT ON
@@ -2224,18 +2213,18 @@ AS
     DECLARE @lastUpdated datetimeoffset(7) = SYSDATETIMEOFFSET()
 
     -- Acquire and hold an exclusive table lock for the entire transaction to prevent parameters from being added or modified during upsertion.
-    UPDATE dbo.SearchParamStatusRegistry
+    UPDATE dbo.SearchParam
     WITH (TABLOCKX)
     SET Status = sps.Status, LastUpdated = @lastUpdated, IsPartiallySupported = sps.IsPartiallySupported
-    FROM dbo.SearchParamStatusRegistry INNER JOIN @searchParamStatuses as sps
-    ON dbo.SearchParamStatusRegistry.Uri = sps.Uri
+    FROM dbo.SearchParam INNER JOIN @searchParams as sps
+    ON dbo.SearchParam.Uri = sps.Uri
 
-    INSERT INTO dbo.SearchParamStatusRegistry
+    INSERT INTO dbo.SearchParam
         (Uri, Status, LastUpdated, IsPartiallySupported)
     SELECT sps.Uri, sps.Status, @lastUpdated, sps.IsPartiallySupported
-    FROM @searchParamStatuses AS sps
+    FROM @searchParams AS sps
     WHERE sps.Uri NOT IN
-        (SELECT Uri FROM dbo.SearchParamStatusRegistry)
+        (SELECT Uri FROM dbo.SearchParam)
 
     COMMIT TRANSACTION
 GO
