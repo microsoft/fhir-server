d9049245a5398e2e95b2450629c6434f7ea9d0f0
Valentinas Janeiko <val.janeiko@gmail.com>
2020-08-17 11:45:54 +0100

    Added PathBase to allow hosting FHIR Server on a subpath

--- src/Microsoft.Health.Fhir.Api/Registration/FhirServerApplicationBuilderExtensions.cs
+++ src/Microsoft.Health.Fhir.Api/Registration/FhirServerApplicationBuilderExtensions.cs
@@ -3,9 +3,13 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
+using System.Threading.Tasks;
 using EnsureThat;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using Microsoft.Health.Api.Registration;
+using Microsoft.Health.Fhir.Api.Configs;
 using Microsoft.Health.Fhir.Api.Features.Routing;
 
 namespace Microsoft.AspNetCore.Builder
@@ -23,10 +27,56 @@ namespace Microsoft.AspNetCore.Builder
 
             app.UseHealthChecksExtension(new PathString(KnownRoutes.HealthCheck));
 
+            var config = app.ApplicationServices.GetService(typeof(IOptions<FhirServerConfiguration>)) as IOptions<FhirServerConfiguration>;
+            EnsureArg.IsNotNull(config, nameof(config));
+
+            var pathBase = new PathString(config.Value.PathBase.TrimEnd('/'));
+            if (pathBase.HasValue)
+            {
+                app.UseMiddleware<PathBaseMiddleware>(pathBase);
+            }
+
             app.UseStaticFiles();
             app.UseMvc();
 
             return app;
+        }
+
+        private class PathBaseMiddleware
+        {
+            private readonly RequestDelegate _next;
+            private readonly PathString _pathBase;
+
+            public PathBaseMiddleware(RequestDelegate next, PathString pathBase)
+            {
+                if (!pathBase.HasValue)
+                {
+                    throw new ArgumentException($"{nameof(pathBase)} cannot be null or empty.");
+                }
+
+                _next = next ?? throw new ArgumentNullException(nameof(next));
+                _pathBase = pathBase;
+            }
+
+            public async Task Invoke(HttpContext context)
+            {
+                if (context == null)
+                {
+                    throw new ArgumentNullException(nameof(context));
+                }
+
+                var originalPathBase = context.Request.PathBase;
+                context.Request.PathBase = originalPathBase.Add(_pathBase);
+
+                try
+                {
+                    await _next(context);
+                }
+                finally
+                {
+                    context.Request.PathBase = originalPathBase;
+                }
+            }
         }
     }
 }
