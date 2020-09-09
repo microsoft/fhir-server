f474cd8118ddf6ab9867a0508614d2d8397cda2f
namadabu <41395116+namadabu@users.noreply.github.com>
2020-09-09 14:28:58 -0700

    Add support for Reindex request against a single resource (#1247)

---
+++ src/Microsoft.Health.Fhir.Core/Messages/Reindex/ReindexSingleResourceResponse.cs
@@ -0,0 +1,22 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using EnsureThat;
+using Microsoft.Health.Fhir.Core.Models;
+
+namespace Microsoft.Health.Fhir.Core.Messages.Reindex
+{
+    public class ReindexSingleResourceResponse
+    {
+        public ReindexSingleResourceResponse(ResourceElement parameter)
+        {
+            EnsureArg.IsNotNull(parameter, nameof(parameter));
+
+            ParameterResource = parameter;
+        }
+
+        public ResourceElement ParameterResource { get; }
+    }
+}
