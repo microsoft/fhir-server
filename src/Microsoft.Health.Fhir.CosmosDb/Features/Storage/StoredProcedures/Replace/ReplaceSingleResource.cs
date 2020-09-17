f2060eaa68c35325745f266160e9122e60bd3ff5
namadabu <41395116+namadabu@users.noreply.github.com>
2020-09-17 13:14:08 -0700

    Support POST $reindex for a single resource (#1269)

---
+++ src/Microsoft.Health.Fhir.CosmosDb/Features/Storage/StoredProcedures/Replace/ReplaceSingleResource.cs
@@ -0,0 +1,31 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.Azure.Cosmos.Scripts;
+
+namespace Microsoft.Health.Fhir.CosmosDb.Features.Storage.StoredProcedures.Replace
+{
+    internal class ReplaceSingleResource : StoredProcedureBase
+    {
+        public async Task<FhirCosmosResourceWrapper> Execute(Scripts client, FhirCosmosResourceWrapper resource, string matchVersionId, CancellationToken cancellationToken)
+        {
+            EnsureArg.IsNotNull(client, nameof(client));
+            EnsureArg.IsNotNull(resource, nameof(resource));
+
+            StoredProcedureExecuteResponse<FhirCosmosResourceWrapper> result =
+                await ExecuteStoredProc<FhirCosmosResourceWrapper>(
+                    client,
+                    resource.PartitionKey,
+                    cancellationToken,
+                    resource,
+                    matchVersionId);
+
+            return result.Resource;
+        }
+    }
+}
