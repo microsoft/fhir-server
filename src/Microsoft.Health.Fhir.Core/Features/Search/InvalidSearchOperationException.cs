54e430867e2595f4cc52e74ab42d1f32048345e9
Elad Iwanir <13205761+eladiw@users.noreply.github.com>
2020-10-14 09:59:25 +0300

    returning correct issue code for bad requessts

--- src/Microsoft.Health.Fhir.Core/Features/Search/InvalidSearchOperationException.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Search/InvalidSearchOperationException.cs
@@ -25,7 +25,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Search
 
             Issues.Add(new OperationOutcomeIssue(
                 OperationOutcomeConstants.IssueSeverity.Error,
-                OperationOutcomeConstants.IssueType.Forbidden,
+                OperationOutcomeConstants.IssueType.Invalid,
                 message));
         }
     }
