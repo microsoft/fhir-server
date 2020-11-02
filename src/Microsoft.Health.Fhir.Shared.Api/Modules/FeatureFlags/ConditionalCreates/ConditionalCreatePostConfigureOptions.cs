dfb2d15fd6662a351f16890be11124a2f1093227
Ivan Matantsev <ivmatan@microsoft.com>
2020-11-02 14:17:18 -0800

    Remove feature flags for conditional update and conditional create.

--- src/Microsoft.Health.Fhir.Shared.Api/Modules/FeatureFlags/ConditionalCreates/ConditionalCreatePostConfigureOptions.cs
+++ src/Microsoft.Health.Fhir.Shared.Api/Modules/FeatureFlags/ConditionalCreates/ConditionalCreatePostConfigureOptions.cs
@@ -17,40 +17,33 @@ namespace Microsoft.Health.Fhir.Api.Modules.FeatureFlags.ConditionalCreates
 {
     public class ConditionalCreatePostConfigureOptions : IPostConfigureOptions<MvcOptions>
     {
-        private readonly FeatureConfiguration _features;
         private readonly IConfiguredConformanceProvider _configuredConformanceProvider;
 
         public ConditionalCreatePostConfigureOptions(
             IOptions<FeatureConfiguration> features,
             IConfiguredConformanceProvider configuredConformanceProvider)
         {
-            EnsureArg.IsNotNull(features, nameof(features));
             EnsureArg.IsNotNull(configuredConformanceProvider, nameof(configuredConformanceProvider));
 
-            _features = features.Value;
             _configuredConformanceProvider = configuredConformanceProvider;
         }
 
         public void PostConfigure(string name, MvcOptions options)
         {
-            if (_features.SupportsConditionalCreate)
-            {
-                // Turns on conditional creates, even when not configured in the DefaultCapabilities.json
-                // If this is not enabled in the capability statement
-                // ConditionalCreateResourceRequest will fail with a MethodNotAllowedException
-
-                _configuredConformanceProvider
-                    .ConfigureOptionalCapabilities(x =>
+            // Turns on conditional creates, even when not configured in the DefaultCapabilities.json
+            // If this is not enabled in the capability statement
+            // ConditionalCreateResourceRequest will fail with a MethodNotAllowedException
+            _configuredConformanceProvider
+                .ConfigureOptionalCapabilities(x =>
+                {
+                    foreach (ListedResourceComponent r in x.Rest.Server().Resource)
                     {
-                        foreach (ListedResourceComponent r in x.Rest.Server().Resource)
+                        if (r.Interaction.Any(y => string.Equals(y.Code, TypeRestfulInteraction.Create, StringComparison.Ordinal)))
                         {
-                            if (r.Interaction.Any(y => string.Equals(y.Code, TypeRestfulInteraction.Create, StringComparison.Ordinal)))
-                            {
-                                r.ConditionalCreate = true;
-                            }
+                            r.ConditionalCreate = true;
                         }
-                    });
-            }
+                    }
+                });
         }
     }
 }
