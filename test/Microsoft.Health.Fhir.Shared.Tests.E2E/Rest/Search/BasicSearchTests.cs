4e8381d5c3536da682eb0bf5dc498cabc931cdb3
Ivan Matantsev <ivmatan@microsoft.com>
2020-11-10 12:23:08 -0800

    Add "POST" search rerouting middleware. (#1414)
    * Add "POST" search rerouting middleware.

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/BasicSearchTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/BasicSearchTests.cs
@@ -172,7 +172,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             await ExecuteAndValidateBundle("?_type=Patient", patients);
 
             Bundle bundle = await Client.SearchPostAsync(null, default, ("_type", "Patient"));
-            ValidateBundle(bundle, "_search", patients);
+            ValidateBundle(bundle, "?_type=Patient", patients);
 
             bundle = await Client.SearchAsync("?_type=Observation,Patient");
             Assert.True(bundle.Entry.Count > patients.Length);
@@ -181,11 +181,11 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
 
             await ExecuteAndValidateBundle($"?_type=Observation,Patient&_id={observation.Id}", observation);
             bundle = await Client.SearchPostAsync(null, default, ("_type", "Patient,Observation"), ("_id", observation.Id));
-            ValidateBundle(bundle, "_search", observation);
+            ValidateBundle(bundle, $"?_type=Patient,Observation&_id={observation.Id}", observation);
 
             await ExecuteAndValidateBundle($"?_type=Observation,Patient&_id={organization.Id}");
             bundle = await Client.SearchPostAsync(null, default, ("_type", "Patient,Observation"), ("_id", organization.Id));
-            ValidateBundle(bundle, "_search");
+            ValidateBundle(bundle, $"?_type=Patient,Observation&_id={organization.Id}");
         }
 
         [Fact]
@@ -282,6 +282,29 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             }
 
             ValidateBundle(results, patients);
+        }
+
+        [Fact]
+        [Trait(Traits.Priority, Priority.One)]
+        public async Task GivenPostSearchWithCount_WhenSearched_ThenNextLinkUrlWouldYeildMoreResults()
+        {
+            // Create the resources
+            Patient[] patients = await Client.CreateResourcesAsync<Patient>(4);
+            var pageSize = 2;
+            Bundle bundle = await Client.SearchPostAsync(null, default, ("_type", "Patient"), ("_count", pageSize.ToString()));
+
+            var expectedFirstBundle = patients.Length > pageSize ? patients.ToList().GetRange(0, pageSize).ToArray() : patients;
+            ValidateBundle(bundle, "?_type=Patient&_count=2", expectedFirstBundle);
+
+            var nextLink = bundle.NextLink?.ToString();
+            if (nextLink != null)
+            {
+                FhirResponse<Bundle> secondBundle = await Client.SearchAsync(nextLink);
+
+                // Truncating host and appending continuation token
+                nextLink = "?_type=Patient&_count=2" + nextLink.Substring(nextLink.IndexOf("&ct"));
+                ValidateBundle(secondBundle, nextLink, patients.ToList().GetRange(pageSize, patients.Length - pageSize).ToArray());
+            }
         }
 
         [Fact]
@@ -560,14 +583,14 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
         {
             var code = Guid.NewGuid().ToString();
             NamingSystem library = await Client.CreateAsync(new NamingSystem
-                {
-                    Name = "test",
-                    Status = PublicationStatus.Draft,
-                    Kind = NamingSystem.NamingSystemType.Codesystem,
-                    Date = "2019",
-                    UniqueId = new List<NamingSystem.UniqueIdComponent> { new NamingSystem.UniqueIdComponent { Type = NamingSystem.NamingSystemIdentifierType.Uri, Value = "https://localhost" } },
-                    Type = new CodeableConcept("https://localhost/", code),
-                });
+            {
+                Name = "test",
+                Status = PublicationStatus.Draft,
+                Kind = NamingSystem.NamingSystemType.Codesystem,
+                Date = "2019",
+                UniqueId = new List<NamingSystem.UniqueIdComponent> { new NamingSystem.UniqueIdComponent { Type = NamingSystem.NamingSystemIdentifierType.Uri, Value = "https://localhost" } },
+                Type = new CodeableConcept("https://localhost/", code),
+            });
 
             await ExecuteAndValidateBundle($"NamingSystem?type={code}", library);
         }
