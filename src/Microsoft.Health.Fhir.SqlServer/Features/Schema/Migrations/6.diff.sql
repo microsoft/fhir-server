577f673034253bb4c2b4d97686d3ac1cdf16963a
rotodd <rotodd@microsoft.com>
2020-11-11 11:14:19 -0800

    Adds tvp comment.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/6.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/6.diff.sql
@@ -8,6 +8,7 @@ ADD
     LastUpdated datetimeoffset(7) NULL,
     IsPartiallySupported bit NULL
 
+-- We adopted this naming convention for table-valued parameters because they are immutable.
 CREATE TYPE dbo.SearchParamTableType_1 AS TABLE
 (
     Uri varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/6.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/6.sql
@@ -1875,6 +1875,7 @@ GO
     Search Parameter Status Information
 **************************************************************/
 
+-- We adopted this naming convention for table-valued parameters because they are immutable.
 CREATE TYPE dbo.SearchParamTableType_1 AS TABLE
 (
     Uri varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
