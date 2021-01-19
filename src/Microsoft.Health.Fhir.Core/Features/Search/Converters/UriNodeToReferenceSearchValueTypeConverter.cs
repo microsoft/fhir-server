f0218c4a1ef105b562fe0c8c837596c8f0482e18
Brendan Kowitz <bkowitz@microsoft.com>
2021-01-19 15:17:55 -0800

    Adds more Uri/Url support

---
+++ src/Microsoft.Health.Fhir.Core/Features/Search/Converters/UriNodeToReferenceSearchValueTypeConverter.cs
@@ -0,0 +1,48 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using EnsureThat;
+using Hl7.Fhir.ElementModel;
+using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
+
+namespace Microsoft.Health.Fhir.Core.Features.Search.Converters
+{
+    /// <summary>
+    /// A converter used to convert from <see cref="Uri"/> to a list of <see cref="ReferenceSearchValue"/>.
+    /// </summary>
+    public class UriNodeToReferenceSearchValueTypeConverter : FhirNodeToSearchValueTypeConverter<ReferenceSearchValue>
+    {
+        private readonly IReferenceSearchValueParser _referenceSearchValueParser;
+
+        public UriNodeToReferenceSearchValueTypeConverter(IReferenceSearchValueParser referenceSearchValueParser)
+            : base("uri", "url")
+        {
+            EnsureArg.IsNotNull(referenceSearchValueParser, nameof(referenceSearchValueParser));
+
+            _referenceSearchValueParser = referenceSearchValueParser;
+        }
+
+        protected override IEnumerable<ISearchValue> Convert(ITypedElement value)
+        {
+            string uri = value.Value?.ToString();
+
+            if (uri == null)
+            {
+                yield break;
+            }
+
+            // Contained resources will not be searchable.
+            if (uri.StartsWith("#", StringComparison.Ordinal)
+                || uri.StartsWith("urn:", StringComparison.Ordinal))
+            {
+                yield break;
+            }
+
+            yield return _referenceSearchValueParser.Parse(uri);
+        }
+    }
+}
--- src/Microsoft.Health.Fhir.Shared.Core/Features/Search/Converters/FhirUrlToUriSearchValueTypeConverter.cs
+++ src/Microsoft.Health.Fhir.Shared.Core/Features/Search/Converters/FhirUrlToUriSearchValueTypeConverter.cs
@@ -3,30 +3,27 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+#if !Stu3
 using System.Collections.Generic;
-using Hl7.Fhir.ElementModel;
+using Hl7.Fhir.Model;
 using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
 
 namespace Microsoft.Health.Fhir.Core.Features.Search.Converters
 {
     /// <summary>
-    /// A converter used to convert from <see cref="FhirUri"/> to a list of <see cref="UriSearchValue"/>.
+    /// A converter used to convert from <see cref="Hl7.Fhir.Model.FhirUrl"/> to a list of <see cref="Microsoft.Health.Fhir.Core.Features.Search.SearchValues.UriSearchValue"/>.
     /// </summary>
-    public class UriNodeToUriSearchValueTypeConverter : FhirNodeToSearchValueTypeConverter<UriSearchValue>
+    public class FhirUrlToUriSearchValueTypeConverter : FhirElementToSearchValueTypeConverter<FhirUrl, UriSearchValue>
     {
-        public UriNodeToUriSearchValueTypeConverter()
-            : base("uri", "url")
-        {
-        }
-
-        protected override IEnumerable<ISearchValue> Convert(ITypedElement value)
+        protected override IEnumerable<UriSearchValue> ConvertTo(FhirUrl value)
         {
             if (value.Value == null)
             {
                 yield break;
             }
 
-            yield return new UriSearchValue(value.Value?.ToString());
+            yield return new UriSearchValue(value.Value);
         }
     }
 }
+#endif
