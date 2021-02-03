33adbbf90115436697420b0f4755383101ef9f27
Brendan Kowitz <bkowitz@microsoft.com>
2021-02-03 11:48:07 -0800

    Turn off splitting components for Uri search queries (#1630)

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/UriSearchTestFixture.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/UriSearchTestFixture.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using Hl7.Fhir.Model;
 using Microsoft.Health.Fhir.Client;
@@ -20,19 +21,24 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
 
         public IReadOnlyList<ValueSet> ValueSets { get; private set; }
 
+        public string FixtureTag { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             // Prepare the resources used for URI search tests.
-            await TestFhirClient.DeleteAllResources(ResourceType.ValueSet);
+            FixtureTag = Guid.NewGuid().ToString();
 
             ValueSets = await TestFhirClient.CreateResourcesAsync<ValueSet>(
                 vs => AddValueSet(vs, "http://somewhere.com/test/system"),
-                vs => AddValueSet(vs, "urn://localhost/test"));
+                vs => AddValueSet(vs, "urn://localhost/test"),
+                vs => AddValueSet(vs, "http://example.org/rdf#54135-9"));
 
             void AddValueSet(ValueSet vs, string url)
             {
                 vs.Status = PublicationStatus.Active;
                 vs.Url = url;
+                vs.Meta = new Meta();
+                vs.Meta.Tag.Add(new Coding(null, FixtureTag));
             }
         }
     }
