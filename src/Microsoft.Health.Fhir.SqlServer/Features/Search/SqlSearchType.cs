9cdd20347cbb3f4ec82a8b682a5dbb23193829ea
rotodd <rotodd@microsoft.com>
2021-02-17 16:52:13 -0800

    Adds comments.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Search/SqlSearchType.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Search/SqlSearchType.cs
@@ -7,12 +7,22 @@ using System;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features.Search
 {
+    /// <summary>
+    /// Enum to specify the type of search to carry out.
+    /// </summary>
+    /// <remarks>
+    /// With the exception of None, the flags attribute requires enumeration constants to be powers of two, that is, 1, 2, 4, 8.
+    /// </remarks>
     [Flags]
     internal enum SqlSearchType
     {
-        // The flags attribute requires enumeration constants to be powers of two, that is, 1, 2, 4, 8
+        // Default value, set if we do not need to consider history or reindexing
         None = 0,
+
+        // Set if we are including previous resource versions or deleted resources
         History = 1,
+
+        // Set if the search parameter hash value needs to be considered in a search
         Reindex = 2,
     }
 }
