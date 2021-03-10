991be0e39670a54c7a3de3348f1a0ccc936859b7
Jared Erwin <jaerwin@microsoft.com>
2021-03-10 09:51:31 -0800

    Use logic from QueryGeneratorFactory to avoid duplication

--- src/Microsoft.Health.Fhir.SqlServer/Features/Search/SqlServerSearchParameterValidator.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Search/SqlServerSearchParameterValidator.cs
@@ -3,13 +3,11 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
 using EnsureThat;
 using Microsoft.Health.Fhir.Core.Features.Search;
-using Microsoft.Health.Fhir.Core.Features.Search.SearchValues;
 using Microsoft.Health.Fhir.Core.Models;
+using Microsoft.Health.Fhir.SqlServer.Features.Search.Expressions.Visitors;
 using Microsoft.Health.Fhir.SqlServer.Features.Storage;
-using Microsoft.Health.Fhir.ValueSets;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features.Search
 {
@@ -29,35 +27,17 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search
             EnsureArg.IsNotNull(searchParameter, nameof(searchParameter));
             errorMessage = null;
 
-            switch (searchParameter.Type)
-            {
-                case SearchParamType.Token:
-                case SearchParamType.Date:
-                case SearchParamType.Number:
-                case SearchParamType.Quantity:
-                case SearchParamType.Reference:
-                case SearchParamType.String:
-                case SearchParamType.Uri:
-                    return true;
-                case SearchParamType.Composite:
-                    Type searchValueType = _searchParameterToSearchValueTypeMap.GetSearchValueType(searchParameter);
-                    if (searchValueType == typeof(ValueTuple<TokenSearchValue, QuantitySearchValue>)
-                        || searchValueType == typeof(ValueTuple<ReferenceSearchValue, TokenSearchValue>)
-                        || searchValueType == typeof(ValueTuple<TokenSearchValue, DateTimeSearchValue>)
-                        || searchValueType == typeof(ValueTuple<TokenSearchValue, StringSearchValue>)
-                        || searchValueType == typeof(ValueTuple<TokenSearchValue, NumberSearchValue, NumberSearchValue>))
-                        {
-                            return true;
-                        }
-                        else
-                        {
-                            errorMessage = string.Format(Resources.SearchParameterTypeNotSupportedBySQLServer, searchParameter.Type);
-                            return false;
-                        }
+            var factory = new SearchParamTableExpressionQueryGeneratorFactory(_searchParameterToSearchValueTypeMap);
 
-                default:
-                    errorMessage = string.Format(Resources.SearchParameterTypeNotSupportedBySQLServer, searchParameter.Type);
-                    return false;
+            try
+            {
+                factory.GetGenerator(searchParameter);
+                return true;
+            }
+            catch
+            {
+                errorMessage = string.Format(Resources.SearchParameterTypeNotSupportedBySQLServer, searchParameter.Type);
+                return false;
             }
         }
     }
