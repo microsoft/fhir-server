a080589dda7745e30c6682eaff1788008306457c
rotodd <rotodd@microsoft.com>
2021-03-24 10:54:25 -0700

    Removes unused tvps.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/8.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/8.sql
@@ -127,12 +127,6 @@ CREATE UNIQUE CLUSTERED INDEX IXC_Claim on dbo.ClaimType
     Name
 )
 
-CREATE TYPE dbo.ResourceWriteClaimTableType_1 AS TABLE
-(
-    ClaimTypeId tinyint NOT NULL,
-    ClaimValue nvarchar(128) NOT NULL
-)
-
 CREATE TYPE dbo.BulkResourceWriteClaimTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -166,12 +160,6 @@ CREATE TABLE dbo.CompartmentType
 CREATE UNIQUE CLUSTERED INDEX IXC_CompartmentType on dbo.CompartmentType
 (
     Name
-)
-
-CREATE TYPE dbo.CompartmentAssignmentTableType_1 AS TABLE
-(
-    CompartmentTypeId tinyint NOT NULL,
-    ReferenceResourceId varchar(64) COLLATE Latin1_General_100_CS_AS NOT NULL
 )
 
 CREATE TYPE dbo.BulkCompartmentAssignmentTableType_1 AS TABLE
@@ -216,15 +204,6 @@ GO
 /*************************************************************
     Reference Search Param
 **************************************************************/
-
-CREATE TYPE dbo.ReferenceSearchParamTableType_2 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    BaseUri varchar(128) COLLATE Latin1_General_100_CS_AS NULL,
-    ReferenceResourceTypeId smallint NULL,
-    ReferenceResourceId varchar(64) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    ReferenceResourceVersion int NULL
-)
 
 CREATE TYPE dbo.BulkReferenceSearchParamTableType_1 AS TABLE
 (
@@ -277,13 +256,6 @@ GO
     Token Search Param
 **************************************************************/
 
-CREATE TYPE dbo.TokenSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SystemId int NULL,
-    Code varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL
-)
-
 CREATE TYPE dbo.BulkTokenSearchParamTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -329,12 +301,6 @@ GO
     Token Text
 **************************************************************/
 
-CREATE TYPE dbo.TokenTextTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    Text nvarchar(400) COLLATE Latin1_General_CI_AI NOT NULL
-)
-
 CREATE TYPE dbo.BulkTokenTextTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -376,13 +342,6 @@ GO
 /*************************************************************
     String Search Param
 **************************************************************/
-
-CREATE TYPE dbo.StringSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    Text nvarchar(256) COLLATE Latin1_General_100_CI_AI_SC NOT NULL,
-    TextOverflow nvarchar(max) COLLATE Latin1_General_100_CI_AI_SC NULL
-)
 
 CREATE TYPE dbo.BulkStringSearchParamTableType_1 AS TABLE
 (
@@ -442,12 +401,6 @@ GO
     URI Search Param
 **************************************************************/
 
-CREATE TYPE dbo.UriSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    Uri varchar(256) COLLATE Latin1_General_100_CS_AS NOT NULL
-)
-
 CREATE TYPE dbo.BulkUriSearchParamTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -496,14 +449,6 @@ GO
 --  (1) SingleValue is not null and LowValue and HighValue are both null, or
 --  (2) SingleValue is null and LowValue and HighValue are both not null
 -- We make use of filtered nonclustered indexes to keep queries over the ranges limited to those rows that actually have ranges
-
-CREATE TYPE dbo.NumberSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SingleValue decimal(18,6) NULL,
-    LowValue decimal(18,6) NULL,
-    HighValue decimal(18,6) NULL
-)
 
 CREATE TYPE dbo.BulkNumberSearchParamTableType_1 AS TABLE
 (
@@ -577,16 +522,6 @@ GO
 **************************************************************/
 
 -- See comment above for number search params for how we store ranges
-
-CREATE TYPE dbo.QuantitySearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SystemId int NULL,
-    QuantityCodeId int NULL,
-    SingleValue decimal(18,6) NULL,
-    LowValue decimal(18,6) NULL,
-    HighValue decimal(18,6) NULL
-)
 
 CREATE TYPE dbo.BulkQuantitySearchParamTableType_1 AS TABLE
 (
@@ -668,14 +603,6 @@ GO
 /*************************************************************
     Date Search Param
 **************************************************************/
-
-CREATE TYPE dbo.DateTimeSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    StartDateTime datetimeoffset(7) NOT NULL,
-    EndDateTime datetimeoffset(7) NOT NULL,
-    IsLongerThanADay bit NOT NULL
-)
 
 CREATE TYPE dbo.BulkDateTimeSearchParamTableType_1 AS TABLE
 (
@@ -765,17 +692,6 @@ GO
     Reference$Token Composite Search Param
 **************************************************************/
 
-CREATE TYPE dbo.ReferenceTokenCompositeSearchParamTableType_2 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    BaseUri1 varchar(128) COLLATE Latin1_General_100_CS_AS NULL,
-    ReferenceResourceTypeId1 smallint NULL,
-    ReferenceResourceId1 varchar(64) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    ReferenceResourceVersion1 int NULL,
-    SystemId2 int NULL,
-    Code2 varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL
-)
-
 CREATE TYPE dbo.BulkReferenceTokenCompositeSearchParamTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -832,15 +748,6 @@ GO
     Token$Token Composite Search Param
 **************************************************************/
 
-CREATE TYPE dbo.TokenTokenCompositeSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SystemId1 int NULL,
-    Code1 varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    SystemId2 int NULL,
-    Code2 varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL
-)
-
 CREATE TYPE dbo.BulkTokenTokenCompositeSearchParamTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -891,16 +798,6 @@ GO
 /*************************************************************
     Token$DateTime Composite Search Param
 **************************************************************/
-
-CREATE TYPE dbo.TokenDateTimeCompositeSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SystemId1 int NULL,
-    Code1 varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    StartDateTime2 datetimeoffset(7) NOT NULL,
-    EndDateTime2 datetimeoffset(7) NOT NULL,
-    IsLongerThanADay2 bit NOT NULL
-)
 
 CREATE TYPE dbo.BulkTokenDateTimeCompositeSearchParamTableType_1 AS TABLE
 (
@@ -1007,18 +904,6 @@ GO
     Token$Quantity Composite Search Param
 **************************************************************/
 
-CREATE TYPE dbo.TokenQuantityCompositeSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SystemId1 int NULL,
-    Code1 varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    SystemId2 int NULL,
-    QuantityCodeId2 int NULL,
-    SingleValue2 decimal(18,6) NULL,
-    LowValue2 decimal(18,6) NULL,
-    HighValue2 decimal(18,6) NULL
-)
-
 CREATE TYPE dbo.BulkTokenQuantityCompositeSearchParamTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -1113,15 +998,6 @@ GO
     Token$String Composite Search Param
 **************************************************************/
 
-CREATE TYPE dbo.TokenStringCompositeSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SystemId1 int NULL,
-    Code1 varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    Text2 nvarchar(256) COLLATE Latin1_General_100_CI_AI_SC NOT NULL,
-    TextOverflow2 nvarchar(max) COLLATE Latin1_General_100_CI_AI_SC NULL
-)
-
 CREATE TYPE dbo.BulkTokenStringCompositeSearchParamTableType_1 AS TABLE
 (
     Offset int NOT NULL,
@@ -1193,21 +1069,6 @@ GO
 -- except that we pass in a HasRange bit though the TVP. The alternative would have
 -- for a computed column, but a computed column cannot be used in as a index filter
 -- (even if it is a persisted computed column).
-
-
-CREATE TYPE dbo.TokenNumberNumberCompositeSearchParamTableType_1 AS TABLE
-(
-    SearchParamId smallint NOT NULL,
-    SystemId1 int NULL,
-    Code1 varchar(128) COLLATE Latin1_General_100_CS_AS NOT NULL,
-    SingleValue2 decimal(18,6) NULL,
-    LowValue2 decimal(18,6) NULL,
-    HighValue2 decimal(18,6) NULL,
-    SingleValue3 decimal(18,6) NULL,
-    LowValue3 decimal(18,6) NULL,
-    HighValue3 decimal(18,6) NULL,
-    HasRange bit NOT NULL
-)
 
 CREATE TYPE dbo.BulkTokenNumberNumberCompositeSearchParamTableType_1 AS TABLE
 (
