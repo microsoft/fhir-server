29af3c4a8c3894834775c47c0f7487b4a50cdea6
rotodd <rotodd@microsoft.com>
2021-04-02 16:59:45 -0700

    Removes null check.

--- src/Microsoft.Health.Fhir.Api/Features/ApiNotifications/ApiNotificationMiddleware.cs
+++ src/Microsoft.Health.Fhir.Api/Features/ApiNotifications/ApiNotificationMiddleware.cs
@@ -64,7 +64,7 @@ namespace Microsoft.Health.Fhir.Api.Features.ApiNotifications
                 }
                 finally
                 {
-                    apiNotification.Latency = timer?.ElapsedTime ?? TimeSpan.Zero;
+                    apiNotification.Latency = timer.ElapsedTime;
 
                     try
                     {
