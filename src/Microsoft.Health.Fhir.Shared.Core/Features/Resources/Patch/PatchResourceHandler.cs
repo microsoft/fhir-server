24cd24b4c6d4b363957ef1540d884216a3469da6
Edaena Salinas <edaena@gmail.com>
2021-04-16 11:07:09 -0700

    Add csproj changes.

--- src/Microsoft.Health.Fhir.Shared.Core/Features/Resources/Patch/PatchResourceHandler.cs
+++ src/Microsoft.Health.Fhir.Shared.Core/Features/Resources/Patch/PatchResourceHandler.cs
@@ -15,7 +15,7 @@ using Microsoft.Health.Fhir.Core.Features.Persistence;
 using Microsoft.Health.Fhir.Core.Features.Security;
 using Microsoft.Health.Fhir.Core.Messages.Patch;
 
-namespace Microsoft.Health.Fhir.Core.Features.Resources.Delete
+namespace Microsoft.Health.Fhir.Core.Features.Resources.Patch
 {
     public class PatchResourceHandler : BaseResourceHandler, IRequestHandler<PatchResourceRequest, PatchResourceResponse>
     {
