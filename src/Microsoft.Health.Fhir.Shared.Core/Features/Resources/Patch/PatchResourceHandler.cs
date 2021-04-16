831b23a8ad20c3b036beb5aeef4d6058b16be19c
Edaena Salinas <edaena@gmail.com>
2021-04-16 09:19:29 -0700

    Add handler for patch operation.

---
+++ src/Microsoft.Health.Fhir.Shared.Core/Features/Resources/Patch/PatchResourceHandler.cs
@@ -0,0 +1,71 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using MediatR;
+using Microsoft.Health.Core.Features.Security.Authorization;
+using Microsoft.Health.Fhir.Core.Exceptions;
+using Microsoft.Health.Fhir.Core.Features.Conformance;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Core.Features.Security;
+using Microsoft.Health.Fhir.Core.Messages.Patch;
+
+namespace Microsoft.Health.Fhir.Core.Features.Resources.Delete
+{
+    public class PatchResourceHandler : BaseResourceHandler, IRequestHandler<PatchResourceRequest, PatchResourceResponse>
+    {
+        public PatchResourceHandler(
+            IFhirDataStore fhirDataStore,
+            Lazy<IConformanceProvider> conformanceProvider,
+            IResourceWrapperFactory resourceWrapperFactory,
+            ResourceIdProvider resourceIdProvider,
+            IAuthorizationService<DataActions> authorizationService)
+            : base(fhirDataStore, conformanceProvider, resourceWrapperFactory, resourceIdProvider, authorizationService)
+        {
+        }
+
+        public async Task<PatchResourceResponse> Handle(PatchResourceRequest message, CancellationToken cancellationToken)
+        {
+            EnsureArg.IsNotNull(message, nameof(message));
+
+            if (await AuthorizationService.CheckAccess(DataActions.Write, cancellationToken) != DataActions.Write)
+            {
+                throw new UnauthorizedFhirActionException();
+            }
+
+            var key = message.ResourceKey;
+
+            if (!string.IsNullOrEmpty(key.VersionId))
+            {
+                throw new MethodNotAllowedException(Core.Resources.DeleteVersionNotAllowed);
+            }
+
+            string version = null;
+
+            if (message.PatchDocument != null)
+            {
+                // To-do: Patch document operation
+                // Call the data store Patch operation
+                Console.WriteLine("Patch operation placeholder");
+                /* UpsertOutcome result = await FhirDataStore.PatchAsync(
+                    deletedWrapper,
+                    weakETag: null,
+                    allowCreate: true,
+                    keepHistory: keepHistory,
+                    cancellationToken: cancellationToken);*/
+            }
+
+            if (string.IsNullOrWhiteSpace(version))
+            {
+                return new PatchResourceResponse(new ResourceKey(key.ResourceType, key.Id));
+            }
+
+            return new PatchResourceResponse(new ResourceKey(key.ResourceType, key.Id, version), WeakETag.FromVersionId(version));
+        }
+    }
+}
