7defdd8e0cc44be0ea62b8264b36542590a8a2de
Edaena Salinas <edaena@gmail.com>
2021-04-21 13:08:02 -0700

    Update resource designer.

--- src/Microsoft.Health.Fhir.Core/Resources.Designer.cs
+++ src/Microsoft.Health.Fhir.Core/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Health.Fhir.Core {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -715,6 +715,33 @@ namespace Microsoft.Health.Fhir.Core {
         internal static string OrDelimiter {
             get {
                 return ResourceManager.GetString("OrDelimiter", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Patching immutable properties is not allowed..
+        /// </summary>
+        internal static string PatchImmutablePropertiesIsNotValid {
+            get {
+                return ResourceManager.GetString("PatchImmutablePropertiesIsNotValid", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Error patching resource: &apos;{0}&apos;..
+        /// </summary>
+        internal static string PatchResourceError {
+            get {
+                return ResourceManager.GetString("PatchResourceError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Patching a specific record version is not supported..
+        /// </summary>
+        internal static string PatchVersionNotAllowed {
+            get {
+                return ResourceManager.GetString("PatchVersionNotAllowed", resourceCulture);
             }
         }
         
