0871440f32fe5fce81f1cfa0313fa855261e15c5
skwwt <tong.wu@live.com>
2021-05-16 08:09:18 +0800

    add more comments

--- src/Microsoft.Health.Fhir.Core/Features/Operations/Import/IImportErrorSerializer.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/Import/IImportErrorSerializer.cs
@@ -7,8 +7,17 @@ using System;
 
 namespace Microsoft.Health.Fhir.Core.Features.Operations.Import
 {
+    /// <summary>
+    /// Serializer for error of import operation
+    /// </summary>
     public interface IImportErrorSerializer
     {
+        /// <summary>
+        /// Serialize import error into string format.
+        /// </summary>
+        /// <param name="index">Error index in file.</param>
+        /// <param name="ex">Exception</param>
+        /// <returns>Error in string format.</returns>
         public string Serialize(long index, Exception ex);
     }
 }
--- src/Microsoft.Health.Fhir.Core/Features/Operations/Import/IImportErrorStore.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/Import/IImportErrorStore.cs
@@ -8,10 +8,21 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Health.Fhir.Core.Features.Operations.Import
 {
+    /// <summary>
+    /// Store for import error
+    /// </summary>
     public interface IImportErrorStore
     {
+        /// <summary>
+        /// Error file location.
+        /// </summary>
         public string ErrorFileLocation { get; }
 
+        /// <summary>
+        /// Upload import error to store.
+        /// </summary>
+        /// <param name="importErrors">Import errors in string format.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
         public Task UploadErrorsAsync(string[] importErrors, CancellationToken cancellationToken);
     }
 }
