7557cc963d644caa955f455d8e7347da3ad6a143
Richa Bansal <ribans@microsoft.com>
2021-06-18 16:56:21 -0700

    Sets default value to maxQueryConcurrency

--- src/Microsoft.Health.Fhir.CosmosDb/Configs/CosmosDataStoreParallelQueryOptions.cs
+++ src/Microsoft.Health.Fhir.CosmosDb/Configs/CosmosDataStoreParallelQueryOptions.cs
@@ -3,6 +3,8 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
+
 namespace Microsoft.Health.Fhir.CosmosDb.Configs
 {
     public class CosmosDataStoreParallelQueryOptions
@@ -10,7 +12,7 @@ namespace Microsoft.Health.Fhir.CosmosDb.Configs
         /// <summary>
         /// Gets the maximum degree of parallelism for the SDK to use when querying physical partitions in parallel.
         /// </summary>
-        public int MaxQueryConcurrency { get; set; }
+        public int MaxQueryConcurrency { get; set; } = Environment.ProcessorCount * 10;
 
         /// <summary>
         /// Enables the parallelism if sequential query execution takes more time
