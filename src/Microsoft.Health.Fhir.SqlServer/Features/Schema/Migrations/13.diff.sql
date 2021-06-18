27578eb197caf46dcbe91bf8265e90411be17adf
Batjargal Bayarsaikhan (Zen3 Infosolutions America Inc) <v-batjargalb@microsoft.com>
2021-06-17 17:48:41 -0700

    Added UNIQUE constraint on ResourceChangeType, updated desc, and fixed a test failure.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/13.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/13.diff.sql
@@ -22,62 +22,62 @@ AS
     SET NOCOUNT ON
 
     SET XACT_ABORT ON
-    BEGIN TRANSACTION
+BEGIN TRANSACTION
 
     DECLARE @resourceSurrogateIds TABLE(ResourceSurrogateId bigint NOT NULL)
 
-            DELETE FROM dbo.Resource
+    DELETE FROM dbo.Resource
         OUTPUT deleted.ResourceSurrogateId
     INTO @resourceSurrogateIds
     WHERE ResourceTypeId = @resourceTypeId AND ResourceId = @resourceId
       AND NOT(@keepCurrentVersion=1 and IsHistory=0)
 
-            DELETE FROM dbo.ResourceWriteClaim
+    DELETE FROM dbo.ResourceWriteClaim
     WHERE ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.CompartmentAssignment
+    DELETE FROM dbo.CompartmentAssignment
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.ReferenceSearchParam
+    DELETE FROM dbo.ReferenceSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.TokenSearchParam
+    DELETE FROM dbo.TokenSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.TokenText
+    DELETE FROM dbo.TokenText
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.StringSearchParam
+    DELETE FROM dbo.StringSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.UriSearchParam
+    DELETE FROM dbo.UriSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.NumberSearchParam
+    DELETE FROM dbo.NumberSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.QuantitySearchParam
+    DELETE FROM dbo.QuantitySearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.DateTimeSearchParam
+    DELETE FROM dbo.DateTimeSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.ReferenceTokenCompositeSearchParam
+    DELETE FROM dbo.ReferenceTokenCompositeSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.TokenTokenCompositeSearchParam
+    DELETE FROM dbo.TokenTokenCompositeSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.TokenDateTimeCompositeSearchParam
+    DELETE FROM dbo.TokenDateTimeCompositeSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.TokenQuantityCompositeSearchParam
+    DELETE FROM dbo.TokenQuantityCompositeSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.TokenStringCompositeSearchParam
+    DELETE FROM dbo.TokenStringCompositeSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
-            DELETE FROM dbo.TokenNumberNumberCompositeSearchParam
+    DELETE FROM dbo.TokenNumberNumberCompositeSearchParam
     WHERE ResourceTypeId = @resourceTypeId AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM @resourceSurrogateIds)
 
     COMMIT TRANSACTION
