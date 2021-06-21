94cad2f901f7ecc2a71f2386f4a7321d6819a170
Ivan Matantsev <Ivanidze.kirov@gmail.com>
2021-06-21 11:24:30 -0700

    CosmosDB

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/BasicSearchTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/BasicSearchTests.cs
@@ -104,7 +104,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
 
         [Fact]
         [Trait(Traits.Priority, Priority.One)]
-        [HttpIntegrationFixtureArgumentSets(DataStore.SqlServer)]
+        [HttpIntegrationFixtureArgumentSets(DataStore.CosmosDb)]
         public async Task GivenResourcesWithReference_WhenSearchedWithReferenceAndIdParameter_ThenOnlyResourcesMatchingAllSearchParamsShouldBeReturned()
         {
             Patient patientWithMatchingReference = (await Client.CreateResourcesAsync<Patient>(p =>
