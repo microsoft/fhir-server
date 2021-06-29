08ba2754e93a5b90f2b62426916c2ef53f76fbd8
Yue Fei <59813791+moria97@users.noreply.github.com>
2021-06-29 20:04:08 +0800

    Add artifact store in configuration (#2042)
    * Add artifact store registration

---
+++ src/Microsoft.Health.Fhir.Core/Features/ArtifactStore/OciArtifactInfo.cs
@@ -0,0 +1,42 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+using System;
+
+namespace Microsoft.Health.Fhir.Core.Features.ArtifactStore
+{
+    // Describes Open Container Initiative (OCI) artifacts managed in ACR
+    // https://docs.microsoft.com/en-us/azure/container-registry/container-registry-oci-artifacts
+    public class OciArtifactInfo
+    {
+        public string LoginServer { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string Digest { get; set; }
+
+        public bool ContainsOciImage(string server, string name, string digest)
+        {
+            // If LoginServer is not provided, return false because this ArtifactInfo is invalid.
+            if (string.IsNullOrEmpty(LoginServer) || !string.Equals(LoginServer, server, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            // Compare imageName if ImageName is provided in ArtifactInfo.
+            if (!string.IsNullOrEmpty(ImageName) && !string.Equals(ImageName, name, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            // Compare digest if Digest is provided in ArtifactInfo.
+            if (!string.IsNullOrEmpty(Digest) && !string.Equals(Digest, digest, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
