aa2225e9271a7663235cff7c5cb467d23a03c4b7
Batjargal Bayarsaikhan <b.batja@gmail.com>
2021-07-07 20:33:02 -0400

    SQL Schema changes for resource changes (#2032)
    When the resource change capture feature is enabled,
    the CUD information of Fhir resources will be stored in
    a ResourceChangeData table on the SQL database.
    
    * Adds a ResourceChangeData table that stores resource change information.
    * Adds a ResourceChangeType table which is a reference table to store change types.
    * Adds a CaptureResourceChanges procedure which inserts resource change data 
    and is called from the UpsertResource proc.
    * Adds a FetchResourceChanges procedure which returns the number 
    of resource change records from startId.
    * Adds a new version of the UpsertResource procedure (UpsertResource_4).
    * Adds a core feature config called SupportsResourceChangeCapture.
    * Adds a new schema version (version 14).
    
    
    Co-authored-by: Batjargal Bayarsaikhan (Zen3 Infosolutions America Inc) <v-batjargalb@microsoft.com>

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerFhirStorageTestsFixture.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerFhirStorageTestsFixture.cs
@@ -119,6 +119,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
 
             var upsertResourceTvpGeneratorV6 = serviceProvider.GetRequiredService<V6.UpsertResourceTvpGenerator<ResourceMetadata>>();
             var upsertResourceTvpGeneratorV7 = serviceProvider.GetRequiredService<V7.UpsertResourceTvpGenerator<ResourceMetadata>>();
+            var upsertResourceTvpGeneratorV13 = serviceProvider.GetRequiredService<V13.UpsertResourceTvpGenerator<IReadOnlyList<ResourceWrapper>>>();
             var upsertResourceTvpGeneratorVLatest = serviceProvider.GetRequiredService<VLatest.UpsertResourceTvpGenerator<IReadOnlyList<ResourceWrapper>>>();
             var upsertSearchParamsTvpGenerator = serviceProvider.GetRequiredService<VLatest.UpsertSearchParamsTvpGenerator<List<ResourceSearchParameterStatus>>>();
             var reindexResourceTvpGenerator = serviceProvider.GetRequiredService<VLatest.ReindexResourceTvpGenerator<IReadOnlyList<ResourceWrapper>>>();
@@ -144,6 +145,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
                 searchParameterToSearchValueTypeMap,
                 upsertResourceTvpGeneratorV6,
                 upsertResourceTvpGeneratorV7,
+                upsertResourceTvpGeneratorV13,
                 upsertResourceTvpGeneratorVLatest,
                 reindexResourceTvpGenerator,
                 bulkReindexResourceTvpGenerator,
