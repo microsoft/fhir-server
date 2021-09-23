69e42dd0fef87307b089538b817e382be9a35729
Robin Todd <rotodd@microsoft.com>
2021-09-23 15:24:38 -0700

    Adds conformance provider history tests.

---
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Conformance/ConformanceProviderExtensionTests.cs
@@ -0,0 +1,67 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Threading;
+using Hl7.Fhir.Model;
+using Microsoft.Health.Fhir.Core.Extensions;
+using Microsoft.Health.Fhir.Core.Features.Conformance;
+using Microsoft.Health.Fhir.Tests.Common.Mocks;
+using NSubstitute;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Conformance
+{
+    public class ConformanceProviderExtensionTests
+    {
+        private readonly IConformanceProvider _conformanceProvider;
+
+        public ConformanceProviderExtensionTests()
+        {
+            _conformanceProvider = Substitute.For<ConformanceProviderBase>();
+        }
+
+        [Fact]
+        public async void GivenCoreConfigWithNoVersionVersioningPolicy_WhenCheckingIfKeepHistory_ThenFalseIsReturned()
+        {
+            const ResourceType resourceType = ResourceType.Patient;
+
+            CapabilityStatement statement = CapabilityStatementMock.GetMockedCapabilityStatement();
+            CapabilityStatementMock.SetupMockResource(statement, resourceType, null, null, CapabilityStatement.ResourceVersionPolicy.NoVersion);
+
+            _conformanceProvider.GetCapabilityStatementOnStartup().Returns(statement.ToResourceElement());
+
+            bool keepHistory = await _conformanceProvider.CanKeepHistory(resourceType.ToString(), CancellationToken.None);
+            Assert.False(keepHistory);
+        }
+
+        [Fact]
+        public async void GivenCoreConfigWithVersionedVersioningPolicy_WhenCheckingIfKeepHistory_ThenTrueIsReturned()
+        {
+            const ResourceType resourceType = ResourceType.Patient;
+
+            CapabilityStatement statement = CapabilityStatementMock.GetMockedCapabilityStatement();
+            CapabilityStatementMock.SetupMockResource(statement, resourceType, null, null, CapabilityStatement.ResourceVersionPolicy.Versioned);
+
+            _conformanceProvider.GetCapabilityStatementOnStartup().Returns(statement.ToResourceElement());
+
+            bool keepHistory = await _conformanceProvider.CanKeepHistory(resourceType.ToString(), CancellationToken.None);
+            Assert.True(keepHistory);
+        }
+
+        [Fact]
+        public async void GivenCoreConfigWithVersionedUpdateVersioningPolicy_WhenCheckingIfKeepHistory_ThenTrueIsReturned()
+        {
+            const ResourceType resourceType = ResourceType.Patient;
+
+            CapabilityStatement statement = CapabilityStatementMock.GetMockedCapabilityStatement();
+            CapabilityStatementMock.SetupMockResource(statement, resourceType, null, null, CapabilityStatement.ResourceVersionPolicy.VersionedUpdate);
+
+            _conformanceProvider.GetCapabilityStatementOnStartup().Returns(statement.ToResourceElement());
+
+            bool keepHistory = await _conformanceProvider.CanKeepHistory(resourceType.ToString(), CancellationToken.None);
+            Assert.True(keepHistory);
+        }
+    }
+}
