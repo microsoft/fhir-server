38fa85a0d31d651b37bd7650f88d30f0c2dd0860
Batjargal Bayarsaikhan (Zen3 Infosolutions America Inc) <v-batjargalb@microsoft.com>
2021-10-14 10:40:04 -0700

    Added schema version 19

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/SchemaVersionConstants.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/SchemaVersionConstants.cs
@@ -18,6 +18,6 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Schema
         public const int SupportsResourceChangeCaptureSchemaVersion = (int)SchemaVersion.V14;
         public const int BulkReindexReturnsFailuresVersion = (int)SchemaVersion.V16;
         public const int AddMinMaxForDateAndStringSearchParamVersion = (int)SchemaVersion.V18;
-        public const int SupportsPartitionedResourceChangeDataVersion = (int)SchemaVersion.V18;
+        public const int SupportsPartitionedResourceChangeDataVersion = (int)SchemaVersion.V19;
     }
 }
