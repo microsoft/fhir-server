a14d4508f051009127db80a7abd4d84b6cf10238
Brendan Kowitz <bkowitz@microsoft.com>
2021-10-14 09:49:02 -0700

    Compress continuation tokens

--- src/Microsoft.Health.Fhir.Core/Features/Search/ContinuationTokenConverter.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Search/ContinuationTokenConverter.cs
@@ -4,18 +4,36 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
+using System.IO;
+using System.IO.Compression;
 using EnsureThat;
 using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.IO;
 
 namespace Microsoft.Health.Fhir.Core.Features.Search
 {
     public sealed class ContinuationTokenConverter
     {
+        private static readonly RecyclableMemoryStreamManager StreamManager = new();
+
         public static string Decode(string encodedContinuationToken)
         {
             try
             {
-                return System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(encodedContinuationToken));
+                byte[] continuationTokenBytes = Convert.FromBase64String(encodedContinuationToken);
+
+                try
+                {
+                    using MemoryStream memoryStream = StreamManager.GetStream(continuationTokenBytes);
+                    using var deflate = new DeflateStream(memoryStream, CompressionMode.Decompress);
+                    using var reader = new StreamReader(deflate);
+                    return reader.ReadToEnd();
+                }
+                catch (InvalidDataException)
+                {
+                    // Fall back to compatibility with non-compressed tokens
+                    return System.Text.Encoding.UTF8.GetString(continuationTokenBytes);
+                }
             }
             catch (FormatException)
             {
@@ -26,7 +44,15 @@ namespace Microsoft.Health.Fhir.Core.Features.Search
         public static string Encode(string continuationToken)
         {
             EnsureArg.IsNotEmptyOrWhiteSpace(continuationToken);
-            return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(continuationToken));
+
+            var buffer = System.Text.Encoding.UTF8.GetBytes(continuationToken);
+
+            using MemoryStream memoryStream = StreamManager.GetStream();
+            using var deflate = new DeflateStream(memoryStream, CompressionLevel.Fastest);
+            deflate.Write(buffer);
+            deflate.Flush();
+
+            return Convert.ToBase64String(memoryStream.ToArray());
         }
     }
 }
