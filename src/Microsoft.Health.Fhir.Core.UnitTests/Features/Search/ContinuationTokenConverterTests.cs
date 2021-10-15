3293f750cc44be6f6cb36a1ad5bd816eff25a0a8
Brendan Kowitz <bkowitz@microsoft.com>
2021-10-15 10:03:24 -0700

    Compress continuation tokens (#2279)

---
+++ src/Microsoft.Health.Fhir.Core.UnitTests/Features/Search/ContinuationTokenConverterTests.cs
@@ -0,0 +1,59 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Text;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Core.Features.Search;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Search
+{
+    public class ContinuationTokenConverterTests
+    {
+        [Fact]
+        public void GivenAString_WhenEcodingAndDecoding_ThenOriginalStringIsPreserved()
+        {
+            var data = Guid.NewGuid().ToString();
+
+            var encoded = ContinuationTokenConverter.Encode(data);
+            var decoded = ContinuationTokenConverter.Decode(encoded);
+
+            Assert.Equal(data, decoded);
+        }
+
+        [Fact]
+        public void GivenAnOldStringInBase64_WhenDecoding_ThenOriginalStringIsPreserved()
+        {
+            var data = Guid.NewGuid().ToString();
+
+            var encodedPrevious = Convert.ToBase64String(Encoding.UTF8.GetBytes(data));
+
+            var decoded = ContinuationTokenConverter.Decode(encodedPrevious);
+
+            Assert.Equal(data, decoded);
+        }
+
+        [Fact]
+        public void GivenAnInvalidString_WhenDecoding_ThenAnErrorIsThrown()
+        {
+            var data = Guid.NewGuid().ToString();
+
+            var encodedPrevious = Convert.ToBase64String(Encoding.UTF8.GetBytes(data)).Insert(5, "aaaafffff");
+
+            Assert.Throws<BadRequestException>(() => ContinuationTokenConverter.Decode(encodedPrevious));
+        }
+
+        [Fact]
+        public void GivenShortBase64WhenDecoding_ThenCorrectValueIsReturned()
+        {
+            var data = "YWJj";
+
+            var decoded = ContinuationTokenConverter.Decode(data);
+
+            Assert.Equal("abc", decoded);
+        }
+    }
+}
