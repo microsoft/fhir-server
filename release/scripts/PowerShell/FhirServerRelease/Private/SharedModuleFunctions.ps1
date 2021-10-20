23b7ee05c9c263658de846698b1768021b466403
LTA-Thinking <rbrucej@gmail.com>
2021-10-20 06:09:38 -0700

    Change service audience

--- release/scripts/PowerShell/FhirServerRelease/Private/SharedModuleFunctions.ps1
+++ release/scripts/PowerShell/FhirServerRelease/Private/SharedModuleFunctions.ps1
@@ -60,7 +60,7 @@ function Get-ServiceAudience {
     # AppId Uri in single tenant applications will require use of default scheme or verified domains
     # It needs to be in one of the many formats mentioned in https://docs.microsoft.com/en-us/azure/active-directory/develop/reference-breaking-changes
     # We use the format api://<tenantId>/<string>
-    return "api://$TenantId/$ServiceName"
+    return "https://$ServiceName.$TenantId"
 }
 
 function Get-UserId { 
