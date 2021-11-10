51d3437bc1162754f621b7c2762a078daae8a717
apurvabhale <apurvabhale@microsoft.com>
2021-11-09 23:07:25 -0800

    Removed some extra files

---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Scripts/TransactionCheck.sql
@@ -0,0 +1,11 @@
+﻿/***********************************************************************
+ NOTE: just checking first object, since this is run in transaction
+***************************************************************************/
+IF EXISTS (
+    SELECT *
+    FROM sys.tables
+    WHERE name = 'Instance')
+BEGIN
+    ROLLBACK TRANSACTION
+    RETURN
+END
