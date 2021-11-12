17812062ffdf373901ab55bd6f20b99edb1d5fc7
Batjargal Bayarsaikhan (Zen3 Infosolutions America Inc) <v-batjargalb@microsoft.com>
2021-11-11 18:04:26 -0800

    Adds comments on RemovePartitionFromResourceChanges_2.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/21.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/21.diff.sql
@@ -5,6 +5,10 @@
 --     Switches a partition from a resource change data table to a staging table.
 --     Then merges two partitions based on a given partition boundary value.
 --     After that, truncates the staging table to purge the old resource change data.
+--     The RANGE RIGHT partition is used on ResourceChangeData table.
+--     For the sliding window scenario, if you want to drop the left-most partition,
+--     a partitionBoundaryToMerge argument should be the lowest partition boundary value and
+--     a partitionNumberToSwtichOut argument should be 2 assuming that partition 1 is always empty.
 --
 --     @partitionNumberToSwtichOut
 --         * The partition number to switch out.
