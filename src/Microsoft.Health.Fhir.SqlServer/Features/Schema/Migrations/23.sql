92a6e05fbe8972c3dd7dedf25f30828bc27aa367
Batjargal Bayarsaikhan (Zen3 Infosolutions America Inc) <v-batjargalb@microsoft.com>
2021-12-01 20:05:51 -0800

    Used CTE expression.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/23.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/23.sql
@@ -1625,45 +1625,48 @@ CREATE PROCEDURE dbo.FetchResourceChanges_3
 AS
 BEGIN
     SET NOCOUNT ON;
-    DECLARE @partitions TABLE (
-        partitionBoundary DATETIME2 (7));
+    IF (@lastProcessedUtcDateTime IS NULL)
+        BEGIN
+            SET @lastProcessedUtcDateTime = CONVERT (DATETIME2 (7), N'1970-01-01T00:00:00.0000000');
+        END
     DECLARE @precedingPartitionBoundary AS DATETIME2 (7) = (SELECT   TOP (1) CAST (prv.value AS DATETIME2 (7)) AS value
                                                             FROM     sys.partition_range_values AS prv
                                                                      INNER JOIN
                                                                      sys.partition_functions AS pf
                                                                      ON pf.function_id = prv.function_id
                                                             WHERE    pf.name = N'PartitionFunction_ResourceChangeData_Timestamp'
+                                                                     AND SQL_VARIANT_PROPERTY(prv.Value, 'BaseType') = 'datetime2'
                                                                      AND CAST (prv.value AS DATETIME2 (7)) < DATEADD(HOUR, DATEDIFF(HOUR, 0, @lastProcessedUtcDateTime), 0)
                                                             ORDER BY prv.boundary_id DESC);
     IF (@precedingPartitionBoundary IS NULL)
         BEGIN
-            SET @precedingPartitionBoundary = CONVERT (DATETIME2 (7), N'1970-01-01T00:00:00.0000000');
+            SET @precedingPartitionBoundary = DATEADD(HOUR, DATEDIFF(HOUR, 0, @lastProcessedUtcDateTime) - 1, 0);
         END
-    INSERT INTO @partitions
-    SELECT CAST (prv.value AS DATETIME2 (7))
-    FROM   sys.partition_range_values AS prv
-           INNER JOIN
-           sys.partition_functions AS pf
-           ON pf.function_id = prv.function_id
-    WHERE  pf.name = N'PartitionFunction_ResourceChangeData_Timestamp'
-           AND CAST (prv.value AS DATETIME2 (7)) >= DATEADD(HOUR, DATEDIFF(HOUR, 0, @precedingPartitionBoundary), 0)
-           AND CAST (prv.value AS DATETIME2 (7)) < DATEADD(HOUR, 1, SYSUTCDATETIME());
+    WITH     PartitionBoundaries
+    AS       (SELECT CAST (prv.value AS DATETIME2 (7)) AS PartitionBoundary
+              FROM   sys.partition_range_values AS prv
+                     INNER JOIN
+                     sys.partition_functions AS pf
+                     ON pf.function_id = prv.function_id
+              WHERE  pf.name = N'PartitionFunction_ResourceChangeData_Timestamp'
+                     AND SQL_VARIANT_PROPERTY(prv.Value, 'BaseType') = 'datetime2'
+                     AND CAST (prv.value AS DATETIME2 (7)) BETWEEN @precedingPartitionBoundary AND DATEADD(HOUR, 1, SYSUTCDATETIME()))
     SELECT   TOP (@pageSize) Id,
                              Timestamp,
                              ResourceId,
                              ResourceTypeId,
                              ResourceVersion,
                              ResourceChangeTypeId
-    FROM     @partitions AS p CROSS APPLY (SELECT   TOP (@pageSize) Id,
-                                                                    Timestamp,
-                                                                    ResourceId,
-                                                                    ResourceTypeId,
-                                                                    ResourceVersion,
-                                                                    ResourceChangeTypeId
-                                           FROM     dbo.ResourceChangeData WITH (REPEATABLEREAD)
-                                           WHERE    Id >= @startId
-                                                    AND $PARTITION.PartitionFunction_ResourceChangeData_Timestamp (Timestamp) = $PARTITION.PartitionFunction_ResourceChangeData_Timestamp (p.partitionBoundary)
-                                           ORDER BY Id ASC) AS rcd
+    FROM     PartitionBoundaries AS p CROSS APPLY (SELECT   TOP (@pageSize) Id,
+                                                                            Timestamp,
+                                                                            ResourceId,
+                                                                            ResourceTypeId,
+                                                                            ResourceVersion,
+                                                                            ResourceChangeTypeId
+                                                   FROM     dbo.ResourceChangeData WITH (REPEATABLEREAD)
+                                                   WHERE    Id >= @startId
+                                                            AND $PARTITION.PartitionFunction_ResourceChangeData_Timestamp (Timestamp) = $PARTITION.PartitionFunction_ResourceChangeData_Timestamp (p.PartitionBoundary)
+                                                   ORDER BY Id ASC) AS rcd
     ORDER BY rcd.Id ASC;
 END
 
--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/FetchResourceChanges_3.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/FetchResourceChanges_3.sql
@@ -25,23 +25,24 @@ BEGIN
 
     SET NOCOUNT ON;
 
-    DECLARE @partitions TABLE (partitionBoundary datetime2(7));
-    DECLARE @precedingPartitionBoundary datetime2(7) = (SELECT TOP(1) CAST(prv.value as datetime2(7)) AS value FROM sys.partition_range_values AS prv
-                                                           INNER JOIN sys.partition_functions AS pf ON pf.function_id = prv.function_id
-                                                       WHERE pf.name = N'PartitionFunction_ResourceChangeData_Timestamp'
-                                                           AND CAST(prv.value AS datetime2(7)) < DATEADD(HOUR, DATEDIFF(HOUR, 0, @lastProcessedUtcDateTime), 0)
-                                                       ORDER BY prv.boundary_id DESC);
-
-    IF (@precedingPartitionBoundary IS NULL) BEGIN
-        SET @precedingPartitionBoundary = CONVERT(datetime2(7), N'1970-01-01T00:00:00.0000000');
+    IF (@lastProcessedUtcDateTime IS NULL) BEGIN
+        /* Sets the partition anchor datetime as the last processed DateTime. */
+        SET @lastProcessedUtcDateTime = CONVERT(datetime2(7), N'1970-01-01T00:00:00.0000000');
     END;
 
-    INSERT INTO @partitions 
-        SELECT CAST(prv.value AS datetime2(7)) FROM sys.partition_range_values AS prv
-            INNER JOIN sys.partition_functions AS pf ON pf.function_id = prv.function_id
-        WHERE pf.name = N'PartitionFunction_ResourceChangeData_Timestamp'
-              AND CAST(prv.value AS datetime2(7)) >= DATEADD(HOUR, DATEDIFF(HOUR, 0, @precedingPartitionBoundary), 0)
-              AND CAST(prv.value AS datetime2(7)) < DATEADD(HOUR, 1, SYSUTCDATETIME());
+    /* Finds the prior partition to the current partition where the last processed watermark lies. It is a normal scenario when a prior watermark exists. */
+    DECLARE @precedingPartitionBoundary datetime2(7) = (SELECT TOP(1) CAST(prv.value as datetime2(7)) AS value FROM sys.partition_range_values AS prv
+                                                               INNER JOIN sys.partition_functions AS pf ON pf.function_id = prv.function_id
+                                                           WHERE pf.name = N'PartitionFunction_ResourceChangeData_Timestamp'
+                                                               AND SQL_VARIANT_PROPERTY(prv.Value, 'BaseType') = 'datetime2'
+                                                               AND CAST(prv.value AS datetime2(7)) < DATEADD(HOUR, DATEDIFF(HOUR, 0, @lastProcessedUtcDateTime), 0)
+                                                           ORDER BY prv.boundary_id DESC);
+
+    IF (@precedingPartitionBoundary IS NULL) BEGIN
+        /* It happens when no prior watermark exists or the last processed datetime is older than the last retention datetime.
+           Substracts one hour from the rounded DateTime to include a partition where the watermark lies, in the partition boundary filter below. */
+        SET @precedingPartitionBoundary = DATEADD(HOUR, DATEDIFF(HOUR, 0, @lastProcessedUtcDateTime) - 1, 0);
+    END;
 
     /* Given the fact that Read Committed Snapshot isolation level is enabled on the FHIR database, 
        using the Repeatable Read isolation level table hint to avoid skipping resource changes 
@@ -52,22 +53,31 @@ BEGIN
        the search condition of the select statement will wait until the current transaction is completed. 
        Other transactions can insert new rows that match the search conditions of statements issued by the current transaction.
        But, other transactions will be blocked to insert new rows during the execution of the select query, 
-       and wait until the execution completes. */	  
+       and wait until the execution completes. */
+    WITH PartitionBoundaries
+    AS (
+        SELECT CAST(prv.value as datetime2(7)) AS PartitionBoundary FROM sys.partition_range_values AS prv
+            INNER JOIN sys.partition_functions AS pf ON pf.function_id = prv.function_id
+        WHERE pf.name = N'PartitionFunction_ResourceChangeData_Timestamp'
+                AND SQL_VARIANT_PROPERTY(prv.Value, 'BaseType') = 'datetime2'
+                AND CAST(prv.value AS datetime2(7)) BETWEEN @precedingPartitionBoundary AND DATEADD(HOUR, 1, SYSUTCDATETIME())
+    )
     SELECT TOP(@pageSize) Id,
-      Timestamp,
-      ResourceId,
-      ResourceTypeId,
-      ResourceVersion,
-      ResourceChangeTypeId
-    FROM @partitions AS p CROSS APPLY (
+        Timestamp,
+        ResourceId,
+        ResourceTypeId,
+        ResourceVersion,
+        ResourceChangeTypeId
+    FROM PartitionBoundaries AS p 
+    CROSS APPLY (
         SELECT TOP(@pageSize) Id,
-          Timestamp,
-          ResourceId,
-          ResourceTypeId,
-          ResourceVersion,
-          ResourceChangeTypeId
+            Timestamp,
+            ResourceId,
+            ResourceTypeId,
+            ResourceVersion,
+            ResourceChangeTypeId
         FROM dbo.ResourceChangeData WITH (REPEATABLEREAD)
-            WHERE Id >= @startId AND $PARTITION.PartitionFunction_ResourceChangeData_Timestamp(Timestamp) = $PARTITION.PartitionFunction_ResourceChangeData_Timestamp(p.partitionBoundary)
+            WHERE Id >= @startId AND $PARTITION.PartitionFunction_ResourceChangeData_Timestamp(Timestamp) = $PARTITION.PartitionFunction_ResourceChangeData_Timestamp(p.PartitionBoundary)
         ORDER BY Id ASC
         ) AS rcd
     ORDER BY rcd.Id ASC;
