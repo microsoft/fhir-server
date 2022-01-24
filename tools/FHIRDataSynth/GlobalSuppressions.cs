6dad962a325b7683722ec44254f23fbb67b2fd51
Tom Markoc <v-tommarkoc@microsoft.com>
2022-01-24 01:25:42 -0800

    new line at eof

--- tools/FHIRDataSynth/GlobalSuppressions.cs
+++ tools/FHIRDataSynth/GlobalSuppressions.cs
@@ -23,17 +23,14 @@ using System.Diagnostics.CodeAnalysis;
 // [assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1502:Element should not be on a single line", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1005:Single line comments should begin with single space", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1129:Do not use default value type constructor", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1002:Semicolons should be spaced correctly", Justification = "<PenTODOding>", Scope = "module")]
 [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1401:Fields should be private", Justification = "<TODO>", Scope = "module")]
 [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1201:Elements should appear in the correct order", Justification = "<TODO>", Scope = "module")]
 
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1008:Opening parenthesis should be spaced correctly", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1206:Declaration keywords should follow order", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1009:Closing parenthesis should be spaced correctly", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1303:Const field names should begin with upper-case letter", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1028:Code should not contain trailing whitespace", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1119:Statement should not use unnecessary parenthesis", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1025:Code should not contain multiple whitespace in a row", Justification = "<TODO>", Scope = "module")]
@@ -44,12 +41,10 @@ using System.Diagnostics.CodeAnalysis;
 // [assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1413:Use trailing comma in multi-line initializers", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1314:Type parameter names should begin with T", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1128:Put constructor initializers on their own line", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1127:Generic type constraints should be on their own line", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1122:Use string.Empty for empty strings", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1121:Use built-in type alias", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1117:Parameters should be on same line or separate lines", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.ReadabilityRules", "SA1107:Code should not contain multiple statements on one line", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("Design", "CA1056:URI-like properties should not be strings", Justification = "<TODO>", Scope = "module")]
@@ -58,15 +53,12 @@ using System.Diagnostics.CodeAnalysis;
 
 // [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("Style", "IDE0090:Use 'new(...)'", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("Style", "IDE0007:Use implicit type", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("Performance", "CA1805:Do not initialize unnecessarily", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("Globalization", "CA1310:Specify StringComparison for correctness", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("Style", "IDE0057:Use range operator", Justification = "<TODO>", Scope = "module")]
-
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1003:Symbols should be spaced correctly", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1024:Colons Should Be Spaced Correctly", Justification = "<TODO>", Scope = "module")]
 // [assembly: SuppressMessage("Style", "IDE0034:Simplify 'default' expression", Justification = "<TODO>", Scope = "module")]
