ce64a4b5beadb74442b7195af4729084c43f4427
Sergey Galuzo <sergal@microsoft.com>
2022-03-02 11:29:03 -0800

    +1

--- tools/Import/Program.cs
+++ tools/Import/Program.cs
@@ -4,13 +4,27 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
+using System.Configuration;
+using System.Diagnostics;
 
-namespace Import
+namespace IndexRebuilder
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+        private static readonly int Threads = int.Parse(ConfigurationManager.AppSettings["Threads"]);
+        private static readonly bool RebuildClustered = bool.Parse(ConfigurationManager.AppSettings["RebuildClustered"]);
+        private static readonly string EventLogQuery = ConfigurationManager.AppSettings["EventLogQuery"];
+
+        public static void Main()
         {
+            Console.WriteLine($"IndexRebuilder.Start: Store={StoreUtils.ShowConnectionString(ConnectionString)} Threads={Threads} at {DateTime.UtcNow:s}");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine($"To monitor progress please run in the target database: {EventLogQuery}");
+
+            //var indexRebuilder = new IndexRebuilder(ConnectionString, Threads, RebuildClustered);
+            //indexRebuilder.Run(out var cancel, out var tables);
+            //Console.WriteLine($"IndexRebuilder.{(cancel.IsSet ? "FAILED" : "End")}: Store={StoreUtils.ShowConnectionString(ConnectionString)} Threads={Threads} Tables={tables} at {DateTime.Now:s} elapsed={sw.Elapsed.TotalSeconds:N0} sec.");
         }
     }
 }
