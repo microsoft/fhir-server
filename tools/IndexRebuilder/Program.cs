3b59701289df4c54278b13fd31c64e304cf1f0c2
Sergey Galuzo <sergal@microsoft.com>
2022-03-12 19:01:03 -0800

    utils

--- tools/IndexRebuilder/Program.cs
+++ tools/IndexRebuilder/Program.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Configuration;
 using System.Diagnostics;
+using Microsoft.Health.Fhir.Store.SqlUtils;
 
 namespace Microsoft.Health.Fhir.IndexRebuilder
 {
@@ -15,16 +16,17 @@ namespace Microsoft.Health.Fhir.IndexRebuilder
         private static readonly int Threads = int.Parse(ConfigurationManager.AppSettings["Threads"]);
         private static readonly bool RebuildClustered = bool.Parse(ConfigurationManager.AppSettings["RebuildClustered"]);
         private static readonly string EventLogQuery = ConfigurationManager.AppSettings["EventLogQuery"];
+        private static readonly SqlService Store = new SqlService(ConnectionString);
 
         public static void Main()
         {
-            Console.WriteLine($"IndexRebuilder.Start: Store={StoreUtils.ShowConnectionString(ConnectionString)} Threads={Threads} at {DateTime.UtcNow:s}");
+            Console.WriteLine($"IndexRebuilder.Start: Store={Store.ShowConnectionString()} Threads={Threads} at {DateTime.UtcNow:s}");
             var sw = Stopwatch.StartNew();
             Console.WriteLine($"To monitor progress please run in the target database: {EventLogQuery}");
 
             var indexRebuilder = new IndexRebuilder(ConnectionString, Threads, RebuildClustered);
             indexRebuilder.Run(out var cancel, out var tables);
-            Console.WriteLine($"IndexRebuilder.{(cancel.IsSet ? "FAILED" : "End")}: Store={StoreUtils.ShowConnectionString(ConnectionString)} Threads={Threads} Tables={tables} at {DateTime.Now:s} elapsed={sw.Elapsed.TotalSeconds:N0} sec.");
+            Console.WriteLine($"IndexRebuilder.{(cancel.IsSet ? "FAILED" : "End")}: Store={Store.ShowConnectionString()} Threads={Threads} Tables={tables} at {DateTime.Now:s} elapsed={sw.Elapsed.TotalSeconds:N0} sec.");
         }
     }
 }
