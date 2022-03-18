0eef4aa5565db7988abe20107847e15c3d78ecbd
Brendan Kowitz <bkowitz@microsoft.com>
2022-03-18 11:17:54 -0700

    Allows Composite custom search parameters (#2502)

--- src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Search/SearchParameters/SearchParameterDefinitionManagerTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Search/SearchParameters/SearchParameterDefinitionManagerTests.cs
@@ -325,6 +325,55 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Search
         }
 
         [Fact]
+        public async Task GivenASearchParameterDefinitionManager_WhenAddingACompositeSearchParameter_ThenComponentsAreResolvedAndValidated()
+        {
+            #if Stu3
+            ResourceReference CreateDefinition(string reference)
+            {
+                return new ResourceReference(reference);
+            }
+            #else
+            string CreateDefinition(string reference)
+            {
+                return reference;
+            }
+            #endif
+
+            // Initialize a search parameter
+            var searchParam = new SearchParameter()
+            {
+                Url = "http://test/Patient-test",
+                Type = Hl7.Fhir.Model.SearchParamType.Composite,
+                Base = new List<ResourceType?>() { ResourceType.Patient },
+                Expression = "Patient",
+                Name = "test",
+                Code = "test",
+                Component = new List<SearchParameter.ComponentComponent>
+                {
+                    new()
+                    {
+                        Definition = CreateDefinition("http://hl7.org/fhir/SearchParameter/Resource-id"),
+                        Expression = "id",
+                    },
+                    new()
+                    {
+                        Definition = CreateDefinition("http://hl7.org/fhir/SearchParameter/Resource-lastUpdated"),
+                        Expression = "meta.lastUpdated",
+                    },
+                },
+            };
+
+            _searchParameterSupportResolver
+                .IsSearchParameterSupported(Arg.Is<SearchParameterInfo>(p => p.Name == "test" && p.Component.All(c => c.ResolvedSearchParameter != null)))
+                .Returns((true, false));
+
+            await _searchParameterOperations.AddSearchParameterAsync(searchParam.ToTypedElement(), CancellationToken.None);
+
+            var addedParam = _searchParameterDefinitionManager.GetSearchParameter("http://test/Patient-test");
+            Assert.NotNull(addedParam);
+        }
+
+        [Fact]
         public void GivenASearchParameterDefinitionManager_WhenGettingSearchParameterHashForMissingResourceType_ThenNullIsReturned()
         {
             var searchParamHash = _searchParameterDefinitionManager.GetSearchParameterHashForResourceType("Foo");
