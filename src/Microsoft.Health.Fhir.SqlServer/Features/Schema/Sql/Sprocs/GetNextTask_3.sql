ad0102950884b9f7a9f786e42d4f0ebe0864e0f7
Peiyuan Zhou <peizhou@microsoft.com>
2022-03-21 18:59:10 +0800

    remove getnexttask.sql duplicate completeTask SP

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/GetNextTask_3.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/GetNextTask_3.sql
@@ -13,15 +13,14 @@
 --         * The ID of the task record
 --     @taskHeartbeatTimeoutThresholdInSeconds
 --         * Timeout threshold in seconds for heart keep alive
-
-GO
-CREATE OR ALTER PROCEDURE [dbo].[GetNextTask_3]
+--
+CREATE PROCEDURE [dbo].[GetNextTask_3]
 @queueId VARCHAR (64), @taskHeartbeatTimeoutThresholdInSeconds INT=600
 AS
 
 SET NOCOUNT ON;
-DECLARE @Lock VARCHAR(200) = 'GetNextTask_Q='+@queueId
-        ,@TaskId VARCHAR (64) = NULL
+DECLARE @lock VARCHAR(200) = 'GetNextTask_Q='+@queueId
+        ,@taskId VARCHAR (64) = NULL
         ,@expirationDateTime AS DATETIME2 (7)
         ,@heartbeatDateTime AS DATETIME2 (7) = SYSUTCDATETIME();
 SELECT @expirationDateTime = DATEADD(second, -@taskHeartbeatTimeoutThresholdInSeconds, SYSUTCDATETIME());
@@ -29,91 +28,54 @@ SELECT @expirationDateTime = DATEADD(second, -@taskHeartbeatTimeoutThresholdInSe
 BEGIN TRY
     BEGIN TRANSACTION
 
-    EXECUTE sp_getapplock @Lock, 'Exclusive'
+    EXECUTE sp_getapplock @lock, 'Exclusive'
 
 -- try new tasks first
     UPDATE T
       SET Status = 2 -- running
          ,StartDateTime = SYSUTCDATETIME()
          ,HeartbeatDateTime = SYSUTCDATETIME()
-         ,Worker = host_name() 
-         ,@TaskId = T.TaskId
+         ,Worker = host_name()
+         ,RunId = CAST (NEWID() AS NVARCHAR (50)) 
+         ,@taskId = T.TaskId
       FROM dbo.TaskInfo T WITH (PAGLOCK)
            JOIN (SELECT TOP 1 
                         TaskId
                    FROM dbo.TaskInfo WITH (INDEX = IX_Status_QueueId)
-                   WHERE QueueId = @QueueId
+                   WHERE QueueId = @queueId
                      AND Status = 1 -- Created
                    ORDER BY 
                         TaskId
                 ) S
-             ON T.QueueId = @QueueId AND T.TaskId = S.TaskId 
+             ON T.QueueId = @queueId AND T.TaskId = S.TaskId 
 
-  IF @TaskId IS NULL
+  IF @taskId IS NULL
   -- old ones now
     UPDATE T
       SET StartDateTime = SYSUTCDATETIME()
         ,HeartbeatDateTime = SYSUTCDATETIME()
-        ,Worker = HOST_NAME() 
-        ,@TaskId = T.TaskId
+        ,Worker = HOST_NAME()
+        ,RunId = CAST (NEWID() AS NVARCHAR (50))
+        ,@taskId = T.TaskId
         ,RestartInfo = ISNULL(RestartInfo,'')+' Prev: Worker='+Worker+' Start='+convert(varchar,SYSUTCDATETIME(),121) 
       FROM dbo.TaskInfo T WITH (PAGLOCK)
           JOIN (SELECT TOP 1 
                         TaskId
-                  FROM dbo.TaskInfo WITH (INDEX = IX_Status)
-                  WHERE QueueId = @QueueId
+                  FROM dbo.TaskInfo WITH (INDEX = IX_Status_QueueId)
+                  WHERE QueueId = @queueId
                     AND Status = 2 -- running
                     AND HeartbeatDateTime <= @expirationDateTime
                   ORDER BY 
                         TaskId
                 ) S
-            ON T.QueueId = @QueueId AND T.TaskId = S.TaskId 
+            ON T.QueueId = @queueId AND T.TaskId = S.TaskId 
 
   COMMIT TRANSACTION
 
-  EXECUTE dbo.GetTaskDetails @TaskId = @TaskId
+  EXECUTE dbo.GetTaskDetails @TaskId = @taskId
 END TRY
 BEGIN CATCH
   IF @@trancount > 0 ROLLBACK TRANSACTION
   THROW
 END CATCH
-
-GO
-
-CREATE OR ALTER PROCEDURE [dbo].[CompleteTask]
-@taskId VARCHAR (64), @taskResult VARCHAR (MAX), @runId VARCHAR (50)
-AS
-SET NOCOUNT ON;
-SET XACT_ABORT ON;
-BEGIN TRANSACTION;
-IF NOT EXISTS (SELECT *
-               FROM   [dbo].[TaskInfo]
-               WHERE  TaskId = @taskId
-                      AND RunId = @runId)
-    BEGIN
-        THROW 50404, 'Task not exist or runid not match', 1;
-    END
-DECLARE @heartbeatDateTime AS DATETIME2 (7) = SYSUTCDATETIME();
-UPDATE dbo.TaskInfo
-SET    Status            = 3,
-       HeartbeatDateTime = @heartbeatDateTime,
-       EndDateTime = SYSUTCDATETIME(),
-       Result            = @taskResult
-WHERE  TaskId = @taskId;
-SELECT TaskId,
-       QueueId,
-       Status,
-       TaskTypeId,
-       RunId,
-       IsCanceled,
-       RetryCount,
-       MaxRetryCount,
-       HeartbeatDateTime,
-       InputData,
-       TaskContext,
-       Result
-FROM   [dbo].[TaskInfo]
-WHERE  TaskId = @taskId;
-COMMIT TRANSACTION;
-
 GO
