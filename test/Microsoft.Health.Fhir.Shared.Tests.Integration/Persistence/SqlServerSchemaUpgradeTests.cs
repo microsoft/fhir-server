b898156c2d00c6b6b11103bba0c35aabaee0bb76
Peiyuan Zhou <peizhou@microsoft.com>
2022-03-24 15:15:21 +0800

    remove delete DB for CI test

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerSchemaUpgradeTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerSchemaUpgradeTests.cs
@@ -50,28 +50,21 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
 
             SqlServerFhirStorageTestHelper testHelper1 = null;
             SqlServerFhirStorageTestHelper testHelper2 = null;
-            try
-            {
-                // Create two databases, one where we apply the the maximum supported version's snapshot SQL schema file
-                (testHelper1, _) = await SetupTestHelperAndCreateDatabase(
-                    snapshotDatabaseName,
-                    SchemaVersionConstants.Max,
-                    forceIncrementalSchemaUpgrade: false);
 
-                // And one where we apply .diff.sql files to upgrade the schema version to the maximum supported version.
-                (testHelper2, _) = await SetupTestHelperAndCreateDatabase(
-                    diffDatabaseName,
-                    SchemaVersionConstants.Max,
-                    forceIncrementalSchemaUpgrade: true);
+            // Create two databases, one where we apply the the maximum supported version's snapshot SQL schema file
+            (testHelper1, _) = await SetupTestHelperAndCreateDatabase(
+                snapshotDatabaseName,
+                SchemaVersionConstants.Max,
+                forceIncrementalSchemaUpgrade: false);
 
-                bool isEqual = CompareDatabaseSchemas(snapshotDatabaseName, diffDatabaseName);
-                Assert.True(isEqual);
-            }
-            finally
-            {
-                await testHelper1.DeleteDatabase(snapshotDatabaseName);
-                await testHelper2.DeleteDatabase(diffDatabaseName);
-            }
+            // And one where we apply .diff.sql files to upgrade the schema version to the maximum supported version.
+            (testHelper2, _) = await SetupTestHelperAndCreateDatabase(
+                diffDatabaseName,
+                SchemaVersionConstants.Max,
+                forceIncrementalSchemaUpgrade: true);
+
+            bool isEqual = CompareDatabaseSchemas(snapshotDatabaseName, diffDatabaseName);
+            Assert.True(isEqual);
         }
 
         [Theory]
