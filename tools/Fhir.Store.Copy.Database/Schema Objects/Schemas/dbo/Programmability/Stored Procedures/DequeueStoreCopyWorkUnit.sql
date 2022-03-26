7b2f6d5dc2631b3868016a073b051cfec1251193
Sergey Galuzo <sergal@microsoft.com>
2022-03-26 14:01:34 -0700

    timeout

--- tools/Fhir.Store.Copy.Database/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DequeueStoreCopyWorkUnit.sql
+++ tools/Fhir.Store.Copy.Database/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/DequeueStoreCopyWorkUnit.sql
@@ -18,15 +18,16 @@ DECLARE @SP varchar(100) = 'DequeueStoreCopyWorkUnit'
        ,@PartitionId tinyint = @StartPartitionId
        ,@MaxPartitions tinyint = 16 -- !!! hardcoded
        ,@LookedAtPartitions tinyint = 0
+       ,@HeartbeatTimeout int
 
 BEGIN TRY
+  SET TRANSACTION ISOLATION LEVEL READ COMMITTED 
+
   IF @Stop = 0
   BEGIN
     WHILE @UnitId IS NULL AND @LookedAtPartitions <= @MaxPartitions
     BEGIN
       SET @Lock = 'DequeueStoreCopyWorkUnit_'+convert(varchar, @PartitionId)
-
-      SET TRANSACTION ISOLATION LEVEL READ COMMITTED 
 
       BEGIN TRANSACTION  
 
@@ -45,6 +46,47 @@ BEGIN TRY
                      FROM dbo.StoreCopyWorkQueue WITH (INDEX = IX_Status_PartitionId)
                      WHERE PartitionId = @PartitionId
                        AND Status = 0
+                     ORDER BY 
+                          UnitId
+                  ) S
+               ON PartitionId = @PartitionId AND T.UnitId = S.UnitId
+      SET @Rows = @@rowcount
+
+      COMMIT TRANSACTION
+
+      IF @UnitId IS NULL
+      BEGIN
+        SET @PartitionId = CASE WHEN @PartitionId = 15 THEN 0 ELSE @PartitionId + 1 END
+        SET @LookedAtPartitions = @LookedAtPartitions + 1 
+      END
+    END
+
+    -- Do timed out items. Logic is not looking at heartbeat time, because this logic is not plugged in 
+    SET @HeartbeatTimeout = isnull((SELECT Number FROM dbo.Parameters WHERE Id = 'StoreCopy.HeartbeatTimeoutSec'),3600)
+    SET @LookedAtPartitions = 0
+    WHILE @UnitId IS NULL AND @LookedAtPartitions <= @MaxPartitions
+    BEGIN
+      SET @Lock = 'DequeueStoreCopyWorkUnit_'+convert(varchar, @PartitionId)
+
+      BEGIN TRANSACTION  
+
+      EXECUTE sp_getapplock @Lock, 'Exclusive'
+
+      UPDATE T
+        SET StartDate = getUTCdate()
+           ,HeartBeatDate = getUTCdate()
+           ,Worker = @Worker 
+           ,Status = 1 -- running
+           ,@ResourceTypeId = T.ResourceTypeId
+           ,@UnitId = T.UnitId
+           ,Info = isnull(Info,'')+' Prev: Worker='+Worker+' Start='+convert(varchar,StartDate,121)
+        FROM dbo.StoreCopyWorkQueue T WITH (PAGLOCK)
+             JOIN (SELECT TOP 1 
+                          UnitId
+                     FROM dbo.StoreCopyWorkQueue WITH (INDEX = IX_Status_PartitionId)
+                     WHERE PartitionId = @PartitionId
+                       AND Status = 1
+                       AND datediff(second,StartDate,getUTCdate()) > @HeartbeatTimeout
                      ORDER BY 
                           UnitId
                   ) S
