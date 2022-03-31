81a7565d7c71413d5beebf7bc3f0caf14d6d8d6e
apurvabhale <apurvabhale@microsoft.com>
2022-03-31 09:32:32 -0700

    Few more updates to test and test files

--- test/Microsoft.Health.Fhir.R5.Tests.E2E/Rest/VersionSpecificTests.cs
+++ test/Microsoft.Health.Fhir.R5.Tests.E2E/Rest/VersionSpecificTests.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest
         [Fact]
         public async Task GivenR5Server_WhenCapabilityStatementIsRetrieved_ThenCorrectVersionShouldBeReturned()
         {
-            await TestCapabilityStatementFhirVersion("4.6.0");
+            await TestCapabilityStatementFhirVersion("5.0.0-snapshot1");
         }
 
         [Fact]
