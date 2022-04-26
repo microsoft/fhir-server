777cab414d0062518ebfe923bc097eca84aeb499
Jared Erwin <jaerwin@microsoft.com>
2022-04-26 16:14:24 -0700

    Check if constraint exists

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/31.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/31.diff.sql
@@ -2,6 +2,10 @@
     This migration adds new constraing on data length of rawresource
 **************************************************************/
 
+IF  EXISTS (SELECT * FROM sys.objects WHERE name = N'RawResourceLength' AND type = N'C')
+    ALTER TABLE dbo.Resource DROP CONSTRAINT RawResourceLength
+GO
+
 ALTER TABLE dbo.Resource WITH NOCHECK
     ADD CONSTRAINT RawResourceLength CHECK (DATALENGTH(RawResource) > 49)
 
