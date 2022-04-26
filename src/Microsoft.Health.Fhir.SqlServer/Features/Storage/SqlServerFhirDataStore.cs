a6a7ac782dd26bfe8008fe101c6af1b560a3c048
Matt Serdar <99214729+v-mserdar@users.noreply.github.com>
2022-04-26 15:29:56 -0700

    Adding centralized sqlexception handler

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirDataStore.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirDataStore.cs
@@ -250,8 +250,6 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
                     {
                         switch (e.Number)
                         {
-                            case SqlErrorCodes.TimeoutExpired:
-                                throw new RequestTimeoutException(Resources.ExecutionTimeoutExpired);
                             case SqlErrorCodes.Conflict:
                                 // someone else beat us to it, re-read and try comparing again - Compared resource was updated
                                 continue;
