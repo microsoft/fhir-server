13904343f3c6b8df73a53614dbba68826faaa9df
Sergey Galuzo <sergal@microsoft.com>
2022-04-27 16:20:31 -0700

    V0

---
+++ tools/Exporter/Program.cs
@@ -0,0 +1,309 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using Microsoft.Health.Fhir.SqlServer.Database;
+using Microsoft.Health.Fhir.SqlServer.Features.Storage;
+using Microsoft.Health.Fhir.Store.Utils;
+
+namespace Microsoft.Health.Fhir.Store.Export
+{
+    public static class Program
+    {
+        private static readonly string SourceConnectionString = ConfigurationManager.ConnectionStrings["SourceDatabase"].ConnectionString;
+        private static readonly string QueueConnectionString = ConfigurationManager.ConnectionStrings["QueueDatabase"].ConnectionString;
+        private static readonly string BlobConnectionString = ConfigurationManager.AppSettings["BlobConnectionString"];
+        private static readonly string BlobContainerName = ConfigurationManager.AppSettings["BlobContainerName"];
+        private static readonly int Threads = int.Parse(ConfigurationManager.AppSettings["Threads"]);
+        private static readonly string ResourceType = ConfigurationManager.AppSettings["ResourceType"];
+        private static readonly int UnitSize = int.Parse(ConfigurationManager.AppSettings["UnitSize"]);
+        private static readonly int MaxRetries = int.Parse(ConfigurationManager.AppSettings["MaxRetries"]);
+        private static readonly bool ReadsEnabled = bool.Parse(ConfigurationManager.AppSettings["ReadsEnabled"]);
+        private static readonly bool WritesEnabled = bool.Parse(ConfigurationManager.AppSettings["WritesEnabled"]);
+        private static readonly bool DecompressEnabled = bool.Parse(ConfigurationManager.AppSettings["DecompressEnabled"]);
+        private static readonly bool RebuildWorkQueue = bool.Parse(ConfigurationManager.AppSettings["RebuildWorkQueue"]);
+        private static readonly int ReportingPeriodSec = int.Parse(ConfigurationManager.AppSettings["ReportingPeriodSec"]);
+        private static readonly DateTime StartDate = DateTime.Parse(ConfigurationManager.AppSettings["StartDate"]);
+        private static readonly DateTime EndDate = DateTime.Parse(ConfigurationManager.AppSettings["EndDate"]);
+        private static readonly SqlService Source = new SqlService(SourceConnectionString);
+        private static readonly SqlService Queue = new SqlService(QueueConnectionString);
+        private static bool stop = false;
+        private static CompressedRawResourceConverter _compressedRawResourceConverter = new CompressedRawResourceConverter();
+        private static long _resourcesTotal = 0L;
+        private static Stopwatch _swReport = Stopwatch.StartNew();
+        private static Stopwatch _sw = Stopwatch.StartNew();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Source=[{Source.ShowConnectionString()}]");
+            Console.WriteLine($"Queue=[{Queue.ShowConnectionString()}]");
+            var method = args.Length > 0 ? args[0] : "na";
+            if (method == "setupdb")
+            {
+                SetupDb.Publish(QueueConnectionString, "Microsoft.Health.Fhir.SqlServer.Database.dacpac");
+                SetupDb.Publish(QueueConnectionString, "Fhir.Store.Copy.Database.dacpac");
+            }
+            else
+            {
+                if (RebuildWorkQueue)
+                {
+                    PopulateJobQueue(ResourceType, UnitSize);
+                }
+
+                Export();
+            }
+        }
+
+        public static void Export()
+        {
+            var tasks = new List<Task>();
+            for (var i = 0; i < Threads; i++)
+            {
+                var thread = i;
+                tasks.Add(BatchExtensions.StartTask(() =>
+                {
+                    Export(thread);
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"Export.{ResourceType}.threads={Threads}: {(stop ? "FAILED" : "completed")} at {DateTime.Now:s}, resources={_resourcesTotal} speed={_resourcesTotal / _sw.Elapsed.TotalSeconds:N0} resources/sec elapsed={_sw.Elapsed.TotalSeconds:N0} sec.");
+        }
+
+        private static void Export(int thread)
+        {
+            Console.WriteLine($"Export.{thread}: started at {DateTime.UtcNow:s}");
+            var resourceTypeId = (short?)0;
+            while (resourceTypeId.HasValue && !stop)
+            {
+                string minId = null;
+                string maxId = null;
+                var unitId = 0L;
+                var version = 0L;
+                var retries = 0;
+                var maxRetries = MaxRetries;
+retry:
+                try
+                {
+                    Queue.DequeueJob(null, out var _, out unitId, out version, out resourceTypeId, out minId, out maxId);
+                    if (resourceTypeId.HasValue)
+                    {
+                        var container = GetContainer(BlobConnectionString, BlobContainerName);
+                        var resources = Export(resourceTypeId.Value, container, long.Parse(minId), long.Parse(maxId));
+                        Interlocked.Add(ref _resourcesTotal, resources);
+                        Queue.CompleteJob(unitId, false, version, resources);
+                    }
+
+                    if (_swReport.Elapsed.TotalSeconds > ReportingPeriodSec)
+                    {
+                        lock (_swReport)
+                        {
+                            if (_swReport.Elapsed.TotalSeconds > ReportingPeriodSec)
+                            {
+                                Console.WriteLine($"Export.{ResourceType}.threads={Threads}.Writes={WritesEnabled}.Decompress={DecompressEnabled}.Reads={ReadsEnabled}: Resources={_resourcesTotal} secs={(int)_sw.Elapsed.TotalSeconds} speed={(int)(_resourcesTotal / _sw.Elapsed.TotalSeconds)} resources/sec");
+                                _swReport.Restart();
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Export.{ResourceType}.{thread}.{minId}.{maxId}: error={e}");
+                    Queue.LogEvent($"Export", "Error", $"{ResourceType}.{thread}.{minId}.{maxId}", text: e.ToString());
+                    retries++;
+                    var isRetryable = e.IsRetryable();
+                    if (isRetryable)
+                    {
+                        maxRetries++;
+                    }
+
+                    if (retries < maxRetries)
+                    {
+                        Thread.Sleep(isRetryable ? 1000 : 200 * retries);
+                        goto retry;
+                    }
+
+                    stop = true;
+                    if (resourceTypeId.HasValue)
+                    {
+                        Queue.CompleteJob(unitId, true, version);
+                    }
+
+                    throw;
+                }
+            }
+
+            ////Console.WriteLine($"Export.{ResourceType}.{thread}: {(stop ? "FAILED" : "completed")} at {DateTime.Now:s}, elapsed={_sw.Elapsed.TotalSeconds:N0} sec.");
+        }
+
+        private static int Export(short resourceTypeId, BlobContainerClient container, long minId, long maxId)
+        {
+            if (!ReadsEnabled)
+            {
+                return 0;
+            }
+
+            var resources = Source.GetData(resourceTypeId, minId, maxId).ToList(); // ToList will fource reading from SQL even when writes are disabled
+            var strings = new List<string>();
+            if (DecompressEnabled)
+            {
+                foreach (var res in resources)
+                {
+                    using var mem = new MemoryStream(res);
+                    strings.Add(_compressedRawResourceConverter.ReadCompressedRawResource(mem).Result);
+                }
+            }
+
+            if (WritesEnabled)
+            {
+                WriteBatchOfLines(container, strings, $"{ResourceType}-{minId}-{maxId}.ndjson");
+            }
+
+            return resources.Count;
+        }
+
+        private static void WriteBatchOfLines(BlobContainerClient container, IEnumerable<string> batch, string blobName)
+        {
+retry:
+            try
+            {
+                using var stream = container.GetBlockBlobClient(blobName).OpenWrite(true);
+                using var writer = new StreamWriter(stream);
+                foreach (var line in batch)
+                {
+                    writer.WriteLine(line);
+                }
+
+                writer.Flush();
+            }
+            catch (Exception e)
+            {
+                if (e.ToString().Contains("ConditionNotMet", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine(e);
+                    goto retry;
+                }
+
+                throw;
+            }
+        }
+
+        private static BlobContainerClient GetContainer(string connectionString, string containerName)
+        {
+            try
+            {
+                var blobServiceClient = new BlobServiceClient(connectionString);
+                var blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+                if (!blobContainerClient.Exists())
+                {
+                    lock (_sw) // lock on anything global
+                    {
+                        blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                        if (!blobContainerClient.Exists())
+                        {
+                            var container = blobServiceClient.CreateBlobContainer(containerName);
+                            Console.WriteLine($"Created container {container.Value.Name}");
+                        }
+                    }
+                }
+
+                return blobContainerClient;
+            }
+            catch
+            {
+                Console.WriteLine($"Unable to parse stroage reference or connect to storage account {connectionString}.");
+                throw;
+            }
+        }
+
+        private static void PopulateJobQueue(string resourceType, int unitSize)
+        {
+            using var sourceConn = new SqlConnection(Source.ConnectionString);
+            sourceConn.Open();
+            using var select = new SqlCommand(
+                @"
+SELECT convert(varchar,UnitId)
+       +';'+convert(varchar,(SELECT ResourceTypeId FROM dbo.ResourceType B WHERE B.Name = @ResourceType))
+       +';'+convert(varchar,min(ResourceSurrogateId))
+       +';'+convert(varchar,max(ResourceSurrogateId))
+       +';'+convert(varchar,count(*))
+  FROM (SELECT UnitId = isnull(convert(int, (row_number() OVER (ORDER BY ResourceSurrogateId) - 1) / @UnitSize), 0)
+              ,ResourceSurrogateId
+          FROM dbo.Resource
+          WHERE ResourceTypeId = (SELECT ResourceTypeId FROM dbo.ResourceType B WHERE B.Name = @ResourceType)
+            AND IsHistory = 0
+            AND ResourceSurrogateId BETWEEN datediff_big(millisecond,'0001-01-01',@StartDate) * 80000 AND datediff_big(millisecond,'0001-01-01',@EndDate) * 80000 + (80000 - 1)
+       ) A
+  GROUP BY
+       UnitId
+  ORDER BY
+       UnitId
+  OPTION (MAXDOP 8) -- 0 7:17 -- 1 29:14 -- 8 6:43
+                 ",
+                sourceConn)
+            { CommandTimeout = 3600 };
+            select.Parameters.AddWithValue("@UnitSize", unitSize);
+            select.Parameters.AddWithValue("@ResourceType", resourceType);
+            select.Parameters.AddWithValue("@StartDate", StartDate);
+            select.Parameters.AddWithValue("@EndDate", EndDate);
+            using var reader = select.ExecuteReader();
+            var strings = new List<string>();
+            while (reader.Read())
+            {
+                strings.Add(reader.GetString(0));
+            }
+
+            var queueConn = new SqlConnection(Queue.ConnectionString);
+            queueConn.Open();
+            using var drop = new SqlCommand("IF object_id('##StoreCopyWorkQueue') IS NOT NULL DROP TABLE ##StoreCopyWorkQueue", queueConn) { CommandTimeout = 60 };
+            drop.ExecuteNonQuery();
+            using var create = new SqlCommand("CREATE TABLE ##StoreCopyWorkQueue (String varchar(255))", queueConn) { CommandTimeout = 60 };
+            create.ExecuteNonQuery();
+
+            using var cmd = new SqlCommand("INSERT INTO ##StoreCopyWorkQueue SELECT String FROM @Strings", queueConn) { CommandTimeout = 60 };
+            var resourceIdsParam = new SqlParameter { ParameterName = "@Strings" };
+            resourceIdsParam.AddStringList(strings);
+            cmd.Parameters.Add(resourceIdsParam);
+            cmd.ExecuteNonQuery();
+
+            using var insert = new SqlCommand(
+                @"
+TRUNCATE TABLE dbo.JobQueue
+
+DECLARE @Definitions StringList
+INSERT INTO @Definitions
+  SELECT ResourceTypeId+';'+MinId+';'+MaxId+';'+ResourceCount
+    FROM (SELECT UnitId = max(CASE WHEN ordinal = 1 THEN value END)
+                ,ResourceTypeId = max(CASE WHEN ordinal = 2 THEN value END)
+                ,MinId = max(CASE WHEN ordinal = 3 THEN value END)
+                ,MaxId = max(CASE WHEN ordinal = 4 THEN value END)
+                ,ResourceCount = max(CASE WHEN ordinal = 5 THEN value END)
+            FROM ##StoreCopyWorkQueue
+                 CROSS APPLY string_split(String, ';', 1)
+            GROUP BY
+                 String
+         ) A
+
+EXECUTE dbo.EnqueueJobs @QueueType = 1, @Definitions = @Definitions
+                ",
+                queueConn)
+            { CommandTimeout = 600 };
+            insert.ExecuteNonQuery();
+
+            queueConn.Close();
+        }
+    }
+}
---
+++ tools/Exporter/SqlService.cs
@@ -0,0 +1,147 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Microsoft.Health.Fhir.Store.Export
+{
+    internal class SqlService : SqlUtils.SqlService
+    {
+        private byte _partitionId;
+        private object _partitioinLocker = new object();
+        private byte _numberOfPartitions;
+        private byte _queueType = 1;
+
+        internal SqlService(string connectionString)
+            : base(connectionString, null)
+        {
+            _numberOfPartitions = 16;
+            _partitionId = 0;
+        }
+
+        private byte GetNextPartitionId(int? thread)
+        {
+            if (thread.HasValue)
+            {
+                return (byte)(thread.Value % _numberOfPartitions);
+            }
+
+            lock (_partitioinLocker)
+            {
+                _partitionId = _partitionId == _numberOfPartitions - 1 ? (byte)0 : ++_partitionId;
+                return _partitionId;
+            }
+        }
+
+        internal bool JobQueueIsNotEmpty()
+        {
+            using var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            using var command = new SqlCommand("SELECT count(*) FROM dbo.JobQueue WHERE QueueType = @QueueType", conn) { CommandTimeout = 120 };
+            command.Parameters.AddWithValue("@QueueType", _queueType);
+            var cnt = (int)command.ExecuteScalar();
+            return cnt > 0;
+        }
+
+        internal void DequeueJob(int? thread, out long groupId, out long jobId, out long version, out string definition)
+        {
+            definition = null;
+            groupId = -1L;
+            jobId = -1L;
+            version = 0;
+
+            using var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            using var command = new SqlCommand("dbo.DequeueJob", conn) { CommandType = CommandType.StoredProcedure, CommandTimeout = 120 };
+            command.Parameters.AddWithValue("@QueueType", _queueType);
+            command.Parameters.AddWithValue("@StartPartitionId", GetNextPartitionId(thread));
+            command.Parameters.AddWithValue("@Worker", $"{Environment.MachineName}.{Environment.ProcessId}");
+            command.Parameters.AddWithValue("@HeartbeatTimeoutSec", 600);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                //// put job type here
+                groupId = reader.GetInt64(0);
+                jobId = reader.GetInt64(1);
+                definition = reader.GetString(2);
+                version = reader.GetInt64(3);
+            }
+        }
+
+        internal void DequeueJob(int? thread, out long groupId, out long jobId, out long version, out short? resourceTypeId, out string minSurIdOrUrl, out string maxSurId)
+        {
+            DequeueJob(thread, out groupId, out jobId, out version, out var definition);
+            resourceTypeId = null;
+            minSurIdOrUrl = string.Empty;
+            maxSurId = string.Empty;
+            if (definition != null)
+            {
+                var split = definition.Split(";");
+                resourceTypeId = short.Parse(split[0]);
+                minSurIdOrUrl = split[1];
+                maxSurId = split[2];
+            }
+        }
+
+        internal void PutJobHeartbeat(long unitId, int? resourceCount = null)
+        {
+            using var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            using var command = new SqlCommand("dbo.PutJobHeartbeat", conn) { CommandType = CommandType.StoredProcedure, CommandTimeout = 120 };
+            command.Parameters.AddWithValue("@QueueType", _queueType);
+            command.Parameters.AddWithValue("@JobId", unitId);
+            if (resourceCount.HasValue)
+            {
+                command.Parameters.AddWithValue("@Data", resourceCount.Value);
+            }
+
+            command.ExecuteNonQuery();
+        }
+
+        internal void CompleteJob(long unitId, bool failed, long version, int? resourceCount = null)
+        {
+            using var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            using var command = new SqlCommand("dbo.PutJobStatus", conn) { CommandType = CommandType.StoredProcedure, CommandTimeout = 120 };
+            command.Parameters.AddWithValue("@QueueType", _queueType);
+            command.Parameters.AddWithValue("@JobId", unitId);
+            command.Parameters.AddWithValue("@Version", version);
+            command.Parameters.AddWithValue("@Failed", failed);
+            if (resourceCount.HasValue)
+            {
+                command.Parameters.AddWithValue("@Data", resourceCount.Value);
+            }
+            else
+            {
+                command.Parameters.AddWithValue("@Data", DBNull.Value);
+            }
+
+            command.Parameters.AddWithValue("@FinalResult", DBNull.Value);
+
+            command.ExecuteNonQuery();
+        }
+
+        internal IEnumerable<byte[]> GetData(short resourceTypeId, long minId, long maxId)
+        {
+            using var conn = new SqlConnection(ConnectionString);
+            conn.Open();
+            using var cmd = new SqlCommand(
+                @$"
+SELECT RawResource FROM dbo.Resource WHERE ResourceTypeId = @ResourceTypeId AND ResourceSurrogateId BETWEEN @MinId AND @MaxId AND IsHistory = 0",
+                conn)
+            { CommandTimeout = 600 };
+            cmd.Parameters.AddWithValue("@ResourceTypeId", resourceTypeId);
+            cmd.Parameters.AddWithValue("@MinId", minId);
+            cmd.Parameters.AddWithValue("@MaxId", maxId);
+            using var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                yield return reader.GetSqlBytes(0).Value;
+            }
+        }
+    }
+}
