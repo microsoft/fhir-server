e0f656a09e913528dc17d77584ca6966b2e35bf5
Sergey Galuzo <sergal@microsoft.com>
2022-05-06 12:11:54 -0700

    Removed project specifics and temporarily commented trigger definition.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Tables/Parameters.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Tables/Parameters.sql
@@ -29,44 +29,37 @@ CREATE TABLE dbo.ParametersHistory
     ,UpdatedBy   nvarchar(255)    NULL
   )
 GO
-CREATE TRIGGER dbo.ParametersInsUpdDel ON dbo.Parameters
-FOR INSERT, UPDATE, DELETE
-AS
-BEGIN
-  IF EXISTS (SELECT * FROM sys.tables WHERE name = 'Entities') -- i.e. a Distributed db
-     AND EXISTS (SELECT * FROM deleted WHERE Id IN ('CentralServer', 'CentralDatabase', 'CentralVersion') )
-  BEGIN
-    RAISERROR('Distributed databases cannot be unbound/rebound.  Once deactivated in Central.dbo.Shards, the only option is dropping this sharded database', 18, 127)
-    ROLLBACK TRANSACTION
-  END
+--CREATE TRIGGER dbo.ParametersInsUpdDel ON dbo.Parameters
+--FOR INSERT, UPDATE, DELETE
+--AS
+--BEGIN
+--  INSERT INTO dbo.ParametersHistory
+--      (
+--           Id
+--          ,Date
+--          ,Number
+--          ,Bigint
+--          ,Char
+--          ,Binary
+--          ,UpdatedDate
+--          ,UpdatedBy
+--      )
+--    SELECT Id
+--          ,Date
+--          ,Number
+--          ,Bigint
+--          ,Char
+--          ,Binary
+--          ,UpdatedDate
+--          ,UpdatedBy
+--      FROM Deleted
 
-  INSERT INTO dbo.ParametersHistory
-      (
-           Id
-          ,Date
-          ,Number
-          ,Bigint
-          ,Char
-          ,Binary
-          ,UpdatedDate
-          ,UpdatedBy
-      )
-    SELECT Id
-          ,Date
-          ,Number
-          ,Bigint
-          ,Char
-          ,Binary
-          ,UpdatedDate
-          ,UpdatedBy
-      FROM Deleted
-
-  UPDATE A
-    SET UpdatedDate = getUTCdate() 
-       ,UpdatedBy = left(system_user, 100)
-    FROM dbo.Parameters A 
-         JOIN Inserted B 
-           ON B.Id = A.Id
-  RETURN
-END
+--  UPDATE A
+--    SET UpdatedDate = getUTCdate() 
+--       ,UpdatedBy = left(system_user, 100)
+--    FROM dbo.Parameters A 
+--         JOIN Inserted B 
+--           ON B.Id = A.Id
+--  RETURN
+--END
 GO
