b12d9c2aa4500ca600e0d5b5dc56575b764b1d10
LTA-Thinking <rbrucej@gmail.com>
2022-05-10 08:21:19 -0700

    Fix exception message

--- src/Microsoft.Health.Fhir.Core/Features/Operations/Export/ExportJobTask.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/Export/ExportJobTask.cs
@@ -235,7 +235,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Operations.Export
                 // Try to update the job to failed state.
                 _logger.LogError(ex, "Encountered an unhandled exception. The job will be marked as failed.");
 
-                _exportJobRecord.FailureDetails = new JobFailureDetails(ex.Message + ": " + ex.StackTrace, HttpStatusCode.InternalServerError);
+                _exportJobRecord.FailureDetails = new JobFailureDetails(Core.Resources.UnknownError, HttpStatusCode.InternalServerError);
                 await CompleteJobAsync(OperationStatus.Failed, cancellationToken);
             }
             finally
