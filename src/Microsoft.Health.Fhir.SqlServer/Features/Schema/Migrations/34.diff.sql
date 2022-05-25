e93765b3c35ee24ecde7d15262b97f31e4cd410f
Sergey Galuzo <sergal@microsoft.com>
2022-05-25 13:12:13 -0700

    Removed index

---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/34.diff.sql
@@ -0,0 +1,3 @@
+﻿IF EXISTS (SELECT * FROM sys.indexes WHERE object_id = object_id('Resource') AND name = 'IX_Resource_ResourceSurrogateId')
+  DROP INDEX IX_Resource_ResourceSurrogateId ON dbo.Resource
+GO
