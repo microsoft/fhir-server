5ae43d74ed992db157749de5ce8b71dfc5cb2dfa
Sergey Galuzo <sergal@microsoft.com>
2022-05-26 15:40:21 -0700

    Not related - Remove not used columns from EventLog

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/34.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/34.diff.sql
@@ -1,3 +1,9 @@
 ﻿IF EXISTS (SELECT * FROM sys.indexes WHERE object_id = object_id('Resource') AND name = 'IX_Resource_ResourceSurrogateId')
   DROP INDEX IX_Resource_ResourceSurrogateId ON dbo.Resource
 GO
+IF EXISTS (SELECT * FROM sys.columns WHERE object_id = object_id('EventLog') AND name = 'ParentEventId')
+  ALTER TABLE dbo.EventLog DROP COLUMN ParentEventId
+GO
+IF EXISTS (SELECT * FROM sys.columns WHERE object_id = object_id('EventLog') AND name = 'TraceId')
+  ALTER TABLE dbo.EventLog DROP COLUMN TraceId
+GO
