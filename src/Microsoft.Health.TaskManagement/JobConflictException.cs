4f5c89f41d5d8e64140e1980b335d3808a0a9974
tongwu-msft <tong.wu@live.com>
2022-06-10 20:56:02 +0800

    Integrate generic queue to import operation (#2600)
    Integrate generic queue to import operation

---
+++ src/Microsoft.Health.TaskManagement/JobConflictException.cs
@@ -0,0 +1,26 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using EnsureThat;
+
+namespace Microsoft.Health.JobManagement
+{
+    public class JobConflictException : Exception
+    {
+        public JobConflictException(string message)
+            : base(message)
+        {
+            EnsureArg.IsNotNull(message, nameof(message));
+        }
+
+        public JobConflictException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+            EnsureArg.IsNotNull(message, nameof(message));
+            EnsureArg.IsNotNull(innerException, nameof(innerException));
+        }
+    }
+}
