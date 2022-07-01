79b719bfe92a2b015110a92717effbe36dfc078a
yoju0121 <61430790+yoju0121@users.noreply.github.com>
2022-07-01 09:58:03 -0700

    Member match and Reindex should return 400 if non-Parameter object is passed (#2678)
    * Create action filter to check deserialized request body object is of type Parameters (resource)
    
    * Add ValidateParametersResourceAttribute action filter to MemberMatchController and ReindexController
    
    * Add the filter to a single method instead of entire ReindexController class
    
    * Add test for MemberMatch
    
    * Fix test
    
    * Add test for Reindex endpoint
    
    * Fix failing test for xml type
    
    * Use both AttributeTargets class and method

--- src/Microsoft.Health.Fhir.Shared.Api/Controllers/ReindexController.cs
+++ src/Microsoft.Health.Fhir.Shared.Api/Controllers/ReindexController.cs
@@ -60,6 +60,7 @@ namespace Microsoft.Health.Fhir.Api.Controllers
         [HttpPost]
         [Route(KnownRoutes.Reindex)]
         [ServiceFilter(typeof(ValidateReindexRequestFilterAttribute))]
+        [ServiceFilter(typeof(ValidateParametersResourceAttribute))]
         [AuditEventType(AuditEventSubType.Reindex)]
         public async Task<IActionResult> CreateReindexJob([FromBody] Parameters inputParams)
         {
