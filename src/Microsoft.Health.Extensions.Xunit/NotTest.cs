12d52e2f6299e3bcb844fb3a7a45dd538b074449
Tom Markoc (WaferWire Cloud Technologies) <v-tommarkoc@microsoft.com>
2022-07-06 09:24:47 -0700

    fixes no tests found warning

---
+++ src/Microsoft.Health.Extensions.Xunit/NotTest.cs
@@ -0,0 +1,22 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace Microsoft.Health.Extensions.Xunit
+{
+    public class NotTest
+    {
+        /// <summary>
+        /// Once xunit is added to the project, test discoverer will try to find tests in the project. If no test is found, test discoverer will issue a warning.
+        /// To avoid this warning we add the test to the project.
+        /// </summary>
+        [Fact]
+        public void AlwaysSucceed()
+        {
+            Assert.True(true);
+        }
+    }
+}
