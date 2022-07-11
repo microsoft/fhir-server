71018daec6a5350660a940b6a9d10fe79a27168b
SamuilDIntern <106274366+SamuilDIntern@users.noreply.github.com>
2022-07-11 17:49:05 +0000

    Validate Resources Against US-Core Profiles (#2676)
    * Support for external terminology server
    
    * Fixed Issue with $export
    
    * Bug #1459: FIxed issue with $export error code when request type is Batch or Transaction.
    
    * Resources can now be validated against US Core
    
    * Added tests for $validate using external terminology service
    TODO: Need to make tests better.
    
    * Updated US-Core profiles in normative from STU3 -> R4.
    Wrote Tests for $Validate on a patient resource against us-core.
    
    * Improved added tests
    Added GenderIdentity USCORE-profile to normative
    Set "Definitions.zip" as "content" and "Copy Always" so that its path can be found
    Modified some pre-existing tests so that they account for profile validation.
    
    * Fixed appsettings
    
    * Removed unrelated bug-fix code for ExportNotSupportedInBundle
    
    * Validate tests pass on STU3 and R4, most pass on R5.
    Fixed issue with ZipSource which prevented defintion file summaries from being loaded.
    Removed last bit of code irrelevant work.
    
    * Added ValueSet-BirthSex to R4 and R5
    Validate Tests passing for all versions
    
    * Cleaned up csproj for Stu3, R4, and R5 .web
    
    * Updated TestConfiguration.json to use new external terminology service endpoint
    
    * Changed endpoint back to hapi
    
    Co-authored-by: Jared Erwin <jaerwin@microsoft.com>

---
+++ src/Microsoft.Health.Fhir.Core/Features/Validation/AuthorizationMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.Fhir.Core.Features.Validation
+{
+    internal class AuthorizationMessageHandler : HttpClientHandler
+    {
+        public System.Net.Http.Headers.AuthenticationHeaderValue Authorization { get; set; }
+
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (Authorization != null)
+            {
+                request.Headers.Authorization = Authorization;
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
