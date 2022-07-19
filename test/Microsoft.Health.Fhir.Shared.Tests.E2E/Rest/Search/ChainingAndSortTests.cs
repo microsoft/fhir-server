351d5c231d641182c19d679cf33ec02aa3434c4f
FHIBF <fernfe@microsoft.com>
2022-07-19 10:10:25 -0700

    Adding more comments and validations.

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/ChainingAndSortTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/ChainingAndSortTests.cs
@@ -38,39 +38,52 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             Assert.NotNull(fhirResponse);
             Assert.Equal(HttpStatusCode.OK, fhirResponse.StatusCode);
 
+            const int totalNumberOfHealthcareServices = 15;             // Total number of healthcare services ingested.
+            const int expectedNumberOfEntriesInFirstPage = 10;          // Max number of entries in the first page.
+            const int expectedNumberOfLinks = 2;                        // Expected number of pages/links.
+            const int totalNumberOfFilteredHealthcareServices = 13;     // Expected number of health care services when filters are applied.
+
+            // Check if all HealthcareServices were properly ingested.
+            Bundle bundleAllHealthcaseServices = await _client.SearchAsync(ResourceType.HealthcareService, "_total=accurate");
+            Assert.Equal(expectedNumberOfEntriesInFirstPage, bundleAllHealthcaseServices.Entry.Count);
+            Assert.Equal(expectedNumberOfLinks, bundleAllHealthcaseServices.Link.Count);
+            Assert.Equal(totalNumberOfHealthcareServices, bundleAllHealthcaseServices.Total.Value);
+
             string commonQuery = "name:missing=false&_has:PractitionerRole:service:practitioner=2ec3586b-9454-4c7f-8eaf-7a0e64cecf17&active:not=false&location:missing=false&_has:PractitionerRole:service:active=true";
 
-            const int expectedNumberOfEntriesInFirstPage = 10;
-            const int totalExpectedNumberOfEntries = 13;
-            const int expectedNumberOfLinks = 2;
-
+            // Search for a set of Healthcare Services using chain queries and adittional filtering.
             Bundle bundleWithNoSort = await _client.SearchAsync(ResourceType.HealthcareService, commonQuery);
             Assert.Equal(expectedNumberOfEntriesInFirstPage, bundleWithNoSort.Entry.Count);
             Assert.Equal(expectedNumberOfLinks, bundleWithNoSort.Link.Count);
 
+            // [ Reuse the first query ] + Sort records by name.
             Bundle bundleWithSort = await _client.SearchAsync(ResourceType.HealthcareService, commonQuery + "&_sort=name");
             Assert.Equal(expectedNumberOfEntriesInFirstPage, bundleWithSort.Entry.Count);
             Assert.Equal(expectedNumberOfLinks, bundleWithSort.Link.Count);
 
+            // [ Reuse the first query ] + Return only the total count of records.
             Bundle bundleWithNoSortAndSummaryCount = await _client.SearchAsync(ResourceType.HealthcareService, commonQuery + "&_summary=count");
             Assert.Empty(bundleWithNoSortAndSummaryCount.Entry);
             Assert.Empty(bundleWithNoSortAndSummaryCount.Link);
-            Assert.Equal(totalExpectedNumberOfEntries, bundleWithNoSortAndSummaryCount.Total.Value);
+            Assert.Equal(totalNumberOfFilteredHealthcareServices, bundleWithNoSortAndSummaryCount.Total.Value);
 
+            // [ Reuse the first query ] + Return only the total count of records (with additional _sort expression that will be ignored internally).
             Bundle bundleWithSortAndSummaryCount = await _client.SearchAsync(ResourceType.HealthcareService, commonQuery + "&_sort=name&_summary=count");
             Assert.Empty(bundleWithSortAndSummaryCount.Entry);
             Assert.Empty(bundleWithSortAndSummaryCount.Link);
-            Assert.Equal(totalExpectedNumberOfEntries, bundleWithSortAndSummaryCount.Total.Value);
+            Assert.Equal(totalNumberOfFilteredHealthcareServices, bundleWithSortAndSummaryCount.Total.Value);
 
+            // [ Reuse the first query ] + Get total number of records returned.
             Bundle bundleWithNoSortAndTotalAccurate = await _client.SearchAsync(ResourceType.HealthcareService, commonQuery + "&_total=accurate");
             Assert.Equal(expectedNumberOfEntriesInFirstPage, bundleWithNoSortAndTotalAccurate.Entry.Count);
             Assert.Equal(expectedNumberOfLinks, bundleWithNoSortAndTotalAccurate.Link.Count);
-            Assert.Equal(totalExpectedNumberOfEntries, bundleWithNoSortAndTotalAccurate.Total.Value);
+            Assert.Equal(totalNumberOfFilteredHealthcareServices, bundleWithNoSortAndTotalAccurate.Total.Value);
 
+            // [ Reuse the first query ] + Sort records by name and get total number of records returned.
             Bundle bundleWithSortAndTotalAccurate = await _client.SearchAsync(ResourceType.HealthcareService, commonQuery + "&_sort=name&_total=accurate");
             Assert.Equal(expectedNumberOfEntriesInFirstPage, bundleWithSortAndTotalAccurate.Entry.Count);
             Assert.Equal(expectedNumberOfLinks, bundleWithSortAndTotalAccurate.Link.Count);
-            Assert.Equal(totalExpectedNumberOfEntries, bundleWithSortAndTotalAccurate.Total.Value);
+            Assert.Equal(totalNumberOfFilteredHealthcareServices, bundleWithSortAndTotalAccurate.Total.Value);
         }
     }
 }
