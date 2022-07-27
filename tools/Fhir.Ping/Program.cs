5440361ec0b6292108a61826dd11748bd5195a4a
Sergey Galuzo <sergal@microsoft.com>
2022-07-27 14:03:26 -0700

    Added ping

---
+++ tools/Fhir.Ping/Program.cs
@@ -0,0 +1,25 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Fhir.Ping
+{
+    public static class Program
+    {
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public static void Main(string[] args)
+        {
+            var uri = args.Length > 0 ? args[0] : "http://localhost:5000/Observation?_summary=count&_lastUpdated=lt2020-01-01";
+            var delay = args.Length > 1 ? int.Parse(args[1]) : 1000;
+            Console.WriteLine($"Delay={delay} Uri={uri}");
+            while (true)
+            {
+                var response = HttpClient.GetAsync(new Uri(uri)).Result;
+                Console.WriteLine($"response.StatusCode={response.StatusCode}");
+                Thread.Sleep(delay);
+            }
+        }
+    }
+}
