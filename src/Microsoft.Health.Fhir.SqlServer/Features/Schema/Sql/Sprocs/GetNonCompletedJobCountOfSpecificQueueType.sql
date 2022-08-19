e359781368228155681fbd451e6d4c5104b9dd4f
apurvabhaleMS <86023331+apurvabhaleMS@users.noreply.github.com>
2022-08-19 12:45:29 -0700

    Added SP form paas 3.sql to FHIR's Schema migration script (#2747)

---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/GetNonCompletedJobCountOfSpecificQueueType.sql
@@ -0,0 +1,26 @@
+﻿/***************************************************************************
+    Stored procedures for get NonCompleted Job Count Of SpecificQueueType
+****************************************************************************/
+--
+--  STORED PROCEDURE
+--      GetNonCompletedJobCountOfSpecificQueueType
+--
+--  DESCRIPTION
+--      Count the number of non-completed jobs of specific type.
+--
+--  PARAMETERS
+--      @@queueType
+--          * The type of queue
+--
+CREATE OR ALTER PROCEDURE dbo.GetNonCompletedJobCountOfSpecificQueueType
+    @queueType tinyint
+
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT COUNT(*)
+    FROM dbo.JobQueue
+    WHERE QueueType = @queueType AND (Status = 0 or Status = 1)
+END
+GO
