c4eb703596e8b1a606bb1e21584d0e35f016a02d
Tom Markoc (WaferWire Cloud Technologies) <v-tommarkoc@microsoft.com>
2022-08-23 15:00:04 -0700

    cleanup

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/TokenOverflowTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Search/TokenOverflowTests.cs
@@ -4,23 +4,13 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
-
-// using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Hl7.Fhir.Model;
-
-// using Hl7.Fhir.Rest;
-
-// using Hl7.Fhir.Rest;
 using Hl7.Fhir.Serialization;
-
-// using Microsoft.Azure.Cosmos.Core;
 using Microsoft.Health.Core.Extensions;
 using Microsoft.Health.Fhir.Client;
-
-// using Microsoft.Health.Fhir.Core.Features.Operations;
 using Microsoft.Health.Fhir.SqlServer.Features.Schema.Model;
 using Microsoft.Health.Fhir.Tests.Common;
 using Microsoft.Health.Fhir.Tests.Common.FixtureParameters;
@@ -51,7 +41,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
 
         public async Task InitializeAsync()
         {
-            // await Client.DeleteAllResources(ResourceType.Patient, null);
+            // await Client.DeleteAllResources(ResourceType.Patient, null);// TODO:------------
 
             await Client.DeleteAllResources(ResourceType.Specimen, null);
             await Client.DeleteAllResources(ResourceType.Immunization, null);
@@ -240,118 +230,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             }
         }
 
-        /*
-        [SkippableFact]
-        public async Task GivenResourcesWithAndWithoutTokenOverflow_WhenSearchByTokenString_VerifyCorrectSerachResults()
-        {
-            try
-            {
-                string id = "KirkCTS" + Guid.NewGuid().ToString().ComputeHash().Substring(0, 14).ToLower();
-
-                SearchParameter searchParam = Samples.GetJsonSample<SearchParameter>("CompositeCustomTokenStringSearchParameter");
-                LoadTestPatients(id, out Patient patientAWithTokenOverflow, out Patient patientBWithTokenOverflow, out Patient patientCWithNoTokenOverflow);
-
-                // POST patient A.
-                FhirResponse<Patient> createdPatientA = await Client.CreateAsync(patientAWithTokenOverflow);
-                EnsureSuccessStatusCode(createdPatientA.StatusCode, "Creating patient A.");
-
-                // POST custom composite search parameter.
-                FhirResponse<SearchParameter> createdSearchParam = await Client.CreateAsync(searchParam);
-                EnsureSuccessStatusCode(createdPatientA.StatusCode, "Creating custom composite search parameter.");
-
-                // POST patient B.
-                FhirResponse<Patient> createdPatientB = await Client.CreateAsync(patientBWithTokenOverflow);
-                EnsureSuccessStatusCode(createdPatientA.StatusCode, "Creating patient B.");
-
-                // POST patient C.
-                FhirResponse<Patient> createdPatientC = await Client.CreateAsync(patientCWithNoTokenOverflow);
-                EnsureSuccessStatusCode(createdPatientA.StatusCode, "Creating patient C.");
-
-                // Without x-ms-use-partial-indices header we cannot search for resources created after the search parameter was created.
-                Bundle bundle = await Client.SearchAsync($"Patient?identifier-name-family={patientBWithTokenOverflow.Identifier[0].Value}${patientBWithTokenOverflow.Name[0].Family}");
-                OperationOutcome operationOutcome = GetAndValidateOperationOutcome(bundle);
-                string[] expectedDiagnostics = { string.Format(Core.Resources.SearchParameterNotSupported, "identifier-name-family", "Patient") };
-                OperationOutcome.IssueSeverity[] expectedIssueSeverities = { OperationOutcome.IssueSeverity.Warning };
-                OperationOutcome.IssueType[] expectedCodeTypes = { OperationOutcome.IssueType.NotSupported };
-                ValidateOperationOutcome(expectedDiagnostics, expectedIssueSeverities, expectedCodeTypes, operationOutcome);
-
-                // With x-ms-use-partial-indices header we can search only for resources created after the search parameter was created.
-
-                await ExecuteAndValidateBundle(
-                    $"Patient?identifier-name-family={patientAWithTokenOverflow.Identifier[0].Value}${patientAWithTokenOverflow.Name[0].Family}",
-                    false,
-                    false,
-                    new Tuple<string, string>("x-ms-use-partial-indices", "true"));
-
-                await ExecuteAndValidateBundle(
-                    $"Patient?identifier-name-family={patientBWithTokenOverflow.Identifier[0].Value}${patientBWithTokenOverflow.Name[0].Family}",
-                    false,
-                    false,
-                    new Tuple<string, string>("x-ms-use-partial-indices", "true"),
-                    createdPatientB);
-
-                await ExecuteAndValidateBundle(
-                    $"Patient?identifier-name-family={patientCWithNoTokenOverflow.Identifier[0].Value}${patientCWithNoTokenOverflow.Name[0].Family}",
-                    false,
-                    false,
-                    new Tuple<string, string>("x-ms-use-partial-indices", "true"),
-                    createdPatientC);
-
-                // Reindex DB.
-
-                Uri reindexJobUri;
-
-                // Start a reindex job
-                (_, reindexJobUri) = await Client.PostReindexJobAsync(new Parameters());
-
-                await WaitForReindexStatus(reindexJobUri, "Completed");
-
-                FhirResponse<Parameters> reindexJobResult = await Client.CheckReindexAsync(reindexJobUri);
-                Parameters.ParameterComponent param = reindexJobResult.Resource.Parameter.FirstOrDefault(p => p.Name == "searchParams");
-                _output.WriteLine("ReindexJobDocument:");
-                var serializer = new FhirJsonSerializer();
-                _output.WriteLine(serializer.SerializeToString(reindexJobResult.Resource));
-
-                Assert.Contains(createdSearchParam.Resource.Url, param?.Value?.ToString());
-
-                reindexJobResult = await WaitForReindexStatus(reindexJobUri, "Completed");
-                _output.WriteLine($"Reindex job is completed, it should have reindexed the resources with {id}.");
-
-                bool floatParse = float.TryParse(
-                    reindexJobResult.Resource.Parameter.FirstOrDefault(predicate => predicate.Name == "resourcesSuccessfullyReindexed").Value.ToString(),
-                    out float resourcesReindexed);
-
-                _output.WriteLine($"Reindex job is completed, {resourcesReindexed} resources Reindexed.");
-
-                Assert.True(floatParse);
-                Assert.True(resourcesReindexed > 0.0);
-
-                // After reindexing no need to use x-ms-use-partial-indices, all resources are searchable.
-                await ExecuteAndValidateBundle(
-                    $"Patient?identifier-name-family={patientAWithTokenOverflow.Identifier[0].Value}${patientAWithTokenOverflow.Name[0].Family}",
-                    createdPatientA);
-
-                await ExecuteAndValidateBundle(
-                    $"Patient?identifier-name-family={patientBWithTokenOverflow.Identifier[0].Value}${patientBWithTokenOverflow.Name[0].Family}",
-                    createdPatientB);
-
-                await ExecuteAndValidateBundle(
-                    $"Patient?identifier-name-family={patientCWithNoTokenOverflow.Identifier[0].Value}${patientCWithNoTokenOverflow.Name[0].Family}",
-                    createdPatientC);
-
-                // Invlid composite search parameter returns nothing (we combine patients A and B).
-                await ExecuteAndValidateBundle(
-                    $"Patient?identifier-name-family={patientAWithTokenOverflow.Identifier[0].Value}${patientBWithTokenOverflow.Name[0].Family}");
-            }
-            catch (Exception e)
-            {
-                _output.WriteLine($"Exception: {e.Message}");
-                _output.WriteLine($"Stack Trace: {e.StackTrace}");
-                throw;
-            }
-        }
-        */
-
         [SkippableFact]
         public async Task AAAA_GivenResourcesWithAndWithoutTokenOverflow_WhenSearchByTokenString_VerifyCorrectSerachResults()
         {
@@ -359,7 +237,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
                 "Patient",
                 "Kirk-CTS",
                 "CompositeCustomTokenStringSearchParameter",
-                "Patient-identifier-individual-family",
                 patient => patient.Identifier[0].Value,
                 patient => patient.Name[0].Family);
         }
@@ -371,7 +248,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
                 "Patient",
                 "Kirk-CTD",
                 "CompositeCustomTokenDateTimeSearchParameter",
-                "Patient-identifier-individual-birthdate",
                 patient => patient.Identifier[0].Value,
                 patient => patient.BirthDate);
         }
@@ -383,7 +259,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
                 "Patient",
                 "Kirk-CToT",
                 "CompositeCustomTokenOverflowTokenSearchParameter",
-                "Patient-identifier-individual-phone",
                 patient => patient.Identifier[0].Value,
                 patient => patient.Telecom[0].Value);
         }
@@ -395,7 +270,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
                 "Patient",
                 "Kirk-CTTo",
                 "CompositeCustomTokenTokenOverflowSearchParameter",
-                "individual-phone-Patient-identifier",
                 patient => patient.Telecom[0].Value,
                 patient => patient.Identifier[0].Value);
         }
@@ -407,7 +281,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
                 "Patient",
                 "Kirk-CRT",
                 "CompositeCustomReferenceTokenSearchParameter",
-                "Patient-organization-Patient-identifier",
                 patient => patient.ManagingOrganization.Reference,
                 patient => patient.Identifier[0].Value);
         }
@@ -419,7 +292,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
                 "ChargeItem",
                 "Kirk-CTQ",
                 "CompositeCustomTokenQuantitySearchParameter",
-                "Patient-organization-Patient-identifier",
 #if R4 || R5
                 chargeItem => chargeItem.Identifier[0].Value,
 #else
@@ -435,7 +307,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
                 "RiskAssessment",
                 "Kirk-CTNN",
                 "CompositeCustomTokenNumberNumberSearchParameter",
-                "clinical-identifier-RiskAssessment-probability-RiskAssessment-probability",
 #if R4 || R5
                 riskAssessment => riskAssessment.Identifier[0].Value,
 #else
@@ -453,7 +324,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             string resourceTypeName,
             string resourceNamePrefix,
             string searchParameterTestFileName,
-            string searchParameterName,
             GetOneParameter<T> getParameter1,
             GetOneParameter<T> getParameter2)
             where T : DomainResource
@@ -462,10 +332,10 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Search
             {
                 string rnd = Guid.NewGuid().ToString().ComputeHash().Substring(0, 14).ToLower();
                 string name = $"{resourceNamePrefix}-{rnd}";
-                searchParameterName = $"Search-{name}"; // TODO:!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
 
+                string searchParameterName = $"Search-{name}";
                 SearchParameter searchParam = Samples.GetJsonSample<SearchParameter>(searchParameterTestFileName);
-                searchParam.Id = $"SearchParameter-Id-{name}";
+                searchParam.Id = $"Id-{searchParameterName}";
                 searchParam.Name = searchParameterName;
                 searchParam.Url = "http://hl7.org/fhir/SearchParameter/" + searchParameterName;
                 searchParam.Code = searchParameterName;
