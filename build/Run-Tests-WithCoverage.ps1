136a4f5e847b92b0677b1c6f75f892ae03b91f16
Brendan Kowitz <bkowitz@microsoft.com>
2022-08-26 11:37:55 -0700

    Exclude generated Schema model files

--- build/Run-Tests-WithCoverage.ps1
+++ build/Run-Tests-WithCoverage.ps1
@@ -10,7 +10,7 @@ Write-Host "Test directory: $testResultsDirectory"
 Get-ChildItem -Recurse -Filter *UnitTests*.csproj | 
 Foreach-Object {
   $dir = "$testResultsDirectory/$($_.BaseName)"
-  dotnet test $_.FullName --configuration $buildConfiguration --no-build --logger trx --results-directory "$dir" --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format="json,cobertura" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Include="[Microsoft.Health.Fhir.*]*" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Exclude="[*Tests*]*,[*.Web]*,[*.ValueSets]*" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.MergeWith="$coverageJsonFullPath"
+  dotnet test $_.FullName --configuration $buildConfiguration --no-build --logger trx --results-directory "$dir" --collect:"XPlat Code Coverage" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format="json,cobertura" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Include="[Microsoft.Health.Fhir.*]*" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Exclude="[*Tests*]*,[*.Web]*,[*.ValueSets]*,[*.SqlServer]*.Schema.Model.*" -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.MergeWith="$coverageJsonFullPath"
 
   # by copying back the current set of coverage results and using MergeWith, the results will be aggregated
   if (Test-Path -Path $dir){
