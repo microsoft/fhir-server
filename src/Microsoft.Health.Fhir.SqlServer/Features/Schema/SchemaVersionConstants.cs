4551e6bcf4577ad669788aed5e4340f03ceccd77
Cheng Chen <chengchen2@microsoft.com>
2022-09-19 18:01:41 +0800

    restore 40

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/SchemaVersionConstants.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/SchemaVersionConstants.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Schema
     public static class SchemaVersionConstants
     {
         public const int Min = (int)SchemaVersion.V32;
-        public const int Max = (int)SchemaVersion.V40;
+        public const int Max = (int)SchemaVersion.V41;
         public const int SearchParameterStatusSchemaVersion = (int)SchemaVersion.V6;
         public const int SupportForReferencesWithMissingTypeVersion = (int)SchemaVersion.V7;
         public const int SearchParameterHashSchemaVersion = (int)SchemaVersion.V8;
