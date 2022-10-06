fd95f57f735a2accd8ef21265ace64a468bc55ea
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2022-10-05 17:49:44 -0700

    Adding more details to FHIRException

--- src/Microsoft.Health.Fhir.Shared.Client/FhirException.cs
+++ src/Microsoft.Health.Fhir.Shared.Client/FhirException.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using Hl7.Fhir.Model;
 
 namespace Microsoft.Health.Fhir.Client
@@ -29,7 +30,7 @@ namespace Microsoft.Health.Fhir.Client
 
         public OperationOutcome OperationOutcome => Response.Resource;
 
-        public override string Message => $"{StatusCode}: {OperationOutcome?.Issue?.FirstOrDefault()?.Diagnostics} ({GetOperationId()})";
+        public override string Message => FormatMessage();
 
         public void Dispose()
         {
@@ -46,6 +47,31 @@ namespace Microsoft.Health.Fhir.Client
             }
         }
 
+        private string FormatMessage()
+        {
+            StringBuilder message = new StringBuilder();
+
+            string diagnostic = OperationOutcome?.Issue?.FirstOrDefault()?.Diagnostics;
+            string operationId = GetOperationId();
+
+            message.Append(StatusCode);
+            if (!string.IsNullOrWhiteSpace(diagnostic))
+            {
+                message.Append(": ").Append(diagnostic);
+            }
+
+            message.Append(" (").Append(operationId).Append(')');
+
+            message.AppendLine("==============================================");
+            message.Append("Url: ").AppendLine(Response.Response.RequestMessage?.RequestUri.ToString() ?? "NO_URI_AVAILABLE");
+            message.Append("Response code: ").AppendLine(Response.Response.StatusCode.ToString());
+            message.Append("Reason phrase: ").AppendLine(Response.Response.ReasonPhrase ?? "NO_REASON_PHRASE");
+
+            message.AppendLine("==============================================");
+
+            return message.ToString();
+        }
+
         private string GetOperationId()
         {
             if (Response.Headers.TryGetValues("X-Request-Id", out var values))
@@ -53,7 +79,7 @@ namespace Microsoft.Health.Fhir.Client
                 return values.First();
             }
 
-            return string.Empty;
+            return "NO_FHIR_OPERATION_ID_FOR_THIS_TRANSACTION";
         }
     }
 }
