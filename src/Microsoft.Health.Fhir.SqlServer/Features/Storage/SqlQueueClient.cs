c4ca7605ad62d7bea3941406d1680c8a3ad1171f
Sergey Galuzo <sergal@microsoft.com>
2022-11-02 21:34:43 -0700

    explicit using

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlQueueClient.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlQueueClient.cs
@@ -355,8 +355,10 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
                 periodSec = 1;
             }
 
-            using var heartbeat = new Timer(_ => PutJobHeartbeatFireAndForget(queueType, jobId, version, cancellationToken), null, TimeSpan.FromSeconds(RandomNumberGenerator.GetInt32(periodSec)), heartbeatPeriod);
-            await action(cancellationToken);
+            using (var heartbeat = new Timer(_ => PutJobHeartbeatFireAndForget(queueType, jobId, version, cancellationToken), null, TimeSpan.FromSeconds(RandomNumberGenerator.GetInt32(periodSec)), heartbeatPeriod))
+            {
+                await action(cancellationToken);
+            }
         }
 
         private void PutJobHeartbeatFireAndForget(byte queueType, long jobId, long version, CancellationToken cancellationToken)
