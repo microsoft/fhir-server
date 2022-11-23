6495ecb7c43a6df1e639fae4c48c97aa7d612528
Sergey Galuzo <sergal@microsoft.com>
2022-11-22 22:19:54 -0800

    isAnonym + no ToList()

--- src/Microsoft.Health.Fhir.Core/Features/Operations/Export/ExportOrchestratorJob.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/Export/ExportOrchestratorJob.cs
@@ -47,11 +47,11 @@ namespace Microsoft.Health.Fhir.Core.Features.Operations.Export
         {
             var record = JsonConvert.DeserializeObject<ExportJobRecord>(jobInfo.Definition);
             var surrogateIdRaneSize = (int)record.MaximumNumberOfResourcesPerQuery;
-            var groupJobs = (await _queueClient.GetJobByGroupIdAsync((byte)QueueType.Export, jobInfo.GroupId, true, cancellationToken)).ToList();
-            var ac = !string.IsNullOrEmpty(record.AnonymizationConfigurationCollectionReference) || !string.IsNullOrEmpty(record.AnonymizationConfigurationLocation) || !string.IsNullOrEmpty(record.AnonymizationConfigurationFileETag);
+            var groupJobs = (await _queueClient.GetJobByGroupIdAsync((byte)QueueType.Export, jobInfo.GroupId, true, cancellationToken));
+            var isAnonym = !string.IsNullOrEmpty(record.AnonymizationConfigurationCollectionReference) || !string.IsNullOrEmpty(record.AnonymizationConfigurationLocation) || !string.IsNullOrEmpty(record.AnonymizationConfigurationFileETag);
 
             // for parallel case we enqueue in batches, so we should handle not completed registration
-            if (record.ExportType == ExportJobType.All && record.IsParallel && (record.Filters == null || record.Filters.Count == 0) && !ac)
+            if (record.ExportType == ExportJobType.All && record.IsParallel && (record.Filters == null || record.Filters.Count == 0) && !isAnonym)
             {
                 var resourceTypes = string.IsNullOrEmpty(record.ResourceType)
                                   ? (await _searchService.GetUsedResourceTypes(cancellationToken)).Select(_ => _.Name)
