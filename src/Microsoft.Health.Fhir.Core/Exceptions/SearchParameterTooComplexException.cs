f17b881f42cee024f4920242f2e3818236345608
Tom-Markoch <93687404+Tom-Markoch@users.noreply.github.com>
2022-12-16 11:10:49 -0800

    Personal/v tommarkoc/revinclude include iterate svr abort (#3018)
    * fixes and adds stack overflow protection
    
    * cleanup, exeption in own file
    
    * minor fix
    
    * localised exception messge, null-coalescing operator

---
+++ src/Microsoft.Health.Fhir.Core/Exceptions/SearchParameterTooComplexException.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Fhir.Core.Models;
+
+namespace Microsoft.Health.Fhir.Core.Exceptions
+{
+    /// <summary>
+    /// The exception that is thrown when the search parameter is too complex.
+    /// In the case of input search parameter being too complex, there is a possibility of a stack overflow. Stack overflow exceptions cannot be
+    /// caught in .NET and will abort the process. For that reason we throw this exceptioon when predefined stack depth limit is reached.
+    /// </summary>
+    public class SearchParameterTooComplexException : Core.Exceptions.FhirException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SearchParameterTooComplexException"/> class.
+        /// </summary>
+        public SearchParameterTooComplexException()
+        {
+            Issues.Add(new OperationOutcomeIssue(
+                OperationOutcomeConstants.IssueSeverity.Error,
+                OperationOutcomeConstants.IssueType.TooCostly,
+                Resources.SearchParameterTooComplex));
+        }
+    }
+}
