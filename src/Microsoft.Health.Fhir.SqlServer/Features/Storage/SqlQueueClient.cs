7fc41610d051d89c6d7b8bb3efccb7c807bb0c11
Sergey Galuzo <sergal@microsoft.com>
2022-12-23 16:30:55 -0800

    retries on queue client

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlQueueClient.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlQueueClient.cs
@@ -88,46 +88,38 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 
         public virtual async Task CompleteJobAsync(JobInfo jobInfo, bool requestCancellationOnFailure, CancellationToken cancellationToken)
         {
-            try
-            {
-                using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
-                using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
-
-                // cannot use VLatest as it incorrectly sends nulls
-                sqlCommandWrapper.CommandType = System.Data.CommandType.StoredProcedure;
-                sqlCommandWrapper.CommandText = "dbo.PutJobStatus";
-                sqlCommandWrapper.Parameters.AddWithValue("@QueueType", jobInfo.QueueType);
-                sqlCommandWrapper.Parameters.AddWithValue("@JobId", jobInfo.Id);
-                sqlCommandWrapper.Parameters.AddWithValue("@Version", jobInfo.Version);
-                sqlCommandWrapper.Parameters.AddWithValue("@Failed", jobInfo.Status == JobStatus.Failed);
-                sqlCommandWrapper.Parameters.AddWithValue("@Data", jobInfo.Data.HasValue ? jobInfo.Data.Value : DBNull.Value);
-                sqlCommandWrapper.Parameters.AddWithValue("@FinalResult", jobInfo.Result != null ? jobInfo.Result : DBNull.Value);
-                sqlCommandWrapper.Parameters.AddWithValue("@RequestCancellationOnFailure", requestCancellationOnFailure);
-
-                try
+            await SqlServerFhirDataStore.ExecuteWithRetries(
+                "CompleteJobAsync",
+                async () =>
                 {
-                    await sqlCommandWrapper.ExecuteNonQueryAsync(cancellationToken);
-                }
-                catch (SqlException sqlEx)
-                {
-                    if (sqlEx.Number == SqlErrorCodes.PreconditionFailed)
+                    using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
+                    using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
+
+                    // cannot use VLatest as it incorrectly sends nulls
+                    sqlCommandWrapper.CommandType = System.Data.CommandType.StoredProcedure;
+                    sqlCommandWrapper.CommandText = "dbo.PutJobStatus";
+                    sqlCommandWrapper.Parameters.AddWithValue("@QueueType", jobInfo.QueueType);
+                    sqlCommandWrapper.Parameters.AddWithValue("@JobId", jobInfo.Id);
+                    sqlCommandWrapper.Parameters.AddWithValue("@Version", jobInfo.Version);
+                    sqlCommandWrapper.Parameters.AddWithValue("@Failed", jobInfo.Status == JobStatus.Failed);
+                    sqlCommandWrapper.Parameters.AddWithValue("@Data", jobInfo.Data.HasValue ? jobInfo.Data.Value : DBNull.Value);
+                    sqlCommandWrapper.Parameters.AddWithValue("@FinalResult", jobInfo.Result != null ? jobInfo.Result : DBNull.Value);
+                    sqlCommandWrapper.Parameters.AddWithValue("@RequestCancellationOnFailure", requestCancellationOnFailure);
+
+                    try
                     {
-                        throw new JobNotExistException(sqlEx.Message);
+                        await sqlCommandWrapper.ExecuteNonQueryAsync(cancellationToken);
                     }
+                    catch (SqlException sqlEx)
+                    {
+                        if (sqlEx.Number == SqlErrorCodes.PreconditionFailed)
+                        {
+                            throw new JobNotExistException(sqlEx.Message);
+                        }
 
-                    throw;
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "CompleteJobAsync failed.");
-                if (ex.IsRetryable())
-                {
-                    throw new RetriableJobException(ex.Message, ex);
-                }
-
-                throw;
-            }
+                        throw;
+                    }
+                });
         }
 
         public async Task<IReadOnlyCollection<JobInfo>> DequeueJobsAsync(byte queueType, int numberOfJobsToDequeue, string worker, int heartbeatTimeoutSec, CancellationToken cancellationToken)
@@ -154,149 +146,117 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 
         public async Task<JobInfo> DequeueAsync(byte queueType, string worker, int heartbeatTimeoutSec, CancellationToken cancellationToken, long? jobId = null)
         {
-            try
-            {
-                using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
-                using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
-
-                // cannot use VLatest as it incorrectly asks for optional @InputJobId
-                sqlCommandWrapper.CommandText = "dbo.DequeueJob";
-                sqlCommandWrapper.CommandType = System.Data.CommandType.StoredProcedure;
-                sqlCommandWrapper.Parameters.AddWithValue("@QueueType", queueType);
-                sqlCommandWrapper.Parameters.AddWithValue("@Worker", worker);
-                sqlCommandWrapper.Parameters.AddWithValue("@HeartbeatTimeoutSec", heartbeatTimeoutSec);
-                if (jobId.HasValue)
+            JobInfo jobInfo = null;
+            await SqlServerFhirDataStore.ExecuteWithRetries(
+                "DequeueAsync",
+                async () =>
                 {
-                    sqlCommandWrapper.Parameters.AddWithValue("@InputJobId", jobId.Value);
-                }
+                    using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
+                    using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
 
-                await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
-                JobInfo jobInfo = await sqlDataReader.ReadJobInfoAsync(cancellationToken);
-                if (jobInfo != null)
-                {
-                    jobInfo.QueueType = queueType;
-                }
+                    // cannot use VLatest as it incorrectly asks for optional @InputJobId
+                    sqlCommandWrapper.CommandText = "dbo.DequeueJob";
+                    sqlCommandWrapper.CommandType = System.Data.CommandType.StoredProcedure;
+                    sqlCommandWrapper.Parameters.AddWithValue("@QueueType", queueType);
+                    sqlCommandWrapper.Parameters.AddWithValue("@Worker", worker);
+                    sqlCommandWrapper.Parameters.AddWithValue("@HeartbeatTimeoutSec", heartbeatTimeoutSec);
+                    if (jobId.HasValue)
+                    {
+                        sqlCommandWrapper.Parameters.AddWithValue("@InputJobId", jobId.Value);
+                    }
 
-                return jobInfo;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "DequeueAsync failed.");
-                if (ex.IsRetryable())
-                {
-                    throw new RetriableJobException(ex.Message, ex);
-                }
-
-                throw;
-            }
+                    await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
+                    jobInfo = await sqlDataReader.ReadJobInfoAsync(cancellationToken);
+                    if (jobInfo != null)
+                    {
+                        jobInfo.QueueType = queueType;
+                    }
+                });
+            return jobInfo;
         }
 
         public async Task<IReadOnlyList<JobInfo>> EnqueueAsync(byte queueType, string[] definitions, long? groupId, bool forceOneActiveJobGroup, bool isCompleted, CancellationToken cancellationToken)
         {
-            try
-            {
-                using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
-                using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
-
-                VLatest.EnqueueJobs.PopulateCommand(sqlCommandWrapper, queueType, definitions.Select(d => new StringListRow(d)), groupId, forceOneActiveJobGroup, isCompleted);
-                try
+            IReadOnlyList<JobInfo> results = null;
+            await SqlServerFhirDataStore.ExecuteWithRetries(
+                "EnqueueAsync",
+                async () =>
                 {
-                    await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
+                    using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
+                    using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
 
-                    return await sqlDataReader.ReadJobInfosAsync(cancellationToken);
-                }
-                catch (SqlException sqlEx)
-                {
-                    if (sqlEx.State == 127)
+                    VLatest.EnqueueJobs.PopulateCommand(sqlCommandWrapper, queueType, definitions.Select(d => new StringListRow(d)), groupId, forceOneActiveJobGroup, isCompleted);
+                    try
                     {
-                        throw new JobManagement.JobConflictException(sqlEx.Message);
+                        await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
+
+                        results = await sqlDataReader.ReadJobInfosAsync(cancellationToken);
                     }
+                    catch (SqlException sqlEx)
+                    {
+                        if (sqlEx.State == 127)
+                        {
+                            throw new JobManagement.JobConflictException(sqlEx.Message);
+                        }
 
-                    throw;
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "EnqueueAsync failed.");
-                if (ex.IsRetryable())
-                {
-                    throw new RetriableJobException(ex.Message, ex);
-                }
-
-                throw;
-            }
+                        throw;
+                    }
+                });
+            return results;
         }
 
         public async Task<IReadOnlyList<JobInfo>> GetJobByGroupIdAsync(byte queueType, long groupId, bool returnDefinition, CancellationToken cancellationToken)
         {
-            try
-            {
-                using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
-                using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
-
-                VLatest.GetJobs.PopulateCommand(sqlCommandWrapper, queueType, null, null, groupId, returnDefinition);
-                await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
-
-                return await sqlDataReader.ReadJobInfosAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "GetJobByGroupIdAsync failed.");
-                if (ex.IsRetryable())
+            IReadOnlyList<JobInfo> results = null;
+            await SqlServerFhirDataStore.ExecuteWithRetries(
+                "GetJobByGroupIdAsync",
+                async () =>
                 {
-                    throw new RetriableJobException(ex.Message, ex);
-                }
+                    using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
+                    using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
 
-                throw;
-            }
+                    VLatest.GetJobs.PopulateCommand(sqlCommandWrapper, queueType, null, null, groupId, returnDefinition);
+                    await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
+
+                    results = await sqlDataReader.ReadJobInfosAsync(cancellationToken);
+                });
+            return results;
         }
 
         public async Task<JobInfo> GetJobByIdAsync(byte queueType, long jobId, bool returnDefinition, CancellationToken cancellationToken)
         {
-            try
-            {
-                using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
-                using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
-
-                VLatest.GetJobs.PopulateCommand(sqlCommandWrapper, queueType, jobId, null, null, returnDefinition);
-                await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
-
-                return await sqlDataReader.ReadJobInfoAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "GetJobByIdAsync failed.");
-                if (ex.IsRetryable())
+            JobInfo result = null;
+            await SqlServerFhirDataStore.ExecuteWithRetries(
+                "GetJobByIdAsync",
+                async () =>
                 {
-                    throw new RetriableJobException(ex.Message, ex);
-                }
+                    using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
+                    using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
 
-                throw;
-            }
+                    VLatest.GetJobs.PopulateCommand(sqlCommandWrapper, queueType, jobId, null, null, returnDefinition);
+                    await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
+
+                    result = await sqlDataReader.ReadJobInfoAsync(cancellationToken);
+                });
+            return result;
         }
 
         public async Task<IReadOnlyList<JobInfo>> GetJobsByIdsAsync(byte queueType, long[] jobIds, bool returnDefinition, CancellationToken cancellationToken)
         {
-            try
-            {
-                using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
-                using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
-
-                VLatest.GetJobs.PopulateCommand(sqlCommandWrapper, queueType, null, jobIds.Select(i => new BigintListRow(i)), null, returnDefinition);
-                await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
-
-                return await sqlDataReader.ReadJobInfosAsync(cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "GetJobsByIdsAsync failed.");
-                if (ex.IsRetryable())
+            IReadOnlyList<JobInfo> results = null;
+            await SqlServerFhirDataStore.ExecuteWithRetries(
+                "GetJobByGroupIdAsync",
+                async () =>
                 {
-                    throw new RetriableJobException(ex.Message, ex);
-                }
+                    using SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true);
+                    using SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand();
 
-                throw;
-            }
+                    VLatest.GetJobs.PopulateCommand(sqlCommandWrapper, queueType, null, jobIds.Select(i => new BigintListRow(i)), null, returnDefinition);
+                    await using SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(cancellationToken);
+
+                    results = await sqlDataReader.ReadJobInfosAsync(cancellationToken);
+                });
+            return results;
         }
 
         public bool IsInitialized()
