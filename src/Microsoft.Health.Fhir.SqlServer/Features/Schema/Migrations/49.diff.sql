3fce17992e7c9195dc267078cd676358c629b3ab
Sergey Galuzo <sergal@microsoft.com>
2023-01-11 13:19:13 -0800

    or alter

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/49.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/49.diff.sql
@@ -85,7 +85,7 @@ INSERT INTO dbo.ResourceChangeData
 GO
 --DROP PROCEDURE dbo.MergeResources
 GO
-CREATE PROCEDURE dbo.MergeResources
+CREATE OR ALTER PROCEDURE dbo.MergeResources
 -- This stored procedure can be used for:
 -- 1. Ordinary put with single version per resource in input
 -- 2. Put with history preservation (multiple input versions per resource)
