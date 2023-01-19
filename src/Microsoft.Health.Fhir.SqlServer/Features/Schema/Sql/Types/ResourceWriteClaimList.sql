5096602ba8b373036ae314ecbfcc9ae992e4fde6
Sergey Galuzo <sergal@microsoft.com>
2023-01-19 13:08:33 -0800

    Added astrisk comments

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Types/ResourceWriteClaimList.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Types/ResourceWriteClaimList.sql
@@ -6,6 +6,7 @@ CREATE TYPE dbo.ResourceWriteClaimList AS TABLE
    ,ClaimTypeId              tinyint       NOT NULL
    ,ClaimValue               nvarchar(128) NOT NULL
 
+   --In C# we use LowerInvariant() to mimic case insensitivity, but it does not deal with asterisk insensitivity. Cannot create constraint.
    --PRIMARY KEY (ResourceSurrogateId, ClaimTypeId, ClaimValue)
 )
 GO
--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Types/TokenTextList.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Types/TokenTextList.sql
@@ -7,6 +7,7 @@ CREATE TYPE dbo.TokenTextList AS TABLE
    ,SearchParamId            smallint NOT NULL
    ,Text                     nvarchar(400) COLLATE Latin1_General_CI_AI NOT NULL
 
+   --In C# we use LowerInvariant() to mimic case insensitivity, but it does not deal with asterisk insensitivity. Cannot create constraint.
    --PRIMARY KEY (ResourceTypeId, ResourceSurrogateId, SearchParamId, Text)
 )
 GO
