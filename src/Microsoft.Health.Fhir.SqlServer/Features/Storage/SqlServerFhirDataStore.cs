d12b95002086f9273f783c54d0791b588843aa1c
Sergey Galuzo <sergal@microsoft.com>
2023-01-31 06:26:37 -0800

    timeout on get

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirDataStore.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirDataStore.cs
@@ -425,6 +425,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
             using var conn = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, false);
             using var cmd = conn.CreateRetrySqlCommand();
             VLatest.GetResources.PopulateCommand(cmd, keys.Select(_ => new ResourceKeyListRow(_model.GetResourceTypeId(_.ResourceType), _.Id, _.VersionId == null ? null : int.TryParse(_.VersionId, out var version) ? version : int.MinValue))); // put min value when cannot parse so resource will be not found
+            cmd.CommandTimeout = 180 + (int)(1200.0 / 20000 * keys.Count);
 
             using var reader = await cmd.ExecuteReaderAsync(CommandBehavior.SequentialAccess, cancellationToken);
             var resources = new List<ResourceWrapper>();
