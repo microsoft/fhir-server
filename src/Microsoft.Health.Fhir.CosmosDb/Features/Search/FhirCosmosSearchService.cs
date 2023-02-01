10f9e7bc149a58d4ec155c12f5ae9d7371ece99e
Brendan Kowitz <bkowitz@microsoft.com>
2023-02-01 11:22:16 -0800

    Expression parser should not know about RequestContext (#2926)

--- src/Microsoft.Health.Fhir.CosmosDb/Features/Search/FhirCosmosSearchService.cs
+++ src/Microsoft.Health.Fhir.CosmosDb/Features/Search/FhirCosmosSearchService.cs
@@ -102,8 +102,7 @@ namespace Microsoft.Health.Fhir.CosmosDb.Features.Search
             }
 
             // pull out the _include and _revinclude expressions.
-            bool hasIncludeOrRevIncludeExpressions = ExtractIncludeAndChainedExpressions(
-                searchOptions.Expression,
+            bool hasIncludeOrRevIncludeExpressions = searchOptions.Expression.ExtractIncludeAndChainedExpressions(
                 out Expression expressionWithoutIncludes,
                 out IReadOnlyList<IncludeExpression> includeExpressions,
                 out IReadOnlyList<IncludeExpression> revIncludeExpressions,
@@ -753,53 +752,6 @@ namespace Microsoft.Health.Fhir.CosmosDb.Features.Search
             }
 
             return true;
-        }
-
-        private static bool ExtractIncludeAndChainedExpressions(
-            Expression inputExpression,
-            out Expression expressionWithoutIncludesOrChained,
-            out IReadOnlyList<IncludeExpression> includeExpressions,
-            out IReadOnlyList<IncludeExpression> revIncludeExpressions,
-            out IReadOnlyList<ChainedExpression> chainedExpressions)
-        {
-            switch (inputExpression)
-            {
-                case IncludeExpression ie when ie.Reversed:
-                    expressionWithoutIncludesOrChained = null;
-                    includeExpressions = Array.Empty<IncludeExpression>();
-                    revIncludeExpressions = new[] { ie };
-                    chainedExpressions = Array.Empty<ChainedExpression>();
-                    return true;
-                case IncludeExpression ie:
-                    expressionWithoutIncludesOrChained = null;
-                    includeExpressions = new[] { ie };
-                    revIncludeExpressions = Array.Empty<IncludeExpression>();
-                    chainedExpressions = Array.Empty<ChainedExpression>();
-                    return true;
-                case ChainedExpression ie:
-                    expressionWithoutIncludesOrChained = null;
-                    includeExpressions = Array.Empty<IncludeExpression>();
-                    revIncludeExpressions = Array.Empty<IncludeExpression>();
-                    chainedExpressions = new[] { ie };
-                    return true;
-                case MultiaryExpression me when me.Expressions.Any(e => e is IncludeExpression || e is ChainedExpression):
-                    includeExpressions = me.Expressions.OfType<IncludeExpression>().Where(ie => !ie.Reversed).ToList();
-                    revIncludeExpressions = me.Expressions.OfType<IncludeExpression>().Where(ie => ie.Reversed).ToList();
-                    chainedExpressions = me.Expressions.OfType<ChainedExpression>().ToList();
-                    expressionWithoutIncludesOrChained = (me.Expressions.Count - (includeExpressions.Count + revIncludeExpressions.Count + chainedExpressions.Count)) switch
-                    {
-                        0 => null,
-                        1 => me.Expressions.Single(e => !(e is IncludeExpression || e is ChainedExpression)),
-                        _ => new MultiaryExpression(me.MultiaryOperation, me.Expressions.Where(e => !(e is IncludeExpression || e is ChainedExpression)).ToList()),
-                    };
-                    return true;
-                default:
-                    expressionWithoutIncludesOrChained = inputExpression;
-                    includeExpressions = Array.Empty<IncludeExpression>();
-                    revIncludeExpressions = Array.Empty<IncludeExpression>();
-                    chainedExpressions = Array.Empty<ChainedExpression>();
-                    return false;
-            }
         }
     }
 }
