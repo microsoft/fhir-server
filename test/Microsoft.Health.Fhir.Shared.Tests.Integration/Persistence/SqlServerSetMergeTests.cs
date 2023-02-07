216d143237f4bad04b4cc029ff9e28ac38908cda
Sergey Galuzo <sergal@microsoft.com>
2023-02-06 16:37:29 -0800

    no using

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerSetMergeTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerSetMergeTests.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using Hl7.Fhir.Model;
 using Hl7.Fhir.Serialization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
