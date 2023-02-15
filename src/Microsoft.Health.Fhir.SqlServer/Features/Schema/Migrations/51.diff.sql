7cff931551f0f750dc1c24c7de825c48b8e904b9
Matt Serdar <99214729+v-mserdar@users.noreply.github.com>
2023-02-15 09:37:07 -0800

    revised index so that it doesn't drop

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/51.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/51.diff.sql
@@ -1,8 +1,7 @@
 ﻿-- Adding a new index so that the reindex job can make use of getting counts in large dbs
 GO
-DROP INDEX IF EXISTS IX_Resource_ResourceTypeId_SearchParamHash ON dbo.Resource
-GO
-
-CREATE NONCLUSTERED INDEX IX_Resource_ResourceTypeId_SearchParamHash
-    ON dbo.Resource(ResourceTypeId ASC, SearchParamHash ASC);
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_Resource_ResourceTypeId_SearchParamHash' AND object_id = object_id('Resource')) BEGIN
+	CREATE NONCLUSTERED INDEX IX_Resource_ResourceTypeId_SearchParamHash
+		ON dbo.Resource(ResourceTypeId ASC, SearchParamHash ASC);
+END
 GO
