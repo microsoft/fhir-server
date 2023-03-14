1ee2c11f0de7180c6c153a10143b75e672923ce0
Brendan Kowitz <bkowitz@microsoft.com>
2023-03-14 16:39:51 -0700

    Adds more retries in cosmosqueueclient (#3174)

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/QueueClientTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/QueueClientTests.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
 
     [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
     [Trait(Traits.Category, Categories.DataSourceValidation)]
-    [FhirStorageTestsFixtureArgumentSets(DataStore.SqlServer)]
+    [FhirStorageTestsFixtureArgumentSets(DataStore.All)]
     public class QueueClientTests : IClassFixture<FhirStorageTestsFixture>
     {
         private readonly ITestOutputHelper _testOutputHelper;
@@ -156,7 +156,8 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
                 new CancellationTokenSource());
             await Task.Delay(TimeSpan.FromSeconds(1));
             JobInfo jobInfo2 = await _queueClient.DequeueAsync(queueType, "test-worker", 0, CancellationToken.None);
-            Assert.Equal(jobInfo1.Result, jobInfo2.Result);
+
+            Assert.Equal(jobInfo1.Result, jobInfo2?.Result);
         }
 
         [Fact]
