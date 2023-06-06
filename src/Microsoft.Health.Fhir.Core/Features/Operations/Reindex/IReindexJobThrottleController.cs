f39048e51d03e979613afa0d2299243ff40ddf4e
Paul Taladay <patalada@microsoft.com>
2023-06-06 13:38:02 -0700

    Revert "Removed throttlecontroller"
    This reverts commit e03c72283a3154eabfefde383bbd24bb668cf48c.

---
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/Reindex/IReindexJobThrottleController.cs
@@ -0,0 +1,35 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Fhir.Core.Features.Operations.Reindex.Models;
+
+namespace Microsoft.Health.Fhir.Core.Features.Operations.Reindex
+{
+    public interface IReindexJobThrottleController
+    {
+        void Initialize(ReindexJobRecord reindexJobRecord, int? provisionedDatastoreCapacity);
+
+        /// <summary>
+        /// Gets the current delay to achieve a target resource utilization
+        /// </summary>
+        /// <returns>delay in milliseconds</returns>
+        int GetThrottleBasedDelay();
+
+        /// <summary>
+        /// Captures the currently recorded database consumption
+        /// </summary>
+        /// <returns>Returns an average database resource consumtion per second</returns>
+        double UpdateDatastoreUsage();
+
+        /// <summary>
+        /// If one single query consumes more than the target datastore resources
+        /// reduce the batch size to help acheive the desired level of usage
+        /// If one query is not too expensive, this will return the same number as is configured
+        /// for the Reindex job.
+        /// </summary>
+        /// <returns>The query batch size</returns>
+        public uint GetThrottleBatchSize();
+    }
+}
