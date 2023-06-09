592a3086840ae0b1cd56db961ecc78f3507c740e
LTA-Thinking <rbrucej@gmail.com>
2023-06-09 09:43:51 -0700

    Add notification on Bulk Delete job finishing

--- src/Microsoft.Health.Fhir.Core/Features/Operations/BulkDelete/BulkDeleteProcessingJob.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/BulkDelete/BulkDeleteProcessingJob.cs
@@ -8,12 +8,14 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using EnsureThat;
+using MediatR;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Health.Core.Features.Context;
 using Microsoft.Health.Extensions.DependencyInjection;
 using Microsoft.Health.Fhir.Core.Features.Context;
 using Microsoft.Health.Fhir.Core.Features.Operations;
 using Microsoft.Health.Fhir.Core.Features.Operations.BulkDelete;
+using Microsoft.Health.Fhir.Core.Features.Operations.BulkDelete.Mediator;
 using Microsoft.Health.Fhir.Core.Features.Persistence;
 using Microsoft.Health.Fhir.Core.Messages.Delete;
 using Microsoft.Health.JobManagement;
@@ -26,13 +28,16 @@ namespace Microsoft.Health.Fhir.Api.Features.Operations.BulkDelete
     {
         private readonly Func<IScoped<IDeleter>> _deleterFactory;
         private readonly RequestContextAccessor<IFhirRequestContext> _contextAccessor;
+        private readonly IMediator _mediator;
 
         public BulkDeleteProcessingJob(
             Func<IScoped<IDeleter>> deleterFactory,
-            RequestContextAccessor<IFhirRequestContext> contextAccessor)
+            RequestContextAccessor<IFhirRequestContext> contextAccessor,
+            IMediator mediator)
         {
             _deleterFactory = EnsureArg.IsNotNull(deleterFactory, nameof(deleterFactory));
             _contextAccessor = EnsureArg.IsNotNull(contextAccessor, nameof(contextAccessor));
+            _mediator = EnsureArg.IsNotNull(mediator, nameof(mediator));
         }
 
         public async Task<string> ExecuteAsync(JobInfo jobInfo, IProgress<string> progress, CancellationToken cancellationToken)
@@ -61,8 +66,12 @@ namespace Microsoft.Health.Fhir.Api.Features.Operations.BulkDelete
 
                 using IScoped<IDeleter> deleter = _deleterFactory();
                 var itemsDeleted = await deleter.Value.DeleteMultipleAsync(new ConditionalDeleteResourceRequest(definition.Type, (IReadOnlyList<Tuple<string, string>>)definition.SearchParameters, definition.DeleteOperation, -1), cancellationToken);
+
                 var result = new BulkDeleteResult();
                 result.ResourcesDeleted.Add(definition.Type, itemsDeleted);
+
+                await _mediator.Publish(new BulkDeleteMetricsNotification(jobInfo.Id, itemsDeleted), cancellationToken);
+
                 return JsonConvert.SerializeObject(result);
             }
             finally
---
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/BulkDelete/Mediator/BulkDeleteMetricsNotification.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Fhir.Core.Features.Metrics;
+using Microsoft.Health.Fhir.ValueSets;
+
+namespace Microsoft.Health.Fhir.Core.Features.Operations.BulkDelete.Mediator
+{
+    public class BulkDeleteMetricsNotification : IMetricsNotification
+    {
+        public BulkDeleteMetricsNotification(long id, long resourcesDeleted)
+        {
+            FhirOperation = AuditEventSubType.BulkDelete;
+            ResourceType = null;
+
+            JobId = id;
+            ResourcesDeleted = resourcesDeleted;
+            Status = OperationStatus.Completed;
+        }
+
+        public string FhirOperation { get; }
+
+        public string ResourceType { get; }
+
+        public long ResourcesDeleted { get; }
+
+        public long JobId { get; }
+
+        public OperationStatus Status { get; }
+    }
+}
