62145fdd587ea1e1a8e9b9a328725eaf7ccd26a4
Sergey Galuzo <sergal@microsoft.com>
2023-07-22 18:56:25 -0700

    correct version

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/SchemaVersionConstants.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/SchemaVersionConstants.cs
@@ -8,7 +8,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Schema
     public static class SchemaVersionConstants
     {
         public const int Min = (int)SchemaVersion.V61;
-        public const int Max = (int)SchemaVersion.V60;
+        public const int Max = (int)SchemaVersion.V61;
         public const int MinForUpgrade = (int)SchemaVersion.V56; // this is used for upgrade tests only
         public const int SearchParameterStatusSchemaVersion = (int)SchemaVersion.V6;
         public const int SupportForReferencesWithMissingTypeVersion = (int)SchemaVersion.V7;
