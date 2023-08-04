ca61af0bf3a3b2d4f276aa3ab8e728e884be9b21
Sergey Galuzo <sergal@microsoft.com>
2023-08-03 17:26:33 -0700

    remove time dependent function

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/Operations/Import/SqlServerIndexesRebuildTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/Operations/Import/SqlServerIndexesRebuildTests.cs
@@ -285,7 +285,8 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Features.Operations.Import
                 {
                     //// Our home grown SQL schema generator does not understand that statements can be formatted differently but contain identical SQL
                     //// Skipping queue objects
-                    var objectsToSkip = new[] { "DequeueJob", "EnqueueJobs", "GetJobs", "GetResourcesByTypeAndSurrogateIdRange", "GetResourceSurrogateIdRanges", "LogEvent", "PutJobCancelation", "PutJobHeartbeat", "PutJobStatus", "CompartmentAssignment" };
+                    var objectsToSkip = new[] { "DequeueJob", "EnqueueJobs", "GetJobs", "GetResourcesByTypeAndSurrogateIdRange", "GetResourceSurrogateIdRanges", "LogEvent", "PutJobCancelation", "PutJobHeartbeat", "PutJobStatus", "CompartmentAssignment" }.ToList();
+                    objectsToSkip.Add("PartitionFunction_ResourceChangeData_Timestamp"); // definition is not predictable as it has start time component
                     if (schemaDifference.SourceObject != null && objectsToSkip.Any(_ => schemaDifference.SourceObject.Name.ToString().Contains(_)))
                     {
                         continue;
