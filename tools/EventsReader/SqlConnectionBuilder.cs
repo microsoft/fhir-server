627c0a31044d21756a1439318e873d28a6a915ca
Richa Bansal <ribans@microsoft.com>
2023-08-14 12:24:54 -0700

    revert

--- tools/EventsReader/SqlConnectionBuilder.cs
+++ tools/EventsReader/SqlConnectionBuilder.cs
@@ -18,7 +18,8 @@ namespace Microsoft.Health.Internal.Fhir.EventsReader
         }
 
         #pragma warning disable CA2000
-        public async Task<SqlConnection> GetSqlConnectionAsync(string initialCatalog = null, CancellationToken cancellationToken = default)
+
+        public async Task<SqlConnection> GetSqlConnectionAsync(string initialCatalog = null, int? maxPoolSize = null, CancellationToken cancellationToken = default)
         {
             await Task.CompletedTask;
             return new SqlConnection(_connectionString);
