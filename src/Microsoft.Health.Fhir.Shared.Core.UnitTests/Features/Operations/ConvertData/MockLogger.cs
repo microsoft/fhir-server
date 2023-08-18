e1e667c0f84a9afc1af6d6cb0f33f53748ac4a60
Will Yochum <wiyochum@microsoft.com>
2023-08-18 12:43:57 -0400

    add namespace

--- src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Operations/ConvertData/MockLogger.cs
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Operations/ConvertData/MockLogger.cs
@@ -6,14 +6,17 @@
 using System;
 using Microsoft.Extensions.Logging;
 
-public abstract class MockLogger<T> : ILogger<T>
+namespace Microsoft.Health.Fhir.Shared.Core.UnitTests.Features.Operations.ConvertData
 {
-    void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-     => Log(logLevel, formatter(state, exception));
+    public abstract class MockLogger<T> : ILogger<T>
+    {
+        void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+         => Log(logLevel, formatter(state, exception));
 
-    public abstract void Log(LogLevel logLevel, object state, Exception exception = null);
+        public abstract void Log(LogLevel logLevel, object state, Exception exception = null);
 
-    public virtual bool IsEnabled(LogLevel logLevel) => true;
+        public virtual bool IsEnabled(LogLevel logLevel) => true;
 
-    public abstract IDisposable BeginScope<TState>(TState state);
+        public abstract IDisposable BeginScope<TState>(TState state);
+    }
 }
