70984d632cfef747ec6afb456dee5d2494c84cb4
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2023-10-05 18:33:04 -0700

    Adding new Encryption section to FHIR service.

--- src/Microsoft.Health.Fhir.Core/Configs/CustomerManagedKeyEncryption.cs
+++ src/Microsoft.Health.Fhir.Core/Configs/CustomerManagedKeyEncryption.cs
@@ -4,10 +4,6 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.Health.Fhir.Core.Configs
 {
--- src/Microsoft.Health.Fhir.Core/Configs/EncryptionConfiguration.cs
+++ src/Microsoft.Health.Fhir.Core/Configs/EncryptionConfiguration.cs
@@ -3,12 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Microsoft.Health.Fhir.Core.Configs
 {
     public sealed class EncryptionConfiguration
