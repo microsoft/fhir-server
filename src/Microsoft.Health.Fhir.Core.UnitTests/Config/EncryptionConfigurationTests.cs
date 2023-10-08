91241e1e509a81c201b48bed1d7f6b77216ad109
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2023-10-08 16:42:00 -0700

    Adding new method to identify when encryption configuration is set. Adding new tests.

---
+++ src/Microsoft.Health.Fhir.Core.UnitTests/Config/EncryptionConfigurationTests.cs
@@ -0,0 +1,77 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using Microsoft.Health.Fhir.Core.Configs;
+using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.Core.UnitTests.Config
+{
+    [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
+    [Trait(Traits.Category, Categories.Operations)]
+    public sealed class EncryptionConfigurationTests
+    {
+        [Fact]
+        public void GivenAnEncryptionConfiguration_WhenContainsValues_ReturnAsEncryptionConfigurationSet()
+        {
+            EncryptionConfiguration encryption0 = new EncryptionConfiguration()
+            {
+                CustomerManagedKeyEncryption = new CustomerManagedKeyEncryption()
+                {
+                    KeyEncryptionKeyIdentity = new EncryptionKeyIdentity()
+                    {
+                        FederatedClientId = "food",
+                    },
+                },
+            };
+            Assert.True(encryption0.IsEncryptionSet());
+
+            EncryptionConfiguration encryption1 = new EncryptionConfiguration()
+            {
+                CustomerManagedKeyEncryption = new CustomerManagedKeyEncryption()
+                {
+                    KeyEncryptionKeyUrl = new Uri("htts://fhir.com"),
+                },
+            };
+            Assert.True(encryption1.IsEncryptionSet());
+        }
+
+        [Fact]
+        public void GivenAnEncryptionConfiguration_WhenThereAreNoValues_ReturnAsEncryptionConfigurationNotSet()
+        {
+            EncryptionConfiguration encryption0 = new EncryptionConfiguration();
+            Assert.False(encryption0.IsEncryptionSet());
+
+            EncryptionConfiguration encryption1 = new EncryptionConfiguration()
+            {
+                CustomerManagedKeyEncryption = new CustomerManagedKeyEncryption()
+                {
+                    KeyEncryptionKeyIdentity = new EncryptionKeyIdentity()
+                    {
+                        FederatedClientId = null,
+                    },
+                },
+            };
+            Assert.False(encryption1.IsEncryptionSet());
+
+            EncryptionConfiguration encryption2 = new EncryptionConfiguration()
+            {
+                CustomerManagedKeyEncryption = new CustomerManagedKeyEncryption()
+                {
+                    KeyEncryptionKeyUrl = null,
+                },
+            };
+            Assert.False(encryption2.IsEncryptionSet());
+
+            EncryptionConfiguration encryption3 = new EncryptionConfiguration()
+            {
+                CustomerManagedKeyEncryption = null,
+            };
+            Assert.False(encryption3.IsEncryptionSet());
+        }
+    }
+}
--- src/Microsoft.Health.Fhir.Core/Configs/EncryptionConfiguration.cs
+++ src/Microsoft.Health.Fhir.Core/Configs/EncryptionConfiguration.cs
@@ -8,5 +8,26 @@ namespace Microsoft.Health.Fhir.Core.Configs
     public sealed class EncryptionConfiguration
     {
         public CustomerManagedKeyEncryption CustomerManagedKeyEncryption { get; set; }
+
+        public bool IsEncryptionSet()
+        {
+            if (CustomerManagedKeyEncryption != null)
+            {
+                if (CustomerManagedKeyEncryption.KeyEncryptionKeyUrl != null)
+                {
+                    return true;
+                }
+
+                if (CustomerManagedKeyEncryption.KeyEncryptionKeyIdentity != null)
+                {
+                    if (!string.IsNullOrEmpty(CustomerManagedKeyEncryption.KeyEncryptionKeyIdentity.FederatedClientId))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
