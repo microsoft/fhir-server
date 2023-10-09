b3d50c69809eaccb0f7c3080e09de4a4e4d0e2f4
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2023-10-09 08:47:26 -0700

    Fix constant in test.

--- src/Microsoft.Health.Fhir.Core.UnitTests/Config/EncryptionConfigurationTests.cs
+++ src/Microsoft.Health.Fhir.Core.UnitTests/Config/EncryptionConfigurationTests.cs
@@ -24,7 +24,7 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Config
                 {
                     KeyEncryptionKeyIdentity = new EncryptionKeyIdentity()
                     {
-                        FederatedClientId = "food",
+                        FederatedClientId = "foo",
                     },
                 },
             };
