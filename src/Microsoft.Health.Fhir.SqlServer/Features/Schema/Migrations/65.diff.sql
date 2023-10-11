7e1884d262706b2b770402fc339ee878cfbd7a4d
Sergey Galuzo <sergal@microsoft.com>
2023-10-11 16:07:00 -0700

    2 stored procs

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/65.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/65.diff.sql
@@ -1,6 +1,44 @@
-﻿--DROP PROCEDURE dbo.DequeueJob
+ALTER PROCEDURE dbo.GetActiveJobs @QueueType tinyint, @GroupId bigint = NULL
+AS
+set nocount on
+DECLARE @SP varchar(100) = 'GetActiveJobs'
+       ,@Mode varchar(100) = 'Q='+isnull(convert(varchar,@QueueType),'NULL')
+                           +' G='+isnull(convert(varchar,@GroupId),'NULL')
+       ,@st datetime = getUTCdate()
+       ,@JobIds BigintList
+       ,@PartitionId tinyint
+       ,@MaxPartitions tinyint = 16 -- !!! hardcoded
+       ,@LookedAtPartitions tinyint = 0
+       ,@Rows int = 0
+
+BEGIN TRY
+  SET @PartitionId = @MaxPartitions * rand()
+
+  WHILE @LookedAtPartitions < @MaxPartitions
+  BEGIN
+    IF @GroupId IS NULL
+      INSERT INTO @JobIds SELECT JobId FROM dbo.JobQueue WHERE PartitionId = @PartitionId AND QueueType = @QueueType AND Status IN (0,1)
+    ELSE
+      INSERT INTO @JobIds SELECT JobId FROM dbo.JobQueue WHERE PartitionId = @PartitionId AND QueueType = @QueueType AND GroupId = @GroupId AND Status IN (0,1)
+
+    SET @Rows += @@rowcount
+
+    SET @PartitionId = CASE WHEN @PartitionId = 15 THEN 0 ELSE @PartitionId + 1 END
+    SET @LookedAtPartitions += 1 
+  END
+
+  IF @Rows > 0
+    EXECUTE dbo.GetJobs @QueueType = @QueueType, @JobIds = @JobIds
+
+  EXECUTE dbo.LogEvent @Process=@SP,@Mode=@Mode,@Status='End',@Start=@st,@Rows=@Rows
+END TRY
+BEGIN CATCH
+  IF error_number() = 1750 THROW -- Real error is before 1750, cannot trap in SQL.
+  EXECUTE dbo.LogEvent @Process=@SP,@Mode=@Mode,@Status='Error';
+  THROW
+END CATCH
 GO
-CREATE PROCEDURE dbo.DequeueJob @QueueType tinyint, @Worker varchar(100), @HeartbeatTimeoutSec int, @InputJobId bigint = NULL, @CheckTimeoutJobs bit = 0
+ALTER PROCEDURE dbo.DequeueJob @QueueType tinyint, @Worker varchar(100), @HeartbeatTimeoutSec int, @InputJobId bigint = NULL, @CheckTimeoutJobs bit = 0
 AS
 set nocount on
 DECLARE @SP varchar(100) = 'DequeueJob'
