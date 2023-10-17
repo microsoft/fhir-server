1cf0345c7656bc75c9f585ae49dc3fc9e6dda3ef
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2023-10-17 15:26:26 -0700

    [CMK] Encryption configuration + New FHIR runtime configurations (#3542)
    * New FHIR runtime configurations.
    New configuration classes to handle encryption.
    
    * Adding new Encryption section to FHIR service.
    
    * Adding new method to identify when encryption configuration is set.
    Adding new tests.
    
    * Fix constant in test.
    
    * Making sure that KeyEncryptionKeyUrl is required and FederatedClientId is optional.

---
+++ src/Microsoft.Health.Fhir.Core/Configs/EncryptionConfiguration.cs
@@ -0,0 +1,27 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Fhir.Core.Configs
+{
+    public sealed class EncryptionConfiguration
+    {
+        public CustomerManagedKeyEncryption CustomerManagedKeyEncryption { get; set; }
+
+        public bool IsEncryptionSet()
+        {
+            if (CustomerManagedKeyEncryption != null)
+            {
+                // KeyEncryptionKeyUrl is the only required property.
+                // KeyEncryptionKeyIdentity.FederatedClientId is optional.
+                if (CustomerManagedKeyEncryption.KeyEncryptionKeyUrl != null)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
