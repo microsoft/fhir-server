3eb27cc05d1aaa57041b4feef86d79874fc64954
Richa Bansal <ribans@microsoft.com>
2023-10-18 16:34:32 -0700

    Rename duration property for shoebox logs

--- src/Microsoft.Health.Fhir.Api/Features/Audit/AuditHelper.cs
+++ src/Microsoft.Health.Fhir.Api/Features/Audit/AuditHelper.cs
@@ -28,6 +28,7 @@ namespace Microsoft.Health.Fhir.Api.Features.Audit
     /// </summary>
     public class AuditHelper : IAuditHelper
     {
+        internal const string ProcessingDurationMs = "ProcessingDurationMs";
         internal const string DefaultCallerAgent = "Microsoft.Health.Fhir.Server";
         internal const string UnknownOperationType = "Unknown";
 
@@ -122,7 +123,7 @@ namespace Microsoft.Health.Fhir.Api.Features.Audit
                 if (durationMs != null)
                 {
                     additionalProperties = new Dictionary<string, string>();
-                    additionalProperties["durationMs"] = durationMs.ToString();
+                    additionalProperties[ProcessingDurationMs] = durationMs.ToString();
                 }
 
                 _auditLogger.LogAudit(
