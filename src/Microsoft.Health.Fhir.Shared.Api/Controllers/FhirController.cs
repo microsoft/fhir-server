f3796b567d1c1aba271ae2d144f02e7881cd20d2
Abigail Nicolas Sayago <abigailn@microsoft.com>
2023-10-18 15:29:53 -0700

    Add Conditional Update validation and tests

--- src/Microsoft.Health.Fhir.Shared.Api/Controllers/FhirController.cs
+++ src/Microsoft.Health.Fhir.Shared.Api/Controllers/FhirController.cs
@@ -229,6 +229,7 @@ namespace Microsoft.Health.Fhir.Api.Controllers
         [HttpPut]
         [Route(KnownRoutes.ResourceType)]
         [AuditEventType(AuditEventSubType.ConditionalUpdate)]
+        [ServiceFilter(typeof(SearchParameterFilterAttribute))]
         public async Task<IActionResult> ConditionalUpdate([FromBody] Resource resource)
         {
             IReadOnlyList<Tuple<string, string>> conditionalParameters = GetQueriesForSearch();
