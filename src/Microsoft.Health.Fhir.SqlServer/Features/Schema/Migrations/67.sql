9cec2fb6cebfcf5f60c2d5c69e0cc85f05de76e2
SergeyGaluzo <95932081+SergeyGaluzo@users.noreply.github.com>
2023-10-30 20:20:07 -0700

    Updating versioin 66 to 67 (#3577)

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/67.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/67.sql
@@ -15,7 +15,7 @@ IF EXISTS (SELECT *
 
 GO
 INSERT  INTO dbo.SchemaVersion
-VALUES (66, 'started');
+VALUES (67, 'started');
 
 CREATE PARTITION FUNCTION PartitionFunction_ResourceTypeId(SMALLINT)
     AS RANGE RIGHT
