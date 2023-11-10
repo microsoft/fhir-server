5f1faeedc98b412c19caa0de8959f9977a790db3
Paul Taladay <patalada@microsoft.com>
2023-11-10 15:50:59 -0800

    Added spacing between method names.

--- src/Microsoft.Health.Fhir.Core/Features/Search/Registry/ISearchParameterStatusManager.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Search/Registry/ISearchParameterStatusManager.cs
@@ -13,12 +13,19 @@ namespace Microsoft.Health.Fhir.Core.Features.Search.Registry
     public interface ISearchParameterStatusManager
     {
         Task AddSearchParameterStatusAsync(IReadOnlyCollection<string> searchParamUris, CancellationToken cancellationToken);
+
         Task ApplySearchParameterStatus(IReadOnlyCollection<ResourceSearchParameterStatus> updatedSearchParameterStatus, CancellationToken cancellationToken);
+
         Task DeleteSearchParameterStatusAsync(string url, CancellationToken cancellationToken);
+
         Task EnsureInitializedAsync(CancellationToken cancellationToken);
+
         Task<IReadOnlyCollection<ResourceSearchParameterStatus>> GetAllSearchParameterStatus(CancellationToken cancellationToken);
+
         Task<IReadOnlyCollection<ResourceSearchParameterStatus>> GetSearchParameterStatusUpdates(CancellationToken cancellationToken);
+
         Task Handle(SearchParameterDefinitionManagerInitialized notification, CancellationToken cancellationToken);
+
         Task UpdateSearchParameterStatusAsync(IReadOnlyCollection<string> searchParameterUris, SearchParameterStatus status, CancellationToken cancellationToken);
     }
 }
