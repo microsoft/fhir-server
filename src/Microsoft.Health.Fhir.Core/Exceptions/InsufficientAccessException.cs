75ff02f5c43454e68e10b121487a6738c3079fd0
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2023-11-23 14:18:57 -0800

    Insufficient access exception

---
+++ src/Microsoft.Health.Fhir.Core/Exceptions/InsufficientAccessException.cs
@@ -0,0 +1,20 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Health.Fhir.Core.Exceptions
+{
+    /// <summary>
+    /// Exception raised when the service does not have sufficient permissions to execute an action.
+    /// </summary>
+    public class InsufficientAccessException : FhirException
+    {
+        public InsufficientAccessException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
