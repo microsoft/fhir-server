8b20fce0ffd0534a13ce819c20b72a371b5dcf88
Paul Taladay <patalada@microsoft.com>
2023-12-01 13:34:59 -0800

    Uncommenting ReindexJobTests to verify if those are causing issues as well. It looks to be targetted withing the reindexorchestratorJobTests

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/Operations/Reindex/ReindexJobTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/Operations/Reindex/ReindexJobTests.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-/*
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -1069,4 +1068,3 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Features.Operations.Reindex
         }
     }
 }
-*/
