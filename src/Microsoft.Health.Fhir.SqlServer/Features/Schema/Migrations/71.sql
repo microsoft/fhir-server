ca9c310e9d0d84590552e28d6736f854279622f1
Sergey Galuzo <sergal@microsoft.com>
2023-12-21 19:27:39 -0800

    no join in delete

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/71.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/71.sql
@@ -3376,12 +3376,10 @@ BEGIN TRY
                    ON B.ResourceTypeId = @ResourceTypeId
                       AND B.ResourceSurrogateId = A.ResourceSurrogateId
             OPTION (MAXDOP 1);
-            DELETE B
-            FROM   @SurrogateIds AS A
-                   INNER LOOP JOIN
-                   dbo.TokenSearchParam AS B WITH (INDEX (1), FORCESEEK, PAGLOCK)
-                   ON B.ResourceTypeId = @ResourceTypeId
-                      AND B.ResourceSurrogateId = A.ResourceSurrogateId
+            DELETE dbo.TokenSearchParam
+            WHERE  ResourceTypeId = @ResourceTypeId
+                   AND ResourceSurrogateId IN (SELECT ResourceSurrogateId
+                                               FROM   @SurrogateIds)
             OPTION (MAXDOP 1);
             DELETE B
             FROM   @SurrogateIds AS A
@@ -3390,12 +3388,10 @@ BEGIN TRY
                    ON B.ResourceTypeId = @ResourceTypeId
                       AND B.ResourceSurrogateId = A.ResourceSurrogateId
             OPTION (MAXDOP 1);
-            DELETE B
-            FROM   @SurrogateIds AS A
-                   INNER LOOP JOIN
-                   dbo.StringSearchParam AS B WITH (INDEX (1), FORCESEEK, PAGLOCK)
-                   ON B.ResourceTypeId = @ResourceTypeId
-                      AND B.ResourceSurrogateId = A.ResourceSurrogateId
+            DELETE dbo.StringSearchParam
+            WHERE  ResourceTypeId = @ResourceTypeId
+                   AND ResourceSurrogateId IN (SELECT ResourceSurrogateId
+                                               FROM   @SurrogateIds)
             OPTION (MAXDOP 1);
             DELETE B
             FROM   @SurrogateIds AS A
@@ -5530,133 +5526,6 @@ GO
 IF EXISTS (SELECT *
            FROM   sys.objects
            WHERE  type = 'u'
-                  AND name = 'TokenSearchParam')
-    EXECUTE sp_rename 'TokenSearchParam', 'TokenSearchParam_Table';
-
-
-GO
-IF EXISTS (SELECT *
-           FROM   sys.objects
-           WHERE  type = 'v'
-                  AND name = 'TokenSearchParam')
-    DROP VIEW dbo.TokenSearchParam;
-
-
-GO
-IF object_id('tempdb..#RTs') IS NOT NULL
-    DROP TABLE #RTs;
-
-
-GO
-DECLARE @Template AS VARCHAR (MAX) = '
-IF object_id(''TokenSearchParam_XXX'') IS NULL
-  CREATE TABLE dbo.TokenSearchParam_XXX
-  (
-      ResourceTypeId       smallint     NOT NULL
-     ,ResourceSurrogateId  bigint       NOT NULL
-     ,SearchParamId        smallint     NOT NULL
-     ,SystemId             int          NULL
-     ,Code                 varchar(256) COLLATE Latin1_General_100_CS_AS NOT NULL
-     ,CodeOverflow         varchar(max) COLLATE Latin1_General_100_CS_AS NULL
-   
-     ,CONSTRAINT CHK_TokenSearchParam_XXX_CodeOverflow CHECK (len(Code) = 256 OR CodeOverflow IS NULL)
-     ,CONSTRAINT CHK_TokenSearchParam_XXX_ResourceTypeId_XXX CHECK (ResourceTypeId = XXX)
-  )
-
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = object_id(''TokenSearchParam_XXX'') AND name = ''IXC_ResourceSurrogateId_SearchParamId'')
-  CREATE CLUSTERED INDEX IXC_ResourceSurrogateId_SearchParamId ON dbo.TokenSearchParam_XXX (ResourceSurrogateId, SearchParamId) 
-    WITH (DATA_COMPRESSION = PAGE)
-
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = object_id(''TokenSearchParam_XXX'') AND name = ''IX_SearchParamId_Code_INCLUDE_SystemId'')
-  CREATE INDEX IX_SearchParamId_Code_INCLUDE_SystemId ON dbo.TokenSearchParam_XXX (SearchParamId, Code) INCLUDE (SystemId) 
-    WITH (DATA_COMPRESSION = PAGE)', @CreateTable AS VARCHAR (MAX), @CreateView AS VARCHAR (MAX) = '
-CREATE VIEW dbo.TokenSearchParam
-AS', @InsertTrigger AS VARCHAR (MAX) = '
-CREATE TRIGGER dbo.TokenSearchParamIns ON dbo.TokenSearchParam INSTEAD OF INSERT
-AS
-set nocount on', @DeleteTrigger AS VARCHAR (MAX) = '
-CREATE TRIGGER dbo.TokenSearchParamDel ON dbo.TokenSearchParam INSTEAD OF DELETE
-AS
-set nocount on';
-
-SELECT RT
-INTO   #RTs
-FROM   (SELECT 4 AS RT
-        UNION
-        SELECT 14
-        UNION
-        SELECT 15
-        UNION
-        SELECT 19
-        UNION
-        SELECT 28
-        UNION
-        SELECT 35
-        UNION
-        SELECT 40
-        UNION
-        SELECT 44
-        UNION
-        SELECT 53
-        UNION
-        SELECT 61
-        UNION
-        SELECT 62
-        UNION
-        SELECT 76
-        UNION
-        SELECT 79
-        UNION
-        SELECT 96
-        UNION
-        SELECT 100
-        UNION
-        SELECT 103
-        UNION
-        SELECT 108
-        UNION
-        SELECT 110
-        UNION
-        SELECT 138) AS A;
-
-DECLARE @RT AS VARCHAR (100), @First AS BIT = 1;
-
-WHILE EXISTS (SELECT *
-              FROM   #RTs)
-    BEGIN
-        SET @RT = (SELECT TOP 1 RT
-                   FROM   #RTs);
-        SET @CreateTable = @Template;
-        SET @CreateTable = replace(@CreateTable, 'XXX', @RT);
-        EXECUTE (@CreateTable);
-        IF @First = 0
-            SET @CreateView = @CreateView + '
-UNION ALL';
-        SET @CreateView = @CreateView + '
-SELECT *, IsHistory = convert(bit,0) FROM dbo.TokenSearchParam_' + @RT;
-        SET @First = 0;
-        SET @InsertTrigger = @InsertTrigger + '
-INSERT INTO dbo.TokenSearchParam_' + @RT + ' 
-        (ResourceTypeId,ResourceSurrogateId,SearchParamId,SystemId,Code,CodeOverflow) 
-  SELECT ResourceTypeId,ResourceSurrogateId,SearchParamId,SystemId,Code,CodeOverflow 
-    FROM Inserted 
-    WHERE ResourceTypeId = ' + @RT;
-        SET @DeleteTrigger = @DeleteTrigger + '
-DELETE FROM dbo.TokenSearchParam_' + @RT + ' WHERE ResourceTypeId = ' + @RT + ' AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM Deleted WHERE ResourceTypeId = ' + @RT + ')';
-        DELETE #RTs
-        WHERE  RT = @RT;
-    END
-
-EXECUTE (@CreateView);
-
-EXECUTE (@InsertTrigger);
-
-EXECUTE (@DeleteTrigger);
-
-GO
-IF EXISTS (SELECT *
-           FROM   sys.objects
-           WHERE  type = 'u'
                   AND name = 'StringSearchParam')
     EXECUTE sp_rename 'StringSearchParam', 'StringSearchParam_Table';
 
@@ -5770,6 +5639,133 @@ INSERT INTO dbo.StringSearchParam_' + @RT + '
     WHERE ResourceTypeId = ' + @RT;
         SET @DeleteTrigger = @DeleteTrigger + '
 DELETE FROM dbo.StringSearchParam_' + @RT + ' WHERE ResourceTypeId = ' + @RT + ' AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM Deleted WHERE ResourceTypeId = ' + @RT + ')';
+        DELETE #RTs
+        WHERE  RT = @RT;
+    END
+
+EXECUTE (@CreateView);
+
+EXECUTE (@InsertTrigger);
+
+EXECUTE (@DeleteTrigger);
+
+GO
+IF EXISTS (SELECT *
+           FROM   sys.objects
+           WHERE  type = 'u'
+                  AND name = 'TokenSearchParam')
+    EXECUTE sp_rename 'TokenSearchParam', 'TokenSearchParam_Table';
+
+
+GO
+IF EXISTS (SELECT *
+           FROM   sys.objects
+           WHERE  type = 'v'
+                  AND name = 'TokenSearchParam')
+    DROP VIEW dbo.TokenSearchParam;
+
+
+GO
+IF object_id('tempdb..#RTs') IS NOT NULL
+    DROP TABLE #RTs;
+
+
+GO
+DECLARE @Template AS VARCHAR (MAX) = '
+IF object_id(''TokenSearchParam_XXX'') IS NULL
+  CREATE TABLE dbo.TokenSearchParam_XXX
+  (
+      ResourceTypeId       smallint     NOT NULL
+     ,ResourceSurrogateId  bigint       NOT NULL
+     ,SearchParamId        smallint     NOT NULL
+     ,SystemId             int          NULL
+     ,Code                 varchar(256) COLLATE Latin1_General_100_CS_AS NOT NULL
+     ,CodeOverflow         varchar(max) COLLATE Latin1_General_100_CS_AS NULL
+   
+     ,CONSTRAINT CHK_TokenSearchParam_XXX_CodeOverflow CHECK (len(Code) = 256 OR CodeOverflow IS NULL)
+     ,CONSTRAINT CHK_TokenSearchParam_XXX_ResourceTypeId_XXX CHECK (ResourceTypeId = XXX)
+  )
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = object_id(''TokenSearchParam_XXX'') AND name = ''IXC_ResourceSurrogateId_SearchParamId'')
+  CREATE CLUSTERED INDEX IXC_ResourceSurrogateId_SearchParamId ON dbo.TokenSearchParam_XXX (ResourceSurrogateId, SearchParamId) 
+    WITH (DATA_COMPRESSION = PAGE)
+
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = object_id(''TokenSearchParam_XXX'') AND name = ''IX_SearchParamId_Code_INCLUDE_SystemId'')
+  CREATE INDEX IX_SearchParamId_Code_INCLUDE_SystemId ON dbo.TokenSearchParam_XXX (SearchParamId, Code) INCLUDE (SystemId) 
+    WITH (DATA_COMPRESSION = PAGE)', @CreateTable AS VARCHAR (MAX), @CreateView AS VARCHAR (MAX) = '
+CREATE VIEW dbo.TokenSearchParam
+AS', @InsertTrigger AS VARCHAR (MAX) = '
+CREATE TRIGGER dbo.TokenSearchParamIns ON dbo.TokenSearchParam INSTEAD OF INSERT
+AS
+set nocount on', @DeleteTrigger AS VARCHAR (MAX) = '
+CREATE TRIGGER dbo.TokenSearchParamDel ON dbo.TokenSearchParam INSTEAD OF DELETE
+AS
+set nocount on';
+
+SELECT RT
+INTO   #RTs
+FROM   (SELECT 4 AS RT
+        UNION
+        SELECT 14
+        UNION
+        SELECT 15
+        UNION
+        SELECT 19
+        UNION
+        SELECT 28
+        UNION
+        SELECT 35
+        UNION
+        SELECT 40
+        UNION
+        SELECT 44
+        UNION
+        SELECT 53
+        UNION
+        SELECT 61
+        UNION
+        SELECT 62
+        UNION
+        SELECT 76
+        UNION
+        SELECT 79
+        UNION
+        SELECT 96
+        UNION
+        SELECT 100
+        UNION
+        SELECT 103
+        UNION
+        SELECT 108
+        UNION
+        SELECT 110
+        UNION
+        SELECT 138) AS A;
+
+DECLARE @RT AS VARCHAR (100), @First AS BIT = 1;
+
+WHILE EXISTS (SELECT *
+              FROM   #RTs)
+    BEGIN
+        SET @RT = (SELECT TOP 1 RT
+                   FROM   #RTs);
+        SET @CreateTable = @Template;
+        SET @CreateTable = replace(@CreateTable, 'XXX', @RT);
+        EXECUTE (@CreateTable);
+        IF @First = 0
+            SET @CreateView = @CreateView + '
+UNION ALL';
+        SET @CreateView = @CreateView + '
+SELECT *, IsHistory = convert(bit,0) FROM dbo.TokenSearchParam_' + @RT;
+        SET @First = 0;
+        SET @InsertTrigger = @InsertTrigger + '
+INSERT INTO dbo.TokenSearchParam_' + @RT + ' 
+        (ResourceTypeId,ResourceSurrogateId,SearchParamId,SystemId,Code,CodeOverflow) 
+  SELECT ResourceTypeId,ResourceSurrogateId,SearchParamId,SystemId,Code,CodeOverflow 
+    FROM Inserted 
+    WHERE ResourceTypeId = ' + @RT;
+        SET @DeleteTrigger = @DeleteTrigger + '
+DELETE FROM dbo.TokenSearchParam_' + @RT + ' WHERE ResourceTypeId = ' + @RT + ' AND ResourceSurrogateId IN (SELECT ResourceSurrogateId FROM Deleted WHERE ResourceTypeId = ' + @RT + ')';
         DELETE #RTs
         WHERE  RT = @RT;
     END
