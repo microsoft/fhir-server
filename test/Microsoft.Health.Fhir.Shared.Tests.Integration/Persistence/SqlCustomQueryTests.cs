dfb8aaed2cd7d0b117393210bfd26b54daad3e9a
Sergey Galuzo <sergal@microsoft.com>
2023-12-31 19:35:44 -0800

    Corrected stored proc

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlCustomQueryTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlCustomQueryTests.cs
@@ -9,7 +9,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Hl7.Fhir.ElementModel.Types;
 using Hl7.Fhir.Rest;
-using MathNet.Numerics;
 using Microsoft.Data.SqlClient;
 using Microsoft.Health.Fhir.Core.Features.Search;
 using Microsoft.Health.Fhir.Core.Models;
@@ -55,7 +54,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
             CustomQueries.WaitTime = 1;
 
             // use more unique call to avoid racing
-            var query = new[] { Tuple.Create("birthdate", "gt1800-01-01"), Tuple.Create("address-city", "City"), Tuple.Create("address-state", "State") };
+            var query = new[] { Tuple.Create("birthdate", "gt1800-01-01"), Tuple.Create("birthdate", "lt2000-01-01"), Tuple.Create("address-city", "City"), Tuple.Create("address-state", "State") };
 
             // Query before adding an sproc to the database
             await _fixture.SearchService.SearchAsync(KnownResourceTypes.Patient, query, CancellationToken.None);
@@ -129,87 +128,20 @@ SELECT TOP 1 O.name
         {
             _fixture.SqlHelper.ExecuteSqlCmd(@$"
 CREATE OR ALTER PROCEDURE[dbo].[CustomQuery_{hash}]
-   @p0 smallint = 690
-  ,@p1 datetime2 = '1800-01-01T23:59:59.9999999Z'
-  ,@p2 smallint = 103
-  ,@p3 smallint = 685
-  ,@p4 nvarchar(256) = N'City%'
-  ,@p5 smallint = 688
-  ,@p6 nvarchar(256) = N'State%'
-  ,@p7 smallint = 217
-  ,@p8 smallint = 28
-  ,@p9 smallint = 202
-  ,@p10 nvarchar(256) = N'http://snomed.info/sct'
-  ,@p11 varchar(256) = '444814009'
-  ,@p12 int = 11
+   @p0 smallint
+  ,@p1 datetime2
+  ,@p2 smallint
+  ,@p3 datetime2
+  ,@p4 smallint
+  ,@p5 nvarchar(256)
+  ,@p6 smallint
+  ,@p7 nvarchar(256)
+  ,@p8 int
 AS
 set nocount on
-DECLARE @FilteredData AS TABLE(T1 smallint, Sid1 bigint, IsMatch bit, IsPartial bit, Row int)
-;WITH
-cte0 AS
-(
-  SELECT ResourceTypeId AS T1, ResourceSurrogateId AS Sid1
-    FROM dbo.DateTimeSearchParam
-    WHERE IsHistory = 0
-      AND SearchParamId = @p0
-      AND EndDateTime > @p1
-      AND ResourceTypeId = @p2
-)
-,cte1 AS
-(
-  SELECT ResourceTypeId AS T1, ResourceSurrogateId AS Sid1
-    FROM dbo.StringSearchParam
-         JOIN cte0 ON ResourceTypeId = T1 AND ResourceSurrogateId = Sid1
-    WHERE IsHistory = 0
-      AND SearchParamId = @p3
-      AND Text LIKE @p4
-      AND ResourceTypeId = @p2
-)
-,cte2 AS
-(
-  SELECT ResourceTypeId AS T1, ResourceSurrogateId AS Sid1
-    FROM dbo.StringSearchParam
-         JOIN cte1 ON ResourceTypeId = T1 AND ResourceSurrogateId = Sid1
-    WHERE IsHistory = 0
-      AND SearchParamId = @p5
-      AND Text LIKE @p6
-      AND ResourceTypeId = @p2
-)
-,cte3 AS
-(
-  SELECT refSource.ResourceTypeId AS T2, refSource.ResourceSurrogateId AS Sid2, refTarget.ResourceTypeId AS T1, refTarget.ResourceSurrogateId AS Sid1
-    FROM dbo.ReferenceSearchParam refSource
-         JOIN dbo.Resource refTarget ON refSource.ReferenceResourceTypeId = refTarget.ResourceTypeId AND refSource.ReferenceResourceId = refTarget.ResourceId
-         JOIN cte2 ON refTarget.ResourceTypeId = T1 AND refTarget.ResourceSurrogateId = Sid1
-    WHERE refSource.SearchParamId = @p7
-      AND refTarget.IsHistory = 0
-      AND refSource.IsHistory = 0
-      AND refSource.ResourceTypeId IN (@p8)
-      AND refSource.ReferenceResourceTypeId IN (@p2)
-      AND refTarget.ResourceTypeId = @p2
-)
-,cte4 AS
-(
-  SELECT T1, Sid1, ResourceTypeId AS T2, ResourceSurrogateId AS Sid2
-    FROM dbo.TokenSearchParam
-         JOIN cte3 ON ResourceTypeId = T2 AND ResourceSurrogateId = Sid2
-    WHERE IsHistory = 0
-      AND SearchParamId = @p9
-      AND SystemId IN (SELECT SystemId FROM dbo.System WHERE Value = @p10)
-      AND Code = @p11 
-)
-,cte5 AS
-(
-  SELECT DISTINCT TOP(@p12) T1, Sid1, 1 AS IsMatch, 0 AS IsPartial
-    FROM cte4
-    ORDER BY T1 ASC, Sid1 ASC
-)
-/* HASH mjWyjUhfaQSwdGxzuqf2zIoL40RwSu3TGuwOHSjeC98= */
-SELECT DISTINCT r.ResourceTypeId, r.ResourceId, r.Version, r.IsDeleted, r.ResourceSurrogateId, r.RequestMethod, CAST(IsMatch AS bit) AS IsMatch, CAST(IsPartial AS bit) AS IsPartial, r.IsRawResourceMetaSet, r.SearchParamHash, r.RawResource
+SELECT DISTINCT r.ResourceTypeId, r.ResourceId, r.Version, r.IsDeleted, r.ResourceSurrogateId, r.RequestMethod, CAST(1 AS bit) AS IsMatch, CAST(0 AS bit) AS IsPartial, r.IsRawResourceMetaSet, r.SearchParamHash, r.RawResource
   FROM dbo.Resource r
-       JOIN cte5 ON r.ResourceTypeId = cte5.T1 AND r.ResourceSurrogateId = cte5.Sid1
-  WHERE IsHistory = 0
-  ORDER BY r.ResourceTypeId ASC, r.ResourceSurrogateId ASC
+  WHERE 1 = 2
             ").Wait();
         }
     }
