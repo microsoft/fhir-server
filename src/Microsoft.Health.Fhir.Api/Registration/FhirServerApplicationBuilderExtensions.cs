a104984781e516374e3e0efecf1bdfeaf4bada57
Richa Bansal <ribans@microsoft.com>
2024-01-05 15:31:34 -0800

    Adds trailing backslash

--- src/Microsoft.Health.Fhir.Api/Registration/FhirServerApplicationBuilderExtensions.cs
+++ src/Microsoft.Health.Fhir.Api/Registration/FhirServerApplicationBuilderExtensions.cs
@@ -43,8 +43,11 @@ namespace Microsoft.AspNetCore.Builder
             EnsureArg.IsNotNull(app, nameof(app));
 
             var config = app.ApplicationServices.GetRequiredService<IOptions<FhirServerConfiguration>>();
+            var pathBase = config.Value.PathBase;
 
-            var pathBase = config.Value.PathBase?.TrimEnd('/');
+            // Endpoint routing does not add trailing backslash if path is emtpy(e.g. for bundles)
+            pathBase = EnsureBackslashExists(pathBase);
+
             if (!string.IsNullOrWhiteSpace(pathBase))
             {
                 var pathString = new PathString(pathBase);
@@ -89,6 +92,16 @@ namespace Microsoft.AspNetCore.Builder
                 });
 
             return app;
+        }
+
+        private static string EnsureBackslashExists(string pathBase)
+        {
+            if (string.IsNullOrEmpty(pathBase))
+            {
+                return pathBase;
+            }
+
+            return pathBase.EndsWith('/') ? pathBase : pathBase + "/";
         }
 
         private class PathBaseMiddleware
