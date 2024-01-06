171b05503ccebe8aaa663fa6be86078c770ef22e
Brendan Kowitz <bkowitz@microsoft.com>
2024-01-05 21:00:26 -0800

    [Cosmos] Allows Conditional Search to execute with _maxParallel (#3600)
    * Allows Conditional Search to execute in max parallel
    * Expanding the usage of max parallelism validation adding new HTTP header to handle max parallelism.
    * Added new test to check if the OptimizeConcurrency flag is sent to the search service.
    * Add new tests validating if the FHIR Context property bag is correctly populated
    
    Co-authored-by: apurvabhale <apurvabhale@microsoft.com>
    Co-authored-by: Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>

---
+++ src/Microsoft.Health.Fhir.Shared.Client/FhirBundleOptions.cs
@@ -0,0 +1,32 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Identity;
+
+namespace Microsoft.Health.Fhir.Client
+{
+    public sealed class FhirBundleOptions
+    {
+#pragma warning disable IDE1006 // Naming Styles
+        public static readonly FhirBundleOptions Default = new();
+#pragma warning restore IDE1006 // Naming Styles
+
+        public FhirBundleOptions()
+        {
+            BundleProcessingLogic = FhirBundleProcessingLogic.Parallel;
+            MaximizeConditionalQueryParallelism = false;
+            ProfileValidation = false;
+        }
+
+        public FhirBundleProcessingLogic BundleProcessingLogic { get; set; }
+
+        public bool MaximizeConditionalQueryParallelism { get; set; }
+
+        public bool ProfileValidation { get; set; }
+    }
+}
