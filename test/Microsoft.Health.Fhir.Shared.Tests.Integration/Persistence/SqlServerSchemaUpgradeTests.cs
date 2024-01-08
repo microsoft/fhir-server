acb433f5ed67140b8647fa064e2c27c2ed3e895f
Mikael Weaver <mikael.weaver@microsoft.com>
2024-01-08 06:25:16 -0800

    Fixed SQL int test newline integration

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerSchemaUpgradeTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerSchemaUpgradeTests.cs
@@ -331,35 +331,28 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
 
         private string Normalize(string text)
         {
+            // normalize newlines
+            text = text.Replace("\r\n", "\n").Replace("\r", "\n");
+
             // remove inline comments
             while (text.Contains("--"))
             {
                 var indexStart = text.IndexOf("--");
-                var indexEndNewLine = text.IndexOf("\n", indexStart);
-                var indexEndCarriageReturn = text.IndexOf("\r\n", indexStart);
-
-                var indexEnd = indexEndNewLine;
-
-                if (indexEndCarriageReturn != -1 && (indexEndNewLine == -1 || indexEndCarriageReturn < indexEndNewLine))
-                {
-                    indexEnd = indexEndCarriageReturn;
-                }
+                var indexEnd = text.IndexOf("\n", indexStart);
 
                 if (indexEnd == -1)
                 {
-                    // If there's no newline character, remove everything after the comment start.
                     text = text.Substring(0, indexStart);
                 }
                 else
                 {
-                    // Adjust for length of newline characters.
-                    var lengthOfNewLineChars = (text[indexEnd] == '\r') ? 2 : 1;
-                    text = text.Substring(0, indexStart) + text.Substring(indexEnd + lengthOfNewLineChars);
+                    text = text.Substring(0, indexStart) + text.Substring(indexEnd + 1);
                 }
             }
 
             return text.ToLowerInvariant()
-                       .Replace(Environment.NewLine, " ")
+                       .Replace("\r\n", " ")
+                       .Replace("\n", " ")
                        .Replace("\r", " ")
                        .Replace("\t", " ")
                        .Replace(";", " ")
