e3e47d8051fd42937d578a392945dd42a95be6d1
Anil Mahajan <anil.mahajan@xoriant.com>
2024-01-09 19:06:32 +0530

    decorated newly added test case class with Trait attribute

--- src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Features/Filters/ValidateIdSegmentAttributeTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Features/Filters/ValidateIdSegmentAttributeTests.cs
@@ -22,6 +22,9 @@ using Xunit;
 
 namespace Microsoft.Health.Fhir.Api.UnitTests.Features.Filters
 {
+    [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
+    [Trait(Traits.Category, Categories.Validate)]
+    [Trait(Traits.Category, Categories.Web)]
     public class ValidateIdSegmentAttributeTests
     {
         [Theory]
