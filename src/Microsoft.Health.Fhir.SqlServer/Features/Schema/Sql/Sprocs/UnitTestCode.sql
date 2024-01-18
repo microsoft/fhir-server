937513a3f5d1d18ea9775d6884b93ff16003a73e
Sergey Galuzo <sergal@microsoft.com>
2024-01-18 08:03:32 -0800

    transactions and unit test code

---
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/UnitTestCode.sql
@@ -0,0 +1,26 @@
+﻿  DECLARE @Objects TABLE (Name varchar(100) PRIMARY KEY)
+  INSERT INTO @Objects SELECT name FROM sys.objects WHERE type = 'u' AND name LIKE '%SearchParam'
+
+  DECLARE @Tbl varchar(100)
+         ,@TblTable varchar(100)
+         ,@SQL varchar(max)
+
+  WHILE EXISTS (SELECT * FROM @Objects)
+  BEGIN
+    SET @Tbl = (SELECT TOP 1 Name FROM @Objects)
+    SET @TblTable = @Tbl+'_Table'
+    SET @SQL = ''
+
+    SELECT TOP 100 @SQL = @SQL + CASE WHEN @SQL <> '' THEN ',' ELSE '' END + CASE WHEN name = 'IsHistory' THEN 'IsHistory = convert(bit,0)' ELSE name END FROM sys.columns WHERE object_id = object_id(@Tbl) ORDER BY column_id
+    SET @SQL = 'CREATE VIEW '+@Tbl+' AS SELECT '+@SQL+' FROM '+@TblTable
+  
+    BEGIN TRANSACTION
+
+    EXECUTE sp_rename @Tbl, @TblTable
+    EXECUTE(@SQL)
+
+    COMMIT TRANSACTION
+
+    DELETE FROM @Objects WHERE Name = @Tbl
+  END
+
