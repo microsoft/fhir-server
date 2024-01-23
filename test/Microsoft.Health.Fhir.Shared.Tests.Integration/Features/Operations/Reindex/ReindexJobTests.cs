8abdb69ee8e2970b22d16e64968a49cf39446706
Paul Taladay <patalada@microsoft.com>
2024-01-23 14:44:17 -0800

    Removed collection class as that did not do what it was intended to.

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/Operations/Reindex/ReindexJobTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/Operations/Reindex/ReindexJobTests.cs
@@ -53,7 +53,6 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Features.Operations.Reindex
     [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
     [Trait(Traits.Category, Categories.IndexAndReindex)]
     [FhirStorageTestsFixtureArgumentSets(DataStore.All)]
-    [Collection("FhirStorageTestsCollection")]
     public class ReindexJobTests : IClassFixture<FhirStorageTestsFixture>, IAsyncLifetime
     {
         private readonly FhirStorageTestsFixture _fixture;
@@ -181,6 +180,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Features.Operations.Reindex
         [InlineData(JobRecordProperties.MaximumConcurrency, ReindexJobRecord.MinMaximumConcurrency - 1)]
         [InlineData(JobRecordProperties.MaximumNumberOfResourcesPerQuery, ReindexJobRecord.MinMaximumNumberOfResourcesPerQuery - 1)]
         [InlineData("Foo", 4)]
+        [Collection("FhirStorageTestsCollection")]
         public async Task GivenOutOfRangeReindexParameter_WhenCreatingAReindexJob_ThenExceptionShouldBeThrown(string jobRecordProperty, int value)
         {
             string errorMessage = "Test error message";
