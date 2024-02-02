21e4d6caefcc1b99b76ef7d607a02acd1c10586c
Sergey Galuzo <sergal@microsoft.com>
2024-02-02 08:12:43 -0800

    removed obsolete comment

--- src/Microsoft.Health.Fhir.Core.UnitTests/Features/Operations/Export/ExportProcessingJobTests.cs
+++ src/Microsoft.Health.Fhir.Core.UnitTests/Features/Operations/Export/ExportProcessingJobTests.cs
@@ -36,10 +36,6 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Operations.Export
             var processingJob = new ExportProcessingJob(MakeMockJob, new TestQueueClient());
             var taskResult = await processingJob.ExecuteAsync(GenerateJobInfo(expectedResults), CancellationToken.None);
             Assert.Equal(expectedResults, taskResult);
-
-            // For some reason checking the progress result is flaky. Sometimes it passes and sometimes it is blank. There seems to be a timing error, but debugging it causes it not to happen.
-            // All the steps are synchronous, so I don't see where the issue is happening.
-            // Assert.Equal(expectedResults, progressResult);
         }
 
         [Fact]
