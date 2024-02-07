359307e421a10bacf2a36e724633a5e0263f999b
Brendan Kowitz <bkowitz@microsoft.com>
2024-02-07 10:00:08 -0800

    Updates

--- src/Microsoft.Health.Fhir.Api/Features/BackgroundJobService/JobFactory.cs
+++ src/Microsoft.Health.Fhir.Api/Features/BackgroundJobService/JobFactory.cs
@@ -21,20 +21,22 @@ namespace Microsoft.Health.Fhir.Api.Features.BackgroundJobService
     public class JobFactory : IJobFactory
     {
         private readonly IScopeProvider<IEnumerable<Func<IJob>>> _jobFactories;
+        private readonly ILogger<JobFactory> _logger;
         private readonly Dictionary<int, int> _jobFactoryLookup;
 
         public JobFactory(IScopeProvider<IEnumerable<Func<IJob>>> jobFactories, ILogger<JobFactory> logger)
         {
             EnsureArg.IsNotNull(jobFactories, nameof(jobFactories));
             _jobFactories = jobFactories;
+            _logger = logger;
             _jobFactoryLookup = new Dictionary<int, int>();
 
-            using var jobs = jobFactories.Invoke();
-            foreach (var jobFunc in jobs.Value.Select((lazy, i) => (Instance: lazy, Index: i)))
+            using IScoped<IEnumerable<Func<IJob>>> jobs = jobFactories.Invoke();
+            foreach ((Func<IJob> Instance, int Index) jobFunc in jobs.Value.Select((lazy, i) => (Instance: lazy, Index: i)))
             {
                 try
                 {
-                    var jobInstance = jobFunc.Instance.Invoke();
+                    IJob jobInstance = jobFunc.Instance.Invoke();
                     if (jobInstance.GetType().GetCustomAttribute(typeof(JobTypeIdAttribute), false) is JobTypeIdAttribute jobTypeAttr)
                     {
                         _jobFactoryLookup.Add(jobTypeAttr.JobTypeId, jobFunc.Index);
@@ -46,7 +48,7 @@ namespace Microsoft.Health.Fhir.Api.Features.BackgroundJobService
                 }
                 catch (Exception ex)
                 {
-                    logger.LogError(ex, "Failed to create job factory.");
+                    logger.LogError(ex, "Failed to create job factory");
                     throw;
                 }
             }
@@ -56,27 +58,22 @@ namespace Microsoft.Health.Fhir.Api.Features.BackgroundJobService
         {
             EnsureArg.IsNotNull(jobInfo, nameof(jobInfo));
 
-            IScoped<IEnumerable<Func<IJob>>> scope = _jobFactories.Invoke();
-
-            if (_jobFactoryLookup.TryGetValue(jobInfo.GetJobTypeId() ?? int.MinValue, out var index))
+            if (_jobFactoryLookup.TryGetValue(jobInfo.GetJobTypeId() ?? -1, out var index))
             {
-                return new ScopedJob(scope.Value.ElementAt(index).Invoke(), scope);
+                IScoped<IEnumerable<Func<IJob>>> scope = _jobFactories.Invoke();
+                IJob instance = scope.Value.ElementAt(index).Invoke();
+                _logger.LogJobInformation(jobInfo, "Created job instance {JobInstance}", instance.GetType().Name);
+                return new ScopedJob(instance, scope);
             }
 
-            throw new NotSupportedException($"Unknown task definition. ID: {jobInfo?.Id ?? -1}");
+            throw new NotSupportedException($"Unknown task definition. ID: {jobInfo.Id}, Type: {jobInfo.GetJobTypeId()}");
         }
 
-        private class ScopedJob : IScoped<IJob>
+        private class ScopedJob(IJob value, IDisposable disposableScope) : IScoped<IJob>
         {
-            private readonly IDisposable _disposableScope;
+            private readonly IDisposable _disposableScope = EnsureArg.IsNotNull(disposableScope, nameof(disposableScope));
 
-            public ScopedJob(IJob value, IDisposable disposableScope)
-            {
-                Value = EnsureArg.IsNotNull(value, nameof(value));
-                _disposableScope = EnsureArg.IsNotNull(disposableScope, nameof(disposableScope));
-            }
-
-            public IJob Value { get; }
+            public IJob Value { get; } = EnsureArg.IsNotNull(value, nameof(value));
 
             public void Dispose()
             {
