fc660201392005ef9cef3a1144b0a836fb3ce6f4
Abigail Nicolas Sayago <abigailn@microsoft.com>
2024-02-22 11:16:16 -0800

    Delete references

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/BundleTransactionTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/BundleTransactionTests.cs
@@ -7,8 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Net.Sockets;
 using System.Security.Policy;
 using Hl7.Fhir.Model;
 using Microsoft.Health.Fhir.Client;
