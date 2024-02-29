4fde508bec244f3bd58c5fec582c855de03216d6
Brendan Kowitz <bkowitz@microsoft.com>
2024-02-28 18:06:58 -0800

    Improve fhirtimer

--- src/Microsoft.Health.Fhir.SqlServer/Features/Watchdogs/FhirTimer.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Watchdogs/FhirTimer.cs
@@ -7,55 +7,76 @@ using System;
 using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using EnsureThat;
 using Microsoft.Extensions.Logging;
 using Microsoft.Health.Core;
 using Microsoft.Health.Fhir.Core.Extensions;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features.Watchdogs
 {
-    public abstract class FhirTimer<T>(ILogger<T> logger = null)
+    public class FhirTimer(ILogger logger = null)
     {
+        private bool _active;
+
         private bool _isFailing;
 
-        internal double PeriodSec { get; set; }
+        public double PeriodSec { get; private set; }
 
-        internal DateTimeOffset LastRunDateTime { get; private set; } = DateTime.Parse("2017-12-01");
+        public DateTimeOffset LastRunDateTime { get; private set; } = DateTimeOffset.Parse("2017-12-01");
 
-        internal bool IsFailing => _isFailing;
+        public bool IsFailing => _isFailing;
 
-        protected async Task StartAsync(double periodSec, CancellationToken cancellationToken)
+        public bool IsRunning { get; private set; }
+
+        /// <summary>
+        /// Runs the execution of the timer until the <see cref="CancellationToken"/> is cancelled.
+        /// </summary>
+        public async Task ExecuteAsync(double periodSec, Func<CancellationToken, Task> onNextTick, CancellationToken cancellationToken)
         {
+            EnsureArg.IsNotNull(onNextTick, nameof(onNextTick));
             PeriodSec = periodSec;
 
+            if (_active)
+            {
+                throw new InvalidOperationException("Timer is already running");
+            }
+
+            _active = true;
             await Task.Delay(TimeSpan.FromSeconds(PeriodSec * RandomNumberGenerator.GetInt32(1000) / 1000), cancellationToken);
             using var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(PeriodSec));
 
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                try
+                while (!cancellationToken.IsCancellationRequested)
                 {
-                    await periodicTimer.WaitForNextTickAsync(cancellationToken);
-                }
-                catch (OperationCanceledException)
-                {
-                    // Time to exit
-                    break;
-                }
+                    try
+                    {
+                        await periodicTimer.WaitForNextTickAsync(cancellationToken);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        break;
+                    }
 
-                try
-                {
-                    await RunAsync();
-                    LastRunDateTime = Clock.UtcNow;
-                    _isFailing = false;
-                }
-                catch (Exception e)
-                {
-                    logger.LogWarning(e, "Error executing timer");
-                    _isFailing = true;
+                    try
+                    {
+                        IsRunning = true;
+                        await onNextTick(cancellationToken);
+                        LastRunDateTime = Clock.UtcNow;
+                        _isFailing = false;
+                    }
+                    catch (Exception e)
+                    {
+                        logger?.LogWarning(e, "Error executing timer");
+                        _isFailing = true;
+                    }
                 }
             }
+            finally
+            {
+                _active = false;
+                IsRunning = false;
+            }
         }
-
-        protected abstract Task RunAsync();
     }
 }
