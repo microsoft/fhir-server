5c3b17925bb6bf44f680a5b6d96f852283cf27e2
Brendan Kowitz <bkowitz@microsoft.com>
2024-02-28 18:06:58 -0800

    Improve fhirtimer

--- src/Microsoft.Health.Fhir.SqlServer/Features/Watchdogs/Watchdog.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Watchdogs/Watchdog.cs
@@ -19,7 +19,6 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Watchdogs
         private ISqlRetryService _sqlRetryService;
         private readonly ILogger<T> _logger;
         private readonly WatchdogLease<T> _watchdogLease;
-        private bool _disposed = false;
         private double _periodSec;
         private double _leasePeriodSec;
 
@@ -137,28 +136,6 @@ INSERT INTO dbo.Parameters (Id,Number) SELECT @LeasePeriodSecId, @LeasePeriodSec
             }
 
             return (long)value;
-        }
-
-        public new void Dispose()
-        {
-            Dispose(true);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (_disposed)
-            {
-                return;
-            }
-
-            if (disposing)
-            {
-                _watchdogLease?.Dispose();
-            }
-
-            base.Dispose(disposing);
-
-            _disposed = true;
         }
     }
 }
