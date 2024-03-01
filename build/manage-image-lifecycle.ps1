12a61f730dd1522c1a75b7b4183e4a2aeda069cf
Isao Yamauchi <v-iyamauchi@microsoft.com>
2024-03-01 08:51:20 -0800

    Fixing an issue of adding more than one lineage metadata.

--- build/manage-image-lifecycle.ps1
+++ build/manage-image-lifecycle.ps1
@@ -1,4 +1,6 @@
 $buildNumber = "$(Release.Artifacts.CIBuild.BuildNumber)"
+$build = "$(Build.BuildNumber)"
+$timestamp= $build.Substring($build.IndexOf('-')+1)
 $versions = "stu3", "r4", "r4b", "r5"
 $registry = "healthplatformregistry.azurecr.io"
 $repositories = $versions | ForEach-Object { "$_`_fhir-server"; "public/healthcareapis/$_-fhir-server"; }
@@ -11,7 +13,7 @@ oras login -u $userName -p $password $registry
 foreach ($repo in $repositories) {
     # Get all tags from the repository except the latest ones
     $tags = oras repo tags "${registry}/${repo}"
-    $tags = $tags.Where({ !$_.Contains($buildNumber) -and !$_.Contains("latest") })
+    $tags = $tags.Where({ !$_.Contains($buildNumber) -and !$_.Contains($timestamp) -and !$_.Contains("latest") -and !$_.Contains("release") })
 
     # Attach the lifecycle metadata to older images
     foreach ($tag in $tags) {
@@ -19,18 +21,25 @@ foreach ($repo in $repositories) {
         $artifact = oras discover -o json --artifact-type "application/vnd.microsoft.artifact.lifecycle" $image
         $json = $artifact -join "" | ConvertFrom-Json
         if ($null -eq $json.manifests) {
-            Write-Output "'${tag}' needs the lifestyle metadata."
+            Write-Output "'${image}' needs the lifestyle metadata."
             $annotation = "vnd.microsoft.artifact.lifecycle.end-of-life.date=" + [DateTime]::UtcNow.ToString("O")
             oras attach --artifact-type "application/vnd.microsoft.artifact.lifecycle" --annotation $annotation $image
         }
         else {
-            Write-Output "'${tag}' already has the lifestyle metadata."
+            Write-Output "'${image}' already has the lifestyle metadata."
         }
     }
 
     # Attach the lineage metadata to the new image
-    Write-Output "'${buildNumber}' needs the lineage metadata."
     $image = "${registry}/${repo}:${buildNumber}"
-    $annotation = "vnd.microsoft.artifact.lineage.rolling-tag=${buildNumber}"
-    oras attach --artifact-type "application/vnd.microsoft.artifact.lineage" --annotation $annotation $image
+    $artifact = oras discover -o json --artifact-type "application/vnd.microsoft.artifact.lineage" $image
+    $json = $artifact -join "" | ConvertFrom-Json
+    if ($null -eq $json.manifests) {
+        Write-Output "'${image}' needs the lineage metadata."
+        $annotation = "vnd.microsoft.artifact.lineage.rolling-tag=${buildNumber}"
+        oras attach --artifact-type "application/vnd.microsoft.artifact.lineage" --annotation $annotation $image
+    }
+    else {
+        Write-Output "'${image}' already has the lineage metadata."
+    }
 }
