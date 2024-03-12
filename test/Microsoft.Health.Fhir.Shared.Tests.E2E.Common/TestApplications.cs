26311c95997375ac66bfccb1cef8b4b61a2f9ae0
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2024-03-12 16:02:31 -0700

    App auth

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E.Common/TestApplications.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E.Common/TestApplications.cs
@@ -16,5 +16,17 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Common
         public static TestApplication WrongAudienceClient { get; } = new TestApplication("wrongAudienceClient");
 
         public static TestApplication SmartUserClient { get; } = new TestApplication("smartUserClient");
+
+        public static TestApplication ReadOnlyUser { get; } = new TestApplication("globalReaderUser");
+
+        public static TestApplication ReadWriteUser { get; } = new TestApplication("globalWriterUser");
+
+        public static TestApplication ExportUser { get; } = new TestApplication("globalExporterUser");
+
+        public static TestApplication ConvertDataUser { get; } = new TestApplication("globalConverterUser");
+
+        public static TestApplication BulkImportUser { get; } = new TestApplication("globalImporterUser");
+
+        public static TestApplication AdminUser { get; } = new TestApplication("globalAdminUser");
     }
 }
