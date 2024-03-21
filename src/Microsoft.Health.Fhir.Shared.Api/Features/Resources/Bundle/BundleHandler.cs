9bbf121bb5c2415067fa7d66b21574abb189e2e7
Brendan Kowitz <bkowitz@microsoft.com>
2024-03-21 10:45:58 -0700

    Moves BundleSubRequest constant

--- src/Microsoft.Health.Fhir.Shared.Api/Features/Resources/Bundle/BundleHandler.cs
+++ src/Microsoft.Health.Fhir.Shared.Api/Features/Resources/Bundle/BundleHandler.cs
@@ -817,7 +817,7 @@ namespace Microsoft.Health.Fhir.Api.Features.Resources.Bundle
 
             if (subResource != null)
             {
-                newFhirRequestContext.Properties[SubRequest.SubRequestResource] = subResource;
+                newFhirRequestContext.Properties[BundleSubRequest.Model] = subResource;
             }
 
             // Copy allowed resource actions to the new FHIR Request Context.
