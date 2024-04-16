2fd47c88d4695e8db7ed702ddc542e845888bca0
Brendan Kowitz <bkowitz@microsoft.com>
2024-04-16 13:00:00 -0700

    Implement basic subscription filtering

--- src/Microsoft.Health.Fhir.Subscriptions/Models/ChannelInfo.cs
+++ src/Microsoft.Health.Fhir.Subscriptions/Models/ChannelInfo.cs
@@ -31,5 +31,8 @@ namespace Microsoft.Health.Fhir.Subscriptions.Models
         public SubscriptionChannelType ChannelType { get; set; }
 
         public SubscriptionContentType ContentType { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2227:Collection properties should be read only", Justification = "Json Poco")]
+        public IDictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
     }
 }
