50f98b4c6fb84314fe6896beb2aedb80c03b654d
Brendan Kowitz <bkowitz@microsoft.com>
2024-04-16 15:02:08 -0700

    Implements Channel Interface

--- src/Microsoft.Health.Fhir.Subscriptions/Channels/StorageChannel.cs
+++ src/Microsoft.Health.Fhir.Subscriptions/Channels/StorageChannel.cs
@@ -8,10 +8,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Health.Fhir.Core.Features.Operations.Export;
+using Microsoft.Health.Fhir.Core.Features.Operations.Export.ExportDestinationClient;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Subscriptions.Models;
 
 namespace Microsoft.Health.Fhir.Subscriptions.Channels
 {
+    [ChannelType(SubscriptionChannelType.Storage)]
     public class StorageChannel : ISubscriptionChannel
     {
+        private readonly IExportDestinationClient _exportDestinationClient;
+
+        public StorageChannel(
+            IExportDestinationClient exportDestinationClient)
+        {
+            _exportDestinationClient = exportDestinationClient;
+        }
+
+        public async Task PublishAsync(IReadOnlyCollection<ResourceWrapper> resources, ChannelInfo channelInfo, DateTimeOffset transactionTime, CancellationToken cancellationToken)
+        {
+            await _exportDestinationClient.ConnectAsync(cancellationToken, channelInfo.Properties["container"]);
+
+            foreach (var resource in resources)
+            {
+                string fileName = $"{resource.ToResourceKey()}.json";
+
+                _exportDestinationClient.WriteFilePart(
+                    fileName,
+                    resource.RawResource.Data);
+
+                _exportDestinationClient.CommitFile(fileName);
+            }
+        }
     }
 }
