6826955f5335b45a0462dc8b4cd227ab78b62916
Brendan Kowitz <bkowitz@microsoft.com>
2024-04-15 17:04:04 -0700

    Subscription infra

---
+++ src/Microsoft.Health.Fhir.Subscriptions/Models/ChannelInfo.cs
@@ -0,0 +1,35 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.Fhir.Subscriptions.Models
+{
+    public class ChannelInfo
+    {
+        /// <summary>
+        /// Interval to send 'heartbeat' notification
+        /// </summary>
+        public TimeSpan HeartBeatPeriod { get; set; }
+
+        /// <summary>
+        /// Timeout to attempt notification delivery
+        /// </summary>
+        public TimeSpan Timeout { get; set; }
+
+        /// <summary>
+        /// Maximum number of triggering resources included in notification bundles
+        /// </summary>
+        public int MaxCount { get; set; }
+
+        public SubscriptionChannelType ChannelType { get; set; }
+
+        public SubscriptionContentType ContentType { get; set; }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.Subscriptions/Registration/SubscriptionsModule.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Health.Extensions.DependencyInjection;
+using Microsoft.Health.JobManagement;
+
+namespace Microsoft.Health.Fhir.Subscriptions.Registration
+{
+    public class SubscriptionsModule : IStartupModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.TypesInSameAssemblyAs<SubscriptionsModule>()
+                .AssignableTo<IJob>()
+                .Transient()
+                .AsSelf()
+                .AsService<IJob>();
+        }
+    }
+}
