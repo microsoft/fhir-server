ac06cfadacd8461f175304399abf8c3c2d890cec
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2024-04-17 17:57:17 -0700

    Changes in DataLakeChannel and the project config.

--- src/Microsoft.Health.Fhir.Subscriptions/Persistence/ISubscriptionManager.cs
+++ src/Microsoft.Health.Fhir.Subscriptions/Persistence/ISubscriptionManager.cs
@@ -3,6 +3,9 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Health.Fhir.Subscriptions.Models;
 
 namespace Microsoft.Health.Fhir.Subscriptions.Persistence
