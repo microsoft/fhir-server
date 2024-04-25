769ce40d7618aa365ad86ed9a2c0866f4d3dceff
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2024-04-25 10:20:03 -0700

    [Metrics] FHIR metric emitter (#3753)
    * Adding default metric emitter
    
    * Fix using statements
    
    * Improve metric emission
    
    * Refactoring metric emitters
    
    * Using statements updated
    
    * New tests to test if methods have the metric emitter filters and remove reference to log in FhirController
    
    * Removed not used code
    
    * Renamining classes with better names
    
    * New metric handler implementation.
    
    * Refactoring interfaces and adding more tests.
    
    * Following coding suggestions.

---
+++ src/Microsoft.Health.Fhir.Core/Logging/Metrics/ILatencyMetricHandler.cs
@@ -0,0 +1,15 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Antlr4.Runtime.Tree.Xpath;
+
+namespace Microsoft.Health.Fhir.Core.Logging.Metrics
+{
+    public interface ILatencyMetricHandler<T>
+         where T : ILatencyMetricNotification
+    {
+        void EmitLatency(T notification);
+    }
+}
---
+++ src/Microsoft.Health.Fhir.Shared.Api/Features/Filters/Metrics/BaseEndpointMetricEmitterAttribute.cs
@@ -0,0 +1,31 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Microsoft.Health.Fhir.Api.Features.Filters.Metrics
+{
+    public abstract class BaseEndpointMetricEmitterAttribute : ActionFilterAttribute
+    {
+        private Stopwatch _stopwatch;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _stopwatch = Stopwatch.StartNew();
+
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+
+            EmitMetricOnActionExecuted(context, new ActionExecutedStatistics() { ElapsedMilliseconds = _stopwatch.ElapsedMilliseconds });
+        }
+
+        public abstract void EmitMetricOnActionExecuted(ActionExecutedContext context, ActionExecutedStatistics statistics);
+    }
+}
