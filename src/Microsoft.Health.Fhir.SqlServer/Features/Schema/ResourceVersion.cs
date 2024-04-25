04d6874edf9720ecb159169d228b34c05bf6597f
rajithaaluri <rajithaaluri@microsoft.com>
2024-04-25 14:44:50 -0500

    using type field

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/ResourceVersion.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/ResourceVersion.cs
@@ -10,27 +10,14 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Schema
 {
     public static class ResourceVersion
     {
-        private static bool versionIsLong = true;
-
         public static int GetVersion(SqlDataReader reader, int index)
         {
-            int version = 0;
-            bool retry = true;
-
-            while (retry)
+            if (reader.GetFieldType(index) == typeof(long))
             {
-                try
-                {
-                    version = versionIsLong ? (int)reader.GetInt64(index) : reader.GetInt32(index);
-                    retry = false;
-                }
-                catch (InvalidCastException)
-                {
-                    versionIsLong = !versionIsLong;
-                }
+                return (int)reader.GetInt64(index);
             }
 
-            return version;
+            return reader.GetInt32(index);
         }
     }
 }
