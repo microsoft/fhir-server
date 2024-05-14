66b014e7bc1d5fb95c809f519c0cbc0eac90c46d
Sergey Galuzo <sergal@microsoft.com>
2024-05-13 21:07:00 -0700

    merge

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/81.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/81.sql
@@ -15,7 +15,7 @@ IF EXISTS (SELECT *
 
 GO
 INSERT  INTO dbo.SchemaVersion
-VALUES (79, 'started');
+VALUES (80, 'started');
 
 CREATE PARTITION FUNCTION PartitionFunction_ResourceTypeId(SMALLINT)
     AS RANGE RIGHT
