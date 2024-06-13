09c366456ac9fcd891a6a9830351dfc990e4bae3
SergeyGaluzo <95932081+SergeyGaluzo@users.noreply.github.com>
2024-06-13 14:10:01 -0700

    Update test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/ChangeFeed/SqlServerFhirResourceChangeCaptureDisabledTests.cs
    Co-authored-by: Brendan Kowitz <bkowitz@microsoft.com>

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/ChangeFeed/SqlServerFhirResourceChangeCaptureDisabledTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Features/ChangeFeed/SqlServerFhirResourceChangeCaptureDisabledTests.cs
@@ -40,7 +40,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Features.ChangeFeed
             FhirStorageTestsFixture fhirStorageTestsFixture = null;
             try
             {
-                string databaseName = SqlServerFhirStorageTestsFixture.GetDatabaseName($"ChangeCaptureDisabled");
+                string databaseName = SqlServerFhirStorageTestsFixture.GetDatabaseName("ChangeCaptureDisabled");
 
                 // this will either create the database or upgrade the schema.
                 var coreFeatureConfigOptions = Options.Create(new CoreFeatureConfiguration() { SupportsResourceChangeCapture = false });
