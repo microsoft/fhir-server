6dfd41e7b5877fce43350571c73d68202fb9a27d
Sergey Galuzo <sergal@microsoft.com>
2024-07-17 06:59:30 -0700

    new errors

--- src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
@@ -103,10 +103,15 @@ namespace Microsoft.Health.Fhir.SqlServer.Features
         private static bool HasDatabaseOverloadPattern(string str)
         {
             return (str.Contains("request limit for the database", StringComparison.OrdinalIgnoreCase) && str.Contains("has been reached", StringComparison.OrdinalIgnoreCase))
-                   || str.Contains("fabric compute capacity has exceeded its limits", StringComparison.OrdinalIgnoreCase);
+                   || str.Contains("fabric compute capacity has exceeded its limits", StringComparison.OrdinalIgnoreCase)
+                   || str.Contains("unable to properly update physical metadata", StringComparison.OrdinalIgnoreCase)
+                   || (str.Contains("user session limit for the workspace", StringComparison.OrdinalIgnoreCase) && str.Contains("has been reached", StringComparison.OrdinalIgnoreCase))
+                   ;
 
             ////The request limit for the database is 200 and has been reached.
             ////Unable to complete the action because your organization’s Fabric compute capacity has exceeded its limits. Try again later.
+            ////Unable to properly update physical metadata.
+            ////The user session limit for the workspace is 724 and has been reached.
         }
 
         // TODO: Remove when source of this exception is identified
