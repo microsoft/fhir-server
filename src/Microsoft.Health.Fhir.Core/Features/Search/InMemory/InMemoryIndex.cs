1eaa5336356d5a4a7bd992a6ba8cc34b7bf44379
aponakampalli <ponakampalli.a@northeastern.edu>
2024-07-26 16:12:33 -0700

    integrate in memory search with filter search for subscriptions

--- src/Microsoft.Health.Fhir.Core/Features/Search/InMemory/InMemoryIndex.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Search/InMemory/InMemoryIndex.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Search.InMemory
             Index = new ConcurrentDictionary<string, List<(ResourceKey, IReadOnlyCollection<SearchIndexEntry>)>>();
         }
 
-        internal ConcurrentDictionary<string, List<(ResourceKey Location, IReadOnlyCollection<SearchIndexEntry> Index)>> Index
+        public ConcurrentDictionary<string, List<(ResourceKey Location, IReadOnlyCollection<SearchIndexEntry> Index)>> Index
         {
             get;
         }
