929038cfeeb7dc4cebaca0fd526b23507f6f9528
aponakampalli <ponakampalli.a@northeastern.edu>
2024-07-25 18:08:36 -0700

    set up notification manager

---
+++ src/Microsoft.Health.Fhir.Subscriptions/Channels/WebHookChannel.cs
@@ -0,0 +1,24 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Health.Fhir.Core.Features.Operations.Export.ExportDestinationClient;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Subscriptions.Models;
+
+namespace Microsoft.Health.Fhir.Subscriptions.Channels
+{
+    [ChannelType(SubscriptionChannelType.WebHook)]
+    public class WebHookChannel : ISubscriptionChannel
+    {
+
+        public WebHookChannel()
+        {
+        }
+    }
+}
