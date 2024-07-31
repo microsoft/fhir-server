252966531df5faa54d1a48d442452ff55b74233b
Mikael Weaver <mikaelw@microsoft.com>
2024-07-30 23:48:07 -0700

    testing sql query in startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -e
 echo "Starting SSH"
 #/usr/sbin/sshd -D\
 /usr/sbin/sshd
+
+echo "running test sql command"
+sqlcmd -S sergey-perf.database.windows.net -d FHIR4 --authentication-method ActiveDirectoryManagedIdentity -U ce998957-2e67-457b-a1d1-9734919f4a85 -Q "SELECT COUNT(*) FROM dbo.Resource"
+
 echo "Starting FHIR Server"
 exec dotnet /app/Microsoft.Health.Fhir.Web.dll
 echo "FHIR Server Started"
