e43b6a224766b8031621c5dba1d250da5aafef97
Sergey Galuzo <sergal@microsoft.com>
2024-07-30 18:56:16 -0700

    1 sec

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlComplexQueryTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlComplexQueryTests.cs
@@ -53,6 +53,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
             await ResetQueryStore();
             SqlServerSearchService.ResetReuseQueryPlans();
             await _fixture.SearchService.SearchAsync(KnownResourceTypes.Patient, [Tuple.Create("address-city", "City1")], CancellationToken.None);
+            await Task.Delay(1000);
             await _fixture.SearchService.SearchAsync(KnownResourceTypes.Patient, [Tuple.Create("address-city", "City2")], CancellationToken.None);
             //// values are different but plans are reused
             await CheckQueryStore(2, 1);
@@ -61,6 +62,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
             await ResetQueryStore();
             SqlServerSearchService.ResetReuseQueryPlans();
             await _fixture.SearchService.SearchAsync(KnownResourceTypes.Patient, [Tuple.Create("address-city", "City1")], CancellationToken.None);
+            await Task.Delay(1000);
             await _fixture.SearchService.SearchAsync(KnownResourceTypes.Patient, [Tuple.Create("address-city", "City2")], CancellationToken.None);
             //// values are different and plans are NOT reused
             await CheckQueryStore(2, 2);
@@ -69,6 +71,7 @@ namespace Microsoft.Health.Fhir.Tests.Integration.Persistence
             await ResetQueryStore();
             SqlServerSearchService.ResetReuseQueryPlans();
             await _fixture.SearchService.SearchAsync(KnownResourceTypes.Patient, [Tuple.Create("address-city", "City1")], CancellationToken.None);
+            await Task.Delay(1000);
             await _fixture.SearchService.SearchAsync(KnownResourceTypes.Patient, [Tuple.Create("address-city", "City1")], CancellationToken.None);
             //// values are same and plans are reused
             await CheckQueryStore(2, 1);
