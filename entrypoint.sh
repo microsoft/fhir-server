cc7014470ac56dc68739d9233408220f9a56bf7c
Mikael Weaver <mikaelw@microsoft.com>
2024-07-30 21:55:49 -0700

    fixing ssh stalling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
 #systemctl start ssh
-/usr/sbin/sshd -D
-exec dotnet ./Microsoft.Health.Fhir.Web.dll
+echo "Starting SSH"
+#/usr/sbin/sshd -D\
+/usr/sbin/sshd
+echo "Starting FHIR Server"
+exec dotnet /app/Microsoft.Health.Fhir.Web.dll
+echo "FHIR Server Started"
