062c29ae25ad475e535fccfcdd22faf96b13dee0
Robert Johnson <rojo@microsoft.com>
2024-08-05 13:38:02 -0700

    Add parameter store

---
+++ src/Microsoft.Health.Fhir.Core/Extensions/LoggerExtensions.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Health.Fhir.Core.Features.Parameters;
+
+namespace Microsoft.Health.Fhir.Core.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static void LogVerbose(this ILogger logger, string message, IParameterStore store, params object?[] args)
+        {
+            if (store.GetParameter("LogLevel").CharValue == "Verbose")
+            {
+                logger.LogInformation(message, args);
+            }
+        }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.CosmosDb/Features/Storage/CosmosParameterStore.cs
@@ -0,0 +1,40 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Health.Fhir.Core.Features.Parameters;
+
+namespace Microsoft.Health.Fhir.CosmosDb.Features.Storage
+{
+    public class CosmosParameterStore : IParameterStore
+    {
+        public Task<Parameter> GetParameter(string name, CancellationToken cancellationToken)
+        {
+            var parameter = new Parameter()
+            {
+                Name = "test",
+                DateValue = DateTime.Now,
+                NumberValue = 0,
+                LongValue = 0,
+                CharValue = "test",
+                BooleanValue = false,
+                UpdatedOn = DateTime.Now,
+                UpdatedBy = "default",
+            };
+            return Task.FromResult<Parameter>(null);
+        }
+
+        public Task SetParameter(Parameter parameter, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void ResetCache()
+        {
+        }
+    }
+}
