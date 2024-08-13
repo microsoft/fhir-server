d397e35899d9038ca80d117f3b469c961ffb115a
Isao Yamauchi <v-iyamauchi@microsoft.com>
2024-08-13 13:36:32 -0700

    Removing unnecessary namespaces.

--- src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Validation/ResourceProfileValidatorTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Validation/ResourceProfileValidatorTests.cs
@@ -5,9 +5,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Reflection;
-using DotLiquid.Util;
 using FluentValidation;
 using FluentValidation.Results;
 using Hl7.Fhir.ElementModel;
