f86ecaa41f6cbe7a76b0b3e22dc02a561e322fa2
Robert Johnson <rojo@microsoft.com>
2024-08-22 09:16:44 -0700

    Pagination fix

--- src/Microsoft.Health.Fhir.SqlServer/Features/Search/SqlServerSearchService.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Search/SqlServerSearchService.cs
@@ -146,7 +146,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search
             SqlSearchOptions sqlSearchOptions = new SqlSearchOptions(searchOptions);
 
             SearchResult searchResult = await SearchImpl(sqlSearchOptions, cancellationToken);
-            int resultCount = searchResult.Results.Count();
+            int resultCount = searchResult.Results.Count(r => r.SearchEntryMode == SearchEntryMode.Match);
 
             if (!sqlSearchOptions.IsSortWithFilter &&
                 searchResult.ContinuationToken == null &&
