02a545fa46fd33fe9789f129da52f15a86df665f
Sergey Galuzo <sergal@microsoft.com>
2024-10-27 15:46:49 -0700

    defrag formatting

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/Defrag.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/Defrag.sql
@@ -20,13 +20,12 @@ BEGIN TRY
   SET @IndexId = (SELECT index_id FROM sys.indexes WHERE object_id = object_id(@TableName) AND name = @IndexName)
   SET @Sql = 'ALTER INDEX '+quotename(@IndexName)+' ON dbo.'+quotename(@TableName)+' '+@Operation
            + CASE WHEN @IsPartitioned = 1 THEN ' PARTITION = '+convert(varchar,@PartitionNumber) ELSE '' END
-           + CASE WHEN @Operation = 'REBUILD' THEN ' WITH (ONLINE = ON '
-                       + CASE 
-                           WHEN EXISTS (SELECT * FROM sys.partitions WHERE object_id = object_id(@TableName) AND index_id = @IndexId AND data_compression_desc = 'PAGE')
-                             THEN ', DATA_COMPRESSION = PAGE'
-                           ELSE ')'
-                         END
-                  ELSE '' 
+           + CASE 
+               WHEN @Operation = 'REBUILD'
+                 THEN ' WITH (ONLINE = ON'
+                      + CASE WHEN EXISTS (SELECT * FROM sys.partitions WHERE object_id = object_id(@TableName) AND index_id = @IndexId AND data_compression_desc = 'PAGE') THEN ', DATA_COMPRESSION = PAGE' ELSE '' END
+                      + ')'
+               ELSE ''
              END
   EXECUTE dbo.LogEvent @Process=@SP,@Mode=@Mode,@Status='Start',@Text=@Sql
 
