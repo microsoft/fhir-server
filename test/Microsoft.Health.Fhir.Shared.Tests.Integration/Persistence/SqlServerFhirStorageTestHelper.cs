d8fc0d7db5cc06c642db104062d3bd2dcb594d57
Sergey Galuzo <sergal@microsoft.com>
2024-12-27 11:28:32 -0800

    Accomodate CurrentResource view

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerFhirStorageTestHelper.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerFhirStorageTestHelper.cs
@@ -322,7 +322,7 @@ INSERT INTO dbo.Parameters (Id,Number) SELECT @LeasePeriodSecId, 10
             {
                 outerCommand.CommandText = @"
 SELECT t.name 
-  FROM (SELECT name, object_id FROM sys.objects WHERE name NOT IN ('CurrentResources', 'HistoryResources', 'RawResources') AND type IN ('u','v')) t 
+  FROM (SELECT name, object_id FROM sys.objects WHERE name NOT IN ('CurrentResource', 'CurrentResources', 'HistoryResources', 'RawResources') AND type IN ('u','v')) t 
        JOIN sys.columns c ON c.object_id = t.object_id
   WHERE c.name = 'ResourceSurrogateId'";
 
@@ -336,8 +336,7 @@ SELECT t.name
                         }
 
                         string tableName = reader.GetString(0);
-                        var where = tableName == "Resource" ? "WHERE RawResource <> 0xF" : string.Empty;
-                        sb.AppendLine($"SELECT '{tableName}' as TableName, MAX(ResourceSurrogateId) as MaxResourceSurrogateId FROM dbo.{tableName} {where}");
+                        sb.AppendLine($"SELECT '{tableName}', max(ResourceSurrogateId) FROM dbo.{tableName} {(tableName == "Resource" ? "WHERE RawResource <> 0xF" : string.Empty)}");
                     }
                 }
             }
