9e1d8a64768fb542ddb7b0d3ac22e363423d56c8
Sergey Galuzo <sergal@microsoft.com>
2025-01-14 15:40:45 -0800

    revert disable indexes

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/85.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/85.sql
@@ -1652,119 +1652,6 @@ BEGIN CATCH
 END CATCH
 
 GO
-CREATE PROCEDURE dbo.CleanupResourceIdIntMap
-@ResetAfter BIT=0
-AS
-SET NOCOUNT ON;
-DECLARE @SP AS VARCHAR (100) = 'CleanupResourceIdIntMap', @Mode AS VARCHAR (100) = 'R=' + isnull(CONVERT (VARCHAR, @ResetAfter), 'NULL'), @st AS DATETIME = getUTCdate(), @Id AS VARCHAR (100) = 'CleanupResourceIdIntMap.LastProcessed.TypeId.ResourceIdInt', @ResourceTypeId AS SMALLINT, @ResourceIdInt AS BIGINT, @RowsToProcess AS INT, @ProcessedRows AS INT = 0, @DeletedRows AS INT = 0, @ReportDate AS DATETIME = getUTCdate();
-DECLARE @LastProcessed AS VARCHAR (100) = isnull((SELECT Char
-                                                  FROM   dbo.Parameters
-                                                  WHERE  Id = @Id), '0.0');
-BEGIN TRY
-    INSERT INTO dbo.Parameters (Id, Char)
-    SELECT @SP,
-           'LogEvent';
-    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Start';
-    INSERT INTO dbo.Parameters (Id, Char)
-    SELECT @Id,
-           '0.0';
-    DECLARE @Types TABLE (
-        ResourceTypeId SMALLINT      PRIMARY KEY,
-        Name           VARCHAR (100));
-    DECLARE @ResourceIdInts TABLE (
-        ResourceIdInt BIGINT PRIMARY KEY);
-    INSERT INTO @Types
-    EXECUTE dbo.GetUsedResourceTypes ;
-    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Run', @Target = '@Types', @Action = 'Insert', @Rows = @@rowcount;
-    SET @ResourceTypeId = substring(@LastProcessed, 1, charindex('.', @LastProcessed) - 1);
-    SET @ResourceIdInt = substring(@LastProcessed, charindex('.', @LastProcessed) + 1, 255);
-    DELETE @Types
-    WHERE  ResourceTypeId < @ResourceTypeId;
-    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Run', @Target = '@Types', @Action = 'Delete', @Rows = @@rowcount;
-    WHILE EXISTS (SELECT *
-                  FROM   @Types)
-        BEGIN
-            SET @ResourceTypeId = (SELECT   TOP 1 ResourceTypeId
-                                   FROM     @Types
-                                   ORDER BY ResourceTypeId);
-            SET @ProcessedRows = 0;
-            SET @DeletedRows = 0;
-            SET @RowsToProcess = 1;
-            WHILE @RowsToProcess > 0
-                BEGIN
-                    DELETE @ResourceIdInts;
-                    INSERT INTO @ResourceIdInts
-                    SELECT   TOP 100000 ResourceIdInt
-                    FROM     dbo.ResourceIdIntMap
-                    WHERE    ResourceTypeId = @ResourceTypeId
-                             AND ResourceIdInt > @ResourceIdInt
-                    ORDER BY ResourceIdInt;
-                    SET @RowsToProcess = @@rowcount;
-                    SET @ProcessedRows += @RowsToProcess;
-                    IF @RowsToProcess > 0
-                        SET @ResourceIdInt = (SELECT max(ResourceIdInt)
-                                              FROM   @ResourceIdInts);
-                    SET @LastProcessed = CONVERT (VARCHAR, @ResourceTypeId) + '.' + CONVERT (VARCHAR, @ResourceIdInt);
-                    DELETE A
-                    FROM   @ResourceIdInts AS A
-                    WHERE  EXISTS (SELECT *
-                                   FROM   dbo.CurrentResources AS B
-                                   WHERE  B.ResourceTypeId = @ResourceTypeId
-                                          AND B.ResourceIdInt = A.ResourceIdInt);
-                    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Run', @Target = '@ResourceIdInts.Current', @Action = 'Delete', @Rows = @@rowcount, @Text = @LastProcessed;
-                    DELETE A
-                    FROM   @ResourceIdInts AS A
-                    WHERE  EXISTS (SELECT *
-                                   FROM   dbo.HistoryResources AS B
-                                   WHERE  B.ResourceTypeId = @ResourceTypeId
-                                          AND B.ResourceIdInt = A.ResourceIdInt);
-                    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Run', @Target = '@ResourceIdInts.History', @Action = 'Delete', @Rows = @@rowcount, @Text = @LastProcessed;
-                    DELETE A
-                    FROM   @ResourceIdInts AS A
-                    WHERE  EXISTS (SELECT *
-                                   FROM   dbo.ResourceReferenceSearchParams AS B
-                                   WHERE  B.ReferenceResourceTypeId = @ResourceTypeId
-                                          AND B.ReferenceResourceIdInt = A.ResourceIdInt);
-                    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Run', @Target = '@ResourceIdInts.Reference', @Action = 'Delete', @Rows = @@rowcount, @Text = @LastProcessed;
-                    IF EXISTS (SELECT *
-                               FROM   @ResourceIdInts)
-                        BEGIN
-                            DELETE A
-                            FROM   dbo.ResourceIdIntMap AS A
-                            WHERE  A.ResourceTypeId = @ResourceTypeId
-                                   AND EXISTS (SELECT *
-                                               FROM   @ResourceIdInts AS B
-                                               WHERE  B.ResourceIdInt = A.ResourceIdInt);
-                            SET @DeletedRows += @@rowcount;
-                        END
-                    UPDATE dbo.Parameters
-                    SET    Char = @LastProcessed
-                    WHERE  Id = @Id;
-                    IF datediff(second, @ReportDate, getUTCdate()) > 60
-                        BEGIN
-                            EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Run', @Target = 'ResourceIdIntMap', @Action = 'Select', @Rows = @ProcessedRows, @Text = @LastProcessed;
-                            SET @ReportDate = getUTCdate();
-                            SET @ProcessedRows = 0;
-                        END
-                END
-            DELETE @Types
-            WHERE  ResourceTypeId = @ResourceTypeId;
-            SET @ResourceIdInt = 0;
-        END
-    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Run', @Target = 'ResourceIdIntMap', @Action = 'Delete', @Rows = @DeletedRows, @Text = @LastProcessed;
-    IF @ResetAfter = 1
-        DELETE dbo.Parameters
-        WHERE  Id = @Id;
-    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'End', @Start = @st;
-END TRY
-BEGIN CATCH
-    IF error_number() = 1750
-        THROW;
-    EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Error';
-    THROW;
-END CATCH
-
-GO
 CREATE OR ALTER PROCEDURE dbo.ConfigurePartitionOnResourceChanges
 @numberOfFuturePartitionsToAdd INT
 AS
@@ -2377,7 +2264,7 @@ BEGIN TRY
                               AND IndexName = Ind);
     EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Info', @Target = 'IndexProperties', @Action = 'Insert', @Rows = @@rowcount;
     DELETE @Indexes
-    WHERE  Tbl IN ('Resource', 'ResourceCurrent', 'ResourceHistory')
+    WHERE  Tbl = 'Resource'
            OR IndId = 1;
     EXECUTE dbo.LogEvent @Process = @SP, @Mode = @Mode, @Status = 'Info', @Target = '@Indexes', @Action = 'Delete', @Rows = @@rowcount;
     WHILE EXISTS (SELECT *
