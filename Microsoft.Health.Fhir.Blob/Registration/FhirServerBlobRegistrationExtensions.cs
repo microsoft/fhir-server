8ff51f39f5ea0be6d896a03d91428f0bbaedee3a
Gunjit Chhatwal <gchhatwal@microsoft.com>
2025-03-03 14:47:43 -0600

    blob data access layer skeleton

---
+++ Microsoft.Health.Fhir.Blob/Registration/FhirServerBlobRegistrationExtensions.cs
@@ -0,0 +1,10 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Fhir.Blob.Registration;
+
+public static class FhirServerBlobRegistrationExtensions
+{
+}
