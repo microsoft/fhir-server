0b604c5e42029c9713913b276fd9f3ab8a6e5d93
Pooja Adhikari <poadhika@microsoft.com>
2025-03-21 17:39:11 -0500

    Only limit the service to Gen2 Fhir Service

--- src/Microsoft.Health.Fhir.Shared.Web/Startup.cs
+++ src/Microsoft.Health.Fhir.Shared.Web/Startup.cs
@@ -98,8 +98,11 @@ namespace Microsoft.Health.Fhir.Web
             {
                 services.Configure<ForwardedHeadersOptions>(options =>
                 {
+                    // Only support forwardedhost header for AzureHealthDataServices.
+                    // Default value of forwarded headers will be ForwardedHeaders.None for Azure Api For Fhir.
                     options.ForwardedHeaders =
-                        ForwardedHeaders.XForwardedHost;
+                        runtimeConfiguration is AzureHealthDataServicesRuntimeConfiguration ?
+                            ForwardedHeaders.XForwardedHost : ForwardedHeaders.None;
 
                     // Only loopback proxies are allowed by default.
                     // Clear that restriction because forwarders are enabled by explicit
