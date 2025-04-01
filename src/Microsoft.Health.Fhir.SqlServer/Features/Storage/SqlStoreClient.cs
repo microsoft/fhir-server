2f57037dcd2c5b17ce77ed3de983a96d8d886918
Sergey Galuzo <sergal@microsoft.com>
2025-04-01 09:26:06 -0700

    Wait on exception

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlStoreClient.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlStoreClient.cs
@@ -9,8 +9,10 @@ using System.Data;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading;
 using System.Threading.Tasks;
+using Antlr4.Runtime.Tree;
 using EnsureThat;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
@@ -234,6 +236,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
             // These waits cause intermittent execution timeouts even for very short (~10msec) calls.
             var start = DateTime.UtcNow;
             var timeoutRetries = 0;
+            var delayOnOverloadMilliseconds = 100;
             while (true)
             {
                 try
@@ -245,9 +248,20 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
                 {
                     if (e.IsExecutionTimeout() && timeoutRetries++ < 3)
                     {
-                        _logger.LogWarning(e, $"Error on {nameof(MergeResourcesBeginTransactionAsync)} timeoutRetries={{TimeoutRetries}}", timeoutRetries);
+                        _logger.LogWarning(e, $"Error on {nameof(MergeResourcesBeginTransactionAsync)}: timeoutRetries={{TimeoutRetries}}", timeoutRetries);
                         await TryLogEvent(nameof(MergeResourcesBeginTransactionAsync), "Warn", $"timeout retries={timeoutRetries}", start, cancellationToken);
                         await Task.Delay(5000, cancellationToken);
+                        continue;
+                    }
+
+                    var sqlEx = e as SqlException;
+                    if (sqlEx != null && sqlEx.Number == SqlStoreErrorCodes.MergeResourcesConcurrentCallsIsAboveOptimal)
+                    {
+                        _logger.LogWarning(e, $"Error on {nameof(MergeResourcesBeginTransactionAsync)}: MergeResources concurrent calls is above optimal. Delay={delayOnOverloadMilliseconds} milliseconds.");
+
+                        // TODO: Prepare to throw 429 instead of wait/delay when bundle code is ready
+                        await Task.Delay(delayOnOverloadMilliseconds, cancellationToken);
+                        delayOnOverloadMilliseconds = (int)(delayOnOverloadMilliseconds * (2 + (RandomNumberGenerator.GetInt32(1000) / 1000.0)));
                         continue;
                     }
 
