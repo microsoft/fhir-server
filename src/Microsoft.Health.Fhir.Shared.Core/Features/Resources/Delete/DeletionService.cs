019b1f58df5ad2c7cd70baf2a3e7f7c7188adabb
Tarun Mathew (Centific Technologies Inc) <v-tmathew@microsoft.com>
2025-04-01 14:40:56 -0700

    More descriptive message for truncated include items.

--- src/Microsoft.Health.Fhir.Shared.Core/Features/Resources/Delete/DeletionService.cs
+++ src/Microsoft.Health.Fhir.Shared.Core/Features/Resources/Delete/DeletionService.cs
@@ -433,7 +433,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Persistence
 
         private bool AreIncludeResultsTruncated()
         {
-            return _contextAccessor.RequestContext.BundleIssues.Any(x => string.Equals(x.Diagnostics, Core.Resources.TruncatedIncludeMessage, StringComparison.OrdinalIgnoreCase));
+            return _contextAccessor.RequestContext.BundleIssues.Any(x => KnownTruncatedIncludeMessages.IsKnownMessage(x.Diagnostics));
         }
 
         private static Dictionary<string, long> AppendDeleteResults(Dictionary<string, long> results, IEnumerable<Dictionary<string, long>> newResults)
