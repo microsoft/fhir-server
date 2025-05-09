2a3b15b3e128ccbd942a452b16b58003d536c421
SergeyGaluzo <95932081+SergeyGaluzo@users.noreply.github.com>
2025-05-09 09:13:29 -0700

    Remove version from full.sql and signatures clean up (#4979)
    * Remove version from full.sql and PuJobHeartbeat signature
    
    * Removed @Data
    
    * enqueue

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/89.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/89.diff.sql
@@ -1,6 +1,42 @@
-﻿--DROP PROCEDURE dbo.EnqueueJobs
+ALTER PROCEDURE dbo.PutJobHeartbeat @QueueType tinyint, @JobId bigint, @Version bigint, @CancelRequested bit = 0 OUTPUT
+AS
+set nocount on
+DECLARE @SP varchar(100) = 'PutJobHeartbeat'
+       ,@Mode varchar(100)
+       ,@st datetime = getUTCdate()
+       ,@Rows int = 0
+       ,@PartitionId tinyint = @JobId % 16
+
+SET @Mode = 'Q='+convert(varchar,@QueueType)+' J='+convert(varchar,@JobId)+' P='+convert(varchar,@PartitionId)+' V='+convert(varchar,@Version)
+
+BEGIN TRY
+  UPDATE dbo.JobQueue
+    SET @CancelRequested = CancelRequested
+       ,HeartbeatDate = getUTCdate()
+    WHERE QueueType = @QueueType
+      AND PartitionId = @PartitionId
+      AND JobId = @JobId
+      AND Status = 1
+      AND Version = @Version
+  SET @Rows = @@rowcount
+  
+  IF @Rows = 0 AND NOT EXISTS (SELECT * FROM dbo.JobQueue WHERE QueueType = @QueueType AND PartitionId = @PartitionId AND JobId = @JobId AND Version = @Version AND Status IN (2,3,4))
+  BEGIN
+    IF EXISTS (SELECT * FROM dbo.JobQueue WHERE QueueType = @QueueType AND PartitionId = @PartitionId AND JobId = @JobId)
+      THROW 50412, 'Precondition failed', 1
+    ELSE
+      THROW 50404, 'Job record not found', 1
+  END
+
+  EXECUTE dbo.LogEvent @Process=@SP,@Mode=@Mode,@Status='End',@Start=@st,@Rows=@Rows
+END TRY
+BEGIN CATCH
+  IF error_number() = 1750 THROW -- Real error is before 1750, cannot trap in SQL.
+  EXECUTE dbo.LogEvent @Process=@SP,@Mode=@Mode,@Status='Error';
+  THROW
+END CATCH
 GO
-CREATE PROCEDURE dbo.EnqueueJobs @QueueType tinyint, @Definitions StringList READONLY, @GroupId bigint = NULL, @ForceOneActiveJobGroup bit = 1, @Status tinyint = NULL, @Result varchar(max) = NULL, @StartDate datetime = NULL, @ReturnJobs bit = 1
+ALTER PROCEDURE dbo.EnqueueJobs @QueueType tinyint, @Definitions StringList READONLY, @GroupId bigint = NULL, @ForceOneActiveJobGroup bit = 1, @Status tinyint = NULL, @Result varchar(max) = NULL, @StartDate datetime = NULL, @ReturnJobs bit = 1
 AS
 set nocount on
 DECLARE @SP varchar(100) = 'EnqueueJobs'
@@ -79,6 +115,3 @@ BEGIN CATCH
   THROW
 END CATCH
 GO
---DECLARE @Definitions StringList
---INSERT INTO @Definitions SELECT 'Test'
---EXECUTE dbo.EnqueueJobs 2, @Definitions, @ForceOneActiveJobGroup = 1
