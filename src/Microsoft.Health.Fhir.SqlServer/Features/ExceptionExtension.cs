b8d74b501d002d21db5b8009448f7a7cdd242127
Paola Rodríguez Martínez <paolaro@microsoft.com>
2025-05-29 08:09:44 -0600

    Remove unused using

--- src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 using System;
-using Microsoft.Data.SqlClient;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features
 {
