82d820ac788d8e6e86c4a2f3ed79456193cda30d
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2025-06-05 18:48:18 -0400

    Adding new middleware to simulate some of the work executed by ASP.NET.

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Export/ExportDataTestFixture.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Export/ExportDataTestFixture.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Antlr4.Runtime.Atn;
 using Hl7.Fhir.Model;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
