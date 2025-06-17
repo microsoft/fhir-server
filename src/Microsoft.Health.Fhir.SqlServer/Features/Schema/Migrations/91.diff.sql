4e405d7730960c282c4cfc83ceece5e9db60e0bd
rajithaaluri <rajithaaluri@microsoft.com>
2025-06-17 17:53:46 -0500

    logic fixed

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/91.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/91.diff.sql
@@ -1,7 +1,7 @@
 SET XACT_ABORT ON
 BEGIN TRANSACTION
 
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_ReferenceTokenCompositeSearchParam_CodeOverflow2')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_ReferenceTokenCompositeSearchParam_CodeOverflow2')
 BEGIN    
     ALTER TABLE dbo.ReferenceTokenCompositeSearchParam Drop CONSTRAINT CHK_ReferenceTokenCompositeSearchParam_CodeOverflow2;
     ALTER TABLE dbo.ReferenceTokenCompositeSearchParam
@@ -12,7 +12,7 @@ COMMIT TRANSACTION
 GO
 
 BEGIN TRANSACTION
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenDateTimeCompositeSearchParam_CodeOverflow1')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenDateTimeCompositeSearchParam_CodeOverflow1')
 BEGIN    
     ALTER TABLE dbo.TokenDateTimeCompositeSearchParam Drop CONSTRAINT CHK_TokenDateTimeCompositeSearchParam_CodeOverflow1;
     ALTER TABLE dbo.TokenDateTimeCompositeSearchParam
@@ -23,7 +23,7 @@ COMMIT TRANSACTION
 GO
 
 BEGIN TRANSACTION
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenNumberNumberCompositeSearchParam_CodeOverflow1')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenNumberNumberCompositeSearchParam_CodeOverflow1')
 BEGIN    
     ALTER TABLE dbo.TokenNumberNumberCompositeSearchParam Drop CONSTRAINT CHK_TokenNumberNumberCompositeSearchParam_CodeOverflow1;
     ALTER TABLE dbo.TokenNumberNumberCompositeSearchParam
@@ -34,51 +34,51 @@ COMMIT TRANSACTION
 GO
 
 BEGIN TRANSACTION
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenQuantityCompositeSearchParam_CodeOverflow1')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenQuantityCompositeSearchParam_CodeOverflow1')
 BEGIN    
     ALTER TABLE dbo.TokenQuantityCompositeSearchParam Drop CONSTRAINT CHK_TokenQuantityCompositeSearchParam_CodeOverflow1;
     ALTER TABLE dbo.TokenQuantityCompositeSearchParam
-    ADD CONSTRAINT CHK_TokenQuantityCompositeSearchParam_CodeOverflow1 CHECK (LEN(Code1) = 256
+    ADD CONSTRAINT CHK_TokenQuantityCompositeSearchParam_CodeOverflow1 CHECK (DATALENGTH(Code1) = 256
                                                                               OR CodeOverflow1 IS NULL);
 END
 COMMIT TRANSACTION
 GO
 
 BEGIN TRANSACTION
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenStringCompositeSearchParam_CodeOverflow1')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenStringCompositeSearchParam_CodeOverflow1')
 BEGIN    
     ALTER TABLE dbo.TokenStringCompositeSearchParam Drop CONSTRAINT CHK_TokenStringCompositeSearchParam_CodeOverflow1;
     ALTER TABLE dbo.TokenStringCompositeSearchParam
-    ADD CONSTRAINT CHK_TokenStringCompositeSearchParam_CodeOverflow1 CHECK (LEN(Code1) = 256
+    ADD CONSTRAINT CHK_TokenStringCompositeSearchParam_CodeOverflow1 CHECK (DATALENGTH(Code1) = 256
                                                                             OR CodeOverflow1 IS NULL);
 END
 COMMIT TRANSACTION
 GO
 
 BEGIN TRANSACTION
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenTokenCompositeSearchParam_CodeOverflow1')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenTokenCompositeSearchParam_CodeOverflow1')
 BEGIN    
     ALTER TABLE dbo.TokenTokenCompositeSearchParam Drop CONSTRAINT CHK_TokenTokenCompositeSearchParam_CodeOverflow1;
     ALTER TABLE dbo.TokenTokenCompositeSearchParam
-    ADD CONSTRAINT CHK_TokenTokenCompositeSearchParam_CodeOverflow1 CHECK (LEN(Code1) = 256
+    ADD CONSTRAINT CHK_TokenTokenCompositeSearchParam_CodeOverflow1 CHECK (DATALENGTH(Code1) = 256
                                                                            OR CodeOverflow1 IS NULL);
 END
 COMMIT TRANSACTION
 GO
 
 BEGIN TRANSACTION
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenTokenCompositeSearchParam_CodeOverflow2')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenTokenCompositeSearchParam_CodeOverflow2')
 BEGIN    
     ALTER TABLE dbo.TokenTokenCompositeSearchParam Drop CONSTRAINT CHK_TokenTokenCompositeSearchParam_CodeOverflow2;
     ALTER TABLE dbo.TokenTokenCompositeSearchParam
-    ADD CONSTRAINT CHK_TokenTokenCompositeSearchParam_CodeOverflow2 CHECK (LEN(Code2) = 256
+    ADD CONSTRAINT CHK_TokenTokenCompositeSearchParam_CodeOverflow2 CHECK (DATALENGTH(Code2) = 256
                                                                            OR CodeOverflow2 IS NULL);
 END
 COMMIT TRANSACTION
 GO
 
 BEGIN TRANSACTION
-IF NOT EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenSearchParam_CodeOverflow')
+IF EXISTS(SELECT 1 FROM sys.check_constraints WHERE name = 'CHK_TokenSearchParam_CodeOverflow')
 BEGIN    
     ALTER TABLE dbo.TokenSearchParam Drop CONSTRAINT CHK_TokenSearchParam_CodeOverflow;
     ALTER TABLE dbo.TokenSearchParam
