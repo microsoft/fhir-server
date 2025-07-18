102129520ac07e9bc7a5cccd0ea9605868facbfb
Jose Perales <joseperales@microsoft.com>
2025-07-18 11:39:08 -0400

    devcontainer working on mac os

--- .devcontainer/library-scripts/docker-debian.sh
+++ .devcontainer/library-scripts/docker-debian.sh
@@ -71,8 +71,9 @@ else
         apt-get update
         apt-get -y install --no-install-recommends moby-cli moby-buildx
     else
+        ARCH=$(dpkg --print-architecture)
         curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | (OUT=$(apt-key add - 2>&1) || echo $OUT)
-        echo "deb [arch=amd64] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+        echo "deb [arch=${ARCH}] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
         apt-get update
         apt-get -y install --no-install-recommends docker-ce-cli
     fi
