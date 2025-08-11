029bdc8ac8bf0cf58b2923278243aaf3a1cfaa41
Sergey Galuzo <sergal@microsoft.com>
2025-08-11 16:14:50 -0700

    Remove raw resource size computation

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerWatchdogTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/SqlServerWatchdogTests.cs
@@ -174,22 +174,6 @@ END
 
             // TODO: Temp code to test database stats
             startTime = DateTime.UtcNow;
-            while ((GetEventLogCount("tmp_GetRawResources") == 0) && (DateTime.UtcNow - startTime).TotalSeconds < 60)
-            {
-                await Task.Delay(TimeSpan.FromSeconds(1), cts.Token);
-            }
-
-            Assert.True((DateTime.UtcNow - startTime).TotalSeconds < 60, "tmp_GetRawResources message is not found");
-
-            startTime = DateTime.UtcNow;
-            while ((GetEventLogCount("DatabaseStats.ResourceTypeTotals") == 0) && (DateTime.UtcNow - startTime).TotalSeconds < 60)
-            {
-                await Task.Delay(TimeSpan.FromSeconds(1), cts.Token);
-            }
-
-            Assert.True((DateTime.UtcNow - startTime).TotalSeconds < 60, "DatabaseStats.ResourceTypeTotals message is not found");
-
-            startTime = DateTime.UtcNow;
             while ((GetEventLogCount("DatabaseStats.SearchParamCount") == 0) && (DateTime.UtcNow - startTime).TotalSeconds < 60)
             {
                 await Task.Delay(TimeSpan.FromSeconds(1), cts.Token);
