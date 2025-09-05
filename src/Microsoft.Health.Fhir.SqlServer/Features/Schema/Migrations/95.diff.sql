e431b78c6320c017c5a90455595219d011556006
John Estrada <joestr@microsoft.com>
2025-09-05 14:46:52 -0500

    Updating SearchParam table columns Status, LastUpdated and IsPartiallySupported to "NOT NULL" per request and feedback from Sergey.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/95.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/95.diff.sql
@@ -85,3 +85,16 @@ BEGIN CATCH
   THROW
 END CATCH
 GO
+
+CREATE OR ALTER TABLE dbo.SearchParam
+(
+    SearchParamId           smallint IDENTITY(1,1)      NOT NULL,
+    CONSTRAINT UQ_SearchParam_SearchParamId UNIQUE (SearchParamId),
+    Uri                     varchar(128)                COLLATE Latin1_General_100_CS_AS NOT NULL,
+    CONSTRAINT PKC_SearchParam PRIMARY KEY CLUSTERED (Uri)
+    WITH (DATA_COMPRESSION = PAGE),
+    Status                  varchar(20)                 NOT NULL,
+    LastUpdated             datetimeoffset(7)           NOT NULL,
+    IsPartiallySupported    bit                         NOT NULL
+)
+GO
