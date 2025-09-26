68e7650390423d1f05d01d9a90c75809716fe4a5
Robert Johnson <rojo@microsoft.com>
2025-09-26 08:42:30 -0700

    Fix reference remover

---
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Resources/Delete/RemoveReferenceTests.cs
@@ -0,0 +1,110 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Hl7.Fhir.Model;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Core.Models;
+using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Test.Utilities;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Resources
+{
+    [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
+    [Trait(Traits.Category, Categories.BulkDelete)]
+    public class RemoveReferenceTests
+    {
+        [Fact]
+        public void GivenResourceWithReference_WhenReferenceIsRemoved_ThenReferenceIsRemoved()
+        {
+            var referenceId = "testRef";
+            var patient = new Patient();
+            patient.GeneralPractitioner.Add(new ResourceReference(KnownResourceTypes.Practitioner + "/" + referenceId));
+
+            var id = "testPatient";
+            patient.Id = id;
+
+            var name = new HumanName() { Family = "Testson" };
+            patient.Name.Add(name);
+
+            ReferenceRemover.RemoveReference(patient, referenceId);
+
+            Assert.True(patient.GeneralPractitioner.First().Reference == null);
+            Assert.Equal("Referenced resource deleted", patient.GeneralPractitioner.First().Display);
+            Assert.Equal(id, patient.Id);
+            Assert.Equal(name, patient.Name.First());
+        }
+
+        [Fact]
+        public void GivenResourceWithoutReference_WhenReferenceIsRemoved_ThenNothingIsChanged()
+        {
+            var referenceId = "testRef";
+            var patient = new Patient();
+
+            var id = "testPatient";
+            patient.Id = id;
+
+            var name = new HumanName() { Family = "Testson" };
+            patient.Name.Add(name);
+
+            ReferenceRemover.RemoveReference(patient, referenceId);
+
+            Assert.Equal(id, patient.Id);
+            Assert.Equal(name, patient.Name.First());
+        }
+
+        [Fact]
+        public void GivenResourceWithReferenceDescription_WhenReferenceIsRemoved_ThenNothingIsChanged()
+        {
+            var referenceId = "testRef";
+            var patient = new Patient();
+
+            var reference = new ResourceReference();
+            reference.Display = referenceId;
+            patient.GeneralPractitioner.Add(reference);
+
+            var id = "testPatient";
+            patient.Id = id;
+
+            var name = new HumanName() { Family = "Testson" };
+            patient.Name.Add(name);
+
+            ReferenceRemover.RemoveReference(patient, referenceId);
+
+            Assert.True(patient.GeneralPractitioner.First().Reference == null);
+            Assert.Equal(referenceId, patient.GeneralPractitioner.First().Display);
+            Assert.Equal(id, patient.Id);
+            Assert.Equal(name, patient.Name.First());
+        }
+
+        [Fact]
+        public void GivenResourceWithTwoReferences_WhenReferenceIsRemoved_ThenOtherReferenceIsNotChanged()
+        {
+            var referenceId = "testRef";
+            var patient = new Patient();
+            patient.GeneralPractitioner.Add(new ResourceReference(KnownResourceTypes.Practitioner + "/" + referenceId));
+            patient.GeneralPractitioner.Add(new ResourceReference(KnownResourceTypes.Practitioner + "/other"));
+
+            var id = "testPatient";
+            patient.Id = id;
+
+            var name = new HumanName() { Family = "Testson" };
+            patient.Name.Add(name);
+
+            ReferenceRemover.RemoveReference(patient, referenceId);
+
+            Assert.True(patient.GeneralPractitioner[0].Reference == null);
+            Assert.Equal("Referenced resource deleted", patient.GeneralPractitioner[0].Display);
+
+            Assert.Equal(KnownResourceTypes.Practitioner + "/other", patient.GeneralPractitioner[1].Reference);
+            Assert.True(patient.GeneralPractitioner[1].Display == null);
+
+            Assert.Equal(id, patient.Id);
+            Assert.Equal(name, patient.Name.First());
+        }
+    }
+}
