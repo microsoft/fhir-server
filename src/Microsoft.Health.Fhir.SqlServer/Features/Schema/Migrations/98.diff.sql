f749678240ac6f1b543dd43b96e449e78202145e
John Estrada <joestr@microsoft.com>
2025-10-17 12:55:14 -0500

    adding if exist check to  index

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/98.diff.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/98.diff.sql
@@ -1,4 +1,14 @@
-CREATE NONCLUSTERED INDEX IX_SearchParam_LastUpdated
-ON dbo.SearchParam (LastUpdated)
-WITH (DATA_COMPRESSION = PAGE)
+-- Add index on LastUpdated column for SearchParam table to improve query performance
+-- Only create if it doesn't already exist
+IF NOT EXISTS (
+    SELECT 1 
+    FROM sys.indexes 
+    WHERE object_id = OBJECT_ID('dbo.SearchParam') 
+    AND name = 'IX_SearchParam_LastUpdated'
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX IX_SearchParam_LastUpdated
+    ON dbo.SearchParam (LastUpdated)
+    WITH (DATA_COMPRESSION = PAGE)
+END
 GO
