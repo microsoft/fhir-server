93f67f9773d3317b4e8649c67da929ceabfbaab3
John Estrada <joestr@microsoft.com>
2025-12-01 18:22:15 -0600

    Fix to RetryTheoryDiscovery Added Retry to ConvertDataRequestHandlerTests Removed SearchParameter Creation and usage from BulkUpdateTests to prevent conflicts on race conditions with ReindexTests

--- src/Microsoft.Health.Extensions.Xunit/RetryTheoryDiscoverer.cs
+++ src/Microsoft.Health.Extensions.Xunit/RetryTheoryDiscoverer.cs
@@ -4,6 +4,7 @@
 // -------------------------------------------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Linq;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
@@ -11,25 +12,24 @@ namespace Microsoft.Health.Extensions.Xunit
 {
     /// <summary>
     /// Test case discoverer for <see cref="RetryTheoryAttribute"/>.
+    /// For Theory tests, we need to let xUnit discover the data-driven test cases first,
+    /// then wrap each one with retry logic.
     /// </summary>
-    public class RetryTheoryDiscoverer : IXunitTestCaseDiscoverer
+    public class RetryTheoryDiscoverer : TheoryDiscoverer
     {
-        private readonly TheoryDiscoverer _theoryDiscoverer;
-        private readonly IMessageSink _diagnosticMessageSink;
-
         public RetryTheoryDiscoverer(IMessageSink diagnosticMessageSink)
+            : base(diagnosticMessageSink)
         {
-            _diagnosticMessageSink = diagnosticMessageSink;
-            _theoryDiscoverer = new TheoryDiscoverer(diagnosticMessageSink);
         }
 
-        public IEnumerable<IXunitTestCase> Discover(
+        protected override IEnumerable<IXunitTestCase> CreateTestCasesForDataRow(
             ITestFrameworkDiscoveryOptions discoveryOptions,
             ITestMethod testMethod,
-            IAttributeInfo factAttribute)
+            IAttributeInfo theoryAttribute,
+            object[] dataRow)
         {
-            var maxRetries = factAttribute.GetNamedArgument<int>(nameof(RetryTheoryAttribute.MaxRetries));
-            var delayMs = factAttribute.GetNamedArgument<int>(nameof(RetryTheoryAttribute.DelayBetweenRetriesMs));
+            var maxRetries = theoryAttribute.GetNamedArgument<int>(nameof(RetryTheoryAttribute.MaxRetries));
+            var delayMs = theoryAttribute.GetNamedArgument<int>(nameof(RetryTheoryAttribute.DelayBetweenRetriesMs));
 
             // Use default values if not specified
             if (maxRetries == 0)
@@ -42,19 +42,46 @@ namespace Microsoft.Health.Extensions.Xunit
                 delayMs = 5000;
             }
 
-            // Use the theory discoverer to get the test cases (one per data row)
-            foreach (var testCase in _theoryDiscoverer.Discover(discoveryOptions, testMethod, factAttribute))
+            // Create a RetryTestCase for each data row
+            yield return new RetryTestCase(
+                DiagnosticMessageSink,
+                discoveryOptions.MethodDisplayOrDefault(),
+                discoveryOptions.MethodDisplayOptionsOrDefault(),
+                testMethod,
+                maxRetries,
+                delayMs,
+                dataRow);
+        }
+
+        protected override IEnumerable<IXunitTestCase> CreateTestCasesForTheory(
+            ITestFrameworkDiscoveryOptions discoveryOptions,
+            ITestMethod testMethod,
+            IAttributeInfo theoryAttribute)
+        {
+            var maxRetries = theoryAttribute.GetNamedArgument<int>(nameof(RetryTheoryAttribute.MaxRetries));
+            var delayMs = theoryAttribute.GetNamedArgument<int>(nameof(RetryTheoryAttribute.DelayBetweenRetriesMs));
+
+            // Use default values if not specified
+            if (maxRetries == 0)
             {
-                // Wrap each test case in a RetryTestCase
-                yield return new RetryTestCase(
-                    _diagnosticMessageSink,
+                maxRetries = 3;
+            }
+
+            if (delayMs == 0)
+            {
+                delayMs = 5000;
+            }
+
+            // For theories without data (will be skipped), wrap in retry
+            return base.CreateTestCasesForTheory(discoveryOptions, testMethod, theoryAttribute)
+                .Select(testCase => new RetryTestCase(
+                    DiagnosticMessageSink,
                     discoveryOptions.MethodDisplayOrDefault(),
                     discoveryOptions.MethodDisplayOptionsOrDefault(),
-                    testCase.TestMethod,
+                    testMethod,
                     maxRetries,
                     delayMs,
-                    testCase.TestMethodArguments);
-            }
+                    testCase.TestMethodArguments));
         }
     }
 }
