521683ef1d94f4d2d2e949ddca10e81c76685659
RB Johnson (He/Him) <rojo@microsoft.com>
2025-12-31 14:57:28 -0800

    Add check to allow meta history in transaction bundles

--- src/Microsoft.Health.Fhir.Shared.Api/Features/Resources/Bundle/TransactionBundleValidator.cs
+++ src/Microsoft.Health.Fhir.Shared.Api/Features/Resources/Bundle/TransactionBundleValidator.cs
@@ -12,16 +12,23 @@ using EnsureThat;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Health.Fhir.Core.Exceptions;
+using Microsoft.Health.Fhir.Core.Features;
 using Microsoft.Health.Fhir.Core.Features.Resources;
 using Microsoft.Health.Fhir.Core.Features.Routing;
 using Microsoft.Health.Fhir.Core.Features.Search;
 using Microsoft.Health.Fhir.Core.Models;
+using SharpCompress.Common;
 using static Hl7.Fhir.Model.Bundle;
 
 namespace Microsoft.Health.Fhir.Api.Features.Resources.Bundle
 {
     public class TransactionBundleValidator
     {
+        private static readonly string[] NonConditionalQueryParameters = new string[]
+        {
+            KnownQueryParameterNames.MetaHistory,
+        };
+
         private readonly ResourceReferenceResolver _referenceResolver;
         private readonly ILogger<TransactionBundleValidator> _logger;
 
@@ -78,7 +85,7 @@ namespace Microsoft.Health.Fhir.Api.Features.Resources.Bundle
         private async Task<string> GetResourceId(EntryComponent entry, IDictionary<string, (string resourceId, string resourceType)> idDictionary, CancellationToken cancellationToken)
         {
             // If there is no search or conditional operations, then use the FullUrl for posts and the request url otherwise
-            if (string.IsNullOrWhiteSpace(entry.Request.IfNoneExist) && !entry.Request.Url.Contains('?', StringComparison.Ordinal))
+            if (!IsConditionalRequest(entry.Request))
             {
                 return entry.Request.Method == HTTPVerb.POST ? entry.FullUrl : entry.Request.Url;
             }
@@ -158,6 +165,24 @@ namespace Microsoft.Health.Fhir.Api.Features.Resources.Bundle
             // Check for duplicate resources within a bundle entry is skipped if the request within a entry is not modifying the resource.
             return !(requestMethod == HTTPVerb.GET
                     || requestUrl.Contains('$', StringComparison.InvariantCulture));
+        }
+
+        private static bool IsConditionalRequest(RequestComponent request)
+        {
+            var conditionalRequest = !string.IsNullOrWhiteSpace(request.IfNoneExist);
+
+            if (request.Url.Contains('?', StringComparison.Ordinal))
+            {
+                var queryString = request.Url.Split('?').Last();
+                var queryParameters = queryString.Split('&', StringSplitOptions.RemoveEmptyEntries).Select((string parameter) => parameter.Split('=', 2)[0]);
+
+                if (queryParameters.Any(param => !NonConditionalQueryParameters.Contains(param)))
+                {
+                    conditionalRequest = true;
+                }
+            }
+
+            return conditionalRequest;
         }
     }
 }
