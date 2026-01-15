912b56d17114dec42fce3edd0ddcddf46d98e8d8
RB Johnson (He/Him) <rojo@microsoft.com>
2026-01-15 10:44:52 -0800

    Better fix

--- src/Microsoft.Health.Fhir.Shared.Core/Features/Resources/Delete/DeletionService.cs
+++ src/Microsoft.Health.Fhir.Shared.Core/Features/Resources/Delete/DeletionService.cs
@@ -139,10 +139,6 @@ namespace Microsoft.Health.Fhir.Core.Features.Persistence
         {
             EnsureArg.IsNotNull(request, nameof(request));
 
-            // This is needed for tracking if a request is truncating include results.
-            // The context is shared for the lifetime of the request, so the issues need to be cleared between searches.
-            _contextAccessor.RequestContext.BundleIssues.Clear();
-
             var searchCount = 1000;
             bool tooManyIncludeResults = false;
 
@@ -179,7 +175,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Persistence
             using var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
 
             // If there is more than one page of results included then delete all included results for the first page of resources.
-            if (!request.IsIncludesRequest && AreIncludeResultsTruncated())
+            if (!request.IsIncludesRequest && AreIncludeResultsTruncated(ict))
             {
                 try
                 {
@@ -266,7 +262,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Persistence
                         }
 
                         // If the next page of results has more than one page of included results, delete all pages of included results before deleting the primary results.
-                        if (!request.IsIncludesRequest && AreIncludeResultsTruncated())
+                        if (!request.IsIncludesRequest && AreIncludeResultsTruncated(ict))
                         {
                             try
                             {
@@ -600,11 +596,10 @@ namespace Microsoft.Health.Fhir.Core.Features.Persistence
             }
         }
 
-        private bool AreIncludeResultsTruncated()
+        private bool AreIncludeResultsTruncated(string ict)
         {
-            return _contextAccessor.RequestContext.BundleIssues.Any(
-                x => string.Equals(x.Diagnostics, Core.Resources.TruncatedIncludeMessage, StringComparison.OrdinalIgnoreCase)
-                    || string.Equals(x.Diagnostics, Core.Resources.TruncatedIncludeMessageForIncludes, StringComparison.OrdinalIgnoreCase));
+            return _contextAccessor.RequestContext.BundleIssues.Any(x => string.Equals(x.Diagnostics, Core.Resources.TruncatedIncludeMessage, StringComparison.OrdinalIgnoreCase))
+                || !string.IsNullOrEmpty(ict);
         }
 
         private static Dictionary<string, long> AppendDeleteResults(Dictionary<string, long> results, IEnumerable<Dictionary<string, long>> newResults)
