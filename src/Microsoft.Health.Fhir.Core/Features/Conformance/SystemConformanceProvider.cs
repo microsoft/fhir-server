bed5a44f0fec595cb7c9159eb7f4bdfaef1da2ef
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2026-01-17 00:30:40 -0500

    Capability statement refreshes when profiles are updated (#5330)
    * Forcing capability statement to refresh when profiles are updated
    * Adding background job to update Profiles.
    * Propagate a valid cancellation token to background job.
    * Changes to ensure configurations are passed during tests.
    * Tests to validate the logic of the background loop.

--- src/Microsoft.Health.Fhir.Core/Features/Conformance/SystemConformanceProvider.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Conformance/SystemConformanceProvider.cs
@@ -209,6 +209,12 @@ namespace Microsoft.Health.Fhir.Core.Features.Conformance
                 Stopwatch sw = Stopwatch.StartNew();
                 for (int i = 0; i < _rebuildDelay; i++)
                 {
+                    if (_builder != null && _builder.IsSyncProfilesRequested())
+                    {
+                        _logger.LogInformation("SystemConformanceProvider sync is requested.");
+                        break;
+                    }
+
                     await Task.Delay(TimeSpan.FromMinutes(1), _cancellationTokenSource.Token);
 
                     if (_disposed)
@@ -230,13 +236,21 @@ namespace Microsoft.Health.Fhir.Core.Features.Conformance
                     }
                 }
 
-                if (_builder != null)
+                try
                 {
-                    // Update search params;
-                    _builder.SyncSearchParameters();
+                    if (_builder != null)
+                    {
+                        // Update search params.
+                        _builder.SyncSearchParameters();
 
-                    // Update supported profiles;
-                    await _builder.SyncProfilesAsync(_cancellationTokenSource.Token);
+                        // Update supported profiles.
+                        await _builder.SyncProfilesAsync(_cancellationTokenSource.Token);
+                    }
+                }
+                catch (Exception e)
+                {
+                    // Do not let exceptions escape the background loop.
+                    _logger.LogError(e, "SystemConformanceProvider: Unexpected error during background capability statement rebuild.");
                 }
 
                 await (_metadataSemaphore?.WaitAsync(_cancellationTokenSource.Token) ?? Task.CompletedTask);
