3c2b777cb748b88755310e1f92d4b390a7143575
Isao Yamauchi <v-isyamauchi@microsoft.com>
2026-01-26 13:20:13 -0800

    Adding more export UTs.

--- src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Controllers/ExportControllerTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Api.UnitTests/Controllers/ExportControllerTests.cs
@@ -5,25 +5,30 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 using Hl7.Fhir.Model;
-using Hl7.Fhir.Rest;
 using MediatR;
-using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
 using Microsoft.Health.Core.Features.Context;
 using Microsoft.Health.Fhir.Api.Configs;
 using Microsoft.Health.Fhir.Api.Controllers;
+using Microsoft.Health.Fhir.Api.Features.ActionResults;
 using Microsoft.Health.Fhir.Core.Configs;
 using Microsoft.Health.Fhir.Core.Exceptions;
 using Microsoft.Health.Fhir.Core.Features.ArtifactStore;
 using Microsoft.Health.Fhir.Core.Features.Context;
+using Microsoft.Health.Fhir.Core.Features.Operations.Export;
+using Microsoft.Health.Fhir.Core.Features.Operations.Export.Models;
 using Microsoft.Health.Fhir.Core.Features.Routing;
 using Microsoft.Health.Fhir.Core.Messages.Export;
+using Microsoft.Health.Fhir.Core.Models;
 using Microsoft.Health.Fhir.Core.Registration;
 using Microsoft.Health.Fhir.Tests.Common;
 using Microsoft.Health.Test.Utilities;
@@ -54,6 +59,11 @@ namespace Microsoft.Health.Fhir.Api.UnitTests.Controllers
         public ExportControllerTests()
         {
             _exportEnabledController = GetController(_exportEnabledJobConfiguration, _featureConfiguration, _artifactStoreConfig);
+            _exportEnabledController.ControllerContext = new ControllerContext(
+               new ActionContext(
+                   Substitute.For<HttpContext>(),
+                   new RouteData(),
+                   new ControllerActionDescriptor()));
         }
 
         [Fact]
@@ -348,6 +358,150 @@ namespace Microsoft.Health.Fhir.Api.UnitTests.Controllers
             }
         }
 
+        [Fact]
+        public async Task GivenAnExportRequest_WhenProcessing_ThenCreateExportRequestShouldBeCreatedCorrectly()
+        {
+            await RunCreateExportRequestTest(
+                (since, till, resourceType, groupId, containerName, typeFilter, formatName, isParallel, includeAssociatedData, maxCount, anonymizationConfigCollectionReference, anonymizationConfigLocation, anonymizationConfigFileETag) =>
+                    _exportEnabledController.Export(
+                        since,
+                        till,
+                        resourceType,
+                        containerName,
+                        typeFilter,
+                        formatName,
+                        isParallel,
+                        includeAssociatedData,
+                        maxCount,
+                        anonymizationConfigCollectionReference,
+                        anonymizationConfigLocation,
+                        anonymizationConfigFileETag),
+                ExportJobType.All,
+                KnownResourceTypes.Patient,
+                null,
+                "_history,_deleted");
+        }
+
+        [Fact]
+        public async Task GivenAnExportResourceTypeRequest_WhenProcessing_ThenCreateExportRequestShouldBeCreatedCorrectly()
+        {
+            await RunCreateExportRequestTest(
+                (since, till, resourceType, groupId, containerName, typeFilter, formatName, isParallel, includeAssociatedData, maxCount, anonymizationConfigCollectionReference, anonymizationConfigLocation, anonymizationConfigFileETag) =>
+                    _exportEnabledController.ExportResourceType(
+                        KnownResourceTypes.Patient,
+                        since,
+                        till,
+                        resourceType,
+                        containerName,
+                        typeFilter,
+                        formatName,
+                        maxCount,
+                        anonymizationConfigCollectionReference,
+                        anonymizationConfigLocation,
+                        anonymizationConfigFileETag),
+                ExportJobType.Patient,
+                KnownResourceTypes.Observation);
+        }
+
+        [Fact]
+        public async Task GivenAnExportResourceTypeByIdRequest_WhenProcessing_ThenCreateExportRequestShouldBeCreatedCorrectly()
+        {
+            await RunCreateExportRequestTest(
+                (since, till, resourceType, groupId, containerName, typeFilter, formatName, isParallel, includeAssociatedData, maxCount, anonymizationConfigCollectionReference, anonymizationConfigLocation, anonymizationConfigFileETag) =>
+                    _exportEnabledController.ExportResourceTypeById(
+                        KnownResourceTypes.Group,
+                        groupId,
+                        since,
+                        till,
+                        resourceType,
+                        containerName,
+                        typeFilter,
+                        formatName,
+                        maxCount,
+                        anonymizationConfigCollectionReference,
+                        anonymizationConfigLocation,
+                        anonymizationConfigFileETag),
+                ExportJobType.Group,
+                KnownResourceTypes.Practitioner,
+                Guid.NewGuid().ToString(),
+                null,
+                false);
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.OK)]
+        [InlineData(HttpStatusCode.InternalServerError)]
+        public async Task GivenAGetExportStatusByIdRequest_WhenProcessing_ThenGetExportRequestShouldBeCreatedCorrectly(
+            HttpStatusCode statusCode)
+        {
+            var baseUri = new Uri("https://test.com/");
+            _fhirRequestContextAccessor.RequestContext.Uri.Returns(baseUri);
+
+            _mediator
+                .Send(Arg.Any<GetExportRequest>(), Arg.Any<CancellationToken>())
+                .Returns(
+                    x =>
+                    {
+                        return new GetExportResponse(
+                            statusCode,
+                            new ExportJobResult(
+                                DateTimeOffset.UtcNow,
+                                baseUri,
+                                true,
+                                new List<ExportOutputResponse>(),
+                                new List<ExportOutputResponse>(),
+                                new List<OperationOutcomeIssue>()));
+                    });
+
+            var request = default(GetExportRequest);
+            _mediator.When(
+                x => x.Send(
+                    Arg.Any<GetExportRequest>(),
+                    Arg.Any<CancellationToken>()))
+                .Do(x =>
+                {
+                    request = x.Arg<GetExportRequest>();
+                });
+
+            var id = Guid.NewGuid().ToString();
+            var response = await _exportEnabledController.GetExportStatusById(id);
+
+            Assert.NotNull(request);
+            Assert.Equal(baseUri, request.RequestUri);
+            Assert.Equal(id, request.JobId);
+
+            var result = response as ExportResult;
+            Assert.NotNull(result);
+            Assert.Equal(statusCode == HttpStatusCode.OK ? statusCode : HttpStatusCode.Accepted, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task GivenACancelExportRequest_WhenProcessing_ThenCancelExportRequestShouldBeCreatedCorrectly()
+        {
+            var baseUri = new Uri("https://test.com/");
+            _fhirRequestContextAccessor.RequestContext.Uri.Returns(baseUri);
+
+            _mediator
+                .Send(Arg.Any<CancelExportRequest>(), Arg.Any<CancellationToken>())
+                .Returns(new CancelExportResponse(HttpStatusCode.OK));
+
+            var request = default(CancelExportRequest);
+            _mediator.When(
+                x => x.Send(
+                    Arg.Any<CancelExportRequest>(),
+                    Arg.Any<CancellationToken>()))
+                .Do(x =>
+                {
+                    request = x.Arg<CancelExportRequest>();
+                });
+
+            var id = Guid.NewGuid().ToString();
+            var response = await _exportEnabledController.CancelExport(id);
+
+            Assert.NotNull(request);
+            Assert.Equal(id, request.JobId);
+        }
+
         private ExportController GetController(ExportJobConfiguration exportConfig, FeatureConfiguration features, ArtifactStoreConfiguration artifactStoreConfig, IFhirRuntimeConfiguration fhirConfig = null)
         {
             var operationConfig = new OperationsConfiguration()
@@ -372,6 +526,104 @@ namespace Microsoft.Health.Fhir.Api.UnitTests.Controllers
                 optionsArtifactStoreConfiguration,
                 optionsFeatures,
                 fhirConfig ?? Substitute.For<IFhirRuntimeConfiguration>());
+        }
+
+        private async Task RunCreateExportRequestTest(
+            Func<PartialDateTime, PartialDateTime, string, string, string, string, string, bool?, string, uint, string, string, string, Task<IActionResult>> func,
+            ExportJobType exportJobType,
+            string typeParameter = null,
+            string idParameter = null,
+            string includeAssociatedDataParameter = null,
+            bool isParallel = true)
+        {
+            var loginServer = "abc.azurecr.io";
+            var imageName = "deidconfigs";
+            var since = new PartialDateTime(DateTimeOffset.UtcNow.AddSeconds(-60));
+            var till = new PartialDateTime(DateTimeOffset.UtcNow);
+            var filters = default(string);
+            var resourceType = typeParameter;
+            var groupId = idParameter;
+            var containerName = Guid.NewGuid().ToString();
+            var formatName = Guid.NewGuid().ToString();
+            var includeAssociatedData = includeAssociatedDataParameter;
+            var includeHistory = includeAssociatedDataParameter != null && includeAssociatedDataParameter.Contains("_history");
+            var includeDeleted = includeAssociatedDataParameter != null && includeAssociatedDataParameter.Contains("_deleted");
+            var maxCount = uint.MaxValue;
+            var anonymizationConfigCollectionReference = $"{loginServer}/{imageName}:1ae21c6e33deb90f105982404c867671da624deda7dff364107ec8c2910b4992";
+            var anonymizationConfigLocation = Guid.NewGuid().ToString();
+            var anonymizationConfigFileETag = default(string);
+
+            _artifactStoreConfig.OciArtifacts.Add(
+                new OciArtifactInfo()
+                {
+                    LoginServer = "abc.azurecr.io",
+                    ImageName = "deidconfigs",
+                });
+            _featureConfiguration.SupportsAnonymizedExport = true;
+
+            // Setup additional dependencies needed for test execution.
+            _exportEnabledController.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var baseUri = new Uri("https://test.com/");
+            _fhirRequestContextAccessor.RequestContext = new FhirRequestContext(
+               method: "export",
+               uriString: baseUri.OriginalString,
+               baseUriString: baseUri.OriginalString,
+               correlationId: "export",
+               requestHeaders: new Dictionary<string, StringValues>(),
+               responseHeaders: new Dictionary<string, StringValues>());
+
+            _urlResolver
+                .ResolveOperationResultUrl(Arg.Any<string>(), Arg.Any<string>())
+                .Returns(baseUri);
+
+            // Mock mediator call for CreateExportRequest - throw exception to fail test if we get unexpected value.
+            _mediator
+                .Send(Arg.Any<CreateExportRequest>(), Arg.Any<CancellationToken>())
+                .Returns(new CreateExportResponse("ExportTestJobId"));
+
+            var request = default(CreateExportRequest);
+            _mediator.When(
+                x => x.Send(
+                    Arg.Any<CreateExportRequest>(),
+                    Arg.Any<CancellationToken>()))
+                .Do(x =>
+                {
+                    request = x.Arg<CreateExportRequest>();
+                });
+
+            await func(
+                since,
+                till,
+                resourceType,
+                groupId,
+                containerName,
+                filters,
+                formatName,
+                isParallel,
+                includeAssociatedData,
+                maxCount,
+                anonymizationConfigCollectionReference,
+                anonymizationConfigLocation,
+                anonymizationConfigFileETag);
+
+            Assert.NotNull(request);
+            Assert.Equal(exportJobType, request.RequestType);
+            Assert.Equal(baseUri, request.RequestUri);
+            Assert.Equal(resourceType, request.ResourceType);
+            Assert.Equal(since.ToString(), request.Since?.ToString());
+            Assert.Equal(till.ToString(), request.Till?.ToString());
+            Assert.Equal(filters, request.Filters);
+            Assert.Equal(groupId, request.GroupId);
+            Assert.Equal(containerName, request.ContainerName);
+            Assert.Equal(formatName, request.FormatName);
+            Assert.Equal(isParallel, request.IsParallel);
+            Assert.Equal(includeHistory, request.IncludeHistory);
+            Assert.Equal(includeDeleted, request.IncludeDeleted);
+            Assert.Equal(maxCount, request.MaxCount);
+            Assert.Equal(anonymizationConfigCollectionReference, request.AnonymizationConfigurationCollectionReference);
+            Assert.Equal(anonymizationConfigLocation, request.AnonymizationConfigurationLocation);
+            Assert.Equal(anonymizationConfigFileETag, request.AnonymizationConfigurationFileETag);
         }
     }
 }
