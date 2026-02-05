eccaa42e93be04982cb177c96c707a6bb8cc9033
Sergey Galuzo <sergal@microsoft.com>
2026-02-04 16:18:57 -0800

    removed -60 xec shift

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/Registry/SqlServerSearchParameterStatusDataStore.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/Registry/SqlServerSearchParameterStatusDataStore.cs
@@ -71,7 +71,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage.Registry
                 cmd.CommandText = "dbo.GetSearchParamStatuses";
                 if (_schemaInformation.Current.Value >= 103 && startLastUpdated.HasValue)
                 {
-                    cmd.Parameters.AddWithValue("@StartLastUpdated", startLastUpdated.Value.AddSeconds(-60)); // TODO: Remove shift
+                    cmd.Parameters.AddWithValue("@StartLastUpdated", startLastUpdated.Value);
                 }
 
                 var parameterStatuses = new List<ResourceSearchParameterStatus>();
