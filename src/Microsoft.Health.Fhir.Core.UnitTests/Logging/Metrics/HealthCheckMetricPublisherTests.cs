3ec2328d25b0b0fbcc2a74b0c17ebb9b1ade4305
Abigail Nicolas Sayago <abigailn@microsoft.com>
2026-03-04 10:45:09 -0800

    Improve tests and logic

--- src/Microsoft.Health.Fhir.Core.UnitTests/Logging/Metrics/HealthCheckMetricPublisherTests.cs
+++ src/Microsoft.Health.Fhir.Core.UnitTests/Logging/Metrics/HealthCheckMetricPublisherTests.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Health.Core.Features.Health;
 using Microsoft.Health.Core.Features.Metric;
@@ -21,62 +22,118 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Logging.Metrics
     [Trait(Traits.Category, Categories.Operations)]
     public class HealthCheckMetricPublisherTests
     {
+        private readonly IHealthCheckMetricHandler _metricHandler;
+
         private const string DataStoreHealthCheck = "DataStoreHealthCheck";
         private const string StorageInitializedHealthCheck = "StorageInitializedHealthCheck";
         private const string BehaviorHealthCheck = "BehaviorHealthCheck";
 
-        [Fact]
-        public void GivenHealthyStatus_WhenPublish_ThenMetricIsEmittedWithReportStatusAndDimensions()
+        public HealthCheckMetricPublisherTests()
         {
-            IHealthCheckMetricHandler metricHandler = Substitute.For<IHealthCheckMetricHandler>();
-            var publisher = CreatePublisher(metricHandler);
-
-            var healthReport = new HealthReport(
-                new Dictionary<string, HealthReportEntry>
-                {
-                    { BehaviorHealthCheck, CreateEntry(HealthStatus.Healthy) },
-                    { StorageInitializedHealthCheck, CreateEntry(HealthStatus.Healthy) },
-                    { DataStoreHealthCheck, CreateEntry(HealthStatus.Healthy) },
-                },
-                HealthStatus.Healthy,
-                TimeSpan.FromMilliseconds(1));
-
-            publisher.Publish(healthReport);
-
-            metricHandler.Received(1).EmitHealthMetric(Arg.Is<HealthCheckMetricNotification>(n =>
-                n.OverallStatus == HealthStatus.Healthy.ToString() &&
-                n.Reason == HealthStatusReason.None.ToString() &&
-                n.ArmGeoLocation == "eastus" &&
-                n.ArmResourceId == "/subscriptions/test/resourceGroups/rg/providers/Microsoft.HealthcareApis/services/fhir"));
+            _metricHandler = Substitute.For<IHealthCheckMetricHandler>();
         }
 
         [Fact]
-        public void GivenUnhealthyStatus_WhenPublish_ThenReasonIsServiceUnavailable()
+        public void GivenReportWithServiceUnavailable_WhenPublish_ThenServiceUnavailableIsPublished()
         {
-            IHealthCheckMetricHandler metricHandler = Substitute.For<IHealthCheckMetricHandler>();
-            var publisher = CreatePublisher(metricHandler);
+            var publisher = CreatePublisher(_metricHandler);
 
             var healthReport = new HealthReport(
                 new Dictionary<string, HealthReportEntry>
                 {
-                    { BehaviorHealthCheck, CreateEntry(HealthStatus.Healthy) },
-                    { StorageInitializedHealthCheck, CreateEntry(HealthStatus.Unhealthy) },
-                    { DataStoreHealthCheck, CreateEntry(HealthStatus.Unhealthy) },
+                    { BehaviorHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                    { StorageInitializedHealthCheck, CreateDummyEntry(HealthStatusReason.ServiceUnavailable) },
+                    { DataStoreHealthCheck, CreateDummyEntry(HealthStatusReason.ServiceUnavailable) },
                 },
                 HealthStatus.Unhealthy,
                 TimeSpan.FromMilliseconds(1));
 
             publisher.Publish(healthReport);
 
-            metricHandler.Received(1).EmitHealthMetric(Arg.Is<HealthCheckMetricNotification>(n =>
+            _metricHandler.Received(1).EmitHealthMetric(Arg.Is<HealthCheckMetricNotification>(n =>
                 n.OverallStatus == HealthStatus.Unhealthy.ToString() &&
-                n.Reason == HealthStatusReason.ServiceUnavailable.ToString()));
+                n.Reason == HealthStatusReason.ServiceUnavailable.ToString() &&
+                n.ArmGeoLocation == "eastus" &&
+                n.ArmResourceId == "/subscriptions/test/resourceGroups/rg/providers/Microsoft.HealthcareApis/services/fhir"));
+        }
+
+        [Fact]
+        public void GivenUnhealthyStatusWithNoneReason_WhenPublish_ThenReasonIsServiceUnavailable()
+        {
+            var publisher = CreatePublisher(_metricHandler);
+
+            var healthReport = new HealthReport(
+                new Dictionary<string, HealthReportEntry>
+                {
+                    { BehaviorHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                    { StorageInitializedHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                    { DataStoreHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                },
+                HealthStatus.Unhealthy,
+                TimeSpan.FromMilliseconds(1));
+
+            publisher.Publish(healthReport);
+
+            _metricHandler.Received(1).EmitHealthMetric(Arg.Is<HealthCheckMetricNotification>(n =>
+                n.OverallStatus == HealthStatus.Unhealthy.ToString() &&
+                n.Reason == HealthStatusReason.ServiceUnavailable.ToString() &&
+                n.ArmGeoLocation == "eastus" &&
+                n.ArmResourceId == "/subscriptions/test/resourceGroups/rg/providers/Microsoft.HealthcareApis/services/fhir"));
+        }
+
+        [Fact]
+        public void GivenDegradedStatusWithNoneReason_WhenPublish_ThenReasonIsServiceDegraded()
+        {
+            var publisher = CreatePublisher(_metricHandler);
+
+            var healthReport = new HealthReport(
+                new Dictionary<string, HealthReportEntry>
+                {
+                    { BehaviorHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                    { StorageInitializedHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                    { DataStoreHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                },
+                HealthStatus.Degraded,
+                TimeSpan.FromMilliseconds(1));
+
+            publisher.Publish(healthReport);
+
+            _metricHandler.Received(1).EmitHealthMetric(Arg.Is<HealthCheckMetricNotification>(n =>
+                n.OverallStatus == HealthStatus.Degraded.ToString() &&
+                n.Reason == HealthStatusReason.ServiceDegraded.ToString() &&
+                n.ArmGeoLocation == "eastus" &&
+                n.ArmResourceId == "/subscriptions/test/resourceGroups/rg/providers/Microsoft.HealthcareApis/services/fhir"));
+        }
+
+        [Fact]
+        public void GivenHealthyStatus_WhenPublish_ThenMetricIsEmittedWithReportStatusAndDimensions()
+        {
+            var publisher = CreatePublisher(_metricHandler);
+
+            var healthReport = new HealthReport(
+                new Dictionary<string, HealthReportEntry>
+                {
+                    { BehaviorHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                    { StorageInitializedHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                    { DataStoreHealthCheck, CreateDummyEntry(HealthStatusReason.None) },
+                },
+                HealthStatus.Healthy,
+                TimeSpan.FromMilliseconds(1));
+
+            publisher.Publish(healthReport);
+
+            _metricHandler.Received(1).EmitHealthMetric(Arg.Is<HealthCheckMetricNotification>(n =>
+                n.OverallStatus == HealthStatus.Healthy.ToString() &&
+                n.Reason == HealthStatusReason.None.ToString() &&
+                n.ArmGeoLocation == "eastus" &&
+                n.ArmResourceId == "/subscriptions/test/resourceGroups/rg/providers/Microsoft.HealthcareApis/services/fhir"));
         }
 
         private static HealthCheckMetricPublisher CreatePublisher(IHealthCheckMetricHandler metricHandler)
         {
             return new HealthCheckMetricPublisher(
                 metricHandler,
+                Substitute.For<ILogger<HealthCheckMetricPublisher>>(),
                 Options.Create(new ResourceHealthDimensionOptions
                 {
                     ArmGeoLocation = "eastus",
@@ -84,14 +141,14 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Logging.Metrics
                 }));
         }
 
-        private static HealthReportEntry CreateEntry(HealthStatus status)
+        private static HealthReportEntry CreateDummyEntry(HealthStatusReason reason)
         {
             return new HealthReportEntry(
-                status,
-                description: null,
+                HealthStatus.Healthy,
+                description: "Some description",
                 duration: TimeSpan.FromMilliseconds(1),
                 exception: null,
-                data: new Dictionary<string, object>(),
+                data: new Dictionary<string, object>() { { "Reason", reason } },
                 tags: null);
         }
     }
--- src/Microsoft.Health.Fhir.Core/Logging/Metrics/HealthCheckMetricPublisher.cs
+++ src/Microsoft.Health.Fhir.Core/Logging/Metrics/HealthCheckMetricPublisher.cs
@@ -3,8 +3,11 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using EnsureThat;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Health.Core.Features.Health;
 using Microsoft.Health.Core.Features.Metric;
@@ -16,20 +19,31 @@ namespace Microsoft.Health.Fhir.Core.Logging.Metrics
     /// </summary>
     public class HealthCheckMetricPublisher : IHealthCheckMetricPublisher
     {
+        private static readonly Dictionary<HealthStatus, HealthStatusReason> DefaultStatusToReasonMapping = new Dictionary<HealthStatus, HealthStatusReason>
+        {
+            { HealthStatus.Healthy, HealthStatusReason.None },
+            { HealthStatus.Degraded, HealthStatusReason.ServiceDegraded },
+            { HealthStatus.Unhealthy, HealthStatusReason.ServiceUnavailable },
+        };
+
         private readonly IHealthCheckMetricHandler _metricHandler;
+        private readonly ILogger<HealthCheckMetricPublisher> _logger;
         private readonly ResourceHealthDimensionOptions _resourceHealthDimensionOptions;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HealthCheckMetricPublisher"/> class.
         /// </summary>
         /// <param name="metricHandler">Handler responsible for emitting health metrics.</param>
+        /// <param name="logger">Logger used to report metric publishing issues.</param>
         /// <param name="resourceHealthDimensionOptions">The resource health metric dimensions.</param>
-        public HealthCheckMetricPublisher(IHealthCheckMetricHandler metricHandler, IOptions<ResourceHealthDimensionOptions> resourceHealthDimensionOptions)
+        public HealthCheckMetricPublisher(IHealthCheckMetricHandler metricHandler, ILogger<HealthCheckMetricPublisher> logger, IOptions<ResourceHealthDimensionOptions> resourceHealthDimensionOptions)
         {
             EnsureArg.IsNotNull(metricHandler, nameof(metricHandler));
+            EnsureArg.IsNotNull(logger, nameof(logger));
             EnsureArg.IsNotNull(resourceHealthDimensionOptions, nameof(resourceHealthDimensionOptions));
 
             _metricHandler = metricHandler;
+            _logger = logger;
             _resourceHealthDimensionOptions = resourceHealthDimensionOptions.Value;
         }
 
@@ -38,25 +52,69 @@ namespace Microsoft.Health.Fhir.Core.Logging.Metrics
         {
             EnsureArg.IsNotNull(healthReport, nameof(healthReport));
 
-            HealthStatusReason defaultReason = GetDefaultReasonForStatus(healthReport.Status);
+            HealthStatusReason defaultReason = GetDefaultReasonForStatus(healthReport);
 
-            _metricHandler.EmitHealthMetric(new HealthCheckMetricNotification
+            switch (healthReport.Status)
             {
-                OverallStatus = healthReport.Status.ToString(),
-                Reason = defaultReason.ToString(),
-                ArmGeoLocation = _resourceHealthDimensionOptions?.ArmGeoLocation,
-                ArmResourceId = _resourceHealthDimensionOptions?.ArmResourceId,
-            });
+                case HealthStatus.Healthy:
+                    _metricHandler.EmitHealthMetric(new HealthCheckMetricNotification
+                    {
+                        OverallStatus = healthReport.Status.ToString(),
+                        Reason = defaultReason.ToString(),
+                        ArmGeoLocation = _resourceHealthDimensionOptions?.ArmGeoLocation,
+                        ArmResourceId = _resourceHealthDimensionOptions?.ArmResourceId,
+                    });
+                    break;
+                case HealthStatus.Degraded:
+                    _metricHandler.EmitHealthMetric(new HealthCheckMetricNotification
+                    {
+                        OverallStatus = healthReport.Status.ToString(),
+                        Reason = HealthCheckMetricPublisher.GetHighestSeverityReason(healthReport, defaultReason).ToString(),
+                        ArmGeoLocation = _resourceHealthDimensionOptions?.ArmGeoLocation,
+                        ArmResourceId = _resourceHealthDimensionOptions?.ArmResourceId,
+                    });
+                    break;
+                case HealthStatus.Unhealthy:
+                    _metricHandler.EmitHealthMetric(new HealthCheckMetricNotification
+                    {
+                        OverallStatus = healthReport.Status.ToString(),
+                        Reason = defaultReason.ToString(),
+                        ArmGeoLocation = _resourceHealthDimensionOptions?.ArmGeoLocation,
+                        ArmResourceId = _resourceHealthDimensionOptions?.ArmResourceId,
+                    });
+                    break;
+            }
         }
 
-        private static HealthStatusReason GetDefaultReasonForStatus(HealthStatus status)
+        private HealthStatusReason GetDefaultReasonForStatus(HealthReport report)
         {
-            return status switch
+            if (DefaultStatusToReasonMapping.TryGetValue(report.Status, out HealthStatusReason defaultReason))
             {
-                HealthStatus.Healthy => HealthStatusReason.None,
-                HealthStatus.Degraded => HealthStatusReason.ServiceDegraded,
-                _ => HealthStatusReason.ServiceUnavailable,
-            };
+                return defaultReason;
+            }
+
+            _logger.LogError("No default HealthStatusReason can be found for HealthStatus {HealthStatus}", report.Status);
+            return HealthStatusReason.None;
+        }
+
+        public static HealthStatusReason GetHighestSeverityReason(HealthReport healthReport, HealthStatusReason defaultReason)
+        {
+            HealthStatusReason worstReason = defaultReason;
+
+            foreach (var entry in healthReport.Entries)
+            {
+                if (entry.Value.Data.TryGetValue("Reason", out object reason))
+                {
+                    HealthStatusReason healthStatusReason = Enum.Parse<HealthStatusReason>(reason.ToString());
+
+                    if (healthStatusReason > worstReason)
+                    {
+                        worstReason = healthStatusReason;
+                    }
+                }
+            }
+
+            return worstReason;
         }
     }
 }
