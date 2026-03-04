d751a901b2a7e43223baad4b058159f85d117d3d
Abigail Nicolas Sayago <abigailn@microsoft.com>
2026-03-04 14:19:24 -0800

    Fix build

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Metric/StartupWithMetricHandler.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Metric/StartupWithMetricHandler.cs
@@ -4,14 +4,12 @@
 // -------------------------------------------------------------------------------------------------
 
 using MediatR;
-using System.Threading;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Health.Extensions.DependencyInjection;
 using Microsoft.Health.Fhir.Api.Features.ApiNotifications;
-using Microsoft.Health.Fhir.Core.Logging.Metrics;
 using Microsoft.Health.Fhir.Core.Features.Operations.Export;
+using Microsoft.Health.Fhir.Core.Logging.Metrics;
 using Microsoft.Health.Fhir.CosmosDb.Features.Metrics;
 
 namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Metric
@@ -38,25 +36,6 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Metric
                 .Singleton()
                 .AsSelf()
                 .AsService<IHealthCheckMetricPublisher>();
-        }
-    }
-
-    public class TestHealthCheckMetricPublisher : IHealthCheckMetricPublisher
-    {
-        public bool WasPublished { get; private set; }
-
-        public HealthStatus? LastPublishedStatus { get; private set; }
-
-        public void Reset()
-        {
-            WasPublished = false;
-            LastPublishedStatus = null;
-        }
-
-        public void Publish(HealthReport healthReport)
-        {
-            WasPublished = true;
-            LastPublishedStatus = healthReport.Status;
         }
     }
 }
