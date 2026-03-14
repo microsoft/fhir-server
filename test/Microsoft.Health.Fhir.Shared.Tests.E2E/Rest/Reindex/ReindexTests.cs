f8fe73a99e65007b8dff24c282c90373b0ac6f11
Sergey Galuzo <sergal@microsoft.com>
2026-03-13 17:00:37 -0700

    100

--- test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Reindex/ReindexTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.E2E/Rest/Reindex/ReindexTests.cs
@@ -46,7 +46,7 @@ namespace Microsoft.Health.Fhir.Tests.E2E.Rest.Reindex
         {
             await CancelAnyRunningReindexJobsAsync();
 
-            const int numberOfSearchParams = 200; // increase to 500 when cache is not updated by API calls.
+            const int numberOfSearchParams = 100; // increase to 500 when cache is not updated by API calls.
             const string urlPrefix = "http://example.org/fhir/SearchParameter/";
             var codes = new List<string>();
             var urls = new List<string>();
