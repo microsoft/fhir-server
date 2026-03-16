71b6335736626612ae7b7a056a72c837dd11eec3
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2026-03-16 16:53:16 -0400

    Handle concurrent conflicts
    * Changes on MergeResource stored procedure.
    
    * - Differenciate concurrent updates (50409) from duplicated keys (issue with surrogate IDs) (50424).
    - Handle the two scenarios differently.
    - Decrease the delay time for concurrent updates
    
    * Fixing tests.
    
    * Adding support to new error codes.
    
    * Making sure 106 is correct.
    
    * Fix syntax.
    
    * Undo changes on 106.diff.sql
    
    * Removing additional lines.
    
    * New tests to ensure SQL Conflicts are detected and properly handled.
    
    * Ensuring retries.
    
    * Fixed syntax problem.
    
    * Renaming constant and adding comment to test.
    
    * Addressing PR comments.

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/FhirSqlErrorCodes.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/FhirSqlErrorCodes.cs
@@ -1,23 +1,26 @@
-// -------------------------------------------------------------------------------------------------
+﻿// -------------------------------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
 namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 {
-    /// <summary>
-    /// Known FHIR specific SQL Server error codes
-    /// </summary>
-    internal class SqlStoreErrorCodes
+    public static class FhirSqlErrorCodes
     {
+        /// <summary>
+        /// Constraint Violation.
+        /// </summary>
+        public const int ConstraintViolation = 547;
+
         /// <summary>
         /// Number of concurrent calls to MergeResources is above optimal.
         /// </summary>
         public const int MergeResourcesConcurrentCallsIsAboveOptimal = 50410;
 
         /// <summary>
-        /// Constraint Violation.
+        /// Failed dependency. Not enough resources to perform the operation: limit on surrogate IDs per second reached.
+        /// This is a transient error and retrying the operation may succeed.
         /// </summary>
-        public const int ConstraintViolation = 547;
+        public const int FailedDependency = 50424;
     }
 }
