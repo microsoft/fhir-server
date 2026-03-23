0f2d317ce60ea2c52bfb5c4845072ea3dcde462c
John Estrada <joestr@microsoft.com>
2026-03-23 10:52:03 -0500

    comment out code

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirModel.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirModel.cs
@@ -374,58 +374,58 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 
             // }
 
-            _logger.LogInformation("Initializing search parameters statuses.");
+            // _logger.LogInformation("Initializing search parameters statuses.");
 
-            List<ResourceSearchParameterStatus> fileStatuses = (await _filebasedSearchParameterStatusDataStore
-                    .GetSearchParameterStatuses(cancellationToken)).ToList();
+            // List<ResourceSearchParameterStatus> fileStatuses = (await _filebasedSearchParameterStatusDataStore
+            //        .GetSearchParameterStatuses(cancellationToken)).ToList();
 
-            using var getStatusCmd = new SqlCommand();
-            getStatusCmd.CommandType = CommandType.StoredProcedure;
-            getStatusCmd.CommandText = "dbo.GetSearchParamStatuses";
+            // using var getStatusCmd = new SqlCommand();
+            // getStatusCmd.CommandType = CommandType.StoredProcedure;
+            // getStatusCmd.CommandText = "dbo.GetSearchParamStatuses";
 
-            var existingParams = await getStatusCmd.ExecuteReaderAsync(
-                _sqlRetryService,
-                (reader) =>
-                {
-                    (_, var uri, var stringStatus, var lastUpdated, var isPartiallySupported) = reader.ReadRow(
-                            VLatest.SearchParam.SearchParamId,
-                            VLatest.SearchParam.Uri,
-                            VLatest.SearchParam.Status,
-                            VLatest.SearchParam.LastUpdated,
-                            VLatest.SearchParam.IsPartiallySupported);
-                    return new ResourceSearchParameterStatus
-                    {
-                        Uri = new Uri(uri),
-                        Status = Enum.Parse<SearchParameterStatus>(stringStatus),
-                        LastUpdated = lastUpdated,
-                        IsPartiallySupported = isPartiallySupported,
-                    };
-                },
-                _logger,
-                cancellationToken);
+            // var existingParams = await getStatusCmd.ExecuteReaderAsync(
+            //    _sqlRetryService,
+            //    (reader) =>
+            //    {
+            //        (_, var uri, var stringStatus, var lastUpdated, var isPartiallySupported) = reader.ReadRow(
+            //                VLatest.SearchParam.SearchParamId,
+            //                VLatest.SearchParam.Uri,
+            //                VLatest.SearchParam.Status,
+            //                VLatest.SearchParam.LastUpdated,
+            //                VLatest.SearchParam.IsPartiallySupported);
+            //        return new ResourceSearchParameterStatus
+            //        {
+            //            Uri = new Uri(uri),
+            //            Status = Enum.Parse<SearchParameterStatus>(stringStatus),
+            //            LastUpdated = lastUpdated,
+            //            IsPartiallySupported = isPartiallySupported,
+            //        };
+            //    },
+            //    _logger,
+            //    cancellationToken);
 
-            fileStatuses.RemoveAll((fs) => existingParams.Any((param) => param.Uri == fs.Uri && (param.Status != SearchParameterStatus.Initialized)));
+            // fileStatuses.RemoveAll((fs) => existingParams.Any((param) => param.Uri == fs.Uri && (param.Status != SearchParameterStatus.Initialized)));
 
-            using var resourceExistCmd = new SqlCommand();
-            resourceExistCmd.CommandType = CommandType.Text;
-            resourceExistCmd.CommandText = "SELECT TOP 1 1 FROM dbo.Resource";
-            var hasResources = await resourceExistCmd.ExecuteScalarAsync<int?>(_sqlRetryService, _logger, cancellationToken) == 1;
+            // using var resourceExistCmd = new SqlCommand();
+            // resourceExistCmd.CommandType = CommandType.Text;
+            // resourceExistCmd.CommandText = "SELECT TOP 1 1 FROM dbo.Resource";
+            // var hasResources = await resourceExistCmd.ExecuteScalarAsync<int?>(_sqlRetryService, _logger, cancellationToken) == 1;
 
-            fileStatuses.ForEach(fs =>
-            {
-                fs.Status = hasResources ? SearchParameterStatus.Supported : SearchParameterStatus.Enabled;
-                fs.LastUpdated = existingParams.FirstOrDefault(p => p.Uri == fs.Uri)?.LastUpdated ?? fs.LastUpdated;
-            });
+            // fileStatuses.ForEach(fs =>
+            // {
+            //    fs.Status = hasResources ? SearchParameterStatus.Supported : SearchParameterStatus.Enabled;
+            //    fs.LastUpdated = existingParams.FirstOrDefault(p => p.Uri == fs.Uri)?.LastUpdated ?? fs.LastUpdated;
+            // });
 
-            using var cmd = new SqlCommand();
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandText = "dbo.MergeSearchParams";
+            // using var cmd = new SqlCommand();
+            // cmd.CommandType = CommandType.StoredProcedure;
+            // cmd.CommandText = "dbo.MergeSearchParams";
 
-            new SearchParamListTableValuedParameterDefinition("@SearchParams").AddParameter(cmd.Parameters, new SearchParamListRowGenerator().GenerateRows(fileStatuses));
+            // new SearchParamListTableValuedParameterDefinition("@SearchParams").AddParameter(cmd.Parameters, new SearchParamListRowGenerator().GenerateRows(fileStatuses));
 
-            await cmd.ExecuteNonQueryAsync(_sqlRetryService, _logger, cancellationToken);
+            // await cmd.ExecuteNonQueryAsync(_sqlRetryService, _logger, cancellationToken);
 
-            _logger.LogInformation("Number of Search Parameters initialized");
+            // _logger.LogInformation("Number of Search Parameters initialized");
         }
 
         private int GetStringId(FhirMemoryCache<int> cache, string stringValue, StoredProcedure sproc)
