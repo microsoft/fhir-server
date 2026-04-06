ed96abfbd27d854bb0d2092002c721672e68d93a
Sergey Galuzo <sergal@microsoft.com>
2026-04-06 14:12:01 -0700

    force index

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/GetResourcesByTokens.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/GetResourcesByTokens.sql
@@ -4,15 +4,14 @@ set nocount on
 DECLARE @st datetime = getUTCdate()
        ,@SP varchar(100) = 'GetResourcesByTokens'
        ,@Mode varchar(100) = 'RT='+convert(varchar,@ResourceTypeId)+' SP='+convert(varchar,@SearchParamId)+' Tokens='+convert(varchar,(SELECT count(*) FROM @Tokens))+' T='+convert(varchar,@Top)
-       ,@DummyTop bigint = 9223372036854775807
 
 BEGIN TRY
   IF NOT EXISTS (SELECT * FROM @Tokens WHERE CodeOverflow IS NOT NULL OR SystemValue IS NOT NULL) -- fastest
     SELECT ResourceTypeId, ResourceId, Version, IsDeleted, ResourceSurrogateId, RequestMethod, IsMatch = convert(bit,1), IsPartial = convert(bit,0), IsRawResourceMetaSet, SearchParamHash, RawResource
       FROM (SELECT DISTINCT TOP (@Top) 
                    Sid1 = ResourceSurrogateId
-              FROM (SELECT TOP (@DummyTop) * FROM @Tokens) A 
-                   JOIN dbo.TokenSearchParam B
+              FROM @Tokens A 
+                   JOIN dbo.TokenSearchParam B WITH (INDEX = IX_TokenSearchParam_Code_SystemId) 
                      ON B.Code = A.Code 
                         AND (B.SystemId = A.SystemId OR A.SystemId IS NULL) -- Covered by include in secondary index
               WHERE ResourceTypeId = @ResourceTypeId	
@@ -20,7 +19,7 @@ BEGIN TRY
               ORDER BY
                    ResourceSurrogateId
            ) A
-           JOIN dbo.Resource ON ResourceSurrogateId = Sid1
+           JOIN dbo.Resource WITH (INDEX = 1) ON ResourceSurrogateId = Sid1
       WHERE ResourceTypeId = @ResourceTypeId
       ORDER BY 
            ResourceSurrogateId
@@ -29,14 +28,13 @@ BEGIN TRY
     SELECT ResourceTypeId, ResourceId, Version, IsDeleted, ResourceSurrogateId, RequestMethod, IsMatch = convert(bit,1), IsPartial = convert(bit,0), IsRawResourceMetaSet, SearchParamHash, RawResource
       FROM (SELECT DISTINCT TOP (@Top) 
                    Sid1 = ResourceSurrogateId
-              FROM (SELECT TOP (@DummyTop)
-                           Code
+              FROM (SELECT Code
                           ,CodeOverflow
                           ,SystemId = CASE WHEN SystemValue IS NOT NULL THEN (SELECT SystemId FROM dbo.System WHERE Value = SystemValue) ELSE SystemId END
                           ,SystemValue
                       FROM @Tokens
                    ) A
-                   JOIN dbo.TokenSearchParam B
+                   JOIN dbo.TokenSearchParam B WITH (INDEX = IX_TokenSearchParam_Code_SystemId) 
                      ON B.Code = A.Code 
                         AND (B.SystemId = A.SystemId OR A.SystemId IS NULL AND A.SystemValue IS NULL) -- Covered by include in secondary index
               WHERE ResourceTypeId = @ResourceTypeId	
@@ -44,7 +42,7 @@ BEGIN TRY
               ORDER BY
                    ResourceSurrogateId
            ) A
-           JOIN dbo.Resource ON ResourceSurrogateId = Sid1
+           JOIN dbo.Resource WITH (INDEX = 1) ON ResourceSurrogateId = Sid1
       WHERE ResourceTypeId = @ResourceTypeId
       ORDER BY 
            ResourceSurrogateId
@@ -53,14 +51,13 @@ BEGIN TRY
     SELECT ResourceTypeId, ResourceId, Version, IsDeleted, ResourceSurrogateId, RequestMethod, IsMatch = convert(bit,1), IsPartial = convert(bit,0), IsRawResourceMetaSet, SearchParamHash, RawResource
       FROM (SELECT DISTINCT TOP (@Top) 
                    Sid1 = ResourceSurrogateId
-              FROM (SELECT TOP (@DummyTop)
-                           Code
+              FROM (SELECT Code
                           ,CodeOverflow
                           ,SystemId = CASE WHEN SystemValue IS NOT NULL THEN (SELECT SystemId FROM dbo.System WHERE Value = SystemValue) ELSE SystemId END
                           ,SystemValue
                       FROM @Tokens
                    ) A
-                   JOIN dbo.TokenSearchParam B
+                   JOIN dbo.TokenSearchParam B WITH (INDEX = IX_TokenSearchParam_Code_SystemId) 
                      ON B.Code = A.Code 
                         AND (B.CodeOverflow = A.CodeOverflow OR B.CodeOverflow IS NULL AND A.CodeOverflow IS NULL) -- Causes key lookup
                         AND (B.SystemId = A.SystemId OR A.SystemId IS NULL AND A.SystemValue IS NULL) -- Covered by include in secondary index
@@ -69,11 +66,11 @@ BEGIN TRY
               ORDER BY
                    ResourceSurrogateId
            ) A
-           JOIN dbo.Resource ON ResourceSurrogateId = Sid1
+           JOIN dbo.Resource WITH (INDEX = 1) ON ResourceSurrogateId = Sid1
       WHERE ResourceTypeId = @ResourceTypeId
       ORDER BY 
            ResourceSurrogateId
-      OPTION (MAXDOP 1, OPTIMIZE FOR (@DummyTop = 1))
+      OPTION (MAXDOP 1)
 
   EXECUTE dbo.LogEvent @Process=@SP,@Mode=@Mode,@Status='End',@Start=@st,@Rows=@@rowcount
 END TRY
