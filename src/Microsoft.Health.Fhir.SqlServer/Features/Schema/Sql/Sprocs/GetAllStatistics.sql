0b94f8d420b066ebd93a0ae8655a83d79ab46cdb
RB Johnson (He/Him) <rojo@microsoft.com>
2026-04-20 15:22:13 -0700

    Filled in query plan reuse checker

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/GetAllStatistics.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Sql/Sprocs/GetAllStatistics.sql
@@ -8,81 +8,60 @@ DECLARE @i int = 0,
 		@stat_name nvarchar(128),
 		@table_name nvarchar(128),
 		@min_rows int,
-		@max_rows int,
-		@column nvarchar(128);
+		@max_rows int;
 
-CREATE TABLE #results
+DECLARE @results TABLE
 (
 	stat_name nvarchar(128),
 	table_name nvarchar(128),
-	skew int,
-	primary_column nvarchar(128),
+	skew int
 );
 
-CREATE TABLE #stats_density
+DECLARE @stats_density TABLE
 (
 	all_density float,
 	average_length float,
-	columns nvarchar(128),
-);
-
-CREATE TABLE #stats_histogram
-(
-	range_hi_key nvarchar(128),
-	range_rows int,
-	eq_rows int,
-	distinct_range_rows int,
-	avg_range_rows float,
+	columns nvarchar(128)
 );
 
 SELECT DISTINCT
 	s.name AS stat_name,
 	OBJECT_NAME(s.OBJECT_ID) as table_name
-INTO #stat_names
+INTO stat_names
 FROM sys.stats AS s
 	JOIN sys.stats_columns AS sc
 		ON s.object_id = sc.object_id AND s.stats_id = sc.stats_id
 	JOIN sys.columns AS c
 		ON sc.object_id = c.object_id AND sc.column_id = c.column_id
-WHERE OBJECT_NAME(s.object_id) LIKE '%SearchParam'
-AND s.name LIKE 'ST_%';
+WHERE s.name LIKE 'ST_%';
 
-SELECT @count = count(*) FROM #stat_names;
+SELECT @count = count(*) FROM @stat_names;
 
 WHILE @i < @count
 BEGIN
 	SELECT @stat_name = stat_name, @table_name = table_name
-	FROM #stat_names
+	FROM stat_names
 	ORDER BY table_name, stat_name
 	OFFSET @i ROWS
 	FETCH NEXT 1 ROWS ONLY;
 
-	INSERT #stats_density
-	EXEC('DBCC SHOW_STATISTICS(''' + @table_name + ''',' + @stat_name + ') WITH DENSITY_VECTOR');
-
-	INSERT #stats_histogram
+	INSERT stats_histogram
 	EXEC('DBCC SHOW_STATISTICS(''' + @table_name + ''',' + @stat_name + ') WITH HISTOGRAM');
 
-	SELECT TOP 1 @column = columns
-	FROM #stats_density
-	ORDER BY all_density ASC;
-
 	SELECT @min_rows = MIN(eq_rows), @max_rows = MAX(eq_rows)
-	FROM #stats_histogram;
+	FROM stats_histogram;
 
-	INSERT #results VALUES(@stat_name, @table_name, @max_rows/@min_rows, @column);
+	INSERT results VALUES(@stat_name, @table_name, @max_rows/@min_rows);
 
-	TRUNCATE TABLE #stats_density;
-	TRUNCATE TABLE #stats_histogram;
+	TRUNCATE TABLE stats_histogram;
 
 	SET @i = @i + 1;
 END
 
-SELECT * FROM #results
+SELECT * FROM results
 
-DROP TABLE #results
-DROP TABLE #stat_names
-DROP TABLE #stats_density
-DROP TABLE #stats_histogram
+DROP TABLE results
+DROP TABLE stat_names
+DROP TABLE stats_histogram
 
 GO
--- src/Microsoft.Health.Fhir.SqlServer/Features/Search/QueryPlanReuseChecker.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Search/QueryPlanReuseChecker.cs
@@ -7,7 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using EnsureThat;
@@ -15,32 +17,52 @@ using Microsoft.Build.Framework;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Microsoft.Health.Fhir.Core.Features.Search;
+using Microsoft.Health.Fhir.SqlServer.Features.Search.Expressions.Visitors.QueryGenerators;
 using Microsoft.Health.Fhir.SqlServer.Features.Storage;
+using Microsoft.Health.Fhir.SqlServer.Features.Watchdogs;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features.Search
 {
     internal class QueryPlanReuseChecker
     {
-        // Holds a list of urls for skewed search parameters. If the search parameters are skewed, the query plan should not be reused.
-        private readonly List<string> _skewedParameters = new List<string>();
+        private readonly Regex _skewedParameterRegex = new Regex(@"^ST_.*_WHERE_ResourceTypeId_(\d*)_SearchParamId_(\d*)$");
+        private readonly double _refreshPeriod = 3600;
 
+        // Holds a list of urls for skewed search parameters. If the search parameters are skewed, the query plan should not be reused.
+        private List<IGrouping<string, (string Uri, string ResourceTypeId)>> _skewedParameters = new List<IGrouping<string, (string Uri, string ResourceTypeId)>>();
         private ISqlRetryService _sqlRetryService;
+        private FhirTimer _timer;
         private ILogger<QueryPlanReuseChecker> _logger;
+
+        private bool _isInitialized = false;
 
         public QueryPlanReuseChecker(ISqlRetryService sqlRetryService, ILogger<QueryPlanReuseChecker> logger)
         {
             _sqlRetryService = EnsureArg.IsNotNull(sqlRetryService, nameof(sqlRetryService));
             _logger = EnsureArg.IsNotNull(logger, nameof(logger));
+
+            _timer = new FhirTimer(_logger);
+
+            // this should wait for storage to be ready.
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            RefreshCache(CancellationToken.None);
+            StartRefreshTimer();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         public bool CanReuseQueryPlan(SearchOptions searchOptions)
         {
+            if (!_isInitialized)
+            {
+                return true;
+            }
+
             // Check the skew of the search parameters. If the search parameters are skewed, the query plan should not be reused.
             var parameters = searchOptions.SearchParameters;
 
             foreach (var parameter in parameters)
             {
-                if (_skewedParameters.Contains(parameter.Url.OriginalString))
+                if (_skewedParameters.Any(skew => skew.Key == parameter.Url.OriginalString))
                 {
                     return false;
                 }
@@ -49,20 +71,76 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Search
             return true;
         }
 
-        private async Task RefreshCache()
+        private async Task StartRefreshTimer()
+        {
+            try
+            {
+                await _timer.ExecuteAsync("QueryPlanReuseChecker.RefreshCache", _refreshPeriod, RefreshCache, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "QueryPlanReuseChecker refresh loop failed.");
+            }
+        }
+
+        private async Task RefreshCache(CancellationToken cancellationToken)
         {
             using var cmd = new SqlCommand();
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.CommandText = "dbo.GetAllStatistics";
-            
-            var results = await cmd.ExecuteReaderAsync(
+
+            var results = await cmd.ExecuteReaderAsync<(string ResourceTypeId, string SearchParamId)>(
                 _sqlRetryService,
                 (reader) =>
                 {
+                    var name = reader.GetString(0);
+                    var table = reader.GetString(1);
+                    var skew = reader.GetDouble(2);
 
+                    if (skew > 30)
+                    {
+                        var match = _skewedParameterRegex.Match(name);
+                        if (match.Success)
+                        {
+                            var resourceTypeId = match.Groups[1].Value;
+                            var searchParamId = match.Groups[2].Value;
+
+                            return (resourceTypeId, searchParamId);
+                        }
+                    }
+
+                    return (string.Empty, string.Empty);
                 },
                 _logger,
                 CancellationToken.None);
+
+            var searchParamIds = results.Where(r => !string.IsNullOrEmpty(r.ResourceTypeId) && !string.IsNullOrEmpty(r.SearchParamId)).Select(r => r.SearchParamId).Distinct().Aggregate((a, b) => a + "," + b);
+
+            using var cmd2 = new SqlCommand();
+            cmd2.CommandType = CommandType.Text;
+            cmd2.CommandText = "SELECT SearchParamId, Uri FROM SearchParam WHERE SearchParamId IN (@searchParamIds)";
+            cmd2.Parameters.AddWithValue("@searchParamIds", searchParamIds);
+
+            var searchParamUrls = await cmd2.ExecuteReaderAsync<(string SearchParamId, string Uri)>(
+                _sqlRetryService,
+                (reader) =>
+                {
+                    var searchParamId = reader.GetString(0);
+                    var uri = reader.GetString(1);
+                    return (searchParamId, uri);
+                },
+                _logger,
+                CancellationToken.None);
+
+            var skewedParamsByUri = results.Where(r => !string.IsNullOrEmpty(r.ResourceTypeId) && !string.IsNullOrEmpty(r.SearchParamId)).Select(r =>
+            {
+                var searchParamId = r.SearchParamId;
+                var uri = searchParamUrls.FirstOrDefault(s => s.SearchParamId == searchParamId).Uri;
+                return (uri, r.ResourceTypeId);
+            }).GroupBy(r => r.uri);
+
+            _skewedParameters = skewedParamsByUri.ToList();
+            _isInitialized = true;
         }
     }
 }
