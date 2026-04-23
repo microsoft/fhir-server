2aa823cdbc2a0c23ddab31e0419cff9a6df98fb4
v-isyamauchi-gh <v-isyamauchi@microsoft.com>
2026-04-23 12:39:44 -0700

    Adding the 'instantiates' to the capability statement. (#5469)
    * Adding the 'instantiates' to the capability statement.
    
    * Changing the US core version to 6.1.0.
    
    * Addressing Jared's comment.

---
+++ src/Microsoft.Health.Fhir.Core/Features/Conformance/USCoreInstantiateCapability.cs
@@ -0,0 +1,115 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.Extensions.Logging;
+using Microsoft.Health.Extensions.DependencyInjection;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Core.Features.Search;
+using Microsoft.Health.Fhir.Core.Models;
+
+namespace Microsoft.Health.Fhir.Core.Features.Conformance
+{
+    /// <summary>
+    /// Provides instantiation capability for US Core profiles.
+    /// </summary>
+    public class USCoreInstantiateCapability : IInstantiateCapability
+    {
+        internal const string BaseUrl = "http://hl7.org/fhir/us/core/CapabilityStatement/us-core-server";
+        internal const string UrlPrefix = "http://hl7.org/fhir/us/core/StructureDefinition/";
+        internal const string UnknownVersion = "unknown";
+
+        private readonly Func<IScoped<ISearchService>> _searchServiceFactory;
+        private readonly IResourceDeserializer _resourceDeserializer;
+        private readonly ILogger<USCoreInstantiateCapability> _logger;
+
+        public USCoreInstantiateCapability(
+            Func<IScoped<ISearchService>> searchServiceFactory,
+            IResourceDeserializer resourceDeserializer,
+            ILogger<USCoreInstantiateCapability> logger)
+        {
+            EnsureArg.IsNotNull(searchServiceFactory, nameof(searchServiceFactory));
+            EnsureArg.IsNotNull(resourceDeserializer, nameof(resourceDeserializer));
+            EnsureArg.IsNotNull(logger, nameof(logger));
+
+            _searchServiceFactory = searchServiceFactory;
+            _resourceDeserializer = resourceDeserializer;
+            _logger = logger;
+        }
+
+        public async Task<ICollection<string>> GetCanonicalUrlsAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                using (IScoped<ISearchService> searchService = _searchServiceFactory())
+                {
+                    _logger.LogInformation("Searching US Core profiles...");
+
+                    var result = default(SearchResult);
+                    var continuationToken = default(string);
+                    var versions = new HashSet<string>();
+                    do
+                    {
+                        var parameters = new List<Tuple<string, string>>
+                        {
+                            Tuple.Create("url:below", UrlPrefix),
+                        };
+
+                        if (!string.IsNullOrWhiteSpace(continuationToken))
+                        {
+                            parameters.Add(Tuple.Create(
+                                KnownQueryParameterNames.ContinuationToken,
+                                ContinuationTokenEncoder.Encode(continuationToken)));
+                        }
+
+                        result = await searchService.Value.SearchAsync(
+                            KnownResourceTypes.StructureDefinition,
+                            parameters,
+                            cancellationToken);
+
+                        continuationToken = result?.ContinuationToken;
+                        if (result != null)
+                        {
+                            foreach (var resource in result.Results.Where(x => x.Resource != null).Select(x => x.Resource))
+                            {
+                                versions.Add(GetVersion(resource));
+                            }
+                        }
+                    }
+                    while (!string.IsNullOrEmpty(continuationToken));
+
+                    _logger.LogInformation("{Count} versions of US Core profiles found: {Versions}", versions.Count, string.Join(",", versions));
+
+                    var urls = new List<string>();
+                    foreach (var version in versions.OrderBy(x => x, StringComparer.Ordinal))
+                    {
+                        urls.Add($"{BaseUrl}|{version}");
+                    }
+
+                    return urls;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to search US Core 6 profiles.");
+                throw;
+            }
+        }
+
+        private string GetVersion(ResourceWrapper wrapper)
+        {
+            EnsureArg.IsNotNull(wrapper);
+
+            var resource = _resourceDeserializer.Deserialize(wrapper);
+            var version = resource?.Scalar<string>("Resource.version");
+            return string.IsNullOrEmpty(version) ? UnknownVersion : version;
+        }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Conformance/USCoreInstantiateCapabilityTests.cs
@@ -0,0 +1,255 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Hl7.Fhir.Serialization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Health.Extensions.DependencyInjection;
+using Microsoft.Health.Fhir.Core.Extensions;
+using Microsoft.Health.Fhir.Core.Features.Conformance;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
+using Microsoft.Health.Fhir.Core.Features.Search;
+using Microsoft.Health.Fhir.Core.Models;
+using Microsoft.Health.Fhir.Tests.Common;
+using Microsoft.Health.Test.Utilities;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Xunit;
+
+namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Conformance
+{
+    [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
+    [Trait(Traits.Category, Categories.Conformance)]
+    public class USCoreInstantiateCapabilityTests
+    {
+        private const string RawResourceFormat =
+            @"{{
+                  ""resourceType"" : ""StructureDefinition"",
+                  ""id"" : ""{0}"",
+                  ""url"" : ""http://hl7.org/fhir/us/core/StructureDefinition/{0}"",
+                  ""version"" : ""{1}"",
+                  ""name"" : ""Name"",
+                  ""kind"" : ""resource"",
+                  ""abstract"" : false,
+                  ""type"" : ""Type""
+            }}";
+
+        private static readonly FhirJsonParser Parser = new FhirJsonParser();
+        private readonly USCoreInstantiateCapability _capability;
+        private readonly ISearchService _searchService;
+        private readonly IResourceDeserializer _resourceDeserializer;
+
+        public USCoreInstantiateCapabilityTests()
+        {
+            _searchService = Substitute.For<ISearchService>();
+            _resourceDeserializer = new ResourceDeserializer(
+                (FhirResourceFormat.Json, new Func<string, string, DateTimeOffset, ResourceElement>((data, version, lastUpdated) =>
+                {
+                    return Parser.Parse(data).ToResourceElement();
+                })));
+
+            var scoped = Substitute.For<IScoped<ISearchService>>();
+            scoped.Value.Returns(_searchService);
+
+            var factory = Substitute.For<Func<IScoped<ISearchService>>>();
+            factory.Invoke().Returns(scoped);
+
+            _capability = new USCoreInstantiateCapability(
+                factory,
+                _resourceDeserializer,
+                Substitute.For<ILogger<USCoreInstantiateCapability>>());
+        }
+
+        [Theory]
+        [MemberData(nameof(GetVersionsData))]
+        public async Task GivenSearchRequest_WhenUSCoreProfilesAreFound_ThenCapabilityShouldReturnsCorrectUrls(
+            string[] versions)
+        {
+            var result = CreateSearchResult(versions);
+            _searchService.SearchAsync(
+                Arg.Any<string>(),
+                Arg.Any<IReadOnlyList<Tuple<string, string>>>(),
+                Arg.Any<CancellationToken>(),
+                Arg.Any<bool>(),
+                Arg.Any<ResourceVersionType>(),
+                Arg.Any<bool>(),
+                Arg.Any<bool>())
+                .Returns(result);
+
+            var resourceType = default(string);
+            var parameters = default(IReadOnlyList<Tuple<string, string>>);
+            _searchService
+                .When(x => x.SearchAsync(
+                    Arg.Any<string>(),
+                    Arg.Any<IReadOnlyList<Tuple<string, string>>>(),
+                    Arg.Any<CancellationToken>(),
+                    Arg.Any<bool>(),
+                    Arg.Any<ResourceVersionType>(),
+                    Arg.Any<bool>(),
+                    Arg.Any<bool>()))
+                .Do(x =>
+                {
+                    resourceType = x.Arg<string>();
+                    parameters = x.Arg<IReadOnlyList<Tuple<string, string>>>();
+                });
+
+            var actual = await _capability.GetCanonicalUrlsAsync(CancellationToken.None);
+            var expected = GetExpectedUrls(versions);
+            Assert.True(expected.SetEquals(actual));
+
+            var p = parameters?.ToDictionary(y => y.Item1, y => y.Item2) ?? new Dictionary<string, string>();
+            Assert.True(p.TryGetValue("url:below", out var v0) && string.Equals(v0, USCoreInstantiateCapability.UrlPrefix, StringComparison.OrdinalIgnoreCase));
+            Assert.Equal(KnownResourceTypes.StructureDefinition, resourceType);
+        }
+
+        [Fact]
+        public async Task GivenSearchRequest_WhenResultsDoNotFitInOnePage_ThenCapabilityShouldReturnsCorrectUrls()
+        {
+            var versions = Enumerable.Range(1, 10).Select(x => string.Format("{0}.0.0", x)).ToArray();
+            var result1 = CreateSearchResult(versions.Take(4).ToArray(), Guid.NewGuid().ToString());
+            var result2 = CreateSearchResult(versions.Skip(4).Take(4).ToArray(), Guid.NewGuid().ToString());
+            var result3 = CreateSearchResult(versions.Skip(8).Take(2).ToArray());
+            _searchService.SearchAsync(
+                Arg.Any<string>(),
+                Arg.Any<IReadOnlyList<Tuple<string, string>>>(),
+                Arg.Any<CancellationToken>(),
+                Arg.Any<bool>(),
+                Arg.Any<ResourceVersionType>(),
+                Arg.Any<bool>(),
+                Arg.Any<bool>())
+                .Returns(result1, result2, result3);
+
+            var actual = await _capability.GetCanonicalUrlsAsync(CancellationToken.None);
+            var expected = GetExpectedUrls(versions);
+            Assert.True(expected.SetEquals(actual));
+
+            await _searchService.Received(3).SearchAsync(
+                Arg.Any<string>(),
+                Arg.Any<IReadOnlyList<Tuple<string, string>>>(),
+                Arg.Any<CancellationToken>(),
+                Arg.Any<bool>(),
+                Arg.Any<ResourceVersionType>(),
+                Arg.Any<bool>(),
+                Arg.Any<bool>());
+        }
+
+        [Fact]
+        public async Task GivenSearchRequest_WhenSearchFails_ThenCapabilityShouldRethrowException()
+        {
+            _searchService.SearchAsync(
+                Arg.Any<string>(),
+                Arg.Any<IReadOnlyList<Tuple<string, string>>>(),
+                Arg.Any<CancellationToken>(),
+                Arg.Any<bool>(),
+                Arg.Any<ResourceVersionType>(),
+                Arg.Any<bool>(),
+                Arg.Any<bool>())
+                .Throws(new Exception("failed"));
+            await Assert.ThrowsAnyAsync<Exception>(() => _capability.GetCanonicalUrlsAsync(CancellationToken.None));
+        }
+
+        private static SearchResult CreateSearchResult(
+            string[] versions,
+            string continuationToken = null)
+        {
+            var entries = new List<SearchResultEntry>();
+            foreach (var version in versions)
+            {
+                var resource = new ResourceWrapper(
+                    Guid.NewGuid().ToString(),
+                    version,
+                    KnownResourceTypes.StructureDefinition,
+                    new RawResource(
+                        string.Format(RawResourceFormat, Guid.NewGuid().ToString(), version),
+                        FhirResourceFormat.Json,
+                        false),
+                    null,
+                    DateTimeOffset.UtcNow,
+                    false,
+                    null,
+                    null,
+                    null);
+                entries.Add(new SearchResultEntry(resource));
+            }
+
+            return new SearchResult(
+                entries,
+                continuationToken,
+                null,
+                new List<Tuple<string, string>>());
+        }
+
+        private HashSet<string> GetExpectedUrls(string[] versions)
+        {
+            var urls = new HashSet<string>();
+            foreach (var version in versions.Distinct())
+            {
+                var v = string.IsNullOrEmpty(version) ? USCoreInstantiateCapability.UnknownVersion : version;
+                urls.Add($"{USCoreInstantiateCapability.BaseUrl}|{v}");
+            }
+
+            return urls;
+        }
+
+        public static IEnumerable<object[]> GetVersionsData()
+        {
+            var data = new[]
+            {
+                new object[]
+                {
+                    new string[] { },
+                },
+                new object[]
+                {
+                    new string[] { "6.1.0" },
+                },
+                new object[]
+                {
+                    new string[]
+                    {
+                        "6.0.0",
+                        "6.1.0",
+                        "7.0.0",
+                    },
+                },
+                new object[]
+                {
+                    new string[]
+                    {
+                        "6.1.0",
+                        "6.1.0",
+                        "7.0.0",
+                        "7.0.0",
+                        "8.0.0",
+                    },
+                },
+                new object[]
+                {
+                    new string[]
+                    {
+                        null,
+                        "6.1.0",
+                        string.Empty,
+                        "6.1.0",
+                        "7.0.0",
+                        "7.0.0",
+                        null,
+                        "8.0.0",
+                        null,
+                    },
+                },
+            };
+
+            foreach (var d in data)
+            {
+                yield return d;
+            }
+        }
+    }
+}
