eebf4fb6176118f66c3996a8cb0bf80f944ae334
Sergey Galuzo <sergal@microsoft.com>
2026-06-08 22:47:08 -0700

    extra

--- src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
@@ -29,7 +29,10 @@ namespace Microsoft.Health.Fhir.SqlServer.Features
         public static bool IsSearchParameterConcurrencyConflict(this Exception e)
         {
             var sqlEx = e as SqlException;
-            return sqlEx != null && sqlEx.Number == 50001 && e.Message.StartsWith("optimistic concurrency conflict", StringComparison.OrdinalIgnoreCase);
+            return sqlEx != null
+                         && sqlEx.Number == 50001
+                         && e.Message.StartsWith("optimistic concurrency conflict", StringComparison.OrdinalIgnoreCase)
+                         && e.Message.Contains("expected last updated", StringComparison.OrdinalIgnoreCase);
         }
 
         private static bool HasDeadlockErrorPattern(string str)
