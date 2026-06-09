2123baa93ac6a4f6f9a5d6452ae639be0a3bb2a3
Sergey Galuzo <sergal@microsoft.com>
2026-06-08 22:20:30 -0700

    using

--- src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/ExceptionExtension.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 using System;
-using Antlr4.Runtime.Tree;
 using Microsoft.Data.SqlClient;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features
