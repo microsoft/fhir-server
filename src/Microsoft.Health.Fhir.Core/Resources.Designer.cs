f892152dc16bee176998399647f050c3d9bcbb5e
RB Johnson (He/Him) <rojo@microsoft.com>
2026-06-17 09:46:58 -0700

    Disable includes operation when using iterative includes

--- src/Microsoft.Health.Fhir.Core/Resources.Designer.cs
+++ src/Microsoft.Health.Fhir.Core/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Health.Fhir.Core {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -1940,6 +1940,15 @@ namespace Microsoft.Health.Fhir.Core {
         internal static string TruncatedIncludeMessageForIncludes {
             get {
                 return ResourceManager.GetString("TruncatedIncludeMessageForIncludes", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Include result was truncated. Iterative include results can not be paged though, please narrow your search..
+        /// </summary>
+        internal static string TruncatedIncludeMessageForIterativeInclude {
+            get {
+                return ResourceManager.GetString("TruncatedIncludeMessageForIterativeInclude", resourceCulture);
             }
         }
         
