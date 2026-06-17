52ee55d0b6d80d54394262b456054b5d0d91f9e1
Mikael Weaver <mikaelw@microsoft.com>
2026-06-17 15:17:45 -0700

    undo changes

--- src/Microsoft.Health.Fhir.CosmosDb.Initialization.UnitTests/CosmosDbInitializationTests.cs
+++ src/Microsoft.Health.Fhir.CosmosDb.Initialization.UnitTests/CosmosDbInitializationTests.cs
@@ -68,11 +68,5 @@ namespace Microsoft.Health.Fhir.CosmosDb.Initialization.UnitTests
                 Assert.Equal($"{storedProcedureMetadata.Name}_{storedProcedureHash}", storedProcedureMetadata.FullName);
             }
         }
-
-        [Fact]
-        public void IntentionalFailure_ForUnitTestPublicationValidation()
-        {
-            Assert.Fail("Intentional unit test failure to validate PR pipeline test publication.");
-        }
     }
 }
