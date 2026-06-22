46ea607aa0dd28fd1a4ac2477e480e3b8fb904ff
Sergey Galuzo <sergal@microsoft.com>
2026-06-21 20:30:41 -0700

    call mediator on search param create

--- test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/FhirStorageTests.cs
+++ test/Microsoft.Health.Fhir.Shared.Tests.Integration/Persistence/FhirStorageTests.cs
@@ -1201,11 +1201,10 @@ IF (SELECT count(*) FROM EventLog WHERE Process = 'MergeResources' AND Status = 
                 Code = searchParamName,
             };
 
-            _searchParameterDefinitionManager.AddNewSearchParameters([searchParam.ToTypedElement()]);
+            await _fixture.Mediator.CreateResourceAsync(searchParam.ToResourceElement(), cancellationToken: CancellationToken.None);
 
             await _fixture.SearchParameterOperations.GetAndApplySearchParameterUpdates();
 
-            // Add the search parameter to the datastore
             await _fixture.SearchParameterStatusManager.UpdateSearchParameterStatusAsync([searchParam.Url], SearchParameterStatus.Supported, CancellationToken.None, lastUpdated: _fixture.SearchParameterOperations.SearchParamLastUpdated);
 
             await _fixture.SearchParameterOperations.GetAndApplySearchParameterUpdates();
