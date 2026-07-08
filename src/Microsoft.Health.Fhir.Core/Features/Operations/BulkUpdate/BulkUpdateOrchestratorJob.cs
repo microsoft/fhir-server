d55d28af246a65daab7621feb80d37a744f65e3c
apurvabhale <apurvabhale@microsoft.com>
2026-07-08 14:55:44 -0700

    Remove redundant lt{createDate} _lastUpdated guard from CT path

--- src/Microsoft.Health.Fhir.Core/Features/Operations/BulkUpdate/BulkUpdateOrchestratorJob.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/BulkUpdate/BulkUpdateOrchestratorJob.cs
@@ -174,9 +174,6 @@ namespace Microsoft.Health.Fhir.Core.Features.Operations.BulkUpdate
 
                     var searchParams = definition.SearchParameters?.ToList() ?? new List<Tuple<string, string>>();
 
-                    var createDate = new PartialDateTime(new DateTimeOffset(jobInfo.CreateDate, TimeSpan.Zero));
-                    searchParams.Add(Tuple.Create(KnownQueryParameterNames.LastUpdated, $"lt{createDate}"));
-
                     searchParams.Add(Tuple.Create(KnownQueryParameterNames.Count, definition.MaximumNumberOfResourcesPerQuery.ToString(CultureInfo.InvariantCulture)));
                     if (!string.IsNullOrEmpty(lastEnqueuedMaxContinuationToken))
                     {
