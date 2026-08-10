df57e2c05f5bcbb2680b56ee1d5345c6de9b345e
Sergey Galuzo <sergal@microsoft.com>
2026-08-10 12:08:32 -0700

    cosmetic

--- src/Microsoft.Health.Fhir.Core/Features/Operations/Reindex/ReindexProcessingJob.cs
+++ src/Microsoft.Health.Fhir.Core/Features/Operations/Reindex/ReindexProcessingJob.cs
@@ -271,13 +271,12 @@ namespace Microsoft.Health.Fhir.Core.Features.Operations.Reindex
                     return;
                 }
 
-                var result = await _timeoutRetries.ExecuteAsync(async () =>
+                var resources = await _timeoutRetries.ExecuteAsync(async () =>
                 {
                     using var searchService = _searchServiceFactory();
-                    return await searchService.Value.SearchBySurrogateIdRange(_definition.ResourceType, range.StartId, range.EndId, null, null, _cancellationToken);
+                    return (await searchService.Value.SearchBySurrogateIdRange(_definition.ResourceType, range.StartId, range.EndId, null, null, _cancellationToken)).Results.Select(_ => _.Resource).ToList();
                 });
 
-                var resources = result.Results?.Select(_ => _.Resource).ToList();
                 await ComputeAndWrite(resources, null, store.Value, _cancellationToken);
                 _result.SucceededResourceCount += resources.Count;
                 _jobInfo.Data = _result.SucceededResourceCount;
