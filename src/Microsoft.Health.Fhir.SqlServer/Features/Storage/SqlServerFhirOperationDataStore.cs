c9f8be419d3da66b7589812fdac81b0085a70757
Sergey Galuzo <sergal@microsoft.com>
2026-08-14 16:12:57 -0700

    Removed legacy store

--- src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirOperationDataStore.cs
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Storage/SqlServerFhirOperationDataStore.cs
@@ -25,7 +25,7 @@ using Newtonsoft.Json;
 
 namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 {
-    internal class SqlServerFhirOperationDataStore : FhirOperationDataStoreBase, ILegacyReindexOperationDataStore
+    internal class SqlServerFhirOperationDataStore : FhirOperationDataStoreBase
     {
         private readonly SqlConnectionWrapperFactory _sqlConnectionWrapperFactory;
         private readonly ILogger<SqlServerFhirOperationDataStore> _logger;
@@ -58,63 +58,7 @@ namespace Microsoft.Health.Fhir.SqlServer.Features.Storage
 
         public override async Task<ReindexJobWrapper> GetReindexJobByIdAsync(string jobId, CancellationToken cancellationToken)
         {
-            if (IsLegacyJob(jobId))
-            {
-                // try old job records
-                var oldJobs = (ILegacyReindexOperationDataStore)this;
-                return await oldJobs.GetLegacyReindexJobByIdAsync(jobId, cancellationToken);
-            }
-
             return await base.GetReindexJobByIdAsync(jobId, cancellationToken);
-        }
-
-        private static bool IsLegacyJob(string jobId)
-        {
-            return !long.TryParse(jobId, out long _);
-        }
-
-        async Task<ReindexJobWrapper> ILegacyReindexOperationDataStore.GetLegacyReindexJobByIdAsync(string id, CancellationToken cancellationToken)
-        {
-            EnsureArg.IsNotNullOrWhiteSpace(id, nameof(id));
-
-            using (SqlConnectionWrapper sqlConnectionWrapper = await _sqlConnectionWrapperFactory.ObtainSqlConnectionWrapperAsync(cancellationToken, true))
-            using (SqlCommandWrapper sqlCommandWrapper = sqlConnectionWrapper.CreateRetrySqlCommand())
-            {
-                VLatest.GetReindexJobById.PopulateCommand(sqlCommandWrapper, id);
-
-                using (SqlDataReader sqlDataReader = await sqlCommandWrapper.ExecuteReaderAsync(CommandBehavior.SequentialAccess, cancellationToken))
-                {
-                    if (!await sqlDataReader.ReadAsync(cancellationToken))
-                    {
-                        throw new JobNotFoundException(string.Format(Core.Resources.JobNotFound, id));
-                    }
-
-                    (string rawJobRecord, byte[] rowVersion) = sqlDataReader.ReadRow(VLatest.ReindexJob.RawJobRecord, VLatest.ReindexJob.JobVersion);
-
-                    return CreateReindexJobWrapper(rawJobRecord, rowVersion);
-                }
-            }
-        }
-
-        private static WeakETag GetRowVersionAsEtag(byte[] rowVersionAsBytes)
-        {
-            if (BitConverter.IsLittleEndian)
-            {
-                Array.Reverse(rowVersionAsBytes);
-            }
-
-            var rowVersionAsDecimalString = BitConverter.ToInt64(rowVersionAsBytes, startIndex: 0).ToString();
-
-            return WeakETag.FromVersionId(rowVersionAsDecimalString);
-        }
-
-        private ReindexJobWrapper CreateReindexJobWrapper(string rawJobRecord, byte[] rowVersionAsBytes)
-        {
-            var reindexJobRecord = JsonConvert.DeserializeObject<ReindexJobRecord>(rawJobRecord, _jsonSerializerSettings);
-
-            WeakETag etag = GetRowVersionAsEtag(rowVersionAsBytes);
-
-            return new ReindexJobWrapper(reindexJobRecord, etag);
         }
     }
 }
