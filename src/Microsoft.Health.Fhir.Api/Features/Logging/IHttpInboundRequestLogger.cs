a08d7a255531f8f49c82aee10f0e5301ed4c414a
Fernando Henrique Inocêncio Borba Ferreira <fernfe@microsoft.com>
2026-08-26 10:02:40 -0400

    Extend middleware with Inbound Request Logs (#5755)
    * Extend middleware to log requests in Inbound Request Logs table.

---
+++ src/Microsoft.Health.Fhir.Api/Features/Logging/IHttpInboundRequestLogger.cs
@@ -0,0 +1,15 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Health.Fhir.Api.Features.Logging
+{
+    public interface IHttpInboundRequestLogger
+    {
+        void LogRequest(HttpContext context, Exception exception = null);
+    }
+}
