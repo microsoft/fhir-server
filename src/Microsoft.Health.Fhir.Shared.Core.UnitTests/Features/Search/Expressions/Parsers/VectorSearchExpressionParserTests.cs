a11df9e5a0a863fa35ce306ada61ee3100b32684
Anna Gornyitzki <t-annag@microsoft.com>
2026-08-31 13:17:56 -0700

    Fix test isolation: serialize ModelInfoProvider-mutating vector test to stop parallel NRE

--- src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Search/Expressions/Parsers/VectorSearchExpressionParserTests.cs
+++ src/Microsoft.Health.Fhir.Shared.Core.UnitTests/Features/Search/Expressions/Parsers/VectorSearchExpressionParserTests.cs
@@ -23,6 +23,7 @@ namespace Microsoft.Health.Fhir.Core.UnitTests.Features.Search.Expressions.Parse
 {
     [Trait(Traits.OwningTeam, OwningTeam.Fhir)]
     [Trait(Traits.Category, Categories.Search)]
+    [Collection(nameof(ModelInfoProviderSerialCollection))]
     public class VectorSearchExpressionParserTests
     {
         private const string SearchParameterCode = "semantic-text";
